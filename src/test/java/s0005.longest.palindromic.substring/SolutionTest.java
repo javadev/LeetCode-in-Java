728f5c309571b64dd903294a13cb69af293c3ccc
Valentyn Kolesnikov <javadev75@gmail.com>
2020-04-20 13:04:10 +0300

    Added task 5.

---
+++ src/test/java/s0005.longest.palindromic.substring/SolutionTest.java
@@ -0,0 +1,12 @@
+package s0005.longest.palindromic.substring;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void longestPalindrome() {
+        assertThat(new Solution().longestPalindrome("babad"), equalTo("bab"));
+    }
+}
