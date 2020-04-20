724a7c870c89c1f7326f6df81a07bbe97d84a011
Valentyn Kolesnikov <javadev75@gmail.com>
2020-04-20 12:42:54 +0300

    Added task 3.

---
+++ src/main/java/s0003.longest.substring.without.repeating.characters/Solution.java
@@ -0,0 +1,34 @@
+package s0003.longest.substring.without.repeating.characters;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int lastIndices[] = new int[256];
+        for (int i = 0; i<256; i++) {
+            lastIndices[i] = -1;
+        }
+        
+        int maxLen = 0;
+        int curLen = 0;
+        int start = 0;
+        int bestStart = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if (lastIndices[cur] < start) {
+                lastIndices[cur] = i;
+                curLen++;
+            } else {
+                int lastIndex = lastIndices[cur];
+                start = lastIndex + 1;
+                curLen = i - start + 1;
+                lastIndices[cur] = i;
+            }
+            
+            if (curLen > maxLen) {
+                maxLen = curLen;
+                bestStart = start;
+            }
+        }
+        
+        return maxLen;
+    }
+}
