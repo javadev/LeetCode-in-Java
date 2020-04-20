d94c488ef93b832088288124bd84c0c9c057afa9
Valentyn Kolesnikov <javadev75@gmail.com>
2020-04-20 12:34:36 +0300

    Added task 2.

---
+++ src/test/java/s0002.add.two.numbers/SolutionTest.java
@@ -0,0 +1,18 @@
+package s0002.add.two.numbers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void addTwoNumbers() {
+        ListNode listNode1 = new ListNode(2);
+        listNode1.next = new ListNode(4);
+        listNode1.next.next = new ListNode(3);
+        ListNode listNode2 = new ListNode(5);
+        listNode2.next = new ListNode(6);
+        listNode2.next.next = new ListNode(4);
+        assertThat(new Solution().addTwoNumbers(listNode1, listNode2).toString(), equalTo("7, 0, 8"));
+    }
+}
