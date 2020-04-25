4806791a442ac58a162349f7c0e502214fa785be
Valentyn Kolesnikov <javadev75@gmail.com>
2020-04-25 11:57:51 +0300

    Added task 7.

---
+++ src/test/java/s0007.reverse.integer/SolutionTest.java
@@ -0,0 +1,12 @@
+package s0007.reverse.integer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void reverse() {
+        assertThat(new Solution().reverse(123), equalTo(321));
+    }
+}
