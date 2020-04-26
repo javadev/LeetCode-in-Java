c83cb5533706694214712a8530d708e527ef33b1
Valentyn Kolesnikov <javadev75@gmail.com>
2020-04-26 09:46:23 +0300

    Added task 8.

---
+++ src/test/java/s0008.string.to.integer.atoi/SolutionTest.java
@@ -0,0 +1,12 @@
+package s0008.string.to.integer.atoi;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void myAtoi() {
+        assertThat(new Solution().myAtoi("42"), equalTo(42));
+    }
+}
