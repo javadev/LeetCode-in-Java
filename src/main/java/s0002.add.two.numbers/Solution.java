969a5c657728d10a6cdd8ddf10f4fda0a7b6e2a9
Valentyn Kolesnikov <javadev75@gmail.com>
2021-07-05 15:22:47 +0300

    Fixed comment.

--- src/main/java/s0002.add.two.numbers/Solution.java
+++ src/main/java/s0002.add.two.numbers/Solution.java
@@ -1,8 +1,12 @@
 package s0002.add.two.numbers;
 
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode(int
- * x) { val = x; } }
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
