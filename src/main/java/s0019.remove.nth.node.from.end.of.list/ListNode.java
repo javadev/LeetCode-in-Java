0340ea51c14b921d06b01a9dd9387943da13f490
Valentyn Kolesnikov <javadev75@gmail.com>
2021-08-01 17:13:53 +0300

    Added task 19.

---
+++ src/main/java/s0019.remove.nth.node.from.end.of.list/ListNode.java
@@ -0,0 +1,17 @@
+package s0019.remove.nth.node.from.end.of.list;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
---
+++ src/test/java/s0019.remove.nth.node.from.end.of.list/SolutionTest.java
@@ -0,0 +1,27 @@
+package s0019.remove.nth.node.from.end.of.list;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void fourSum() {
+        ListNode head = new ListNode();
+        head.val = 1;
+        ListNode node1 = new ListNode();
+        node1.val = 2;
+        head.next = node1;
+        ListNode node2 = new ListNode();
+        node2.val = 3;
+        node1.next = node2;
+        ListNode node3 = new ListNode();
+        node3.val = 4;
+        node2.next = node3;
+        ListNode node4 = new ListNode();
+        node4.val = 5;
+        node3.next = node4;
+        assertThat(new Solution().removeNthFromEnd(head, 2).toString(), equalTo("[1, 2, 3, 5]"));
+    }
+}
