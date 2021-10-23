c2efa8315ed362b20873263efb4b8e003cbd9d4d
Valentyn Kolesnikov <javadev75@gmail.com>
2021-10-23 18:43:39 +0300

    Added task 26.

---
+++ src/main/java/s0026.remove.duplicates.from.sorted.array/Solution.java
@@ -0,0 +1,24 @@
+package s0026.remove.duplicates.from.sorted.array;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+        int i = 0;
+        int j = 1;
+        if (n <= 1) {
+            return n;
+        }
+        while (j <= n - 1) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                nums[i + 1] = nums[j];
+                i++;
+                j++;
+            }
+        }
+
+        return i + 1;
+    }
+}
