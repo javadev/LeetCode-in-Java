b38a12bbc23a02dd5f3f24d19a6d02bd2643944a
Valentyn Kolesnikov <javadev75@gmail.com>
2021-10-28 19:44:15 +0300

    Improved task 5.

--- src/main/java/s0005_longest_palindromic_substring/Solution.java
+++ src/main/java/s0005_longest_palindromic_substring/Solution.java
@@ -2,32 +2,34 @@ package s0005_longest_palindromic_substring;
 
 public class Solution {
     public String longestPalindrome(String s) {
-        int start = 0, end = 0;
-        char[] cs = s.toCharArray();
-        for (int i = 0, max = 0, prev = 0; i < cs.length; ++i) {
-            if (i - max - 1 >= 0
-                    && cs[i - max - 1] == cs[i]
-                    && (prev == i - 1 || isPalindrome(cs, i - max, i))) {
-                start = i - max - 1;
-                end = i + 1;
-                max += 2;
-                prev = i;
-            } else if (isPalindrome(cs, i - max, i + 1)) {
-                start = i - max;
-                end = i + 1;
-                ++max;
-                prev = i;
+        char[] newStr = new char[s.length() * 2 + 1];
+        newStr[0] = '#';
+        for (int i = 0; i < s.length(); i++) {
+            newStr[2 * i + 1] = s.charAt(i);
+            newStr[2 * i + 2] = '#';
+        }
+        int[] dp = new int[newStr.length];
+        int friendCenter = 0;
+        int friendRadius = 0;
+        int lpsCenter = 0;
+        int lpsRadius = 0;
+        for (int i = 0; i < newStr.length; i++) {
+            dp[i] =
+                    friendCenter + friendRadius > i
+                            ? Math.min(dp[friendCenter * 2 - i], (friendCenter + friendRadius) - i)
+                            : 1;
+            while (i + dp[i] < newStr.length
+                    && i - dp[i] >= 0
+                    && newStr[i + dp[i]] == newStr[i - dp[i]]) dp[i]++;
+            if (friendCenter + friendRadius < i + dp[i]) {
+                friendCenter = i;
+                friendRadius = dp[i];
+            }
+            if (lpsRadius < dp[i]) {
+                lpsCenter = i;
+                lpsRadius = dp[i];
             }
         }
-        return s.substring(start, end);
-    }
-
-    private static boolean isPalindrome(char[] cs, int start, int end) {
-        for (int i = start, j = end - 1; i < j; ++i, --j) {
-            if (cs[i] != cs[j]) {
-                return false;
-            }
-        }
-        return true;
+        return s.substring((lpsCenter - lpsRadius + 1) / 2, (lpsCenter + lpsRadius - 1) / 2);
     }
 }
