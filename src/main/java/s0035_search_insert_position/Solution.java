bb1efac486528c2654bcfb7d454c9d60dc8a6eda
Valentyn Kolesnikov <javadev75@gmail.com>
2021-10-29 09:39:44 +0300

    Added task 35.

---
+++ src/main/java/s0035_search_insert_position/Solution.java
@@ -0,0 +1,19 @@
+package s0035_search_insert_position;
+
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                hi = mid - 1;
+            } else if (target > nums[mid]) {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+}
