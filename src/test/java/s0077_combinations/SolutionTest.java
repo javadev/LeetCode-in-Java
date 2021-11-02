76553ab4508ddb9b744c6606fbfac7f7f8842c6b
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-02 15:05:11 +0200

    Added tasks 75-77.

---
+++ src/test/java/s0077_combinations/SolutionTest.java
@@ -0,0 +1,24 @@
+package s0077_combinations;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void combine() {
+        int[][] expected = {{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}};
+        List<List<Integer>> expectedList = new ArrayList<>();
+        for (int[] value : expected) {
+            List<Integer> expectedItem = new ArrayList<>();
+            expectedList.add(expectedItem);
+            for (int item : value) {
+                expectedItem.add(item);
+            }
+        }
+        assertThat(new Solution().combine(4, 2), equalTo(expectedList));
+    }
+}
