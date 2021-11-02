1543c44776349987c291e0cb0e549648bc50f2fc
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-02 07:13:21 +0200

    Added tasks 72-74.

---
+++ src/test/java/s0072_edit_distance/SolutionTest.java
@@ -0,0 +1,13 @@
+package s0072_edit_distance;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void minDistance() {
+        assertThat(new Solution().minDistance("horse", "ros"), equalTo(3));
+    }
+}
---
+++ src/test/java/s0073_set_matrix_zeroes/SolutionTest.java
@@ -0,0 +1,15 @@
+package s0073_set_matrix_zeroes;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void setZeroes() {
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        new Solution().setZeroes(matrix);
+        assertThat(matrix, equalTo(new int[][] {{1, 0, 1}, {0, 0, 0}, {1, 0, 1}}));
+    }
+}
