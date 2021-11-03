cee0b29933a696f2230f10982133483a1f5265a4
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-03 12:05:59 +0200

    Added tasks 87-89.

---
+++ src/main/java/s0089_gray_code/Solution.java
@@ -0,0 +1,25 @@
+package s0089_gray_code;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        Integer[] n1 = {0};
+        int shift = 1;
+        while (n > 0) {
+            Integer[] temp = new Integer[n1.length * 2];
+            int pos = 0;
+            for (int i = 0; i < n1.length; i++) {
+                temp[pos++] = n1[i];
+            }
+            for (int i = n1.length - 1; i >= 0; i--) {
+                temp[pos++] = n1[i] | shift;
+            }
+            n1 = temp;
+            shift <<= 1;
+            n--;
+        }
+        return Arrays.asList(n1);
+    }
+}
---
+++ src/test/java/s0088_merge_sorted_array/SolutionTest.java
@@ -0,0 +1,15 @@
+package s0088_merge_sorted_array;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void merge() {
+        int[] array = new int[] {1, 2, 3, 0, 0, 0};
+        new Solution().merge(array, 3, new int[] {2, 5, 6}, 3);
+        assertThat(array, equalTo(new int[] {1, 2, 2, 3, 5, 6}));
+    }
+}
