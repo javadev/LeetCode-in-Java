aa244a0fb24f721e3b30dacb01218326fa2a3a76
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-03 13:28:26 +0200

    Added tasks 90-92.

---
+++ src/main/java/s0090_subsets_ii/Solution.java
@@ -0,0 +1,35 @@
+package s0090_subsets_ii;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("java:S5413")
+public class Solution {
+    List<List<Integer>> allComb = new ArrayList<>();
+    List<Integer> comb = new ArrayList<>();
+    int[] nums;
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        this.nums = nums;
+        dfs(0);
+        allComb.add(new ArrayList<>());
+        return allComb;
+    }
+
+    private void dfs(int start) {
+        if (start > nums.length) {
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            comb.add(nums[i]);
+            allComb.add(new ArrayList<>(comb));
+            dfs(i + 1);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
