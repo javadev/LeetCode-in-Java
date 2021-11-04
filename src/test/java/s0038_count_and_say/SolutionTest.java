a6bd90a5e2bc80a0a88716e4454275068445bf43
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-04 07:30:17 +0200

    Improved tests for task 38.

--- src/test/java/s0038_count_and_say/SolutionTest.java
+++ src/test/java/s0038_count_and_say/SolutionTest.java
@@ -8,6 +8,10 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void countAndSay() {
-        assertThat(new Solution().countAndSay(1), equalTo("1"));
+        Solution solution = new Solution();
+        assertThat(solution.countAndSay(1), equalTo("1"));
+        assertThat(solution.countAndSay(2), equalTo("11"));
+        assertThat(solution.countAndSay(3), equalTo("21"));
+        assertThat(solution.countAndSay(4), equalTo("1211"));
     }
 }
