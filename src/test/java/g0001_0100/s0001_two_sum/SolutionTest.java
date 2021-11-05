b080fcd138a409cee2f19d6d98ac9b95b4ccc447
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-05 06:02:51 +0200

    Fixed import in unit test.

--- src/test/java/g0001_0100/s0001_two_sum/SolutionTest.java
+++ src/test/java/g0001_0100/s0001_two_sum/SolutionTest.java
@@ -3,13 +3,14 @@ package g0001_0100.s0001_two_sum;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
 import org.junit.Test;
 
 public class SolutionTest {
     @Test
     public void twoSum() {
         assertThat(
-                java.util.Arrays.toString(new Solution().twoSum(new int[] {2, 7, 11, 15}, 9)),
+                Arrays.toString(new Solution().twoSum(new int[] {2, 7, 11, 15}, 9)),
                 equalTo("[0, 1]"));
     }
 }
