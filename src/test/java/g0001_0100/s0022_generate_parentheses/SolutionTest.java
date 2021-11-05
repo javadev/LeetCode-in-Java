803d663c904dd5a66b457c769d44216398e58c7b
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-05 19:51:37 +0200

    Replaced toString with value in unit test.

--- src/test/java/g0001_0100/s0022_generate_parentheses/SolutionTest.java
+++ src/test/java/g0001_0100/s0022_generate_parentheses/SolutionTest.java
@@ -3,13 +3,14 @@ package g0001_0100.s0022_generate_parentheses;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
 import org.junit.Test;
 
 public class SolutionTest {
     @Test
     public void generateParenthesis() {
         assertThat(
-                new Solution().generateParenthesis(3).toString(),
-                equalTo("[((())), (()()), (())(), ()(()), ()()()]"));
+                new Solution().generateParenthesis(3),
+                equalTo(Arrays.asList("((()))", "(()())", "(())()", "()(())", "()()()")));
     }
 }
