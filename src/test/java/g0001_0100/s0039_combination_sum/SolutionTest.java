ba3c4ee9d7e8499d167346444270aa86d0e53533
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-05 19:56:58 +0200

    Simplified unit test.

--- src/test/java/g0001_0100/s0039_combination_sum/SolutionTest.java
+++ src/test/java/g0001_0100/s0039_combination_sum/SolutionTest.java
@@ -3,26 +3,14 @@ package g0001_0100.s0039_combination_sum;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com_github_leetcode.ArrayUtils;
 import org.junit.Test;
 
 public class SolutionTest {
     @Test
     public void combinationSum() {
-        List<List<Integer>> expected = new ArrayList<>();
-
-        List<Integer> sublist1 = new ArrayList<>();
-        sublist1.add(7);
-        List<Integer> sublist2 = new ArrayList<>();
-        sublist2.addAll(Arrays.asList(3, 2, 2));
-
-        expected.add(sublist1);
-        expected.add(sublist2);
-
-        List<List<Integer>> actual = new Solution().combinationSum(new int[] {2, 3, 6, 7}, 7);
-
-        assertThat(actual, equalTo(expected));
+        assertThat(
+                new Solution().combinationSum(new int[] {2, 3, 6, 7}, 7),
+                equalTo(ArrayUtils.getLists(new int[][] {{7}, {3, 2, 2}})));
     }
 }
