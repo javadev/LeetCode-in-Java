0e5a1fcf05669c84673d5702f534c354cea98a99
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-10 22:17:06 +0200

    Added task 108.

---
+++ src/main/java/g0101_0200/s0108_convert_sorted_array_to_binary_search_tree/Solution.java
@@ -0,0 +1,28 @@
+package g0101_0200.s0108_convert_sorted_array_to_binary_search_tree;
+
+import com_github_leetcode.TreeNode;
+
+public class Solution {
+    public TreeNode sortedArrayToBST(int[] num) {
+        /*1. Set up recursion*/
+        return makeTree(num, 0, num.length - 1);
+    }
+
+    public TreeNode makeTree(int[] num, int left, int right) {
+        /*2. left as lowest# can't be greater than right*/
+        if (left > right) {
+            return null;
+        }
+
+        /*3. Set median# as node*/
+        int mid = (left + right) / 2;
+        TreeNode midNode = new TreeNode(num[mid]);
+
+        /*4. Set mid node's kids*/
+        midNode.left = makeTree(num, left, mid - 1);
+        midNode.right = makeTree(num, mid + 1, right);
+
+        /*5. Sends node back || Goes back to prev node*/
+        return midNode;
+    }
+}
---
+++ src/test/java/g0101_0200/s0108_convert_sorted_array_to_binary_search_tree/SolutionTest.java
@@ -0,0 +1,15 @@
+package g0101_0200.s0108_convert_sorted_array_to_binary_search_tree;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void sortedArrayToBST() {
+        assertThat(
+                new Solution().sortedArrayToBST(new int[] {-10, -3, 0, 5, 9}).toString(),
+                equalTo("0,-10,null,-3,5,null,9"));
+    }
+}
