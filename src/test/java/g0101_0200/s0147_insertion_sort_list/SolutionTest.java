58f9aafa4663bc32c1d08a2d786c5561b538c2b3
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-11 14:33:50 +0200

    Added tasks 147-148.

---
+++ src/test/java/g0101_0200/s0147_insertion_sort_list/SolutionTest.java
@@ -0,0 +1,18 @@
+package g0101_0200.s0147_insertion_sort_list;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.ListNode;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void insertionSortList() {
+        ListNode listNode1 = new ListNode(4);
+        listNode1.next = new ListNode(2);
+        listNode1.next.next = new ListNode(1);
+        listNode1.next.next.next = new ListNode(3);
+        assertThat(new Solution().insertionSortList(listNode1).toString(), equalTo("1, 2, 3, 4"));
+    }
+}
---
+++ src/test/java/g0101_0200/s0148_sort_list/SolutionTest.java
@@ -0,0 +1,18 @@
+package g0101_0200.s0148_sort_list;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.ListNode;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void sortList() {
+        ListNode listNode1 = new ListNode(4);
+        listNode1.next = new ListNode(2);
+        listNode1.next.next = new ListNode(1);
+        listNode1.next.next.next = new ListNode(3);
+        assertThat(new Solution().sortList(listNode1).toString(), equalTo("1, 2, 3, 4"));
+    }
+}
