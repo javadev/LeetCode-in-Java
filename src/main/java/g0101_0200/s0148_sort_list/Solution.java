8c1ee77e7f2ab3fb2c47f87998f6dcb6d3e68510
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-11 14:18:40 +0200

    Added task 148.

---
+++ src/main/java/g0101_0200/s0148_sort_list/Solution.java
@@ -0,0 +1,50 @@
+package g0101_0200.s0148_sort_list;
+
+import com_github_leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode pre = slow;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+        ListNode first = sortList(head);
+        ListNode second = sortList(slow);
+        ListNode res = new ListNode(1);
+        ListNode cur = res;
+        while (first != null || second != null) {
+            if (first == null) {
+                cur.next = second;
+                break;
+            } else if (second == null) {
+                cur.next = first;
+                break;
+            } else if (first.val <= second.val) {
+                cur.next = first;
+                first = first.next;
+                cur = cur.next;
+            } else {
+                cur.next = second;
+                second = second.next;
+                cur = cur.next;
+            }
+        }
+        return res.next;
+    }
+}
