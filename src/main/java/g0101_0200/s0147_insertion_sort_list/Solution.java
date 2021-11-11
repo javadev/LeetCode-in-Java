42d816008ec0c6f28c5393093a7e7d81e53a3678
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-11 13:35:56 +0200

    Added task 147.

---
+++ src/main/java/g0101_0200/s0147_insertion_sort_list/Solution.java
@@ -0,0 +1,39 @@
+package g0101_0200.s0147_insertion_sort_list;
+
+import com_github_leetcode.ListNode;
+import java.util.Arrays;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+    public ListNode insertionSortList(ListNode head) {
+        ListNode tnode = head;
+        ListNode res = null;
+        int count = 0;
+        while (tnode != null) {
+            count++;
+            tnode = tnode.next;
+        }
+        int nums[] = new int[count];
+        for (int i = 0; i < count; i++) {
+            nums[i] = head.val;
+            head = head.next;
+        }
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 0; i--) {
+            ListNode temp = new ListNode();
+            temp.val = nums[i];
+            temp.next = res;
+            res = temp;
+        }
+        return res;
+    }
+}
