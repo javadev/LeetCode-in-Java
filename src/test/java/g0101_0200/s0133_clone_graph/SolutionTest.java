7fad0cef876c7e467a300859153a1bba215616ba
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-11 11:38:44 +0200

    Added checkstyle plugin.

--- src/test/java/g0101_0200/s0133_clone_graph/SolutionTest.java
+++ src/test/java/g0101_0200/s0133_clone_graph/SolutionTest.java
@@ -14,11 +14,14 @@ public class SolutionTest {
         Node node2 = new Node(2);
         Node node3 = new Node(3);
         Node node4 = new Node(4);
-        Node node1_2_4 = new Node(1, Arrays.asList(node2, node4));
-        Node node2_1_3 = new Node(2, Arrays.asList(node1, node3));
-        Node node3_2_4 = new Node(3, Arrays.asList(node2, node4));
-        Node node4_1_3 = new Node(4, Arrays.asList(node1, node3));
-        Node node = new Node(5, Arrays.asList(node1_2_4, node2_1_3, node3_2_4, node4_1_3));
+        Node node1and2and4 = new Node(1, Arrays.asList(node2, node4));
+        Node node2and1and3 = new Node(2, Arrays.asList(node1, node3));
+        Node node3and2and4 = new Node(3, Arrays.asList(node2, node4));
+        Node node4and1and3 = new Node(4, Arrays.asList(node1, node3));
+        Node node =
+                new Node(
+                        5,
+                        Arrays.asList(node1and2and4, node2and1and3, node3and2and4, node4and1and3));
         assertThat(
                 new Solution().cloneGraph(node).toString(), equalTo("[[2,4],[1,3],[2,4],[1,3]]"));
     }
