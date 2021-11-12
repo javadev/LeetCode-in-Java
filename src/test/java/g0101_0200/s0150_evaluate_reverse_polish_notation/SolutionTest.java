1baab90ce4c419bbefc7c827519a27751e3dccf9
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-12 16:20:53 +0200

    Added tests for task 150.

--- src/test/java/g0101_0200/s0150_evaluate_reverse_polish_notation/SolutionTest.java
+++ src/test/java/g0101_0200/s0150_evaluate_reverse_polish_notation/SolutionTest.java
@@ -9,5 +9,14 @@ public class SolutionTest {
     @Test
     public void evalRPN() {
         assertThat(new Solution().evalRPN(new String[] {"2", "1", "+", "3", "*"}), equalTo(9));
+        assertThat(new Solution().evalRPN(new String[] {"4", "13", "5", "/", "+"}), equalTo(6));
+        assertThat(
+                new Solution()
+                        .evalRPN(
+                                new String[] {
+                                    "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5",
+                                    "+"
+                                }),
+                equalTo(22));
     }
 }
