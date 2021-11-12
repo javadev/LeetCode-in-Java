d7f85977d94aa676dbb922108accdd7a6db346dc
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-12 05:23:31 +0200

    Simplified test for task 120.

--- src/test/java/g0101_0200/s0120_triangle/SolutionTest.java
+++ src/test/java/g0101_0200/s0120_triangle/SolutionTest.java
@@ -3,20 +3,17 @@ package g0101_0200.s0120_triangle;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com_github_leetcode.ArrayUtils;
 import org.junit.Test;
 
 public class SolutionTest {
     @Test
-    public void triangle() {
-        List<List<Integer>> input = new ArrayList<>();
-        input.add(Arrays.asList(2));
-        input.add(Arrays.asList(3, 4));
-        input.add(Arrays.asList(6, 5, 7));
-        input.add(Arrays.asList(4, 1, 8, 3));
-
-        assertThat(new Solution().minimumTotal(input), equalTo(11));
+    public void minimumTotal() {
+        assertThat(
+                new Solution()
+                        .minimumTotal(
+                                ArrayUtils.getLists(
+                                        new int[][] {{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}})),
+                equalTo(11));
     }
 }
