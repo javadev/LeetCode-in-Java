629acd8e2d10f71604b7a37150fba9a391bc2736
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-14 16:14:26 +0200

    Fixed checkstyle.

--- src/main/java/g0101_0200/s0154_find_minimum_in_rotated_sorted_array_ii/Solution.java
+++ src/main/java/g0101_0200/s0154_find_minimum_in_rotated_sorted_array_ii/Solution.java
@@ -2,17 +2,20 @@ package g0101_0200.s0154_find_minimum_in_rotated_sorted_array_ii;
 
 public class Solution {
     public int findMin(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
         return find(0, nums.length - 1, nums);
     }
 
     int find(int left, int right, int[] nums) {
-        if (left + 1 >= right) return Math.min(nums[left], nums[right]);
-
+        if (left + 1 >= right) {
+            return Math.min(nums[left], nums[right]);
+        }
         int mid = left + (right - left) / 2;
-        if (nums[left] == nums[right] && nums[left] == nums[mid])
+        if (nums[left] == nums[right] && nums[left] == nums[mid]) {
             return Math.min(find(left, mid, nums), find(mid, right, nums));
-
+        }
         if (nums[left] >= nums[right]) {
             if (nums[mid] >= nums[left]) {
                 return find(mid, right, nums);
