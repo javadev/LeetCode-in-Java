54fd1b2ed92aec9bbf3cf1aae2cb41038258d812
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-14 16:11:56 +0200

    Added task 155.

---
+++ src/main/java/g0101_0200/s0155_min_stack/MinStack.java
@@ -0,0 +1,54 @@
+package g0101_0200.s0155_min_stack;
+
+public class MinStack {
+
+    Node currentNode;
+
+    /** initialize your data structure here. */
+    public MinStack() {}
+
+    public void push(int val) {
+        if (currentNode == null) {
+            currentNode = new Node(val, val, null, null);
+        } else {
+            currentNode.nextNode = new Node(Math.min(currentNode.min, val), val, currentNode, null);
+
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    public void pop() {
+        currentNode = currentNode.previousNode;
+    }
+
+    public int top() {
+        return currentNode.data;
+    }
+
+    public int getMin() {
+        return currentNode.min;
+    }
+
+    class Node {
+        int min;
+        int data;
+        Node nextNode;
+        Node previousNode;
+
+        Node(int min, int data, Node previousNode, Node nextNode) {
+            this.min = min;
+            this.data = data;
+            this.previousNode = previousNode;
+            this.nextNode = nextNode;
+        }
+    }
+}
+
+/*
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
