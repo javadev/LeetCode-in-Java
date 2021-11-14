1ede7d7cfeb32424f9dbcf2c829053f7a054ff22
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-14 16:02:34 +0200

    Added task 153.

---
+++ src/test/java/g0101_0200/s0153_find_minimum_in_rotated_sorted_array/SolutionTest.java
@@ -0,0 +1,13 @@
+package g0101_0200.s0153_find_minimum_in_rotated_sorted_array;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void findMin() {
+        assertThat(new Solution().findMin(new int[] {3, 4, 5, 1, 2}), equalTo(1));
+    }
+}
