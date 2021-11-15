d035363a6d03cb389a2dde9acb13a7727bd953f3
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-15 13:24:01 +0200

    Added tasks 189-191.

---
+++ src/test/java/g0101_0200/s0189_rotate_array/SolutionTest.java
@@ -0,0 +1,22 @@
+package g0101_0200.s0189_rotate_array;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void rotate() {
+        int[] array = new int[] {1, 2, 3, 4, 5, 6, 7};
+        new Solution().rotate(array, 3);
+        assertThat(array, equalTo(new int[] {5, 6, 7, 1, 2, 3, 4}));
+    }
+
+    @Test
+    public void rotate2() {
+        int[] array = new int[] {-1, -100, 3, 99};
+        new Solution().rotate(array, 2);
+        assertThat(array, equalTo(new int[] {3, 99, -1, -100}));
+    }
+}
---
+++ src/test/java/g0101_0200/s0191_number_of_1_bits/SolutionTest.java
@@ -0,0 +1,18 @@
+package g0101_0200.s0191_number_of_1_bits;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void hammingWeight() {
+        assertThat(new Solution().hammingWeight(0b00000000000000000000000000001011), equalTo(3));
+    }
+
+    @Test
+    public void hammingWeight2() {
+        assertThat(new Solution().hammingWeight(0b00000000000000000000000010000000), equalTo(1));
+    }
+}
