10c5a7c73a9d9fbde04f63271a98810d8823aa76
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-15 13:00:10 +0200

    Added task 189.

---
+++ src/main/java/g0101_0200/s0189_rotate_array/Solution.java
@@ -0,0 +1,21 @@
+package g0101_0200.s0189_rotate_array;
+
+public class Solution {
+    public void reverse(int[] nums, int l, int r) {
+        while (l <= r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int t = n - (k % n);
+        reverse(nums, 0, t - 1);
+        reverse(nums, t, n - 1);
+        reverse(nums, 0, n - 1);
+    }
+}
