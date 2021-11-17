8dad14dee467250308638e9ec6fd28dc01e556f2
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-17 15:31:02 +0200

    Fixed sonar warning.

--- src/main/java/g0201_0300/s0209_minimum_size_subarray_sum/Solution.java
+++ src/main/java/g0201_0300/s0209_minimum_size_subarray_sum/Solution.java
@@ -1,6 +1,5 @@
 package g0201_0300.s0209_minimum_size_subarray_sum;
 
-@SuppressWarnings("java:S2589")
 public class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         int i = 0;
@@ -11,7 +10,7 @@ public class Solution {
             sum += nums[j];
             if (sum < target) {
                 j++;
-            } else if (sum >= target) {
+            } else {
                 while (i <= j) {
                     if (sum - nums[i] >= target) {
                         sum = sum - nums[i];
