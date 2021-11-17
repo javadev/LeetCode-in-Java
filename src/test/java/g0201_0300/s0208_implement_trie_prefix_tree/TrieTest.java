599883a78c7fd8d21df5c56624ce2177b536af64
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-17 15:17:45 +0200

    Added task 208.

---
+++ src/test/java/g0201_0300/s0208_implement_trie_prefix_tree/TrieTest.java
@@ -0,0 +1,23 @@
+package g0201_0300.s0208_implement_trie_prefix_tree;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class TrieTest {
+    @Test
+    public void trie() {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        // return True
+        assertThat(trie.search("apple"), equalTo(true));
+        // return False
+        assertThat(trie.search("app"), equalTo(false));
+        // return True
+        assertThat(trie.startsWith("app"), equalTo(true));
+        trie.insert("app");
+        // return True
+        assertThat(trie.search("app"), equalTo(true));
+    }
+}
