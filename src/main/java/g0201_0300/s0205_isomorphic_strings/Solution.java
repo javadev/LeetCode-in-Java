137736dd5b8e4a11457f7b813a6af2c9d85e7b15
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-17 08:25:40 +0200

    Added task 205.

---
+++ src/main/java/g0201_0300/s0205_isomorphic_strings/Solution.java
@@ -0,0 +1,37 @@
+package g0201_0300.s0205_isomorphic_strings;
+
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] map = new int[128];
+
+        char[] str = s.toCharArray();
+        char[] tar = t.toCharArray();
+        int n = str.length;
+        for (int i = 0; i < n; i++) {
+            if (map[tar[i]] == 0) {
+                if (search(map, str[i], tar[i]) != -1) {
+                    return false;
+                }
+                map[tar[i]] = str[i];
+            } else {
+                if (map[tar[i]] != str[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private int search(int[] map, int tar, int skip) {
+        for (int i = 0; i < 128; i++) {
+            if (i == skip) {
+                continue;
+            }
+            if (map[i] != 0 && map[i] == tar) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/g0201_0300/s0205_isomorphic_strings/SolutionTest.java
@@ -0,0 +1,23 @@
+package g0201_0300.s0205_isomorphic_strings;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void isIsomorphic() {
+        assertThat(new Solution().isIsomorphic("egg", "add"), equalTo(true));
+    }
+
+    @Test
+    public void isIsomorphic2() {
+        assertThat(new Solution().isIsomorphic("foo", "bar"), equalTo(false));
+    }
+
+    @Test
+    public void isIsomorphic3() {
+        assertThat(new Solution().isIsomorphic("paper", "title"), equalTo(true));
+    }
+}
