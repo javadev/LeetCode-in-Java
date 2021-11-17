95e4de9fd8c95758538f38bb524699c7da7ccbcd
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-17 15:23:17 +0200

    Added task 209.

---
+++ src/main/java/g0201_0300/s0209_minimum_size_subarray_sum/Solution.java
@@ -0,0 +1,33 @@
+package g0201_0300.s0209_minimum_size_subarray_sum;
+
+public class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while (j < nums.length) {
+            sum += nums[j];
+            if (sum < target) {
+                j++;
+            } else if (sum >= target) {
+                while (i <= j) {
+                    if (sum - nums[i] >= target) {
+                        sum = sum - nums[i];
+                        i++;
+                    } else {
+                        break;
+                    }
+                }
+                if (j - i + 1 < min) {
+                    min = j - i + 1;
+                }
+                j++;
+            }
+        }
+        if (min == Integer.MAX_VALUE) {
+            return 0;
+        }
+        return min;
+    }
+}
