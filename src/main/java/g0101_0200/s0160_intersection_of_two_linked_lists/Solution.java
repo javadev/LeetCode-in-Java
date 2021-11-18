4ac7d26b31b0e88006a831aff49f69485d7ce6d7
Ivan Menov <vanetomenov@gmail.com>
2021-11-18 12:11:27 +0200

    fix

--- src/main/java/g0101_0200/s0160_intersection_of_two_linked_lists/Solution.java
+++ src/main/java/g0101_0200/s0160_intersection_of_two_linked_lists/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode node1 = headA;
         ListNode node2 = headB;
-        while (!node1.toString().equals(node2.toString())) {
+        while (node1.val != node2.val) {
             node1 = node1 == null ? headB : node1.next;
             node2 = node2 == null ? headA : node2.next;
         }
