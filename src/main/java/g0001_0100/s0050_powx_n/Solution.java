28ca6307b53333828482643d59a1b3315dca262e
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-19 15:38:49 +0200

    Update Solution.java

--- src/main/java/g0001_0100/s0050_powx_n/Solution.java
+++ src/main/java/g0001_0100/s0050_powx_n/Solution.java
@@ -4,11 +4,9 @@ public class Solution {
     public double myPow(double x, int n) {
         long nn = n;
         double res = 1;
-
         if (n < 0) {
             nn *= -1;
         }
-
         while (nn > 0) {
             if (nn % 2 == 1) {
                 nn--;
@@ -18,11 +16,9 @@ public class Solution {
                 nn /= 2;
             }
         }
-
         if (n < 0) {
-            return (1.0 / res);
+            return 1.0 / res;
         }
-
         return res;
     }
 }
