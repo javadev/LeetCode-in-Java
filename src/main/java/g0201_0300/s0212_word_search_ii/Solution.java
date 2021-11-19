ac078e2b12ba4b527c360d9c971db3a23c3bfd5c
Ivan Menov <vanetomenov@gmail.com>
2021-11-19 10:47:43 +0200

    add braces

--- src/main/java/g0201_0300/s0212_word_search_ii/Solution.java
+++ src/main/java/g0201_0300/s0212_word_search_ii/Solution.java
@@ -8,7 +8,9 @@ public class Solution {
     private Tree root;
 
     public List<String> findWords(char[][] board, String[] words) {
-        if (board.length < 1 || board[0].length < 1) return Collections.emptyList();
+        if (board.length < 1 || board[0].length < 1) {
+            return Collections.emptyList();
+        }
         root = new Tree();
         for (String word : words) {
             Tree.addWord(root, word);
@@ -24,20 +26,34 @@ public class Solution {
 
     private void dfs(char[][] board, int i, int j, Tree cur, List<String> collected) {
         char c = board[i][j];
-        if (c == '-') return;
+        if (c == '-') {
+            return;
+        }
         cur = cur.getChild(c);
-        if (cur == null) return;
+        if (cur == null) {
+            return;
+        }
         if (cur.end != null) {
             String s = cur.end;
             collected.add(s);
             cur.end = null;
-            if (cur.len() == 0) Tree.deleteWord(root, s);
+            if (cur.len() == 0) {
+                Tree.deleteWord(root, s);
+            }
         }
         board[i][j] = '-';
-        if (i > 0) dfs(board, i - 1, j, cur, collected);
-        if (i + 1 < board.length) dfs(board, i + 1, j, cur, collected);
-        if (j > 0) dfs(board, i, j - 1, cur, collected);
-        if (j + 1 < board[0].length) dfs(board, i, j + 1, cur, collected);
+        if (i > 0) {
+            dfs(board, i - 1, j, cur, collected);
+        }
+        if (i + 1 < board.length) {
+            dfs(board, i + 1, j, cur, collected);
+        }
+        if (j > 0) {
+            dfs(board, i, j - 1, cur, collected);
+        }
+        if (j + 1 < board[0].length) {
+            dfs(board, i, j + 1, cur, collected);
+        }
         board[i][j] = c;
     }
 }
