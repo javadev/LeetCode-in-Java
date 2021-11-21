c5afaaeba9a7ce47fbeace308a3736d0479446b4
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-21 21:18:30 +0200

    Update Solution.java

--- src/main/java/g0101_0200/s0121_best_time_to_buy_and_sell_stock/Solution.java
+++ src/main/java/g0101_0200/s0121_best_time_to_buy_and_sell_stock/Solution.java
@@ -2,10 +2,8 @@ package g0101_0200.s0121_best_time_to_buy_and_sell_stock;
 
 public class Solution {
     public int maxProfit(int[] prices) {
-
         int maxProfit = 0;
         int min = prices[0];
-
         for (int i = 1; i < prices.length; i++) {
             if (prices[i] > min) {
                 maxProfit = Math.max(maxProfit, prices[i] - min);
@@ -13,7 +11,6 @@ public class Solution {
                 min = prices[i];
             }
         }
-
         return maxProfit;
     }
 }
