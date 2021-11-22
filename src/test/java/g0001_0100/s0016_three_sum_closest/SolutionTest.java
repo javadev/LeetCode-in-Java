2e6e910a1edd55a9ef278b1434fbacda91fc0dbf
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-22 07:07:57 +0200

    Added tests for 16.

--- src/test/java/g0001_0100/s0016_three_sum_closest/SolutionTest.java
+++ src/test/java/g0001_0100/s0016_three_sum_closest/SolutionTest.java
@@ -8,8 +8,25 @@ import org.junit.Test;
 public class SolutionTest {
     @Test
     public void threeSumClosest() {
-        Solution solution = new Solution();
-        assertThat(solution.threeSumClosest(new int[] {-1, 2, 1, -4}, 1), equalTo(2));
-        assertThat(solution.threeSumClosest(new int[] {0, 0, 0}, 1), equalTo(0));
+        assertThat(new Solution().threeSumClosest(new int[] {-1, 2, 1, -4}, 1), equalTo(2));
+    }
+
+    @Test
+    public void threeSumClosest2() {
+        assertThat(new Solution().threeSumClosest(new int[] {0, 0, 0}, 1), equalTo(0));
+    }
+
+    @Test
+    public void threeSumClosest3() {
+        assertThat(
+                new Solution().threeSumClosest(new int[] {1, 2, 4, 8, 16, 32, 64, 128}, 82),
+                equalTo(82));
+    }
+
+    @Test
+    public void threeSumClosest4() {
+        assertThat(
+                new Solution().threeSumClosest(new int[] {4, 0, 5, -5, 3, 3, 0, -4, -5}, -2),
+                equalTo(-2));
     }
 }
