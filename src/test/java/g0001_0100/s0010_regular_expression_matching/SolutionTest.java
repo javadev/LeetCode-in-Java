0ab2c539172ae62ec6166b9c1e8ad4cd2ec1e8ed
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-22 07:19:09 +0200

    Added tests for 10.

--- src/test/java/g0001_0100/s0010_regular_expression_matching/SolutionTest.java
+++ src/test/java/g0001_0100/s0010_regular_expression_matching/SolutionTest.java
@@ -10,4 +10,24 @@ public class SolutionTest {
     public void isMatch() {
         assertThat(new Solution().isMatch("aa", "a"), equalTo(false));
     }
+
+    @Test
+    public void isMatch2() {
+        assertThat(new Solution().isMatch("aa", "a*"), equalTo(true));
+    }
+
+    @Test
+    public void isMatch3() {
+        assertThat(new Solution().isMatch("ab", ".*"), equalTo(true));
+    }
+
+    @Test
+    public void isMatch4() {
+        assertThat(new Solution().isMatch("aab", "c*a*b"), equalTo(true));
+    }
+
+    @Test
+    public void isMatch5() {
+        assertThat(new Solution().isMatch("mississippi", "mis*is*p*."), equalTo(false));
+    }
 }
