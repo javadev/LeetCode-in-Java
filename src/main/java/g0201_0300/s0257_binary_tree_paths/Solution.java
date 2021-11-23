eaa1b361d27eb1539c7e40da56abe96f235e725c
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-23 15:41:28 +0200

    Added task 257.

---
+++ src/main/java/g0201_0300/s0257_binary_tree_paths/Solution.java
@@ -0,0 +1,55 @@
+package g0201_0300.s0257_binary_tree_paths;
+
+import com_github_leetcode.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private List<String> result;
+    private StringBuilder sb;
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        result = new ArrayList<>();
+        if (root == null) return result;
+        sb = new StringBuilder();
+        walkThrough(root);
+        return result;
+    }
+
+    private void walkThrough(TreeNode root) {
+        assert root != null;
+        int length = sb.length();
+        sb.append(root.val);
+        length = sb.length() - length;
+        if (root.left == null && root.right == null) {
+            // leaf node.
+            result.add(sb.toString());
+            sb.delete(sb.length() - length, sb.length());
+            return;
+        }
+        sb.append("->");
+        length += 2;
+        if (root.left != null) {
+            walkThrough(root.left);
+        }
+        if (root.right != null) {
+            walkThrough(root.right);
+        }
+        sb.delete(sb.length() - length, sb.length());
+    }
+}
---
+++ src/test/java/g0201_0300/s0257_binary_tree_paths/SolutionTest.java
@@ -0,0 +1,25 @@
+package g0201_0300.s0257_binary_tree_paths;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.TreeNode;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void binaryTreePaths() {
+        TreeNode treeNode =
+                new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3));
+        assertThat(
+                new Solution().binaryTreePaths(treeNode),
+                equalTo(Arrays.asList("1->2->5", "1->3")));
+    }
+
+    @Test
+    public void binaryTreePaths2() {
+        TreeNode treeNode = new TreeNode(1);
+        assertThat(new Solution().binaryTreePaths(treeNode), equalTo(Arrays.asList("1")));
+    }
+}
