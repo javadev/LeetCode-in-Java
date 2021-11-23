a17c14a0773f38a08a9cf081cc4493a5889dfe26
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-23 15:50:20 +0200

    Added task 258.

---
+++ src/main/java/g0201_0300/s0258_add_digits/Solution.java
@@ -0,0 +1,13 @@
+package g0201_0300.s0258_add_digits;
+
+public class Solution {
+    public int addDigits(int num) {
+        if (num == 0) {
+            return 0;
+        }
+        if (num % 9 == 0) {
+            return 9;
+        }
+        return num % 9;
+    }
+}
