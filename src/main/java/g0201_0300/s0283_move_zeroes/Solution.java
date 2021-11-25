35fbc3adc108b75689bf238aa22495f71a602ae8
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-25 14:27:41 +0200

    Added task 283.

---
+++ src/main/java/g0201_0300/s0283_move_zeroes/Solution.java
@@ -0,0 +1,22 @@
+package g0201_0300.s0283_move_zeroes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int first_zero=0;
+        for(int i =0 ; i<nums.length; i++){
+            if(nums[i]!=0){
+                swap(first_zero, i, nums);
+                first_zero++;
+            }
+        }
+    }
+    
+    private void swap(int index1, int index2, int[] numbers){
+        int val_2 = numbers[index2];
+        numbers[index2] = numbers[index1];
+        numbers[index1] = val_2;
+    }    
+}
---
+++ src/test/java/g0201_0300/s0283_move_zeroes/SolutionTest.java
@@ -0,0 +1,16 @@
+package g0201_0300.s0283_move_zeroes;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void moveZeroes() {
+        int[] array = {0,1,0,3,12};
+        new Solution().moveZeroes(array);
+        assertThat(array, equalTo(new int[] {1,3,12,0,0}));
+    }
+}
