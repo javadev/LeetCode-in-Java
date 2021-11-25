98476818aef876eb5768ca1a8583871d6d85921e
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-25 14:07:34 +0200

    Added task 279.

---
+++ src/main/java/g0201_0300/s0279_perfect_squares/Solution.java
@@ -0,0 +1,30 @@
+package g0201_0300.s0279_perfect_squares;
+
+public class Solution {
+    private boolean validSquare(int n) {
+        int root = (int) Math.sqrt(n);
+        return root * root == n;
+    }
+
+    public int numSquares(int n) {
+        if (n < 4) {
+            return n;
+        }
+        if (validSquare(n)) {
+            return 1;
+        }
+        while ((n & 3) == 0) {
+            n >>= 2;
+        }
+        if ((n & 7) == 7) {
+            return 4;
+        }
+        int x = (int) Math.sqrt(n);
+        for (int i = 1; i <= x; i++) {
+            if (validSquare(n - i * i)) {
+                return 2;
+            }
+        }
+        return 3;
+    }
+}
---
+++ src/test/java/g0201_0300/s0279_perfect_squares/SolutionTest.java
@@ -0,0 +1,13 @@
+package g0201_0300.s0279_perfect_squares;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void numSquares() {
+        assertThat(new Solution().numSquares(12), equalTo(3));
+    }
+}
