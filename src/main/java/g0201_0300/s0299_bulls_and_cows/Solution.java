2f2393e9c361fcd946147c10e3852994aa40a298
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-26 11:44:14 +0200

    Added task 299.

---
+++ src/main/java/g0201_0300/s0299_bulls_and_cows/Solution.java
@@ -0,0 +1,27 @@
+package g0201_0300.s0299_bulls_and_cows;
+
+public class Solution {
+    public String getHint(String secret, String guess) {
+        final int[] ans = new int[10];
+        int bulls = 0, cows = 0;
+        for (int i = 0; i < secret.length(); i++) {
+            final int s = Character.getNumericValue(secret.charAt(i));
+            final int g = Character.getNumericValue(guess.charAt(i));
+            if (s == g) {
+                bulls++;
+            } else {
+                // digit s was already seen in guess, is being seen again in secret
+                if (ans[s] < 0) {
+                    cows++;
+                }
+                // digit was already seen in secret, now being seen again in guess
+                if (ans[g] > 0) {
+                    cows++;
+                }
+                ans[s]++;
+                ans[g]--;
+            }
+        }
+        return new StringBuilder().append(bulls).append("A").append(cows).append("B").toString();
+    }
+}
