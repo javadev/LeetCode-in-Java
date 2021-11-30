1cb7758b8301f97e1c415ce75603e836047887cf
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-30 11:24:35 +0200

    Added task 334.

---
+++ src/main/java/g0301_0400/s0334_increasing_triplet_subsequence/Solution.java
@@ -0,0 +1,20 @@
+package g0301_0400.s0334_increasing_triplet_subsequence;
+
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MAX_VALUE;
+        while (i < n) {
+            if (low >= nums[i]) {
+                low = nums[i++];
+            } else if (high >= nums[i]) {
+                high = nums[i++];
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
