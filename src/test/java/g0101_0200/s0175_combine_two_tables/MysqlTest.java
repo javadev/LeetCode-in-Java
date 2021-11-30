7ecfebdc2686c7be949e043c01d3c2d67a4aa227
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-30 08:28:51 +0200

    Added mysql test.

---
+++ src/test/java/g0101_0200/s0175_combine_two_tables/MysqlTest.java
@@ -0,0 +1,55 @@
+package g0101_0200.s0175_combine_two_tables;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.zapodot.junit.db.EmbeddedDatabaseRule;
+import org.zapodot.junit.db.common.CompatibilityMode;
+
+public class MysqlTest {
+    @Rule
+    public final EmbeddedDatabaseRule embeddedDatabaseRule =
+            EmbeddedDatabaseRule.builder()
+                    .withMode(CompatibilityMode.MySQL)
+                    .withInitialSql(
+                            "CREATE TABLE Person(personId INTEGER PRIMARY KEY, lastName VARCHAR(512)"
+                                    + ", firstName VARCHAR(512)); "
+                                    + "INSERT INTO Person(personId, lastName, firstName) "
+                                    + "VALUES (1, 'Wang', 'Allen'); "
+                                    + "INSERT INTO Person(personId, lastName, firstName) "
+                                    + "VALUES (2, 'Alice', 'Bob'); "
+                                    + "CREATE TABLE Address(addressId INTEGER PRIMARY KEY, personId INTEGER"
+                                    + ", city VARCHAR(512), state VARCHAR(512)); "
+                                    + "INSERT INTO Address(addressId, personId, city, state) "
+                                    + "VALUES (1, 2, 'New York City', 'New York'); "
+                                    + "INSERT INTO Address(addressId, personId, city, state) "
+                                    + "VALUES (2, 3, 'Leetcode', 'California'); ")
+                    .build();
+
+    @Test
+    public void testScript() throws SQLException {
+        try (final Connection connection =
+                DriverManager.getConnection(embeddedDatabaseRule.getConnectionJdbcUrl())) {
+            try (final Statement statement = connection.createStatement();
+                    final ResultSet resultSet =
+                            statement.executeQuery(
+                                    "SELECT P.FirstName, P.LastName,A.City,A.State"
+                                            + " FROM Person P"
+                                            + " LEFT JOIN Address A ON P.PersonId = A.PersonId")) {
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getNString(1), equalTo("Allen"));
+                assertThat(resultSet.getNString(2), equalTo("Wang"));
+                assertThat(resultSet.getNString(3), equalTo(null));
+                assertThat(resultSet.getNString(4), equalTo(null));
+            }
+        }
+    }
+}
