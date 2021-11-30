25c4e5978733525b02a6a3021a99f2a0ec502e9b
Valentyn Kolesnikov <javadev75@gmail.com>
2021-11-30 19:43:16 +0200

    Added task 233.

---
+++ src/main/java/g0201_0300/s0233_number_of_digit_one/Solution.java
@@ -0,0 +1,23 @@
+package g0201_0300.s0233_number_of_digit_one;
+
+public class Solution {
+    public int countDigitOne(int n) {
+        int ans = 0;
+        // count total number of 1s appearing in every digit, starting from the last digit
+        for (int k = n, cum = 0, curr10 = 1; k > 0; curr10 *= 10) {
+            int rem = k % 10;
+            int q = k / 10;
+            if (rem == 0) {
+                ans += q * curr10;
+            } else if (rem == 1) {
+                ans += q * curr10 + cum + 1;
+            } else {
+                ans += (q + 1) * curr10;
+            }
+            k = q;
+            // if loop is at 3rd last digit and n = 54321, cum is now = 321
+            cum += rem * curr10;
+        }
+        return ans;
+    }
+}
---
+++ src/main/java/g0201_0300/s0233_number_of_digit_one/readme.md
@@ -0,0 +1,21 @@
+233\. Number of Digit One
+
+Hard
+
+Given an integer `n`, count _the total number of digit_ `1` _appearing in all non-negative integers less than or equal to_ `n`.
+
+**Example 1:**
+
+**Input:** n = 13
+
+**Output:** 6 
+
+**Example 2:**
+
+**Input:** n = 0
+
+**Output:** 0 
+
+**Constraints:**
+
+*   <code>0 <= n <= 10<sup>9</sup></code>
