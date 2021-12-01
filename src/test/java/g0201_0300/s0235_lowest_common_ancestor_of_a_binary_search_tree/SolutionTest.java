1c72d956c8737585e766b6ca43d41ba518460a42
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-01 12:20:23 +0200

    Added task 235.

---
+++ src/test/java/g0201_0300/s0235_lowest_common_ancestor_of_a_binary_search_tree/SolutionTest.java
@@ -0,0 +1,21 @@
+package g0201_0300.s0235_lowest_common_ancestor_of_a_binary_search_tree;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.TreeNode;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void lowestCommonAncestor() {
+        TreeNode leftNodeLeftNode = new TreeNode(0);
+        TreeNode leftNodeRightNode = new TreeNode(4, new TreeNode(3), new TreeNode(5));
+        TreeNode leftNode = new TreeNode(2, leftNodeLeftNode, leftNodeRightNode);
+        TreeNode rightNode = new TreeNode(6, new TreeNode(7), new TreeNode(9));
+        TreeNode root = new TreeNode(6, leftNode, rightNode);
+        assertThat(
+                new Solution().lowestCommonAncestor(root, new TreeNode(2), new TreeNode(8)).val,
+                equalTo(6));
+    }
+}
