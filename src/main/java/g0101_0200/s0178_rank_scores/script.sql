6438952db553ac8e6792878694d8466374af1ad4
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-01 09:57:45 +0200

    Update script.sql

--- src/main/java/g0101_0200/s0178_rank_scores/script.sql
+++ src/main/java/g0101_0200/s0178_rank_scores/script.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select Score, DENSE_RANK() OVER(order by Score Desc) as "Rank" from Scores order by "Rank";
+select Score, DENSE_RANK() OVER(order by Score Desc) as "Rank" from Scores order by "Rank" Asc;
 
