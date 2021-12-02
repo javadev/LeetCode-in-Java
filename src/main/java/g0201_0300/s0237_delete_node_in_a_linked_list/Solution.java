46237089a3d6ca2e4d518bc89dbf655383507e19
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-02 16:00:39 +0200

    Added task 236.

---
+++ src/main/java/g0201_0300/s0237_delete_node_in_a_linked_list/Solution.java
@@ -0,0 +1,22 @@
+package g0201_0300.s0237_delete_node_in_a_linked_list;
+
+import com_github_leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void deleteNode(ListNode node) {
+        while (node.next.next != null) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
