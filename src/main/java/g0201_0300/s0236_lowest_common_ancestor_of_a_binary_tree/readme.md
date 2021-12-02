ab0cc5891cb8872cab180250e447694d57f16f3b
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-02 15:15:55 +0200

    Added task 236.

---
+++ src/main/java/g0201_0300/s0236_lowest_common_ancestor_of_a_binary_tree/readme.md
@@ -0,0 +1,41 @@
+236\. Lowest Common Ancestor of a Binary Tree
+
+Medium
+
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+
+According to the [definition of LCA on Wikipedia](https://en.wikipedia.org/wiki/Lowest_common_ancestor): “The lowest common ancestor is defined between two nodes `p` and `q` as the lowest node in `T` that has both `p` and `q` as descendants (where we allow **a node to be a descendant of itself**).”
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2018/12/14/binarytree.png)
+
+**Input:** root = \[3,5,1,6,2,0,8,null,null,7,4\], p = 5, q = 1
+
+**Output:** 3
+
+**Explanation:** The LCA of nodes 5 and 1 is 3. 
+
+**Example 2:**
+
+![](https://assets.leetcode.com/uploads/2018/12/14/binarytree.png)
+
+**Input:** root = \[3,5,1,6,2,0,8,null,null,7,4\], p = 5, q = 4
+
+**Output:** 5
+
+**Explanation:** The LCA of nodes 5 and 4 is 5, since a node can be a descendant of itself according to the LCA definition. 
+
+**Example 3:**
+
+**Input:** root = \[1,2\], p = 1, q = 2
+
+**Output:** 1 
+
+**Constraints:**
+
+*   The number of nodes in the tree is in the range <code>[2, 10<sup>5</sup>]</code>.
+*   <code>-10<sup>9</sup> <= Node.val <= 10<sup>9</sup></code>
+*   All `Node.val` are **unique**.
+*   `p != q`
+*   `p` and `q` will exist in the tree.
---
+++ src/test/java/g0201_0300/s0236_lowest_common_ancestor_of_a_binary_tree/SolutionTest.java
@@ -0,0 +1,45 @@
+package g0201_0300.s0236_lowest_common_ancestor_of_a_binary_tree;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.TreeNode;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void lowestCommonAncestor() {
+        TreeNode leftNodeLeftNode = new TreeNode(6);
+        TreeNode leftNodeRightNode = new TreeNode(2, new TreeNode(7), new TreeNode(4));
+        TreeNode leftNode = new TreeNode(5, leftNodeLeftNode, leftNodeRightNode);
+        TreeNode rightNode = new TreeNode(1, new TreeNode(0), new TreeNode(8));
+        TreeNode root = new TreeNode(3, leftNode, rightNode);
+        assertThat(
+                new Solution().lowestCommonAncestor(root, new TreeNode(5), new TreeNode(1)).val,
+                equalTo(3));
+    }
+
+    @Test
+    public void lowestCommonAncestor2() {
+        TreeNode leftNodeLeftNode = new TreeNode(6);
+        TreeNode leftNodeRightNode = new TreeNode(2, new TreeNode(7), new TreeNode(4));
+        TreeNode leftNode = new TreeNode(5, leftNodeLeftNode, leftNodeRightNode);
+        TreeNode rightNode = new TreeNode(1, new TreeNode(0), new TreeNode(8));
+        TreeNode root = new TreeNode(3, leftNode, rightNode);
+        assertThat(
+                new Solution().lowestCommonAncestor(root, new TreeNode(5), new TreeNode(4)).val,
+                equalTo(5));
+    }
+
+    @Test
+    public void lowestCommonAncestor3() {
+        assertThat(
+                new Solution()
+                        .lowestCommonAncestor(
+                                new TreeNode(2, new TreeNode(1), null),
+                                new TreeNode(2),
+                                new TreeNode(1))
+                        .val,
+                equalTo(2));
+    }
+}
