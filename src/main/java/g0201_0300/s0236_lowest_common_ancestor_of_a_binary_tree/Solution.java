c0fc83c4182ec537ad894bff46eeb01cb4603d8a
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-02 16:09:42 +0200

    Added tasks 236, 237.

---
+++ src/main/java/g0201_0300/s0236_lowest_common_ancestor_of_a_binary_tree/Solution.java
@@ -0,0 +1,35 @@
+package g0201_0300.s0236_lowest_common_ancestor_of_a_binary_tree;
+
+import com_github_leetcode.TreeNode;
+
+/*
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+        return null;
+    }
+}
---
+++ src/test/java/g0201_0300/s0237_delete_node_in_a_linked_list/SolutionTest.java
@@ -0,0 +1,39 @@
+package g0201_0300.s0237_delete_node_in_a_linked_list;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.ListNode;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void deleteNode() {
+        ListNode headActual = new ListNode(1);
+        headActual.next = new ListNode(2);
+        headActual.next.next = new ListNode(3);
+        headActual.next.next.next = new ListNode(4);
+        new Solution().deleteNode(headActual.next.next);
+        assertThat(headActual.toString(), equalTo("1, 2, 4"));
+    }
+
+    @Test
+    public void deleteNode2() {
+        ListNode headActual = new ListNode(1);
+        headActual.next = new ListNode(2);
+        headActual.next.next = new ListNode(3);
+        headActual.next.next.next = new ListNode(4);
+        new Solution().deleteNode(headActual);
+        assertThat(headActual.toString(), equalTo("2, 3, 4"));
+    }
+
+    @Test
+    public void deleteNode3() {
+        ListNode headActual = new ListNode(1);
+        headActual.next = new ListNode(2);
+        headActual.next.next = new ListNode(3);
+        headActual.next.next.next = new ListNode(4);
+        new Solution().deleteNode(headActual.next);
+        assertThat(headActual.toString(), equalTo("1, 3, 4"));
+    }
+}
