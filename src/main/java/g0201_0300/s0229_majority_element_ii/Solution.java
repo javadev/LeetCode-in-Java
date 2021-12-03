1243172bf50e90c536841c3660b493e878e510c1
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-03 06:54:39 +0200

    Update Solution.java

--- src/main/java/g0201_0300/s0229_majority_element_ii/Solution.java
+++ src/main/java/g0201_0300/s0229_majority_element_ii/Solution.java
@@ -11,6 +11,8 @@ public class Solution {
         int second = 1;
         int count1 = 0;
         int count2 = 0;
+        // now we have two candidates(any integer can be chosed as),and their votes are
+        // zero.
         for (int i = 0; i < len; i++) {
             int temp = nums[i];
             if (temp == first) {
@@ -24,9 +26,17 @@ public class Solution {
                 second = temp;
                 count2++;
             } else {
+                // otherwise,if one of the vote is zero,that's meaning that
+                // we only have or even don't have a candidate.So we set the number to the
+                // candidate.
                 count1--;
                 count2--;
             }
+            // where we have two candidates whose votes bigger than zero,
+            // but the current number is not one of them.Votes decrease by 1 and
+            // the current number complete its "mission" and is skipped at the same time.
+            // once the cycle finished,the target is left after all the counteraction,as its
+            // count is bigger than n/3.
         }
         count1 = 0;
         count2 = 0;
