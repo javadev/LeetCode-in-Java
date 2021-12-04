c7f1339db4001d6ae92f10caeb63bd0e19dc23e8
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-04 09:55:51 +0200

    Added test for task 180.

--- src/main/java/g0101_0200/s0180_consecutive_numbers/script.sql
+++ src/main/java/g0101_0200/s0180_consecutive_numbers/script.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select distinct num as ConsecutiveNums from
-(select num, lag(num,1) over() as l1, lag(num,2) over() as l2
+(select num, lag(num,1) over(order by id) as l1, lag(num,2) over(order by id) as l2
 from Logs) con_thr
 where num = l1 and num = l2
---
+++ src/test/java/g0101_0200/s0180_consecutive_numbers/MysqlTest.java
@@ -0,0 +1,56 @@
+package g0101_0200.s0180_consecutive_numbers;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+import org.junit.Rule;
+import org.junit.Test;
+import org.zapodot.junit.db.EmbeddedDatabaseRule;
+import org.zapodot.junit.db.common.CompatibilityMode;
+
+public class MysqlTest {
+    @Rule
+    public final EmbeddedDatabaseRule embeddedDatabaseRule =
+            EmbeddedDatabaseRule.builder()
+                    .withMode(CompatibilityMode.MySQL)
+                    .withInitialSql(
+                            "CREATE TABLE Logs(id INTEGER PRIMARY KEY, num INTEGER); "
+                                    + "INSERT INTO Logs(id, num) VALUES (1, 1); "
+                                    + "INSERT INTO Logs(id, num) VALUES (2, 1); "
+                                    + "INSERT INTO Logs(id, num) VALUES (3, 1); "
+                                    + "INSERT INTO Logs(id, num) VALUES (4, 2); "
+                                    + "INSERT INTO Logs(id, num) VALUES (5, 1); "
+                                    + "INSERT INTO Logs(id, num) VALUES (6, 2); "
+                                    + "INSERT INTO Logs(id, num) VALUES (7, 2); ")
+                    .build();
+
+    @Test
+    public void testScript() throws SQLException, FileNotFoundException {
+        try (final Connection connection =
+                DriverManager.getConnection(embeddedDatabaseRule.getConnectionJdbcUrl())) {
+            try (final Statement statement = connection.createStatement();
+                    final ResultSet resultSet =
+                            statement.executeQuery(
+                                    new BufferedReader(
+                                                    new FileReader(
+                                                            "src/main/java/g0101_0200/"
+                                                                    + "s0180_consecutive_numbers/script.sql"))
+                                            .lines()
+                                            .collect(Collectors.joining("\n"))
+                                            .replaceAll("#.*?\\r?\\n", ""))) {
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getInt(1), equalTo(1));
+                assertThat(resultSet.next(), equalTo(false));
+            }
+        }
+    }
+}
