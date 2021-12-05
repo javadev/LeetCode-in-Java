5d6727ce87fe70869f584a95d05fac7960f5cf50
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-05 15:05:40 +0200

    Improved mysql tests.

--- src/test/java/g0101_0200/s0180_consecutive_numbers/MysqlTest.java
+++ src/test/java/g0101_0200/s0180_consecutive_numbers/MysqlTest.java
@@ -7,36 +7,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.stream.Collectors;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.zapodot.junit.db.EmbeddedDatabaseExtension;
+import org.zapodot.junit.db.annotations.EmbeddedDatabase;
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest;
 import org.zapodot.junit.db.common.CompatibilityMode;
 
+@EmbeddedDatabaseTest(
+        compatibilityMode = CompatibilityMode.MySQL,
+        initialSqls =
+                "CREATE TABLE Logs(id INTEGER PRIMARY KEY, num INTEGER); "
+                        + "INSERT INTO Logs(id, num) VALUES (1, 1); "
+                        + "INSERT INTO Logs(id, num) VALUES (2, 1); "
+                        + "INSERT INTO Logs(id, num) VALUES (3, 1); "
+                        + "INSERT INTO Logs(id, num) VALUES (4, 2); "
+                        + "INSERT INTO Logs(id, num) VALUES (5, 1); "
+                        + "INSERT INTO Logs(id, num) VALUES (6, 2); "
+                        + "INSERT INTO Logs(id, num) VALUES (7, 2); ")
 class MysqlTest {
-    @RegisterExtension
-    static EmbeddedDatabaseExtension embeddedDatabaseExtension =
-            EmbeddedDatabaseExtension.Builder.h2()
-                    .withMode(CompatibilityMode.MySQL)
-                    .withInitialSql(
-                            "CREATE TABLE Logs(id INTEGER PRIMARY KEY, num INTEGER); "
-                                    + "INSERT INTO Logs(id, num) VALUES (1, 1); "
-                                    + "INSERT INTO Logs(id, num) VALUES (2, 1); "
-                                    + "INSERT INTO Logs(id, num) VALUES (3, 1); "
-                                    + "INSERT INTO Logs(id, num) VALUES (4, 2); "
-                                    + "INSERT INTO Logs(id, num) VALUES (5, 1); "
-                                    + "INSERT INTO Logs(id, num) VALUES (6, 2); "
-                                    + "INSERT INTO Logs(id, num) VALUES (7, 2); ")
-                    .build();
-
     @Test
-    void testScript() throws SQLException, FileNotFoundException {
-        try (final Connection connection =
-                DriverManager.getConnection(embeddedDatabaseExtension.getConnectionJdbcUrl())) {
+    void testScript(@EmbeddedDatabase DataSource dataSource)
+            throws SQLException, FileNotFoundException {
+        try (final Connection connection = dataSource.getConnection()) {
             try (final Statement statement = connection.createStatement();
                     final ResultSet resultSet =
                             statement.executeQuery(
