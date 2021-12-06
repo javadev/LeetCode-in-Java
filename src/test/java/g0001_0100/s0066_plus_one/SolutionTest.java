a8d82adc91ae1746c20c3199a48ab28bd47931b9
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-06 13:27:24 +0200

    Added test for 66.

--- src/test/java/g0001_0100/s0066_plus_one/SolutionTest.java
+++ src/test/java/g0001_0100/s0066_plus_one/SolutionTest.java
@@ -10,4 +10,19 @@ class SolutionTest {
     void plusOne() {
         assertThat(new Solution().plusOne(new int[] {1, 2, 3}), equalTo(new int[] {1, 2, 4}));
     }
+
+    @Test
+    void plusOne2() {
+        assertThat(new Solution().plusOne(new int[] {4,3,2,1}), equalTo(new int[] {4,3,2,2}));
+    }
+
+    @Test
+    void plusOne3() {
+        assertThat(new Solution().plusOne(new int[] {0}), equalTo(new int[] {1}));
+    }
+
+    @Test
+    void plusOne4() {
+        assertThat(new Solution().plusOne(new int[] {9}), equalTo(new int[] {1, 0}));
+    }
 }
