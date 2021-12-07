b8da4be2c13b3a3ffb3d09142dc6f7bf146c00fc
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-07 21:29:40 +0200

    Added task 355.

---
+++ src/main/java/g0301_0400/s0355_design_twitter/Tweet.java
@@ -0,0 +1,14 @@
+package g0301_0400.s0355_design_twitter;
+
+// #Medium #Hash_Table #Design #Heap_Priority_Queue #Linked_List
+
+public class Tweet {
+    int userId;
+    int tweetId;
+    Tweet next;
+
+    Tweet(int uId, int tId) {
+        userId = uId;
+        tweetId = tId;
+    }
+}
---
+++ src/test/java/g0301_0400/s0355_design_twitter/TwitterTest.java
@@ -0,0 +1,28 @@
+package g0301_0400.s0355_design_twitter;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class TwitterTest {
+    @Test
+    void twitter() {
+        Twitter twitter = new Twitter();
+        twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+        // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+        assertThat(twitter.getNewsFeed(1), equalTo(Arrays.asList(5)));
+        // User 1 follows user 2.
+        twitter.follow(1, 2);
+        // User 2 posts a new tweet (id = 6).
+        twitter.postTweet(2, 6);
+        assertThat(twitter.getNewsFeed(1), equalTo(Arrays.asList(6, 5)));
+        // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should
+        // precede tweet id 5 because it is posted after tweet id 5.
+        twitter.getNewsFeed(1);
+        // User 1 unfollows user 2.
+        twitter.unfollow(1, 2);
+        assertThat(twitter.getNewsFeed(1), equalTo(Arrays.asList(5)));
+    }
+}
