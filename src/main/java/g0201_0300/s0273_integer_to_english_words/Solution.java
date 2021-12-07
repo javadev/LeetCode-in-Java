5e23ec4703a09ac96cdfdb56947ac4dc5f975ded
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-07 04:00:42 +0200

    Fixed idea warnings.

--- src/main/java/g0201_0300/s0273_integer_to_english_words/Solution.java
+++ src/main/java/g0201_0300/s0273_integer_to_english_words/Solution.java
@@ -2,6 +2,8 @@ package g0201_0300.s0273_integer_to_english_words;
 
 // #Hard #String #Math #Recursion
 
+import java.util.StringJoiner;
+
 public class Solution {
     private String[] ones = {
         "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine "
@@ -21,43 +23,40 @@ public class Solution {
     private String[] twenties = {
         "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "
     };
-    private String zero = "Zero";
-    private String hundred = "Hundred ";
-    private String thousand = "Thousand ";
-    private String million = "Million ";
-    private String billion = "Billion ";
 
     public String numberToWords(int num) {
         if (num == 0) {
+            String zero = "Zero";
             return zero;
         }
-        StringBuilder sb = new StringBuilder();
-        processThreeDigits(sb, num / 1_000_000_000, billion);
-        processThreeDigits(sb, num / 1_000_000, million);
-        processThreeDigits(sb, num / 1_000, thousand);
-        processThreeDigits(sb, num, null);
-        return sb.toString().trim();
+        StringJoiner joiner = new StringJoiner("");
+        processThreeDigits(joiner, num / 1_000_000_000, "Billion ");
+        processThreeDigits(joiner, num / 1_000_000, "Million ");
+        processThreeDigits(joiner, num / 1_000, "Thousand ");
+        processThreeDigits(joiner, num, null);
+        return joiner.toString().trim();
     }
 
-    private void processThreeDigits(StringBuilder sb, int input, String name) {
+    private void processThreeDigits(StringJoiner joiner, int input, String name) {
         int threeDigit = input % 1000;
         if (threeDigit > 0) {
             if (threeDigit / 100 > 0) {
-                sb.append(ones[threeDigit / 100 - 1]);
-                sb.append(hundred);
+                joiner.add(ones[threeDigit / 100 - 1]);
+                String hundred = "Hundred ";
+                joiner.add(hundred);
             }
             if (threeDigit % 100 >= 20) {
-                sb.append(twenties[(threeDigit % 100) / 10 - 2]);
+                joiner.add(twenties[(threeDigit % 100) / 10 - 2]);
                 if (threeDigit % 10 > 0) {
-                    sb.append(ones[threeDigit % 10 - 1]);
+                    joiner.add(ones[threeDigit % 10 - 1]);
                 }
             } else if (threeDigit % 100 >= 10 && threeDigit % 100 < 20) {
-                sb.append(teens[threeDigit % 10]);
+                joiner.add(teens[threeDigit % 10]);
             } else if (threeDigit % 100 > 0 && threeDigit % 100 < 10) {
-                sb.append(ones[threeDigit % 10 - 1]);
+                joiner.add(ones[threeDigit % 10 - 1]);
             }
             if (name != null) {
-                sb.append(name);
+                joiner.add(name);
             }
         }
     }
