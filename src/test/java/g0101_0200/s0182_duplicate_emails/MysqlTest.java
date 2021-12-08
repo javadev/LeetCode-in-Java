a9a12422df0c016269c99353e30ef846e21a48db
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-08 20:40:41 +0200

    Added test for 182.

---
+++ src/test/java/g0101_0200/s0182_duplicate_emails/MysqlTest.java
@@ -0,0 +1,48 @@
+package g0101_0200.s0182_duplicate_emails;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.zapodot.junit.db.annotations.EmbeddedDatabase;
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest;
+import org.zapodot.junit.db.common.CompatibilityMode;
+
+@EmbeddedDatabaseTest(
+        compatibilityMode = CompatibilityMode.MySQL,
+        initialSqls =
+                "CREATE TABLE Person(id INTEGER PRIMARY KEY, email VARCHAR); "
+                        + "INSERT INTO Person(id, email) VALUES (1, 'a@b.com'); "
+                        + "INSERT INTO Person(id, email) VALUES (2, 'c@d.com'); "
+                        + "INSERT INTO Person(id, email) VALUES (3, 'a@b.com'); ")
+class MysqlTest {
+    @Test
+    void testScript(@EmbeddedDatabase DataSource dataSource)
+            throws SQLException, FileNotFoundException {
+        try (final Connection connection = dataSource.getConnection()) {
+            try (final Statement statement = connection.createStatement();
+                    final ResultSet resultSet =
+                            statement.executeQuery(
+                                    new BufferedReader(
+                                                    new FileReader(
+                                                            "src/main/java/g0101_0200/"
+                                                                    + "s0182_duplicate_emails/script.sql"))
+                                            .lines()
+                                            .collect(Collectors.joining("\n"))
+                                            .replaceAll("#.*?\\r?\\n", ""))) {
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getNString(1), equalTo("a@b.com"));
+                assertThat(resultSet.next(), equalTo(false));
+            }
+        }
+    }
+}
