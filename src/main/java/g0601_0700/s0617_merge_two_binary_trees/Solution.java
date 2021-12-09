1b2808bb03946ee9c424c1f834bc212b16a768ba
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-09 11:56:56 +0200

    Added task 617.

---
+++ src/main/java/g0601_0700/s0617_merge_two_binary_trees/Solution.java
@@ -0,0 +1,27 @@
+package g0601_0700.s0617_merge_two_binary_trees;
+
+// #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
+
+import com_github_leetcode.TreeNode;
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
+ * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null) {
+            return root2;
+        }
+
+        if (root2 == null) {
+            return root1;
+        }
+
+        return new TreeNode(
+                root1.val + root2.val,
+                mergeTrees(root1.left, root2.left),
+                mergeTrees(root1.right, root2.right));
+    }
+}
