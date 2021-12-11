eef5bc547ede107dd9c290586a1ce367afcfb46e
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-11 22:38:40 +0700

    Update Solution.java

--- src/main/java/g0301_0400/s0349_intersection_of_two_arrays/Solution.java
+++ src/main/java/g0301_0400/s0349_intersection_of_two_arrays/Solution.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        boolean occ[] = new boolean[1001];
+        boolean[] occ = new boolean[1001];
         for (int i = 0; i < nums1.length; i++) {
             occ[nums1[i]] = true;
         }
@@ -18,7 +18,7 @@ class Solution {
                 res.add(nums2[i]);
             }
         }
-        int result[] = new int[res.size()];
+        int[] result = new int[res.size()];
         for (int i = 0; i < res.size(); i++) {
             result[i] = res.get(i);
         }
