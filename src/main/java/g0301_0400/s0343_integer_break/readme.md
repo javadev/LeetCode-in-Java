9666db4c1ed11c425b9b332ce39f3790562290dc
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2021-12-12 15:06:52 +0700

    Added tasks 343,344,347.

---
+++ src/main/java/g0301_0400/s0343_integer_break/readme.md
@@ -0,0 +1,27 @@
+﻿343\. Integer Break
+
+Medium
+
+Given an integer `n`, break it into the sum of `k` **positive integers**, where `k >= 2`, and maximize the product of those integers.
+
+Return _the maximum product you can get_.
+
+**Example 1:**
+
+**Input:** n = 2
+
+**Output:** 1
+
+**Explanation:** 2 = 1 + 1, 1 × 1 = 1.
+
+**Example 2:**
+
+**Input:** n = 10
+
+**Output:** 36
+
+**Explanation:** 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+
+**Constraints:**
+
+*   `2 <= n <= 58`
---
+++ src/main/java/g0301_0400/s0347_top_k_frequent_elements/Solution.java
@@ -0,0 +1,26 @@
+package g0301_0400.s0347_top_k_frequent_elements;
+
+// #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table #Sorting
+// #Heap_Priority_Queue #Counting #Divide_and_Conquer #Quickselect #Bucket_Sort
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> queue =
+                new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        queue.addAll(map.entrySet());
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = Objects.requireNonNull(queue.poll()).getKey();
+        }
+        return result;
+    }
+}
