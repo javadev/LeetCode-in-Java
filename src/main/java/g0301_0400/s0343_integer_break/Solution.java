094ea94141ec445ae0643779534b0690352da365
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-12 15:03:03 +0700

    format code

--- src/main/java/g0301_0400/s0343_integer_break/Solution.java
+++ src/main/java/g0301_0400/s0343_integer_break/Solution.java
@@ -9,8 +9,12 @@ public class Solution {
     private final Map<Integer, Integer> memo = new HashMap<>();
 
     public int integerBreak(int n) {
-        if (memo.containsKey(n)) return memo.get(n);
-        if (n == 2) return 1;
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        if (n == 2) {
+            return 1;
+        }
         int ans = 0;
         for (int i = 1; i <= n / 2; i++) {
             int ret = integerBreak(n - i);
--- src/main/java/g0301_0400/s0347_top_k_frequent_elements/Solution.java
+++ src/main/java/g0301_0400/s0347_top_k_frequent_elements/Solution.java
@@ -11,12 +11,16 @@ import java.util.PriorityQueue;
 public class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
         PriorityQueue<Map.Entry<Integer, Integer>> queue =
                 new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
         queue.addAll(map.entrySet());
         int[] result = new int[k];
-        for (int i = 0; i < k; i++) result[i] = Objects.requireNonNull(queue.poll()).getKey();
+        for (int i = 0; i < k; i++) {
+            result[i] = Objects.requireNonNull(queue.poll()).getKey();
+        }
         return result;
     }
 }
