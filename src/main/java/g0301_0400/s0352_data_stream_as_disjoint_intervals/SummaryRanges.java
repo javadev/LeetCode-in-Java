853e4c782eb473c7a55f52d17b01ed8333b3a1b9
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-13 08:56:34 +0700

    improve solution

--- src/main/java/g0301_0400/s0352_data_stream_as_disjoint_intervals/SummaryRanges.java
+++ src/main/java/g0301_0400/s0352_data_stream_as_disjoint_intervals/SummaryRanges.java
@@ -2,57 +2,66 @@ package g0301_0400.s0352_data_stream_as_disjoint_intervals;
 
 // #Hard #Binary_Search #Design #Ordered_Set
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.NavigableSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SummaryRanges {
+    private static class Node {
+        int min;
+        int max;
 
-    private final NavigableSet<int[]> intervals;
-    private final Set<Integer> seen = new HashSet<>();
+        public Node(int min, int max) {
+            this.min = min;
+            this.max = max;
+        }
 
+        public Node(int val) {
+            min = val;
+            max = val;
+        }
+    }
+
+    List<Node> list;
+    // Initialize your data structure here.
     public SummaryRanges() {
-        this.intervals = new TreeSet<>(Comparator.comparingInt(i -> i[0]));
+        list = new ArrayList<>();
     }
 
     public void addNum(int val) {
-        if (seen.contains(val)) {
-            return;
+        int left = 0;
+        int right = list.size() - 1;
+        int index = list.size();
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            Node node = list.get(mid);
+            if (node.min <= val && val <= node.max) {
+                return;
+            } else if (val < node.min) {
+                index = mid;
+                right = mid - 1;
+            } else if (val > node.max) {
+                left = mid + 1;
+            }
         }
-        seen.add(val);
-        int[] interval = new int[] {val, val};
-        this.intervals.add(interval);
-        this.merge(interval);
+        list.add(index, new Node(val));
     }
 
     public int[][] getIntervals() {
-        int[][] res = new int[intervals.size()][2];
-        int i = 0;
-        for (int[] in : intervals) {
-            res[i] = in;
-            i++;
+        for (int i = 1; i < list.size(); i++) {
+            Node prev = list.get(i - 1);
+            Node curr = list.get(i);
+            if (curr.min == prev.max + 1) {
+                prev.max = curr.max;
+                list.remove(i--);
+            }
+        }
+        int len = list.size();
+        int[][] res = new int[len][2];
+        for (int i = 0; i < len; i++) {
+            Node node = list.get(i);
+            res[i][0] = node.min;
+            res[i][1] = node.max;
         }
         return res;
-    }
-
-    private void merge(int[] interval) {
-        // base case
-        if (interval == null) {
-            return;
-        }
-        int[] lower = intervals.lower(interval);
-
-        int[] higher;
-
-        if (lower != null && lower[1] == interval[0] - 1) {
-            lower[1] = interval[1];
-            intervals.remove(interval);
-            higher = intervals.higher(lower);
-        } else {
-            higher = intervals.higher(interval);
-        }
-        merge(higher);
     }
 }
