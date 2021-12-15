c2adb6674c051bae965f60b9182a7f64c74e8a78
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-15 07:53:53 +0700

    Update SolutionTest.java

--- src/test/java/g0301_0400/s0368_largest_divisible_subset/SolutionTest.java
+++ src/test/java/g0301_0400/s0368_largest_divisible_subset/SolutionTest.java
@@ -5,21 +5,20 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void largestDivisibleSubset() {
         assertThat(
-                new Solution().largestDivisibleSubset(new int[]{1, 2, 3}),
+                new Solution().largestDivisibleSubset(new int[] {1, 2, 3}),
                 equalTo(new ArrayList<Integer>(Arrays.asList(1, 2))));
     }
 
     @Test
     void largestDivisibleSubset2() {
         assertThat(
-                new Solution().largestDivisibleSubset(new int[]{1, 2, 4, 8}),
+                new Solution().largestDivisibleSubset(new int[] {1, 2, 4, 8}),
                 equalTo(new ArrayList<Integer>(Arrays.asList(1, 2, 4, 8))));
     }
 }
