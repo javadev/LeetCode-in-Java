5e554ceba4110b7b1fc0dc3b1caea7d0a830fa09
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-15 04:36:41 +0200

    Improved task 864.

--- src/main/java/g0801_0900/s0864_shortest_path_to_get_all_keys/Solution.java
+++ src/main/java/g0801_0900/s0864_shortest_path_to_get_all_keys/Solution.java
@@ -3,88 +3,97 @@ package g0801_0900.s0864_shortest_path_to_get_all_keys;
 // #Hard #Breadth_First_Search #Bit_Manipulation
 
 import java.util.LinkedList;
+import java.util.Queue;
 
+@SuppressWarnings("java:S135")
 public class Solution {
-    class Status {
-        int key;
-        int i;
-        int j;
+    private int m;
+    private int n;
 
-        public Status(int key, int i, int j) {
-            this.key = key;
-            this.i = i;
-            this.j = j;
+    public int shortestPathAllKeys(String[] stringGrid) {
+        // strategy: BFS + masking
+        m = stringGrid.length;
+        n = stringGrid[0].length();
+        char[][] grid = new char[m][n];
+        int index = 0;
+        // convert to char Array
+        for (String s : stringGrid) {
+            grid[index++] = s.toCharArray();
         }
-    }
-
-    public int shortestPathAllKeys(String[] grid) {
-        int success = 0;
-        int startI = 0;
-        int startJ = 0;
-        int rows = grid.length;
-        int cols = grid[0].length();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                char c = grid[i].charAt(j);
-                if (c >= 'A' && c <= 'F') {
-                    success |= 1 << (c - 'A');
+        // number of keys
+        int count = 0;
+        Queue<int[]> q = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // find starting position
+                if (grid[i][j] == '@') {
+                    q.add(new int[] {i, j, 0});
                 }
-
-                if (c == '@') {
-                    startI = i;
-                    startJ = j;
+                // count number of keys
+                if ('a' <= grid[i][j] && grid[i][j] <= 'f') {
+                    count++;
                 }
             }
         }
-        int[][][] dist = new int[1 << 6][rows][cols];
-        for (int i = 0; i < dist.length; i++) {
-            for (int j = 0; j < dist[0].length; j++) {
-                for (int k = 0; k < dist[0][0].length; k++) {
-                    dist[i][j][k] = Integer.MAX_VALUE;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        // this is the amt of keys we need
+        int target = (1 << count) - 1;
+        // keep track of position and current state
+        boolean[][][] visited = new boolean[m][n][target + 1];
+        // set initial position and state to true
+        visited[q.peek()[0]][q.peek()[1]][0] = true;
+        int steps = 0;
+        while (!q.isEmpty()) {
+            // use size to make sure everything is on one level
+            int size = q.size();
+            while (--size >= 0) {
+                int[] curr = q.poll();
+                int x = curr[0];
+                int y = curr[1];
+                int state = curr[2];
+                // found all keys
+                if (state == target) {
+                    return steps;
                 }
-            }
-        }
-        LinkedList<Status> queue = new LinkedList<>();
-        queue.offer(new Status(0, startI, startJ));
-        dist[0][startI][startJ] = 0;
-        int path = 0;
-        int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            while (size-- > 0) {
-                Status status = queue.poll();
-                int key = status.key;
-                int x = status.i;
-                int y = status.j;
-                if (key == success) {
-                    return path;
-                }
-                for (int[] dir : dirs) {
-                    int xx = x + dir[0];
-                    int yy = y + dir[1];
-                    if (xx >= 0
-                            && xx < rows
-                            && yy >= 0
-                            && yy < cols
-                            && grid[xx].charAt(yy) != '#') {
-                        int nextKey = key;
-                        char c = grid[xx].charAt(yy);
-                        if (c >= 'a' && c <= 'f') {
-                            nextKey = key | (1 << (c - 'a'));
-                        }
-
-                        if (c >= 'A' && c <= 'F' && (nextKey & (1 << (c - 'A'))) == 0) {
-                            continue;
-                        }
-                        if (path + 1 < dist[nextKey][xx][yy]) {
-                            dist[nextKey][xx][yy] = path + 1;
-                            queue.offer(new Status(nextKey, xx, yy));
-                        }
+                for (int i = 0; i < 4; i++) {
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    // use new state so we don't mess up current state
+                    int nState = state;
+                    // out of bounds or reached wall
+                    if (!inBounds(nx, ny) || grid[nx][ny] == '#') {
+                        continue;
+                    }
+                    // found key
+                    // use OR to add key to our current state because if we already had the key the
+                    // digit would still be 1/true
+                    if ('a' <= grid[nx][ny] && grid[nx][ny] <= 'f') {
+                        // bit mask our found key
+                        nState = state | (1 << (grid[nx][ny] - 'a'));
+                    }
+                    // found lock
+                    // use & to see if we have the key
+                    // 0 means that the digit we are looking at is 0
+                    // need a 1 at the digit spot which means there is a key there
+                    if ('A' <= grid[nx][ny]
+                            && grid[nx][ny] <= 'F'
+                            && ((nState & (1 << (grid[nx][ny] - 'A'))) == 0)) {
+                        continue;
+                    }
+                    // not seen before
+                    if (!visited[nx][ny][nState]) {
+                        q.add(new int[] {nx, ny, nState});
+                        visited[nx][ny][nState] = true;
                     }
                 }
             }
-            path++;
+            steps++;
         }
         return -1;
+    }
+
+    private boolean inBounds(int x, int y) {
+        return x >= 0 && x < m && y >= 0 && y < n;
     }
 }
