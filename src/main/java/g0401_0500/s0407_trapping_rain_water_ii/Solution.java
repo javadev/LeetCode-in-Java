209a1c0cef2de7f11420039155bffb69ce4aca82
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-16 16:09:57 +0200

    Fixed checkstyle.

--- src/main/java/g0401_0500/s0407_trapping_rain_water_ii/Solution.java
+++ src/main/java/g0401_0500/s0407_trapping_rain_water_ii/Solution.java
@@ -24,8 +24,12 @@ public class Solution {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Cell cell = (Cell) o;
             return row == cell.row && col == cell.col && value == cell.value;
         }
