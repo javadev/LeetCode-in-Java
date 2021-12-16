4d3a01bb22087b7d6071048e9c2e24971ccd754c
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-16 22:17:47 +0200

    Improved task 375.

--- src/main/java/g0301_0400/s0375_guess_number_higher_or_lower_ii/Solution.java
+++ src/main/java/g0301_0400/s0375_guess_number_higher_or_lower_ii/Solution.java
@@ -2,53 +2,44 @@ package g0301_0400.s0375_guess_number_higher_or_lower_ii;
 
 // #Medium #Dynamic_Programming #Math #Game_Theory
 
-/*
-Trivial Case:
-There is only one value to search, n=1 -> no cost return 0
-If there are two possible values n=2, -> return the lowest of them -> 1
-
-If there are any higher numbers to search (1....n)
-divide the search space (1...i-1, i, i+1, ... n)
-The cost = min( i + max(1..i-1, i+1 ... n))
-
-There is no other intelligent way to search and find the cost
-
-Optimization:
-    The best cost will be near the higher side (not in middle)
-    - for some i (close to n), the cost will be min and then start to increase if i is shifted left or right.
-
-*/
-
+@SuppressWarnings("java:S135")
 public class Solution {
-    Integer[][] dp;
+    int[][] matrix;
 
     public int getMoneyAmount(int n) {
-        dp = new Integer[n][n];
-        return solve(1, n);
+        matrix = new int[n + 1][n + 1];
+        return get(1, n);
     }
 
-    private int solve(int from, int to) {
-        if (from >= to) {
-            return 0;
+    private int get(int min, int max) {
+        if (max - min < 3) {
+            return max - min <= 0 ? 0 : max - 1;
         }
-        if (from + 1 == to) {
-            return from;
+        if (matrix[min][max] != 0) {
+            return matrix[min][max];
         }
-        if (dp[from - 1][to - 1] != null) {
-            return dp[from - 1][to - 1];
-        }
-        int result = Integer.MAX_VALUE;
-        for (int mid = to; mid >= from; mid--) {
-            int left = solve(from, mid - 1);
-            int right = solve(mid + 1, to);
-            int max = mid + Math.max(left, right);
-            if (max <= result) {
-                result = max;
-            } else {
+        int select = max - 3;
+        int minRes = Integer.MAX_VALUE;
+        int res;
+        int end = min + ((max - min) >> 1) - 1;
+        int cnt = 0;
+        while (true) {
+            res = select + Math.max(get(min, select - 1), get(select + 1, max));
+            if (res > minRes) {
+                cnt++;
+                if (cnt >= 3) {
+                    break;
+                }
+            }
+            if (res < minRes) {
+                minRes = res;
+            }
+            select--;
+            if (select <= end) {
                 break;
             }
         }
-        dp[from - 1][to - 1] = result;
-        return dp[from - 1][to - 1];
+        matrix[min][max] = minRes;
+        return minRes;
     }
 }
