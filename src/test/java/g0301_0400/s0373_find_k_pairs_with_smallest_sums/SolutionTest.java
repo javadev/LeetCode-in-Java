a77c05f9f4f32f0208060e01e1e09edff921e98d
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-16 03:54:09 +0200

    Improved tests for task 373.

--- src/test/java/g0301_0400/s0373_find_k_pairs_with_smallest_sums/SolutionTest.java
+++ src/test/java/g0301_0400/s0373_find_k_pairs_with_smallest_sums/SolutionTest.java
@@ -3,8 +3,7 @@ package g0301_0400.s0373_find_k_pairs_with_smallest_sums;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import com_github_leetcode.ArrayUtils;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
@@ -12,25 +11,20 @@ class SolutionTest {
     void ksmallestPairs() {
         assertThat(
                 new Solution().ksmallestPairs(new int[] {1, 7, 11}, new int[] {2, 4, 6}, 3),
-                equalTo(
-                        new ArrayList<>(
-                                Arrays.asList(
-                                        Arrays.asList(1, 2),
-                                        Arrays.asList(1, 4),
-                                        Arrays.asList(1, 6)))));
+                equalTo(ArrayUtils.getLists(new int[][] {{1, 2}, {1, 4}, {1, 6}})));
     }
 
     @Test
     void ksmallestPairs2() {
         assertThat(
                 new Solution().ksmallestPairs(new int[] {1, 1, 2}, new int[] {1, 2, 3}, 2),
-                equalTo(new ArrayList<>(Arrays.asList(Arrays.asList(1, 1), Arrays.asList(1, 1)))));
+                equalTo(ArrayUtils.getLists(new int[][] {{1, 1}, {1, 1}})));
     }
 
     @Test
     void ksmallestPairs3() {
         assertThat(
                 new Solution().ksmallestPairs(new int[] {1, 2}, new int[] {3}, 3),
-                equalTo(new ArrayList<>(Arrays.asList(Arrays.asList(1, 3), Arrays.asList(2, 3)))));
+                equalTo(ArrayUtils.getLists(new int[][] {{1, 3}, {2, 3}})));
     }
 }
