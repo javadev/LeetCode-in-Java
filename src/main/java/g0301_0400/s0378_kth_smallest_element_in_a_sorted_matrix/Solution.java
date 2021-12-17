90eab756090884747487e2d4286489143d7a5742
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-17 13:57:13 +0200

    Improved task 378.

--- src/main/java/g0301_0400/s0378_kth_smallest_element_in_a_sorted_matrix/Solution.java
+++ src/main/java/g0301_0400/s0378_kth_smallest_element_in_a_sorted_matrix/Solution.java
@@ -4,49 +4,33 @@ package g0301_0400.s0378_kth_smallest_element_in_a_sorted_matrix;
 
 public class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        if (matrix == null || matrix.length == 0) {
-            return -1;
-        }
-        int start = matrix[0][0];
-        int end = matrix[matrix.length - 1][matrix[0].length - 1];
-        // O(log(max-min)) time
-        while (start + 1 < end) {
-            int mid = start + (end - start) / 2;
-            if (countLessEqual(matrix, mid) < k) {
-                // look towards end
-                start = mid;
+        int n = matrix.length;
+        int l = matrix[0][0];
+        int h = matrix[n - 1][n - 1];
+        while (l <= h) {
+            int m = (l + h) / 2;
+            int cnt = getLessThanCount(matrix, m);
+            if (cnt < k) {
+                l = m + 1;
             } else {
-                // look towards start
-                end = mid;
+                h = m - 1;
             }
         }
-
-        // leave only with start and end, one of them must be the answer
-        // try to see if start fits the criteria first
-        if (countLessEqual(matrix, start) >= k) {
-            return start;
-        } else {
-            return end;
-        }
+        return l;
     }
 
-    // countLessEqual
-    // O(n) Time
-    private int countLessEqual(int[][] matrix, int target) {
-        // binary elimination from top right
-        int row = 0;
-        int col = matrix[0].length - 1;
-        int count = 0;
-        while (row < matrix.length && col >= 0) {
-            if (matrix[row][col] <= target) {
-                // get the count in current row
-                count += col + 1;
-                row++;
-            } else if (matrix[row][col] > target) {
-                // eliminate the current col
-                col--;
+    private int getLessThanCount(int[][] m, int k) {
+        int i = m.length - 1;
+        int j = 0;
+        int cnt = 0;
+        while (i >= 0 && j < m.length) {
+            if (m[i][j] <= k) {
+                j++;
+                cnt += i + 1;
+            } else {
+                i--;
             }
         }
-        return count;
+        return cnt;
     }
 }
