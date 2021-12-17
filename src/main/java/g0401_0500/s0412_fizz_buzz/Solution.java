a1789532b78598ee7418b1353d8af2708ef5b086
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-17 03:45:12 +0200

    Fixed checkstyle.

--- src/main/java/g0401_0500/s0412_fizz_buzz/Solution.java
+++ src/main/java/g0401_0500/s0412_fizz_buzz/Solution.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Solution {
     public List<String> fizzBuzz(int n) {
-        List<String> result = new ArrayList();
+        List<String> result = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 result.add("FizzBuzz");
