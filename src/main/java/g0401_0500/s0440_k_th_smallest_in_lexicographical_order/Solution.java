d25bad7150f68badf560e18e4b53d7ca714e9bf4
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-17 17:01:03 +0200

    Fixed sonar.

--- src/main/java/g0401_0500/s0440_k_th_smallest_in_lexicographical_order/Solution.java
+++ src/main/java/g0401_0500/s0440_k_th_smallest_in_lexicographical_order/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
         int curr = 1;
         k = k - 1;
         while (k > 0) {
-            int steps = calSteps(n, curr, curr + 1);
+            int steps = calSteps(n, curr, curr + 1L);
             if (steps <= k) {
                 curr += 1;
                 k -= steps;
@@ -23,7 +23,7 @@ public class Solution {
     private int calSteps(int n, long n1, long n2) {
         int steps = 0;
         while (n1 <= n) {
-            steps += Math.min(n + 1, n2) - n1;
+            steps += Math.min(n + 1L, n2) - n1;
             n1 *= 10;
             n2 *= 10;
         }
