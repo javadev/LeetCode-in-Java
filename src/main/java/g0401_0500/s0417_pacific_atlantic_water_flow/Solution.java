a5aa89165a59ea7074d417eddfa6c57b4636fe2e
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-18 07:32:29 +0200

    Added task 417.

---
+++ src/main/java/g0401_0500/s0417_pacific_atlantic_water_flow/Solution.java
@@ -0,0 +1,60 @@
+package g0401_0500.s0417_pacific_atlantic_water_flow;
+
+// #Medium #Array #Depth_First_Search #Breadth_First_Search #Matrix
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    private int col = 0;
+    private int row = 0;
+
+    public List<List<Integer>> pacificAtlantic(int[][] matrix) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (matrix.length == 0) {
+            return res;
+        }
+        col = matrix.length;
+        row = matrix[0].length;
+        boolean[][] pacific = new boolean[col][row];
+        boolean[][] atlantic = new boolean[col][row];
+        for (int i = 0; i < col; i++) {
+            dfs(i, 0, matrix, pacific);
+            dfs(i, row - 1, matrix, atlantic);
+        }
+        for (int i = 0; i < row; i++) {
+            dfs(0, i, matrix, pacific);
+            dfs(col - 1, i, matrix, atlantic);
+        }
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(i);
+                    temp.add(j);
+                    res.add(temp);
+                }
+            }
+        }
+        return res;
+    }
+
+    private void dfs(int i, int j, int[][] matrix, boolean[][] visited) {
+        if (i < 0 || j < 0 || i >= matrix.length || j >= matrix[0].length || visited[i][j]) {
+            return;
+        }
+        visited[i][j] = true;
+        if (i < col - 1 && matrix[i][j] <= matrix[i + 1][j]) {
+            dfs(i + 1, j, matrix, visited);
+        }
+        if (i > 0 && matrix[i][j] <= matrix[i - 1][j]) {
+            dfs(i - 1, j, matrix, visited);
+        }
+        if (j < row - 1 && matrix[i][j] <= matrix[i][j + 1]) {
+            dfs(i, j + 1, matrix, visited);
+        }
+        if (j > 0 && matrix[i][j] <= matrix[i][j - 1]) {
+            dfs(i, j - 1, matrix, visited);
+        }
+    }
+}
---
+++ src/test/java/g0401_0500/s0417_pacific_atlantic_water_flow/SolutionTest.java
@@ -0,0 +1,21 @@
+package g0401_0500.s0417_pacific_atlantic_water_flow;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.ArrayUtils;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void pacificAtlantic() {
+        int[][] matrix = {
+            {2, 3, 5},
+            {3, 4, 4},
+            {5, 3, 1},
+        };
+        assertThat(
+                new Solution().pacificAtlantic(matrix),
+                equalTo(ArrayUtils.getLists(new int[][] {{0, 2}, {1, 1}, {1, 2}, {2, 0}})));
+    }
+}
