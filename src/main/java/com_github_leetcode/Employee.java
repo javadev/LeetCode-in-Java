a2aa2988f0d395e366eaae3438927993ad129538
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-18 07:06:34 +0200

    Added common classes.

---
+++ src/main/java/com_github_leetcode/Employee.java
@@ -0,0 +1,19 @@
+package com_github_leetcode;
+
+import java.util.List;
+
+@SuppressWarnings("java:S1104")
+public class Employee {
+    /** It's the unique id of each node; unique id of this employee */
+    public int id;
+    /** the importance value of this employee */
+    public int importance;
+    /** the id of direct subordinates */
+    public List<Integer> subordinates;
+
+    public Employee(int id, int importance, List<Integer> subordinates) {
+        this.id = id;
+        this.importance = importance;
+        this.subordinates = subordinates;
+    }
+}
---
+++ src/main/java/com_github_leetcode/Interval.java
@@ -0,0 +1,54 @@
+package com_github_leetcode;
+
+@SuppressWarnings("java:S1104")
+public class Interval implements Comparable<Interval> {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Interval)) {
+            return false;
+        }
+
+        Interval interval = (Interval) o;
+
+        if (start != interval.start) {
+            return false;
+        }
+        return end == interval.end;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = start;
+        result = 31 * result + end;
+        return result;
+    }
+
+    public Interval(int s, int e) {
+
+        this.start = s;
+        this.end = e;
+    }
+
+    @Override
+    public int compareTo(Interval o) {
+        int compareStart = o.start;
+        // ascending order
+        return this.start - compareStart;
+    }
+
+    @Override
+    public String toString() {
+        return "Interval [start=" + start + ", end=" + end + "]";
+    }
+}
