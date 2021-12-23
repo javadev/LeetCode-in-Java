a01ee88d9d39f4ffdeae81681774eeaff43e423d
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-23 10:50:15 +0700

    Added tasks 450, 451, 452, 453

---
+++ src/main/java/g0401_0500/s0451_sort_characters_by_frequency/Solution.java
@@ -0,0 +1,35 @@
+package g0401_0500.s0451_sort_characters_by_frequency;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class Solution {
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        TreeMap<Integer, List<Character>> reverseMap = new TreeMap<>(Collections.reverseOrder());
+        for (char c : map.keySet()) {
+            int freq = map.get(c);
+            if (!reverseMap.containsKey(freq)) {
+                reverseMap.put(freq, new ArrayList<>());
+            }
+            reverseMap.get(freq).add(c);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int freq : reverseMap.keySet()) {
+            List<Character> list = reverseMap.get(freq);
+            for (char c : list) {
+                for (int i = 0; i < freq; i++) {
+                    sb.append(c);
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
---
+++ src/main/java/g0401_0500/s0453_minimum_moves_to_equal_array_elements/Solution.java
@@ -0,0 +1,17 @@
+package g0401_0500.s0453_minimum_moves_to_equal_array_elements;
+
+// #Medium #Array #Math
+
+public class Solution {
+    public int minMoves(int[] nums) {
+        int min = nums[0];
+        int sum = nums[0];
+        
+		// determining the total sum and smallest element of the input array
+        for (int i = 1; i <= nums.length - 1; i++) {
+            sum += nums[i];
+            min = Math.min(min, nums[i]);
+        }
+        return sum - (min * nums.length);
+    }
+}
