de552de7414a95566c66b03dc37cfd0ebc533f32
ThanhNIT <thanhnguyen.it.work@gmail.com>
2021-12-25 07:53:37 +0700

    format code

--- src/main/java/g0401_0500/s0462_minimum_moves_to_equal_array_elements_ii/Solution.java
+++ src/main/java/g0401_0500/s0462_minimum_moves_to_equal_array_elements_ii/Solution.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
-        int median = (nums.length-1)/2;
+        int median = (nums.length - 1) / 2;
         int ops = 0;
         for (int num : nums) {
-            if (num != nums[median]){
+            if (num != nums[median]) {
                 ops += Math.abs(nums[median] - num);
             }
         }
--- src/test/java/g0401_0500/s0464_can_i_win/SolutionTest.java
+++ src/test/java/g0401_0500/s0464_can_i_win/SolutionTest.java
@@ -1,18 +1,18 @@
 package g0401_0500.s0464_can_i_win;
 
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void canIWin() {
-        assertThat(new Solution().canIWin(10,0), equalTo(true));
+        assertThat(new Solution().canIWin(10, 0), equalTo(true));
     }
 
     @Test
     void canIWin2() {
-        assertThat(new Solution().canIWin(10,1), equalTo(true));
+        assertThat(new Solution().canIWin(10, 1), equalTo(true));
     }
 }
