124f2dba6bd906098eb5623942faa644e9268019
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-26 14:34:00 +0200

    Improved task 452.

--- src/main/java/g0401_0500/s0452_minimum_number_of_arrows_to_burst_balloons/Solution.java
+++ src/main/java/g0401_0500/s0452_minimum_number_of_arrows_to_burst_balloons/Solution.java
@@ -1,27 +1,32 @@
 package g0401_0500.s0452_minimum_number_of_arrows_to_burst_balloons;
 
-import java.util.Arrays;
-
 // #Medium #Array #Sorting #Greedy
 
+import java.util.Arrays;
+
 public class Solution {
-    /**
-     * I'm glad to have come up with this solution on my own on 10/13/2021: we'll have to sort the
-     * balloons by its ending points, a counter case to this is below: {{0, 6}, {0, 9}, {7, 8}} if
-     * we sort by starting points, then it becomes: {0, 6}, {0, 9}, {7, 8}, this way, if we shoot 9,
-     * {0, 6} won't be burst however, if we sort by ending points, then it becomes: {0, 6}, {7, 8},
-     * {0, 9}, then we shoot at 6, then at 8, this gives us the result of bursting all balloons.
-     */
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
-        int minArrows = 1;
-        long end = points[0][1];
-        for (int i = 1; i < points.length; i++) {
-            if (points[i][0] > end) {
-                minArrows++;
-                end = points[i][1];
+        int len = points.length, arrows = 1;
+        Arrays.sort(
+                points,
+                (o1, o2) -> {
+                    if (o1[1] < o2[1]) {
+                        return -1;
+                    } else if (o1[1] > o2[1]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+
+        int index = points[0][1];
+        for (int i = 0; i < len; i++) {
+            if (points[i][0] <= index) {
+                continue;
             }
+            arrows++;
+            index = points[i][1];
         }
-        return minArrows;
+        return arrows;
     }
 }
