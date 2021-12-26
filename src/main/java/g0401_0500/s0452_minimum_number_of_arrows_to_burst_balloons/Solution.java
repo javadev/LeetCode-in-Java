ffe2515eac632daa310862cccce92c06c3552471
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-26 13:41:11 +0100

    Improved task 452.

--- src/main/java/g0401_0500/s0452_minimum_number_of_arrows_to_burst_balloons/Solution.java
+++ src/main/java/g0401_0500/s0452_minimum_number_of_arrows_to_burst_balloons/Solution.java
@@ -1,16 +1,20 @@
 package g0401_0500.s0452_minimum_number_of_arrows_to_burst_balloons;
 
-import java.util.Arrays;
-
 // #Medium #Array #Sorting #Greedy
 
+import java.util.Arrays;
+
 public class Solution {
-    /**
-     * I'm glad to have come up with this solution on my own on 10/13/2021: we'll have to sort the
-     * balloons by its ending points, a counter case to this is below: {{0, 6}, {0, 9}, {7, 8}} if
-     * we sort by starting points, then it becomes: {0, 6}, {0, 9}, {7, 8}, this way, if we shoot 9,
-     * {0, 6} won't be burst however, if we sort by ending points, then it becomes: {0, 6}, {7, 8},
-     * {0, 9}, then we shoot at 6, then at 8, this gives us the result of bursting all balloons.
+    /*
+     * I'm glad to have come up with this solution on my own on 10/13/2021:
+     * we'll have to sort the
+     * balloons by its ending points, a counter case to this is below:
+     * {{0, 6}, {0, 9}, {7, 8}}
+     * if we sort by starting points, then it becomes:
+     * {0, 6}, {0, 9}, {7, 8}
+     * this way, if we shoot 9,
+     * {0, 6} won't be burst however, if we sort by ending points, then it becomes:
+     * {0, 6}, {7, 8}, {0, 9}, then we shoot at 6, then at 8, this gives us the result of bursting all balloons.
      */
     public int findMinArrowShots(int[][] points) {
         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
