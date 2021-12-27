92b63101b662377d64ee252b1f550d13d80ce041
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-27 09:43:38 +0200

    Improved task 470.

--- src/main/java/g0401_0500/s0470_implement_rand10_using_rand7/Solution.java
+++ src/main/java/g0401_0500/s0470_implement_rand10_using_rand7/Solution.java
@@ -8,21 +8,13 @@ public class Solution {
     private final SecureRandom secureRandom = new SecureRandom();
 
     public int rand10() {
-        int num = (rand7() - 1) * 7 + rand7();
-        if (num <= 40) {
-            return (num % 10) + 1;
+        int x = rand7();
+        int y = rand7();
+        int value = (x - 1) * 7 + y;
+        if (value >= 41) {
+            return rand10();
         }
-        num = num - 40;
-        num = (rand7() - 1) * 9 + num;
-        if (num <= 60) {
-            return (num % 10) + 1;
-        }
-        num = num - 60;
-        int res;
-        do {
-            res = (rand7() + 1) * 3 + num;
-        } while (res <= 20);
-        return (res % 10) + 1;
+        return value % 10 + 1;
     }
 
     private int rand7() {
