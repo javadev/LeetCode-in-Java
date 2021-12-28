a440b293a509fb0404a472e526d1968643edcdc9
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-28 05:36:59 +0200

    Renamed variables.

--- src/main/java/g0401_0500/s0473_matchsticks_to_square/Solution.java
+++ src/main/java/g0401_0500/s0473_matchsticks_to_square/Solution.java
@@ -5,56 +5,56 @@ package g0401_0500.s0473_matchsticks_to_square;
 import java.util.Arrays;
 
 public class Solution {
-    private boolean[] vs;
+    private boolean[] used;
     private int m = 0;
     private int sum = 0;
     private int max = 0;
-    private int e = 0;
+    private int target = 0;
 
-    public boolean makesquare(int[] nums) {
-        m = nums.length;
+    public boolean makesquare(int[] matchsticks) {
+        m = matchsticks.length;
         if (m < 4) {
             return false;
         }
-        for (int n : nums) {
+        for (int n : matchsticks) {
             sum += n;
             max = Math.max(max, n);
         }
         if (sum % 4 != 0 || max > sum / 4) {
             return false;
         }
-        Arrays.sort(nums);
-        e = sum / 4;
-        vs = new boolean[m];
-        vs[0] = true;
-        return dfs(nums, 1, nums[0], 4);
+        Arrays.sort(matchsticks);
+        target = sum / 4;
+        used = new boolean[m];
+        used[0] = true;
+        return dfs(matchsticks, 1, matchsticks[0], 4);
     }
 
-    private boolean dfs(int[] nums, int idx, int tot, int cnt) {
-        if (tot == e) {
+    private boolean dfs(int[] matchsticks, int idx, int tot, int cnt) {
+        if (tot == target) {
             if (--cnt == 1) return true;
             for (int i = 0; i < m; i++) {
-                if (vs[i]) {
+                if (used[i]) {
                     continue;
                 }
-                vs[i] = true;
-                boolean res = dfs(nums, i + 1, nums[i], cnt);
-                vs[i] = false;
+                used[i] = true;
+                boolean res = dfs(matchsticks, i + 1, matchsticks[i], cnt);
+                used[i] = false;
                 return res;
             }
         }
         for (int i = idx; i < m; i++) {
-            if (vs[i]) {
+            if (used[i]) {
                 continue;
             }
-            if (nums[i] + tot > e) {
+            if (matchsticks[i] + tot > target) {
                 return false;
             }
-            vs[i] = true;
-            if (dfs(nums, i + 1, tot + nums[i], cnt)) {
+            used[i] = true;
+            if (dfs(matchsticks, i + 1, tot + matchsticks[i], cnt)) {
                 return true;
             }
-            vs[i] = false;
+            used[i] = false;
         }
         return false;
     }
