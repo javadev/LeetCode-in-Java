0570354fe46a5162ca538003b49d266c5c9a6a7e
Valentyn Kolesnikov <javadev75@gmail.com>
2021-12-30 04:32:16 +0200

    Update Solution.java

--- src/main/java/g0401_0500/s0485_max_consecutive_ones/Solution.java
+++ src/main/java/g0401_0500/s0485_max_consecutive_ones/Solution.java
@@ -5,13 +5,15 @@ package g0401_0500.s0485_max_consecutive_ones;
 public class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         int maxLen = 0;
-        for (int i = 0; i < nums.length; i++) {
+        int i = 0;
+        while (i < nums.length) {
             int currentLen = 0;
             while (i < nums.length && nums[i] == 1) {
                 i++;
                 currentLen++;
             }
             maxLen = Math.max(maxLen, currentLen);
+            i++;
         }
         return maxLen;
     }
