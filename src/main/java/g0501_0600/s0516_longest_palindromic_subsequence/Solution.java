907522698b929714d8a15375234a1687249bda61
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-01-03 08:55:08 +0700

    Update Solution.java

--- src/main/java/g0501_0600/s0516_longest_palindromic_subsequence/Solution.java
+++ src/main/java/g0501_0600/s0516_longest_palindromic_subsequence/Solution.java
@@ -4,16 +4,16 @@ package g0501_0600.s0516_longest_palindromic_subsequence;
 
 public class Solution {
     public int longestPalindromeSubseq(String s) {
-        char[] X = s.toCharArray();
-        char[] Y = new StringBuilder(s).reverse().toString().toCharArray();
-        int m = X.length;
-        int n = Y.length;
+        char[] x = s.toCharArray();
+        char[] y = new StringBuilder(s).reverse().toString().toCharArray();
+        int m = x.length;
+        int n = y.length;
         int[][] l = new int[m + 1][n + 1];
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
                 if (i == 0 || j == 0) {
                     l[i][j] = 0;
-                } else if (X[i - 1] == Y[j - 1]) {
+                } else if (x[i - 1] == y[j - 1]) {
                     l[i][j] = l[i - 1][j - 1] + 1;
 
                 } else {
