e96f9226207af157e230abf5e071b9e3e478e02b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-04 18:50:26 +0200

    Code formatting.

--- src/test/java/g0501_0600/s0530_minimum_absolute_difference_in_bst/SolutionTest.java
+++ src/test/java/g0501_0600/s0530_minimum_absolute_difference_in_bst/SolutionTest.java
@@ -11,16 +11,14 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     @Test
     void getMinimumDifference() {
-        TreeNode treeNode =
-                TreeUtils.constructBinaryTree(Arrays.asList(4, 2, 6, 1, 3));
+        TreeNode treeNode = TreeUtils.constructBinaryTree(Arrays.asList(4, 2, 6, 1, 3));
         assertThat(new Solution().getMinimumDifference(treeNode), equalTo(1));
     }
 
     @Test
     void getMinimumDifference2() {
         TreeNode treeNode =
-                TreeUtils.constructBinaryTree(
-                        Arrays.asList(1, 0, 48, null, null, 12, 49));
+                TreeUtils.constructBinaryTree(Arrays.asList(1, 0, 48, null, null, 12, 49));
         assertThat(new Solution().getMinimumDifference(treeNode), equalTo(1));
     }
 }
