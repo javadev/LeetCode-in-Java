17e9f94c6acdf6bbadb64d7b6113fd4f0d240530
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-05 17:07:24 +0200

    Added test for 539.

--- src/test/java/g0501_0600/s0539_minimum_time_difference/SolutionTest.java
+++ src/test/java/g0501_0600/s0539_minimum_time_difference/SolutionTest.java
@@ -33,4 +33,27 @@ class SolutionTest {
                         });
         assertThat(new Solution().findMinDifference(timePoints), equalTo(0));
     }
+
+    @Test
+    void findMinDifference4() {
+        List<String> timePoints = new ArrayList<>();
+        int index = 0;
+        while (index < 301) {
+            int hour = index / 60 % 24;
+            int minute = index % 60;
+            timePoints.add(
+                    String.format(
+                            "%s:%s",
+                            hour < 10 ? "0" + hour : hour, minute < 10 ? "0" + minute : minute));
+            index++;
+            hour = index / 60 % 24;
+            minute = index % 60;
+            timePoints.add(
+                    String.format(
+                            "%s:%s",
+                            hour < 10 ? "0" + hour : hour, minute < 10 ? "0" + minute : minute));
+            index++;
+        }
+        assertThat(new Solution().findMinDifference(timePoints), equalTo(1));
+    }
 }
