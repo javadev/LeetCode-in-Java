40fd7c552bb0adc2ddd99931dd899adc47e0b209
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-09 01:43:39 +0200

    Added tasks 591, 592, 593.

---
+++ src/main/java/g0501_0600/s0591_tag_validator/Solution.java
@@ -0,0 +1,58 @@
+package g0501_0600.s0591_tag_validator;
+
+// #Hard #String #Stack
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    public boolean isValid(String code) {
+        Deque<String> stack = new ArrayDeque<>();
+        for (int i = 0; i < code.length(); ) {
+            if (i > 0 && stack.isEmpty()) {
+                return false;
+            }
+            if (code.startsWith("<![CDATA[", i)) {
+                // "<![CDATA[" length is 9
+                int j = i + 9;
+                i = code.indexOf("]]>", j);
+                if (i < 0) {
+                    return false;
+                }
+                // "]]>" length is 3
+                i += 3;
+            } else if (code.startsWith("</", i)) {
+                int j = i + 2;
+                i = code.indexOf(">", j);
+                if (i < 0 || i == j || i - j > 9) {
+                    return false;
+                }
+                for (int k = j; k < i; k++) {
+                    if (!Character.isUpperCase(code.charAt(k))) {
+                        return false;
+                    }
+                }
+                String s = code.substring(j, i++);
+                if (stack.isEmpty() || !stack.pop().equals(s)) {
+                    return false;
+                }
+            } else if (code.startsWith("<", i)) {
+                int j = i + 1;
+                i = code.indexOf(">", j);
+                if (i < 0 || i == j || i - j > 9) {
+                    return false;
+                }
+                for (int k = j; k < i; k++) {
+                    if (!Character.isUpperCase(code.charAt(k))) {
+                        return false;
+                    }
+                }
+                String s = code.substring(j, i++);
+                stack.push(s);
+            } else {
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
