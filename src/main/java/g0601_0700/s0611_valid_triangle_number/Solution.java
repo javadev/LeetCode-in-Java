46008b75d339a7c663f17c3bfc0c42ce5c6f04d9
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-01-12 23:09:10 +0700

    Added tasks 611, 621, 622

---
+++ src/main/java/g0601_0700/s0611_valid_triangle_number/Solution.java
@@ -0,0 +1,35 @@
+package g0601_0700.s0611_valid_triangle_number;
+
+// #Medium #Array #Sorting #Greedy #Binary_Search #Two_Pointers
+
+public class Solution {
+    public int triangleNumber(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+        int[] count = new int[1001];
+        for (int i : nums) {
+            count[i]++;
+            max = Math.max(max, i);
+        }
+        count[0] = 0;
+        int idx = 0;
+        for (int i = 1; i <= max; ++i) {
+            for (int j = 0; j < count[i]; ++j, ++idx) {
+                nums[idx] = i;
+            }
+            count[i] += count[i - 1];
+        }
+        n = idx;
+        int r = 0;
+        for (int i = 0; i < n - 2; ++i) {
+            for (int j = i + 1; j < n - 1; ++j) {
+                if (nums[i] + nums[j] > max) {
+                    r += (n - j) * (n - j - 1) / 2;
+                    break;
+                }
+                r += count[nums[i] + nums[j] - 1] - j - 1;
+            }
+        }
+        return r;
+    }
+}
---
+++ src/main/java/g0601_0700/s0622_design_circular_queue/MyCircularQueue.java
@@ -0,0 +1,72 @@
+package g0601_0700.s0622_design_circular_queue;
+
+// #Medium #Array #Design #Linked_List #Queue
+
+public class MyCircularQueue {
+    private final DoubleLinkedNode dumyHead = new DoubleLinkedNode(0);
+    private final int maxSize;
+    private int size = 0;
+
+    public MyCircularQueue(int k) {
+        this.maxSize = k;
+        dumyHead.left = dumyHead;
+        dumyHead.right = dumyHead;
+    }
+
+    public boolean enQueue(int value) {
+        if (size == maxSize) {
+            return false;
+        }
+        DoubleLinkedNode node = new DoubleLinkedNode(value);
+        DoubleLinkedNode right = dumyHead.right;
+        dumyHead.right = node;
+        node.left = dumyHead;
+        node.right = right;
+        right.left = node;
+        size++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (size == 0) {
+            return false;
+        }
+        DoubleLinkedNode left = dumyHead.left;
+        dumyHead.left = left.left;
+        dumyHead.left.right = dumyHead;
+        size--;
+        return true;
+    }
+
+    public int Rear() {
+        if (size == 0) {
+            return -1;
+        }
+        return dumyHead.right.val;
+    }
+
+    public int Front() {
+        if (size == 0) {
+            return -1;
+        }
+        return dumyHead.left.val;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == maxSize;
+    }
+
+    static class DoubleLinkedNode {
+        private final int val;
+        private DoubleLinkedNode left;
+        private DoubleLinkedNode right;
+
+        public DoubleLinkedNode(int val) {
+            this.val = val;
+        }
+    }
+}
