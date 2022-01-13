e29ba36e7b8e57660557d4c05c5a75031ea513c4
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-01-13 08:41:21 +0700

    fix smell

--- src/main/java/g0601_0700/s0629_k_inverse_pairs_array/Solution.java
+++ src/main/java/g0601_0700/s0629_k_inverse_pairs_array/Solution.java
@@ -12,14 +12,16 @@ public class Solution {
         int[] dp1 = new int[k + 1];
         dp[0] = 1;
         dp1[0] = 1;
-        int MOD = 1_000_000_007;
+        int mod = 1_000_000_007;
         for (int i = 1; i <= n; i++) {
             int[] temp = dp;
             dp = dp1;
             dp1 = temp;
             for (int j = 1, m = Math.min(k, i * (i - 1) / 2); j <= m; j++) {
-                dp[j] = (dp1[j] + dp[j - 1] - (j >= i ? dp1[j - i] : 0)) % MOD;
-                if (dp[j] < 0) dp[j] += MOD;
+                dp[j] = (dp1[j] + dp[j - 1] - (j >= i ? dp1[j - i] : 0)) % mod;
+                if (dp[j] < 0) {
+                    dp[j] += mod;
+                }
             }
         }
         return dp[k];
--- src/main/java/g0601_0700/s0630_course_schedule_iii/Solution.java
+++ src/main/java/g0601_0700/s0630_course_schedule_iii/Solution.java
@@ -23,71 +23,71 @@ public class Solution {
         }
         return course;
     }
-}
 
-class MaxHeap {
-    private final int[] heap;
-    private int pin;
+    static class MaxHeap {
+        private final int[] heap;
+        private int pin;
 
-    public MaxHeap(int mexLen) {
-        this.heap = new int[mexLen];
-        this.pin = 0;
-    }
-
-    public int[] getHeap() {
-        return heap;
-    }
-
-    public int getPin() {
-        return pin;
-    }
-
-    public void setPin(int pin) {
-        this.pin = pin;
-    }
-
-    public void add(int e) {
-        heap[pin] = e;
-        int temp = pin;
-        pin++;
-        while (temp > 0 && heap[(temp - 1) / 2] < e) {
-            heap[temp] = heap[(temp - 1) / 2];
-            temp = (temp - 1) / 2;
-            heap[temp] = e;
+        public MaxHeap(int mexLen) {
+            this.heap = new int[mexLen];
+            this.pin = 0;
         }
-    }
 
-    public int pop() {
-        int res = heap[0];
-        pin--;
-        heap[0] = heap[pin];
-        int h0 = heap[0];
-        int temp = 0;
-        while (temp * 2 + 1 < pin) {
-            if (temp * 2 + 2 == pin) {
-                if (heap[temp * 2 + 1] > h0) {
-                    heap[temp] = heap[temp * 2 + 1];
-                    temp = temp * 2 + 1;
-                    heap[temp] = h0;
-                } else {
-                    break;
-                }
-            } else {
-                if (h0 < heap[temp * 2 + 1] || h0 < heap[temp * 2 + 2]) {
-                    if (heap[temp * 2 + 1] > heap[temp * 2 + 2]) {
+        public int[] getHeap() {
+            return heap;
+        }
+
+        public int getPin() {
+            return pin;
+        }
+
+        public void setPin(int pin) {
+            this.pin = pin;
+        }
+
+        public void add(int e) {
+            heap[pin] = e;
+            int temp = pin;
+            pin++;
+            while (temp > 0 && heap[(temp - 1) / 2] < e) {
+                heap[temp] = heap[(temp - 1) / 2];
+                temp = (temp - 1) / 2;
+                heap[temp] = e;
+            }
+        }
+
+        public int pop() {
+            int res = heap[0];
+            pin--;
+            heap[0] = heap[pin];
+            int h0 = heap[0];
+            int temp = 0;
+            while (temp * 2 + 1 < pin) {
+                if (temp * 2 + 2 == pin) {
+                    if (heap[temp * 2 + 1] > h0) {
                         heap[temp] = heap[temp * 2 + 1];
                         temp = temp * 2 + 1;
                         heap[temp] = h0;
                     } else {
-                        heap[temp] = heap[temp * 2 + 2];
-                        temp = temp * 2 + 2;
-                        heap[temp] = h0;
+                        break;
                     }
                 } else {
-                    break;
+                    if (h0 < heap[temp * 2 + 1] || h0 < heap[temp * 2 + 2]) {
+                        if (heap[temp * 2 + 1] > heap[temp * 2 + 2]) {
+                            heap[temp] = heap[temp * 2 + 1];
+                            temp = temp * 2 + 1;
+                            heap[temp] = h0;
+                        } else {
+                            heap[temp] = heap[temp * 2 + 2];
+                            temp = temp * 2 + 2;
+                            heap[temp] = h0;
+                        }
+                    } else {
+                        break;
+                    }
                 }
             }
+            return res;
         }
-        return res;
     }
 }
