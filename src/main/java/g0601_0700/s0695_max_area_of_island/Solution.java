44c664426ac119815f28e4ab174aee9e0d339169
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-16 01:14:41 +0200

    Added task 695.

---
+++ src/main/java/g0601_0700/s0695_max_area_of_island/Solution.java
@@ -0,0 +1,36 @@
+package g0601_0700.s0695_max_area_of_island;
+
+// #Medium #Array #Depth_First_Search #Breadth_First_Search #Matrix #Union_Find
+
+public class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int max = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j, m, n, 0);
+                    max = Math.max(area, max);
+                }
+            }
+        }
+        return max;
+    }
+
+    private int dfs(int[][] grid, int i, int j, int m, int n, int area) {
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return area;
+        }
+        grid[i][j] = 0;
+        area++;
+        area = dfs(grid, i + 1, j, m, n, area);
+        area = dfs(grid, i, j + 1, m, n, area);
+        area = dfs(grid, i - 1, j, m, n, area);
+        area = dfs(grid, i, j - 1, m, n, area);
+        return area;
+    }
+}
---
+++ src/test/java/g0601_0700/s0695_max_area_of_island/SolutionTest.java
@@ -0,0 +1,29 @@
+package g0601_0700.s0695_max_area_of_island;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void maxAreaOfIsland() {
+        int[][] grid = {
+            {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        assertThat(new Solution().maxAreaOfIsland(grid), equalTo(6));
+    }
+
+    @Test
+    void maxAreaOfIsland2() {
+        int[][] grid = {{0, 0, 0, 0, 0, 0, 0, 0}};
+        assertThat(new Solution().maxAreaOfIsland(grid), equalTo(0));
+    }
+}
