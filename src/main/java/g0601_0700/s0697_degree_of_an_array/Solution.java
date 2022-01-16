a76411d10d9877af40c1776b59f8c511654406fc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-16 17:58:50 +0200

    Added task 697.

---
+++ src/main/java/g0601_0700/s0697_degree_of_an_array/Solution.java
@@ -0,0 +1,44 @@
+package g0601_0700.s0697_degree_of_an_array;
+
+// #Easy #Array #Hash_Table
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    private static class Value {
+        int count;
+        int start;
+        int end;
+
+        public Value(int c, int s, int e) {
+            count = c;
+            start = s;
+            end = e;
+        }
+    }
+
+    public int findShortestSubArray(int[] nums) {
+        int max = 1;
+        Map<Integer, Value> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int j = nums[i];
+            if (map.containsKey(j)) {
+                Value v = map.get(j);
+                v.count++;
+                max = Math.max(max, v.count);
+                v.end = i;
+            } else {
+                map.put(j, new Value(1, i, i));
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, Value> entry : map.entrySet()) {
+            Value v = entry.getValue();
+            if (v.count == max) {
+                min = Math.min(min, v.end - v.start);
+            }
+        }
+        return min + 1;
+    }
+}
