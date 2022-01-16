0925ca4fce2398130e881132ad51567832c0c700
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-16 18:27:37 +0200

    Added task 693.

---
+++ src/main/java/g0601_0700/s0693_binary_number_with_alternating_bits/Solution.java
@@ -0,0 +1,16 @@
+package g0601_0700.s0693_binary_number_with_alternating_bits;
+
+// #Easy #Bit_Manipulation
+
+public class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev = -1;
+        while (n != 0) {
+            int v = n & 1;
+            n = n >> 1;
+            if (prev == v) return false;
+            prev = v;
+        }
+        return true;
+    }
+}
