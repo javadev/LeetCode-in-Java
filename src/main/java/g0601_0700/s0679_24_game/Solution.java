9cd729d4142c99c5495ba856dda813828333a52e
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-01-22 09:16:06 +0700

    fix smells

--- src/main/java/g0601_0700/s0679_24_game/Solution.java
+++ src/main/java/g0601_0700/s0679_24_game/Solution.java
@@ -6,10 +6,13 @@ public class Solution {
     private static final double EPS = 1e-6;
 
     private boolean backtrack(double[] list, int n) {
-        if (n == 1) return Math.abs(list[0] - 24) < EPS;
+        if (n == 1) {
+            return Math.abs(list[0] - 24) < EPS;
+        }
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                double a = list[i], b = list[j];
+                double a = list[i];
+                double b = list[j];
                 list[j] = list[n - 1];
                 list[i] = a + b;
                 if (backtrack(list, n - 1)) {
@@ -47,10 +50,10 @@ public class Solution {
     }
 
     public boolean judgePoint24(int[] nums) {
-        double[] A = new double[nums.length];
+        double[] a = new double[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            A[i] = nums[i];
+            a[i] = nums[i];
         }
-        return backtrack(A, A.length);
+        return backtrack(a, a.length);
     }
 }
--- src/main/java/g0601_0700/s0680_valid_palindrome_ii/Solution.java
+++ src/main/java/g0601_0700/s0680_valid_palindrome_ii/Solution.java
@@ -17,7 +17,8 @@ public class Solution {
     }
 
     private boolean isPalindrome(String s) {
-        int l = 0, r = s.length() - 1;
+        int l = 0;
+        int r = s.length() - 1;
         while (l < r) {
             if (s.charAt(l) != s.charAt(r)) {
                 return false;
