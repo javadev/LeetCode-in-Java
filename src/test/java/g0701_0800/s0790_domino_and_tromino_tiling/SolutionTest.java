5c9009ba087c478c6b2fc6e6bccaedffe0d863d5
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-24 20:31:06 +0200

    Added tests.

--- src/test/java/g0701_0800/s0790_domino_and_tromino_tiling/SolutionTest.java
+++ src/test/java/g0701_0800/s0790_domino_and_tromino_tiling/SolutionTest.java
@@ -15,4 +15,29 @@ class SolutionTest {
     void numTilings2() {
         assertThat(new Solution().numTilings(1), equalTo(1));
     }
+
+    @Test
+    void numTilings3() {
+        assertThat(new Solution().numTilings(2), equalTo(2));
+    }
+
+    @Test
+    void numTilings4() {
+        assertThat(new Solution().numTilings(3), equalTo(5));
+    }
+
+    @Test
+    void numTilings5() {
+        assertThat(new Solution().numTilings(4), equalTo(11));
+    }
+
+    @Test
+    void numTilings6() {
+        assertThat(new Solution().numTilings(5), equalTo(24));
+    }
+
+    @Test
+    void numTilings7() {
+        assertThat(new Solution().numTilings(6), equalTo(53));
+    }
 }
