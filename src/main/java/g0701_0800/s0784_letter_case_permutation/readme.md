d6f52c038a92b280c8840a54233e1dec2499e63e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-26 16:38:10 +0200

    Added tasks 783, 784.

---
+++ src/main/java/g0701_0800/s0784_letter_case_permutation/readme.md
@@ -0,0 +1,24 @@
+﻿784\. Letter Case Permutation
+
+Medium
+
+Given a string `s`, you can transform every letter individually to be lowercase or uppercase to create another string.
+
+Return _a list of all possible strings we could create_. Return the output in **any order**.
+
+**Example 1:**
+
+**Input:** s = "a1b2"
+
+**Output:** ["a1b2","a1B2","A1b2","A1B2"] 
+
+**Example 2:**
+
+**Input:** s = "3z4"
+
+**Output:** ["3z4","3Z4"] 
+
+**Constraints:**
+
+*   `1 <= s.length <= 12`
+*   `s` consists of lowercase English letters, uppercase English letters, and digits.
---
+++ src/test/java/g0701_0800/s0784_letter_case_permutation/SolutionTest.java
@@ -0,0 +1,22 @@
+package g0701_0800.s0784_letter_case_permutation;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void letterCasePermutation() {
+        assertThat(
+                new Solution().letterCasePermutation("a1b2"),
+                equalTo(Arrays.asList("a1b2", "a1B2", "A1b2", "A1B2")));
+    }
+
+    @Test
+    void letterCasePermutation2() {
+        assertThat(
+                new Solution().letterCasePermutation("3z4"), equalTo(Arrays.asList("3z4", "3Z4")));
+    }
+}
