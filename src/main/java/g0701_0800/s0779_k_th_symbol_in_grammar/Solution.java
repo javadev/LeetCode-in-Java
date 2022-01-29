15612176ca3abb111107cb8aa8c6d0b3bd2e049f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-01-29 22:41:16 +0200

    Fixed sonar.

--- src/main/java/g0701_0800/s0779_k_th_symbol_in_grammar/Solution.java
+++ src/main/java/g0701_0800/s0779_k_th_symbol_in_grammar/Solution.java
@@ -2,6 +2,7 @@ package g0701_0800.s0779_k_th_symbol_in_grammar;
 
 // #Medium #Math #Bit_Manipulation #Recursion
 
+@SuppressWarnings("java:S1172")
 public class Solution {
     /*
      * Time: O(logn)
