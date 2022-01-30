3ee027f8d72b220fb8d0fd1686fffc409ce21a45
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-01-30 09:05:54 +0700

    fix smells

--- src/main/java/g0801_0900/s0801_minimum_swaps_to_make_sequences_increasing/Solution.java
+++ src/main/java/g0801_0900/s0801_minimum_swaps_to_make_sequences_increasing/Solution.java
@@ -3,17 +3,17 @@ package g0801_0900.s0801_minimum_swaps_to_make_sequences_increasing;
 // #Hard #Array #Dynamic_Programming
 
 public class Solution {
-    public int minSwap(int[] A, int[] B) {
+    public int minSwap(int[] listA, int[] listB) {
         int[] dp = new int[2];
         dp[1] = 1;
-        for (int i = 1; i < A.length; i++) {
+        for (int i = 1; i < listA.length; i++) {
             int a = Integer.MAX_VALUE;
             int b = Integer.MAX_VALUE;
-            if (A[i] > A[i - 1] && B[i] > B[i - 1]) {
+            if (listA[i] > listA[i - 1] && listB[i] > listB[i - 1]) {
                 a = dp[0];
                 b = dp[1];
             }
-            if (A[i] > B[i - 1] && B[i] > A[i - 1]) {
+            if (listA[i] > listB[i - 1] && listB[i] > listA[i - 1]) {
                 a = Math.min(a, dp[1]);
                 b = Math.min(b, dp[0]);
             }
--- src/main/java/g0801_0900/s0805_split_array_with_same_average/Solution.java
+++ src/main/java/g0801_0900/s0805_split_array_with_same_average/Solution.java
@@ -13,28 +13,28 @@ public class Solution {
         }
         Arrays.sort(a);
         for (int len = 1; len <= m / 2; len++) {
-            if (sum * len % m == 0) {
-                if (dfs(a, sum * len / m, len, 0)) {
-                    return true;
-                }
+            if (sum * len % m == 0 && dfs(a, sum * len / m, len, 0)) {
+                return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(int[] A, int sum, int len, int idx) {
-        if (len == 0) return sum == 0;
-        if (sum < 0 || idx >= A.length) {
+    private boolean dfs(int[] a, int sum, int len, int idx) {
+        if (len == 0) {
+            return sum == 0;
+        }
+        if (sum < 0 || idx >= a.length) {
             return false;
         }
-        if (A[idx] > sum / len) {
+        if (a[idx] > sum / len) {
             return false;
         }
-        for (int i = idx; i < A.length; i++) {
-            if (i > idx && A[i] == A[i - 1]) {
+        for (int i = idx; i < a.length; i++) {
+            if (i > idx && a[i] == a[i - 1]) {
                 continue;
             }
-            if (dfs(A, sum - A[i], len - 1, i + 1)) {
+            if (dfs(a, sum - a[i], len - 1, i + 1)) {
                 return true;
             }
         }
