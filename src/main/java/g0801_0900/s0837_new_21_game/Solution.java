70bdde2456e645c11f2adaf3009f2fab0527dd12
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-03 08:30:35 +0200

    Fixed idea warnings.

--- src/main/java/g0801_0900/s0837_new_21_game/Solution.java
+++ src/main/java/g0801_0900/s0837_new_21_game/Solution.java
@@ -4,32 +4,24 @@ package g0801_0900.s0837_new_21_game;
 
 public class Solution {
     public double new21Game(int n, int k, int w) {
-
         if (n >= k + w || k == 0) {
             return 1.0;
         }
-
         double[] dp = new double[n + 1];
         dp[0] = 1.0;
-
         double res = 0.0;
         double runningSum = dp[0];
-
         for (int i = 1; i <= n; i++) {
-
-            dp[i] = (runningSum * 1.0) / (w);
-
+            dp[i] = runningSum / w;
             if (i < k) {
                 runningSum += dp[i];
             } else {
                 res += dp[i];
             }
-
             if (i - w >= 0) {
                 runningSum -= dp[i - w];
             }
         }
-
         return res;
     }
 }
