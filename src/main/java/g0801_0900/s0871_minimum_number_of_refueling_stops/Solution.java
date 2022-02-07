5beb250c5417545aa8341353dbd7d5e701758038
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-07 19:10:41 +0200

    Fixed style.

--- src/main/java/g0801_0900/s0871_minimum_number_of_refueling_stops/Solution.java
+++ src/main/java/g0801_0900/s0871_minimum_number_of_refueling_stops/Solution.java
@@ -12,7 +12,10 @@ public class Solution {
             return -1;
         }
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        int start = 0, end = stations.length, currentFuel = startFuel, stops = 0;
+        int start = 0;
+        int end = stations.length;
+        int currentFuel = startFuel;
+        int stops = 0;
         while (currentFuel < target) {
             while (start < end && currentFuel >= stations[start][0]) {
                 pq.add(stations[start++]);
