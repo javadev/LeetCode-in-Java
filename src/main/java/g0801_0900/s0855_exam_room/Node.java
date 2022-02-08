c2bc96847a73a2b730a69c5667f6a9b5fe8efd19
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-08 16:33:29 +0200

    Fixed sonar warning.

---
+++ src/main/java/g0801_0900/s0855_exam_room/Node.java
@@ -0,0 +1,31 @@
+package g0801_0900.s0855_exam_room;
+
+import java.util.Map;
+
+class Node {
+
+    Node pre;
+    Node next;
+    int val;
+
+    Node(int val, Map<Integer, Node> map) {
+        this.val = val;
+        map.put(val, this);
+    }
+
+    int insert(Node left) {
+        Node right = left.next;
+        left.next = this;
+        right.pre = this;
+        this.next = right;
+        this.pre = left;
+        return val;
+    }
+
+    void delete() {
+        Node left = this.pre;
+        Node right = this.next;
+        left.next = right;
+        right.pre = left;
+    }
+}
