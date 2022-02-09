8685de87964a90278e6e143f09772d78a9c2c3d6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-09 10:47:53 +0200

    Fixed style.

--- src/main/java/g0901_1000/s0992_subarrays_with_k_different_integers/Solution.java
+++ src/main/java/g0901_1000/s0992_subarrays_with_k_different_integers/Solution.java
@@ -4,22 +4,23 @@ package g0901_1000.s0992_subarrays_with_k_different_integers;
 
 public class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        int res = 0, prefix = 0;
+        int res = 0;
+        int prefix = 0;
         int[] cnt = new int[nums.length + 1];
-        for (int i = 0, j = 0, unique_count = 0; i < nums.length; i++) {
+        for (int i = 0, j = 0, uniqueCount = 0; i < nums.length; i++) {
             if (cnt[nums[i]]++ == 0) {
-                unique_count++;
+                uniqueCount++;
             }
-            if (unique_count > k) {
+            if (uniqueCount > k) {
                 --cnt[nums[j++]];
                 prefix = 0;
-                unique_count--;
+                uniqueCount--;
             }
             while (cnt[nums[j]] > 1) {
                 ++prefix;
                 --cnt[nums[j++]];
             }
-            if (unique_count == k) {
+            if (uniqueCount == k) {
                 res += prefix + 1;
             }
         }
