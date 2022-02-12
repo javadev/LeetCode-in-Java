ba0c8b7ab20c69c6c45d788fbddb251d0f3f3810
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-12 16:32:27 +0200

    Improved task 905.

--- src/main/java/g0901_1000/s0905_sort_array_by_parity/Solution.java
+++ src/main/java/g0901_1000/s0905_sort_array_by_parity/Solution.java
@@ -3,22 +3,17 @@ package g0901_1000.s0905_sort_array_by_parity;
 // #Easy #Array #Sorting #Two_Pointers
 
 public class Solution {
-    public int[] sortArrayByParity(int[] a) {
-        int j = a.length - 1;
-        for (int i = 0; i < j; i++) {
-            if (a[i] % 2 != 0) {
-                while (j > i && a[j] % 2 != 0) {
-                    j--;
-                }
-                swap(a, i, j);
+    public int[] sortArrayByParity(int[] nums) {
+        int temp;
+        int i = 0;
+        for (int k = 0; k < nums.length; k++) {
+            if (nums[k] % 2 == 0) {
+                temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                i++;
             }
         }
-        return a;
-    }
-
-    private void swap(int[] a, int i, int j) {
-        int tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
+        return nums;
     }
 }
