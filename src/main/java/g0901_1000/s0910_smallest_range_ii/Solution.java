9de87fdf85e3d2da9e4f95d6f228bf417d05bfb0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-13 01:18:18 +0200

    Update Solution.java

--- src/main/java/g0901_1000/s0910_smallest_range_ii/Solution.java
+++ src/main/java/g0901_1000/s0910_smallest_range_ii/Solution.java
@@ -11,13 +11,11 @@ public class Solution {
         int ans = nums[n - 1] - nums[0];
         int min = nums[0] + k;
         int max = nums[n - 1] - k;
-
         for (int i = 0; i < n - 1; i++) {
             int mx = Math.max(max, nums[i] + k);
             int mi = Math.min(min, nums[i + 1] - k);
             ans = Math.min(ans, mx - mi);
         }
-
         return ans;
     }
 }
