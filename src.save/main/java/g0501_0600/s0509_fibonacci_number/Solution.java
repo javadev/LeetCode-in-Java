f00c0d3ed875630dfde63f150f4e5b70f8080af8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-14 10:16:00 +0200

    Improved task 509.

--- src.save/main/java/g0501_0600/s0509_fibonacci_number/Solution.java
+++ src.save/main/java/g0501_0600/s0509_fibonacci_number/Solution.java
@@ -2,21 +2,19 @@ package g0501_0600.s0509_fibonacci_number;
 
 // #Easy #Dynamic_Programming #Math #Recursion #Memoization
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
-    /*
-     * Time: O(n)
-     * Space: O(n)
-     */
+    private int memo[] = new int[31];
+
     public int fib(int n) {
-        List<Integer> list = new ArrayList<>();
-        list.add(0);
-        list.add(1);
-        for (int i = 2; i <= n; i++) {
-            list.add(list.get(i - 1) + list.get(i - 2));
+        if (n == 0) {
+            return 0;
         }
-        return list.get(n);
+        if (n == 1) {
+            return 1;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+        return memo[n] = fib(n - 1) + fib(n - 2);
     }
 }
