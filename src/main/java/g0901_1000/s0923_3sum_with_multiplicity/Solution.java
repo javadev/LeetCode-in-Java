a878cdbfc6b2201b7ef9bf30aed992aa59bf8b13
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-02-14 22:24:22 +0700

    fix smells

--- src/main/java/g0901_1000/s0923_3sum_with_multiplicity/Solution.java
+++ src/main/java/g0901_1000/s0923_3sum_with_multiplicity/Solution.java
@@ -6,25 +6,25 @@ public class Solution {
 
     public int threeSumMulti(int[] arr, int target) {
         int answer = 0;
-        int[] count_right = new int[MAX + 1];
+        int[] countRight = new int[MAX + 1];
         for (int num : arr) {
-            ++count_right[num];
+            ++countRight[num];
         }
-        int[] count_left = new int[MAX + 1];
+        int[] countLeft = new int[MAX + 1];
         for (int j = 0; j < arr.length - 1; j++) {
-            --count_right[arr[j]];
+            --countRight[arr[j]];
             int remains = target - arr[j];
             if (remains <= 2 * MAX) {
                 for (int v = 0; v <= Math.min(remains, MAX); v++) {
                     if (remains - v <= MAX) {
-                        int count = count_right[v] * count_left[remains - v];
+                        int count = countRight[v] * countLeft[remains - v];
                         if (count > 0) {
                             answer = (answer + count) % MOD;
                         }
                     }
                 }
             }
-            ++count_left[arr[j]];
+            ++countLeft[arr[j]];
         }
         return answer;
     }
