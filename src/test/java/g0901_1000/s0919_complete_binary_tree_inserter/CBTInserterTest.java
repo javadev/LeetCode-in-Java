bc4f717c8cac08458aafac6571a4bccd336b14b0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-14 03:17:01 +0200

    Update CBTInserterTest.java

--- src/test/java/g0901_1000/s0919_complete_binary_tree_inserter/CBTInserterTest.java
+++ src/test/java/g0901_1000/s0919_complete_binary_tree_inserter/CBTInserterTest.java
@@ -10,14 +10,11 @@ import org.junit.jupiter.api.Test;
 class CBTInserterTest {
     @Test
     void cBTInserterTest() {
-
         TreeNode treeNode = TreeNode.create(Arrays.asList(1, 2));
         CBTInserter cBTInserter = new CBTInserter(treeNode);
         assertThat(cBTInserter.insert(3), equalTo(1));
         assertThat(cBTInserter.insert(4), equalTo(2));
-
         TreeNode expected = TreeNode.create(Arrays.asList(1, 2, 3, 4));
-
         assertThat(cBTInserter.getRoot().toString(), equalTo(expected.toString()));
     }
 }
