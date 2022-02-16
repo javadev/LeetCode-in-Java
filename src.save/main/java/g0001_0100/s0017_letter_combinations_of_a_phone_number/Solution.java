3d8e9d4f73daa9989497161c9b50834196a8d6e6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-16 13:47:23 +0200

    Improved task 17.

--- src.save/main/java/g0001_0100/s0017_letter_combinations_of_a_phone_number/Solution.java
+++ src.save/main/java/g0001_0100/s0017_letter_combinations_of_a_phone_number/Solution.java
@@ -11,43 +11,27 @@ public class Solution {
         if (digits.isEmpty()) {
             return Collections.emptyList();
         }
-        List<String> words = new ArrayList<>();
-        char[] word = new char[digits.length()];
-        helper(digits, word, 0, words);
-        return words;
+        String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        List<String> ans = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        findCombinations(0, digits, letters, sb, ans);
+        return ans;
     }
 
-    private void helper(String digits, char[] word, int cur, List<String> words) {
-        if (cur == digits.length()) {
-            words.add(new String(word));
-        } else {
-            for (char ch : charsForDigit(digits.charAt(cur))) {
-                word[cur] = ch;
-                helper(digits, word, cur + 1, words);
-            }
+    private void findCombinations(
+            int start, String nums, String[] letters, StringBuilder curr, List<String> ans) {
+        if (curr.length() == nums.length()) {
+            ans.add(curr.toString());
+            return;
         }
-    }
-
-    private char[] charsForDigit(char digit) {
-        switch (digit) {
-            case '2':
-                return new char[] {'a', 'b', 'c'};
-            case '3':
-                return new char[] {'d', 'e', 'f'};
-            case '4':
-                return new char[] {'g', 'h', 'i'};
-            case '5':
-                return new char[] {'j', 'k', 'l'};
-            case '6':
-                return new char[] {'m', 'n', 'o'};
-            case '7':
-                return new char[] {'p', 'q', 'r', 's'};
-            case '8':
-                return new char[] {'t', 'u', 'v'};
-            case '9':
-                return new char[] {'w', 'x', 'y', 'z'};
-            default:
-                return new char[] {};
+        for (int i = start; i < nums.length(); i++) {
+            int n = Character.getNumericValue(nums.charAt(i));
+            for (int j = 0; j < letters[n].length(); j++) {
+                char ch = letters[n].charAt(j);
+                curr.append(ch);
+                findCombinations(i + 1, nums, letters, curr, ans);
+                curr.deleteCharAt(curr.length() - 1);
+            }
         }
     }
 }
