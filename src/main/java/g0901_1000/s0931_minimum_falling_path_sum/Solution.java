6e7f1b0d0e8cad71d2484e33a8f81a6afd46d434
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-17 10:40:19 +0200

    Improved task 931.

--- src/main/java/g0901_1000/s0931_minimum_falling_path_sum/Solution.java
+++ src/main/java/g0901_1000/s0931_minimum_falling_path_sum/Solution.java
@@ -4,28 +4,30 @@ package g0901_1000.s0931_minimum_falling_path_sum;
 
 public class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int size = matrix.length;
-        int[][] dp = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (i == 0) {
-                    dp[i][j] = matrix[i][j];
-                } else {
-                    int lastRow = dp[i - 1][j];
-                    if (j - 1 >= 0) {
-                        lastRow = Math.min(dp[i - 1][j - 1], lastRow);
-                    }
-                    if (j + 1 < size) {
-                        lastRow = Math.min(dp[i - 1][j + 1], lastRow);
-                    }
-                    dp[i][j] = lastRow + matrix[i][j];
+        int l = matrix[0].length;
+        int[] arr = matrix[0];
+        for (int i = 1; i < matrix.length; i++) {
+            int[] cur = new int[l];
+            for (int j = 0; j < l; j++) {
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+                int curCell = arr[j];
+                if (j > 0) {
+                    left = arr[j - 1];
                 }
+                if (j < l - 1) {
+                    right = arr[j + 1];
+                }
+                cur[j] = matrix[i][j] + Math.min(left, Math.min(right, curCell));
+            }
+            arr = cur;
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < l; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        int minSum = Integer.MAX_VALUE;
-        for (int i = 0; i < size; i++) {
-            minSum = Math.min(minSum, dp[size - 1][i]);
-        }
-        return minSum;
+        return min;
     }
 }
