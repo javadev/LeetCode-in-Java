ee66085355bec195a23c3bf1f32c522cb40890f0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-18 16:10:12 +0200

    Added exec time 47-50.

--- src.save/main/java/g0001_0100/s0050_powx_n/Solution.java
+++ src.save/main/java/g0001_0100/s0050_powx_n/Solution.java
@@ -1,6 +1,7 @@
 package g0001_0100.s0050_powx_n;
 
 // #Medium #Top_Interview_Questions #Math #Recursion
+// #2022_02_18_Time_1_ms_(71.59%)_Space_42.2_MB_(26.88%)
 
 public class Solution {
     public double myPow(double x, int n) {
