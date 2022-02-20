d2b9f91354c37ad044c70798e5b5eb9ee3fb3a5a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-20 16:12:57 +0200

    Update Solution.java

--- src/main/java/g0901_1000/s0945_minimum_increment_to_make_array_unique/Solution.java
+++ src/main/java/g0901_1000/s0945_minimum_increment_to_make_array_unique/Solution.java
@@ -4,30 +4,23 @@ package g0901_1000.s0945_minimum_increment_to_make_array_unique;
 
 public class Solution {
     public int minIncrementForUnique(int[] nums) {
-
         int max = 0;
         for (int num : nums) {
             max = Math.max(max, num);
         }
-
         int[] counts = new int[nums.length + max];
         for (int num : nums) {
             counts[num]++;
         }
-
         int minMoves = 0;
         for (int i = 0; i < counts.length; i++) {
-
             if (counts[i] <= 1) {
                 continue;
             }
-
             int remaining = counts[i] - 1;
             minMoves = minMoves + remaining;
-
             counts[i + 1] = counts[i + 1] + remaining;
         }
-
         return minMoves;
     }
 }
