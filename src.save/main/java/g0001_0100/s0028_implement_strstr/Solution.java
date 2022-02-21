ed6d325265ef7089b96dd09688d0c983046920d2
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-21 11:17:48 +0200

    Revert "Improved task 28."

--- src.save/main/java/g0001_0100/s0028_implement_strstr/Solution.java
+++ src.save/main/java/g0001_0100/s0028_implement_strstr/Solution.java
@@ -1,56 +1,20 @@
 package g0001_0100.s0028_implement_strstr;
 
 // #Easy #Top_Interview_Questions #String #Two_Pointers #String_Matching
-// #2022_02_21_Time_12_ms_(89.40%)_Space_42.8_MB_(39.70%)
+// #2022_02_18_Time_462_ms_(62.14%)_Space_144.8_MB_(7.41%)
 
 public class Solution {
     public int strStr(String haystack, String needle) {
-        int idx = -1;
         if (needle.isEmpty()) {
             return 0;
         }
-        int n = haystack.length();
-        int m = needle.length();
-        int i = 0;
-        int j = 0;
-        int[] lps = new int[m];
-        preprocessPattern(needle, m, lps);
-        while (i < n) {
-            if (haystack.charAt(i) == needle.charAt(j)) {
-                i++;
-                j++;
-            }
-            if (j == m) {
-                idx = (i - j);
-                break;
-            } else if (i < n && haystack.charAt(i) != needle.charAt(j)) {
-                if (j != 0) {
-                    j = lps[j - 1];
-                } else {
-                    i++;
-                }
+        int m = haystack.length();
+        int n = needle.length();
+        for (int start = 0; start < m - n + 1; start++) {
+            if (haystack.substring(start, start + n).equals(needle)) {
+                return start;
             }
         }
-        return idx;
-    }
-
-    private void preprocessPattern(String needle, int m, int[] lps) {
-        int l = 0;
-        int i = 1;
-        lps[0] = 0;
-        while (i < m) {
-            if (needle.charAt(i) == needle.charAt(l)) {
-                l++;
-                lps[i] = l;
-                i++;
-            } else {
-                if (l != 0) {
-                    l = lps[l - 1];
-                } else {
-                    lps[i] = l;
-                    i++;
-                }
-            }
-        }
+        return -1;
     }
 }
