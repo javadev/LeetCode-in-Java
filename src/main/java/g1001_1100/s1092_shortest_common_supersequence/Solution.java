3e33767137150aaf7bb53cb02af16b5c7b3fd91d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-23 14:25:57 +0200

    Fixed style.

--- src/main/java/g1001_1100/s1092_shortest_common_supersequence/Solution.java
+++ src/main/java/g1001_1100/s1092_shortest_common_supersequence/Solution.java
@@ -1,20 +1,23 @@
 package g1001_1100.s1092_shortest_common_supersequence;
 
-// #Hard #String #Dynamic_Programming
+// #Hard #String #Dynamic_Programming #2022_02_23_Time_10_ms_(96.12%)_Space_45.9_MB_(59.02%)
 
 public class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
         int m = str1.length();
         int n = str2.length();
-
         int[][] dp = new int[m + 1][n + 1];
-
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
-                if (i == 0) dp[i][j] = j;
-                else if (j == 0) dp[i][j] = i;
-                else if (str1.charAt(i - 1) == str2.charAt(j - 1)) dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                if (i == 0) {
+                    dp[i][j] = j;
+                } else if (j == 0) {
+                    dp[i][j] = i;
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
 
