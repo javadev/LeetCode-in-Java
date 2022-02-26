9f74b74a2a56d884e031e192231997e8651b4477
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-26 18:11:25 +0200

    Fixed sonar.

--- src/main/java/g1001_1100/s1079_letter_tile_possibilities/Solution.java
+++ src/main/java/g1001_1100/s1079_letter_tile_possibilities/Solution.java
@@ -1,28 +1,33 @@
 package g1001_1100.s1079_letter_tile_possibilities;
 
-// #Medium #String #Backtracking #2022_02_26_Time_60_ms_(25.58%)_Space_55.8_MB_(11.39%)
+// #Medium #String #Backtracking #2022_02_26_Time_3_ms_(86.86%)_Space_42.3_MB_(51.05%)
 
-import java.util.HashSet;
+import java.util.Arrays;
 
 public class Solution {
+    private int count;
+
     public int numTilePossibilities(String tiles) {
-        HashSet<String> numPos = new HashSet<>();
-        int[] flag = new int[tiles.length()];
-        getNumbers(tiles, numPos, flag, "");
-        return numPos.size();
+        count = 0;
+        char[] chars = tiles.toCharArray();
+        Arrays.sort(chars);
+        boolean[] visited = new boolean[chars.length];
+        dfs(chars, 0, visited);
+        return count;
     }
 
-    private void getNumbers(String tiles, HashSet<String> numPos, int[] flag, String str) {
-        for (int i = 0; i < tiles.length(); i++) {
-            if (flag[i] == 0) {
-                char ch = tiles.charAt(i);
-                str += ch;
-                flag[i] = 1;
-                numPos.add(str);
-                getNumbers(tiles, numPos, flag, str);
-                str = str.substring(0, str.length() - 1);
-                flag[i] = 0;
+    private void dfs(char[] chars, int length, boolean[] visited) {
+        if (length == chars.length) {
+            return;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (visited[i] || i - 1 >= 0 && chars[i] == chars[i - 1] && !visited[i - 1]) {
+                continue;
             }
+            count++;
+            visited[i] = true;
+            dfs(chars, length + 1, visited);
+            visited[i] = false;
         }
     }
 }
