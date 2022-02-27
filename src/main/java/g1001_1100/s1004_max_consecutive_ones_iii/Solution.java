7c2c6b1ea67bba86069756dad58c05f1ecbba91e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-27 16:24:13 +0200

    Improved task 1029.

--- src/main/java/g1001_1100/s1004_max_consecutive_ones_iii/Solution.java
+++ src/main/java/g1001_1100/s1004_max_consecutive_ones_iii/Solution.java
@@ -1,6 +1,7 @@
 package g1001_1100.s1004_max_consecutive_ones_iii;
 
 // #Medium #Array #Binary_Search #Prefix_Sum #Sliding_Window
+// #2022_02_27_Time_3_ms_(79.01%)_Space_68.2_MB_(65.91%)
 
 public class Solution {
     public int longestOnes(int[] a, int k) {
