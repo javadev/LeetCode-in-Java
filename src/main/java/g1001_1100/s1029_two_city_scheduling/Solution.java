278c2c6b418e23d1214195cfed6a48363cecad2f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-27 16:26:53 +0200

    Improved task 1029.

--- src/main/java/g1001_1100/s1029_two_city_scheduling/Solution.java
+++ src/main/java/g1001_1100/s1029_two_city_scheduling/Solution.java
@@ -1,6 +1,6 @@
 package g1001_1100.s1029_two_city_scheduling;
 
-// #Medium #Array #Sorting #Greedy
+// #Medium #Array #Sorting #Greedy #2022_02_27_Time_1_ms_(97.54%)_Space_39.9_MB_(53.65%)
 
 import java.util.Arrays;
 
