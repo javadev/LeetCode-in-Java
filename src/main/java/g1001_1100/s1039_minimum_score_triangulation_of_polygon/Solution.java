45d5b438c946641c778e35547dff28ac819dbf98
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-27 06:42:48 +0200

    Improved task 1039.

--- src/main/java/g1001_1100/s1039_minimum_score_triangulation_of_polygon/Solution.java
+++ src/main/java/g1001_1100/s1039_minimum_score_triangulation_of_polygon/Solution.java
@@ -5,12 +5,11 @@ package g1001_1100.s1039_minimum_score_triangulation_of_polygon;
 import java.util.Arrays;
 
 public class Solution {
-
-    private static final int[][] DP = new int[101][101];
+    private int[][] dp = new int[101][101];
 
     public int minScoreTriangulation(int[] values) {
         int n = values.length;
-        for (int[] row : DP) {
+        for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
         return util(values, 1, n - 1);
@@ -20,12 +19,10 @@ public class Solution {
         if (i >= j) {
             return 0;
         }
-        if (DP[i][j] != -1) {
-            return DP[i][j];
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-
         int ans = Integer.MAX_VALUE;
-
         for (int k = i; k <= j - 1; k++) {
             int temp =
                     util(values, i, k)
@@ -33,9 +30,8 @@ public class Solution {
                             + (values[i - 1] * values[k] * values[j]);
 
             ans = Math.min(ans, temp);
-            DP[i][j] = ans;
+            dp[i][j] = ans;
         }
-
-        return DP[i][j];
+        return dp[i][j];
     }
 }
