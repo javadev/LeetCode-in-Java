3fa81a710c1f3093fd8c6aa66fb3a1060f813810
Valentyn Kolesnikov <javadev75@gmail.com>
2022-02-28 12:57:04 +0200

    Fixed style.

--- src/main/java/g1001_1100/s1054_distant_barcodes/Solution.java
+++ src/main/java/g1001_1100/s1054_distant_barcodes/Solution.java
@@ -16,14 +16,17 @@ public class Solution {
         }
         List<Map.Entry<Integer, Integer>> list = new ArrayList<>(cnt.entrySet());
         list.sort(Map.Entry.<Integer, Integer>comparingByValue().reversed());
-        int l = barcodes.length, i = 0;
+        int l = barcodes.length;
+        int i = 0;
         int[] res = new int[l];
         for (Map.Entry<Integer, Integer> e : list) {
             int time = e.getValue();
             while (time-- > 0) {
                 res[i] = e.getKey();
                 i += 2;
-                if (i >= barcodes.length) i = 1;
+                if (i >= barcodes.length) {
+                    i = 1;
+                }
             }
         }
         return res;
