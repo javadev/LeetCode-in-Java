12b30c18ba57d42625d4009338a6396c12600bc8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-01 18:28:56 +0200

    Update Solution.java

--- src/main/java/g1101_1200/s1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.java
+++ src/main/java/g1101_1200/s1111_maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.java
@@ -4,17 +4,14 @@ package g1101_1200.s1111_maximum_nesting_depth_of_two_valid_parentheses_strings;
 
 public class Solution {
     public int[] maxDepthAfterSplit(String seq) {
-
         int n = seq.length();
         int[] ans = new int[n];
         char[] chars = seq.toCharArray();
         int depth = 0;
-
         for (int i = 0; i < n; i++) {
             if (chars[i] == '(') {
                 depth++;
                 if (depth % 2 == 0) {
-
                     ans[i] = 0;
                 } else {
                     ans[i] = 1;
