eb2fd74b3c7cc86e4ce2b942468279c2259978f9
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-05 08:25:51 +0200

    Update Solution.java

--- src/main/java/g1101_1200/s1169_invalid_transactions/Solution.java
+++ src/main/java/g1101_1200/s1169_invalid_transactions/Solution.java
@@ -11,10 +11,8 @@ import java.util.TreeMap;
 
 public class Solution {
     public List<String> invalidTransactions(String[] transactions) {
-
         Map<String, TreeMap<Integer, Transaction>> map = new HashMap<>();
         List<String> result = new ArrayList<>();
-
         for (String transaction : transactions) {
             String[] split = transaction.split(",");
             String name = split[0];
@@ -23,19 +21,16 @@ public class Solution {
             map.putIfAbsent(name, new TreeMap<>());
             map.get(name).put(time, new Transaction(time, city));
         }
-
         for (String transaction : transactions) {
             String[] split = transaction.split(",");
             String name = split[0];
             int time = Integer.parseInt(split[1]);
             int amount = Integer.parseInt(split[2]);
             String city = split[3];
-
             if (amount > 1000) {
                 result.add(transaction);
                 continue;
             }
-
             for (Map.Entry<Integer, Transaction> entry :
                     map.get(name).subMap(time - 60, time + 60).entrySet()) {
                 if (Math.abs(time - entry.getKey()) <= 60 && !entry.getValue().city.equals(city)) {
@@ -44,11 +39,10 @@ public class Solution {
                 }
             }
         }
-
         return result;
     }
 
-    static class Transaction {
+    private static class Transaction {
         int amount;
         String city;
 
