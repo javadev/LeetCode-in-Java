55bafd3ba38ce55164c0b9719df254e73780af61
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-11 13:24:02 +0200

    Added tasks 1283, 1284.

---
+++ src/main/java/g1201_1300/s1283_find_the_smallest_divisor_given_a_threshold/Solution.java
@@ -0,0 +1,27 @@
+package g1201_1300.s1283_find_the_smallest_divisor_given_a_threshold;
+
+// #Medium #Array #Binary_Search #2022_03_11_Time_9_ms_(95.49%)_Space_56.5_MB_(8.70%)
+
+public class Solution {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int l = 1;
+        int r = 1000000;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (helper(mid, nums) > threshold) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    private int helper(int mid, int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res += (num + mid - 1) / mid;
+        }
+        return res;
+    }
+}
