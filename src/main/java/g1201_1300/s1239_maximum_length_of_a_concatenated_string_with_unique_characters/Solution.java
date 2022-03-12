3b6bbd553a1a1e343f4fa04aeec467b96b1bb400
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 13:42:38 +0200

    Update Solution.java

--- src/main/java/g1201_1300/s1239_maximum_length_of_a_concatenated_string_with_unique_characters/Solution.java
+++ src/main/java/g1201_1300/s1239_maximum_length_of_a_concatenated_string_with_unique_characters/Solution.java
@@ -14,15 +14,12 @@ public class Solution {
         if (index == arr.size()) {
             return 0;
         }
-
         int ans = 0;
         ans = Math.max(ans, find(index + 1, visChar, arr));
-
         if (checkCurrStringValidOrNot(visChar, arr.get(index))) {
             visChar = updateState(visChar, arr.get(index));
             ans = Math.max(ans, arr.get(index).length() + find(index + 1, visChar, arr));
         }
-
         return ans;
     }
 
@@ -33,15 +30,13 @@ public class Solution {
             }
             vis |= (1 << (c - 'a'));
         }
-
         return true;
     }
 
     private int updateState(int vis, String s) {
         for (char c : s.toCharArray()) {
-            vis |= (1 << (int) (c - 'a'));
+            vis |= (1 << (c - 'a'));
         }
-
         return vis;
     }
 }
