5db08fc1f152b366982d4979d2b9fdc17e1da57a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 16:07:47 +0200

    Improved task 1249.

--- src/main/java/g1201_1300/s1249_minimum_remove_to_make_valid_parentheses/Solution.java
+++ src/main/java/g1201_1300/s1249_minimum_remove_to_make_valid_parentheses/Solution.java
@@ -5,40 +5,30 @@ package g1201_1300.s1249_minimum_remove_to_make_valid_parentheses;
 
 public class Solution {
     public String minRemoveToMakeValid(String s) {
-
         int closingParantheis = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ')') {
                 closingParantheis++;
             }
         }
-
         StringBuilder result = new StringBuilder();
-
         int openingParanthesis = 0;
         for (char ch : s.toCharArray()) {
-
             if (ch == ')' && openingParanthesis == 0) {
                 closingParantheis--;
                 continue;
             }
-
             if (ch == ')') {
                 openingParanthesis--;
             }
-
-            if (ch == '(' && closingParantheis == 0) {
-                continue;
+            if (ch != '(' || closingParantheis != 0) {
+                if (ch == '(') {
+                    openingParanthesis++;
+                    closingParantheis--;
+                }
+                result.append(ch);
             }
-
-            if (ch == '(') {
-                openingParanthesis++;
-                closingParantheis--;
-            }
-
-            result.append(ch);
         }
-
         return result.toString();
     }
 }
