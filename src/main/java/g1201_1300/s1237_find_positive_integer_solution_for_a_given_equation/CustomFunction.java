b2d12f3a2bdc5b8a2d8e8a6686075f45aba967ca
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 07:43:20 +0200

    Update CustomFunction.java

--- src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/CustomFunction.java
+++ src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/CustomFunction.java
@@ -1,5 +1,5 @@
 package g1201_1300.s1237_find_positive_integer_solution_for_a_given_equation;
 
-abstract class CustomFunction {
-    public abstract int f(int x, int y);
+interface CustomFunction {
+    int f(int x, int y);
 }
