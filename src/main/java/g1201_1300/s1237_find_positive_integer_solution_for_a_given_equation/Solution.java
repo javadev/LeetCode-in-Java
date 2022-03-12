4696cedf8dcefe4ec3b2b52c440341825aac2fb4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 08:47:18 +0200

    Fixed sonar.

--- src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/Solution.java
+++ src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/Solution.java
@@ -7,15 +7,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@SuppressWarnings("java:S161")
 public class Solution {
     // This is the custom function interface.
     // You should not implement it, or speculate about its implementation
-    public abstract static class CustomFunction {
+    public interface CustomFunction {
         // Returns f(x, y) for any given positive integers x and y.
         // Note that f(x, y) is increasing with respect to both x and y.
         // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
-        public abstract int f(int x, int y);
+        int f(int x, int y);
     }
 
     public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
