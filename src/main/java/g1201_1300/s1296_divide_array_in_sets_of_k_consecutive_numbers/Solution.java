b09a16cc9901e96e17f81f326568a5623d23f681
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 06:45:22 +0200

    Fixed idea warnings.

--- src/main/java/g1201_1300/s1296_divide_array_in_sets_of_k_consecutive_numbers/Solution.java
+++ src/main/java/g1201_1300/s1296_divide_array_in_sets_of_k_consecutive_numbers/Solution.java
@@ -1,24 +1,23 @@
 package g1201_1300.s1296_divide_array_in_sets_of_k_consecutive_numbers;
 
 // #Medium #Array #Hash_Table #Sorting #Greedy
-// #2022_03_10_Time_70_ms_(89.13%)_Space_79.4_MB_(33.93%)
+// #2022_03_12_Time_39_ms_(97.80%)_Space_55.3_MB_(85.50%)
 
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Solution {
     public boolean isPossibleDivide(int[] nums, int k) {
-        int n = nums.length;
         Arrays.sort(nums);
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        for (int i = 0; i < n; i++) {
-            if (map.get(nums[i]) == 0) {
+        for (int num : nums) {
+            if (map.get(num) == 0) {
                 continue;
             }
-            for (int v = nums[i]; v < nums[i] + k; v++) {
+            for (int v = num; v < num + k; v++) {
                 if (!map.containsKey(v) || map.get(v) == 0) {
                     return false;
                 } else {
