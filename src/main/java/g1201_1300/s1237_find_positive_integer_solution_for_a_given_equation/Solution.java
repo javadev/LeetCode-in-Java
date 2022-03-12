8b757124f1928d773d3972236c514067c94483b5
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 08:43:40 +0200

    Improved task 1237.

--- src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/Solution.java
+++ src/main/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/Solution.java
@@ -8,6 +8,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
+    // This is the custom function interface.
+    // You should not implement it, or speculate about its implementation
+    public abstract static class CustomFunction {
+        // Returns f(x, y) for any given positive integers x and y.
+        // Note that f(x, y) is increasing with respect to both x and y.
+        // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+        public abstract int f(int x, int y);
+    }
 
     public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
         List<List<Integer>> result = new ArrayList<>();
--- src/test/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/SolutionTest.java
+++ src/test/java/g1201_1300/s1237_find_positive_integer_solution_for_a_given_equation/SolutionTest.java
@@ -3,11 +3,23 @@ package g1201_1300.s1237_find_positive_integer_solution_for_a_given_equation;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void findSolution() {
-        assertThat(true, equalTo(true));
+        Solution.CustomFunction customFunction =
+                new Solution.CustomFunction() {
+                    int index = 0;
+
+                    @Override
+                    public int f(int x, int y) {
+                        return index++;
+                    }
+                };
+        assertThat(
+                new Solution().findSolution(customFunction, 1),
+                equalTo(Arrays.asList(Arrays.asList(2, 1000))));
     }
 }
