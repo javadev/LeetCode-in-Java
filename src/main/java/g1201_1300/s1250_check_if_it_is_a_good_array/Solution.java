d87fd3bf1e5761fb6235a807d8366308541d02e1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 13:34:28 +0200

    Update Solution.java

--- src/main/java/g1201_1300/s1250_check_if_it_is_a_good_array/Solution.java
+++ src/main/java/g1201_1300/s1250_check_if_it_is_a_good_array/Solution.java
@@ -3,7 +3,6 @@ package g1201_1300.s1250_check_if_it_is_a_good_array;
 // #Hard #Array #Math #Number_Theory #2022_03_12_Time_4_ms_(58.82%)_Space_60.8_MB_(5.43%)
 
 public class Solution {
-
     private int gcd(int a, int b) {
         if (b == 0) {
             return a;
