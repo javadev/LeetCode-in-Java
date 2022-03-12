ce30f9b1e366f85717638183aa916e42806b7f8f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 17:11:38 +0200

    Fixed formatting.

--- src.save/main/java/g0201_0300/s0226_invert_binary_tree/Solution.java
+++ src.save/main/java/g0201_0300/s0226_invert_binary_tree/Solution.java
@@ -1,6 +1,7 @@
 package g0201_0300.s0226_invert_binary_tree;
 
-// #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree #Data_Structure_I_Day_12_Tree
+// #Easy #Top_100_Liked_Questions #Depth_First_Search #Breadth_First_Search #Tree #Binary_Tree
+// #Data_Structure_I_Day_12_Tree
 
 import com_github_leetcode.TreeNode;
 
