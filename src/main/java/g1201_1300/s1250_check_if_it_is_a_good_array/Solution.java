5933e556873e7b89ca6e52a0d9cd8171ae2651b1
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-03-12 17:55:17 +0700

    format code

--- src/main/java/g1201_1300/s1250_check_if_it_is_a_good_array/Solution.java
+++ src/main/java/g1201_1300/s1250_check_if_it_is_a_good_array/Solution.java
@@ -3,20 +3,20 @@ package g1201_1300.s1250_check_if_it_is_a_good_array;
 // #Hard #Array #Math #Number_Theory #2022_03_12_Time_4_ms_(58.82%)_Space_60.8_MB_(5.43%)
 
 public class Solution {
-    
+
     private int gcd(int a, int b) {
-        if(b==0) {
+        if (b == 0) {
             return a;
         }
-        return gcd(b,a%b);
+        return gcd(b, a % b);
     }
-    
+
     public boolean isGoodArray(int[] nums) {
         int ans = nums[0];
-        for (int element: nums){
+        for (int element : nums) {
             ans = gcd(ans, element);
-            if(ans == 1){
-               return true;
+            if (ans == 1) {
+                return true;
             }
         }
         return false;
