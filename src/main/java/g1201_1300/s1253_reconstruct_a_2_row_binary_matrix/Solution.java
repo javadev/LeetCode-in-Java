45d4dd08240662c96ab4f8b76498fa9e0fe18c91
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-12 13:33:44 +0200

    Update Solution.java

--- src/main/java/g1201_1300/s1253_reconstruct_a_2_row_binary_matrix/Solution.java
+++ src/main/java/g1201_1300/s1253_reconstruct_a_2_row_binary_matrix/Solution.java
@@ -8,13 +8,11 @@ import java.util.List;
 public class Solution {
     public List<List<Integer>> reconstructMatrix(int upper, int lower, int[] colsum) {
         List<List<Integer>> res = new ArrayList<>();
-
         int n = colsum.length;
         int[] upperRow = new int[n];
         int[] lowerRow = new int[n];
         int currentUpperSum = 0;
         int currentLowerSum = 0;
-
         for (int i = 0; i < n; i++) {
             if (colsum[i] >= 1) {
                 upperRow[i] = 1;
@@ -23,32 +21,27 @@ public class Solution {
                 currentLowerSum++;
             }
         }
-
         for (int i = 0; i < n; i++) {
             if (colsum[i] == 1 && currentUpperSum > upper) {
                 currentUpperSum--;
                 upperRow[i] = 0;
             }
         }
-
         for (int i = 0; i < upperRow.length; i++) {
             if (colsum[i] == 1 && upperRow[i] == 1) {
                 currentLowerSum--;
                 lowerRow[i] = 0;
             }
         }
-
         if (currentUpperSum != upper || currentLowerSum != lower) {
             return res;
         }
-
         res.add(new ArrayList<>());
         res.add(new ArrayList<>());
         for (int i = 0; i < n; i++) {
             res.get(0).add(upperRow[i]);
             res.get(1).add(lowerRow[i]);
         }
-
         return res;
     }
 }
