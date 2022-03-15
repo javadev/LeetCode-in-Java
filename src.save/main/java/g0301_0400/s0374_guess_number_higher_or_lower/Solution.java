7c8baaff0a445b87fe70ac7efc4275cf4dedb313
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-15 14:46:38 +0200

    Improved 374.

--- src.save/main/java/g0301_0400/s0374_guess_number_higher_or_lower/Solution.java
+++ src.save/main/java/g0301_0400/s0374_guess_number_higher_or_lower/Solution.java
@@ -9,7 +9,7 @@ package g0301_0400.s0374_guess_number_higher_or_lower;
  * @return -1 if num is lower than the guess number 1 if num is higher than the guess number
  *     otherwise return 0 int guess(int num);
  */
-public class Solution extends GuessGame {
+public class Solution {
     public int guessNumber(int n) {
         int start = 0;
         int end = n;
@@ -24,5 +24,10 @@ public class Solution extends GuessGame {
             }
         }
         return -1;
+    }
+
+    // Assume we pick 7
+    private int guess(int num) {
+        return Integer.compare(7, num);
     }
 }
