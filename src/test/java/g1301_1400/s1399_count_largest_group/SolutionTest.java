94d1721df37a0bedf3ac629bfab6663fbce3e292
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-17 10:12:02 +0200

    Added tasks 1396, 1397, 1399.

---
+++ src/test/java/g1301_1400/s1399_count_largest_group/SolutionTest.java
@@ -0,0 +1,13 @@
+package g1301_1400.s1399_count_largest_group;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void test() {
+        assertThat(new Solution().decode(""), equalTo(""));
+    }
+}
