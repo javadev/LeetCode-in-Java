7e68cff34848e339dede70c1f968d77988046f57
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-19 18:06:54 +0200

    Update Solution.java

--- src/main/java/g1301_1400/s1340_jump_game_v/Solution.java
+++ src/main/java/g1301_1400/s1340_jump_game_v/Solution.java
@@ -3,7 +3,6 @@ package g1301_1400.s1340_jump_game_v;
 // #Hard #Array #Dynamic_Programming #Sorting #2022_03_19_Time_13_ms_(71.33%)_Space_47.7_MB_(46.67%)
 
 public class Solution {
-
     public int maxJumps(int[] arr, int d) {
         int n = arr.length;
         int[] dp = new int[n];
