b44519071a11aec5c143f9d102ace32446d1b15d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-19 18:03:50 +0200

    Update Solution.java

--- src/main/java/g1301_1400/s1338_reduce_array_size_to_the_half/Solution.java
+++ src/main/java/g1301_1400/s1338_reduce_array_size_to_the_half/Solution.java
@@ -12,24 +12,19 @@ import java.util.Map;
 public class Solution {
     public int minSetSize(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int num : arr) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
         List<Integer> freq = new ArrayList<>(map.values());
         Collections.sort(freq, Collections.reverseOrder());
-
         int i = 0;
         int count = 0;
         int totalLength = arr.length;
-
         while (totalLength > arr.length / 2) {
             totalLength -= freq.get(i);
             count++;
             i++;
         }
-
         return count;
     }
 }
