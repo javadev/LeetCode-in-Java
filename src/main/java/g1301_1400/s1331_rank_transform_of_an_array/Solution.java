3fc616ec768b185d6ee2e2de139c8825fd02db03
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-19 13:21:21 +0200

    Update Solution.java

--- src/main/java/g1301_1400/s1331_rank_transform_of_an_array/Solution.java
+++ src/main/java/g1301_1400/s1331_rank_transform_of_an_array/Solution.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 @SuppressWarnings({})
 public class Solution {
     public int[] arrayRankTransform(int[] arr) {
-
         int[] tmp = Arrays.copyOf(arr, arr.length);
         Arrays.sort(tmp);
         HashMap<Integer, Integer> mp = new HashMap<>();
@@ -18,7 +17,6 @@ public class Solution {
                 mp.put(x, i++);
             }
         }
-
         i = 0;
         for (Integer x : arr) {
             arr[i++] = mp.get(x);
