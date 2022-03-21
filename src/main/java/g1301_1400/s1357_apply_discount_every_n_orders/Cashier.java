9237a6b782dccabace0ff15dac61c7bdeecbffec
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-21 08:37:19 +0200

    Improved task 1357.

--- src/main/java/g1301_1400/s1357_apply_discount_every_n_orders/Cashier.java
+++ src/main/java/g1301_1400/s1357_apply_discount_every_n_orders/Cashier.java
@@ -1,44 +1,36 @@
 package g1301_1400.s1357_apply_discount_every_n_orders;
 
-// #Medium #Array #Hash_Table #Design #2022_03_21_Time_209_ms_(27.73%)_Space_74.4_MB_(74.79%)
+// #Medium #Array #Hash_Table #Design #2022_03_21_Time_174_ms_(58.82%)_Space_125.4_MB_(66.39%)
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Cashier {
-
-    private int customer = 1;
-    private final int n;
-    private final double discount;
-    private final int[] products;
-    private final int[] prices;
+    private Map<Integer, Integer> map;
+    private int nthCustomer;
+    private int customerCountTrack;
+    private double discountPercent;
 
     public Cashier(int n, int discount, int[] products, int[] prices) {
-        this.n = n;
-        this.discount = discount;
-        this.products = products;
-        this.prices = prices;
+        map = new HashMap<>();
+        nthCustomer = n;
+        customerCountTrack = 0;
+        discountPercent = discount * .01;
+        for (int i = 0; i < products.length; i++) {
+            map.put(products[i], prices[i]);
+        }
     }
 
     public double getBill(int[] product, int[] amount) {
-        double bill = 0.0;
-        int[] productsInt = this.products;
-        Integer[] localProducts = Arrays.stream(productsInt).boxed().toArray(Integer[]::new);
-        int[] localPrices = this.prices;
+        customerCountTrack++;
+        double sum = 0;
         for (int i = 0; i < product.length; i++) {
-            int ithAmount = amount[i];
-            int id = product[i];
-            int price = localPrices[Arrays.asList(localProducts).indexOf(id)];
-            bill += price * ithAmount;
+            sum += map.get(product[i]) * amount[i];
         }
-        if (this.customer % n == 0) {
-            this.customer++;
-            return discounted(this.discount, bill);
+        // discount customer
+        if (customerCountTrack % nthCustomer == 0) {
+            return sum - (sum * discountPercent);
         }
-        this.customer++;
-        return bill;
-    }
-
-    public double discounted(double discount, double price) {
-        return price * (1 - (discount / 100));
+        return sum;
     }
 }
