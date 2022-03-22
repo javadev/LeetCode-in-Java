5259fbfeeb63c20664226241b49d204b6be88e3e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-22 02:07:35 +0200

    Update Solution.java

--- src/main/java/g1301_1400/s1375_number_of_times_binary_string_is_prefix_aligned/Solution.java
+++ src/main/java/g1301_1400/s1375_number_of_times_binary_string_is_prefix_aligned/Solution.java
@@ -4,17 +4,14 @@ package g1301_1400.s1375_number_of_times_binary_string_is_prefix_aligned;
 
 class Solution {
     public int numTimesAllBlue(int[] flips) {
-
         int ans = 0;
         int max = 0;
         for (int i = 0; i < flips.length; i++) {
             max = Math.max(max, flips[i]);
-
             if (max == i + 1) {
                 ++ans;
             }
         }
-
         return ans;
     }
 }
