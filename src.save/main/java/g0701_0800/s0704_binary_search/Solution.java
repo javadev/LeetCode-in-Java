3da44ae9d61ea3a820821d41036549fd44fb76cb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-23 14:52:59 +0200

    Added exec time 701-709.

--- src.save/main/java/g0701_0800/s0704_binary_search/Solution.java
+++ src.save/main/java/g0701_0800/s0704_binary_search/Solution.java
@@ -1,6 +1,7 @@
 package g0701_0800.s0704_binary_search;
 
-// #Easy #Array #Binary_Search
+// #Easy #Array #Binary_Search #Algorithm_I_Day_1_Binary_Search
+// #2022_03_23_Time_0_ms_(100.00%)_Space_54.8_MB_(20.10%)
 
 public class Solution {
     public int search(int[] nums, int target) {
--- src.save/main/java/g0701_0800/s0709_to_lower_case/Solution.java
+++ src.save/main/java/g0701_0800/s0709_to_lower_case/Solution.java
@@ -1,6 +1,7 @@
 package g0701_0800.s0709_to_lower_case;
 
-// #Easy #String
+// #Easy #String #Programming_Skills_I_Day_9_String
+// #2022_03_23_Time_1_ms_(71.74%)_Space_42_MB_(52.94%)
 
 public class Solution {
     public String toLowerCase(String s) {
