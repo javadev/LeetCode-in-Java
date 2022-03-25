7dc5d481fb71818bd8c3cee547100d186e799fc8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-25 18:04:30 +0200

    Improved task 1403.

--- src/main/java/g1401_1500/s1403_minimum_subsequence_in_non_increasing_order/Solution.java
+++ src/main/java/g1401_1500/s1403_minimum_subsequence_in_non_increasing_order/Solution.java
@@ -1,30 +1,28 @@
 package g1401_1500.s1403_minimum_subsequence_in_non_increasing_order;
 
-// #Easy #Array #Sorting #Greedy #2022_03_25_Time_13_ms_(6.30%)_Space_42.2_MB_(90.93%)
+// #Easy #Array #Sorting #Greedy #2022_03_25_Time_4_ms_(79.60%)_Space_45.7_MB_(11.33%)
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Solution {
     public List<Integer> minSubsequence(int[] nums) {
-        List<Integer> list =
-                Arrays.stream(nums)
-                        .boxed()
-                        .sorted(Collections.reverseOrder())
-                        .collect(Collectors.toCollection(() -> new ArrayList<>(nums.length)));
-        int sum = list.stream().mapToInt(num -> num).sum();
-        int minSum = 0;
+        Arrays.sort(nums);
+        int startIndex = 0;
+        int endIndex = nums.length - 1;
+        int sumOfNonIncludedElements = nums[0];
+        int sumOfIncludedElements = nums[endIndex];
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (minSum > (sum - minSum)) {
-                return result;
+        while (startIndex < endIndex) {
+            if (sumOfNonIncludedElements < sumOfIncludedElements) {
+                sumOfNonIncludedElements += nums[++startIndex];
+            } else {
+                result.add(nums[endIndex]);
+                sumOfIncludedElements += nums[--endIndex];
             }
-            minSum += list.get(i);
-            result.add(list.get(i));
         }
+        result.add(nums[startIndex]);
         return result;
     }
 }
