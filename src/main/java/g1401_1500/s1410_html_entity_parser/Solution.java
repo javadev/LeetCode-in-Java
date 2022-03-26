fe34e6adf7ce3268ae5dc36c1bed31db43170a64
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-26 21:21:48 +0200

    Improved solution.

--- src/main/java/g1401_1500/s1410_html_entity_parser/Solution.java
+++ src/main/java/g1401_1500/s1410_html_entity_parser/Solution.java
@@ -1,6 +1,6 @@
 package g1401_1500.s1410_html_entity_parser;
 
-// #Medium #String #Hash_Table #2022_03_26_Time_22_ms_(97.85%)_Space_54.5_MB_(80.64%)
+// #Medium #String #Hash_Table #2022_03_26_Time_15_ms_(98.92%)_Space_42.6_MB_(100.00%)
 
 public class Solution {
     public String entityParser(String text) {
@@ -10,22 +10,22 @@ public class Solution {
             if (text.charAt(i) != '&') {
                 sb.append(text.charAt(i));
             } else {
-                if (i + 7 <= text.length() && text.startsWith("&frasl;", i)) {
+                if (text.startsWith("&frasl;", i)) {
                     sb.append("/");
                     i += 6;
-                } else if (i + 6 <= text.length() && text.startsWith("&quot;", i)) {
+                } else if (text.startsWith("&quot;", i)) {
                     sb.append("\"");
                     i += 5;
-                } else if (i + 6 <= text.length() && text.startsWith("&apos;", i)) {
+                } else if (text.startsWith("&apos;", i)) {
                     sb.append("'");
                     i += 5;
-                } else if (i + 5 <= text.length() && text.startsWith("&amp;", i)) {
+                } else if (text.startsWith("&amp;", i)) {
                     sb.append("&");
                     i += 4;
-                } else if (i + 4 <= text.length() && text.startsWith("&gt;", i)) {
+                } else if (text.startsWith("&gt;", i)) {
                     sb.append(">");
                     i += 3;
-                } else if (i + 4 <= text.length() && text.startsWith("&lt;", i)) {
+                } else if (text.startsWith("&lt;", i)) {
                     sb.append("<");
                     i += 3;
                 } else {
