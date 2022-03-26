159d4061d2b601f4ef67e96207a988c5d795a8f0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-26 06:06:13 +0200

    Update Solution.java

--- src/main/java/g1401_1500/s1409_queries_on_a_permutation_with_key/Solution.java
+++ src/main/java/g1401_1500/s1409_queries_on_a_permutation_with_key/Solution.java
@@ -6,24 +6,20 @@ package g1401_1500.s1409_queries_on_a_permutation_with_key;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("java:S5413")
 public class Solution {
     public int[] processQueries(int[] queries, int m) {
-
         int[] ans = new int[queries.length];
         List<Integer> list = new ArrayList<>();
-
         for (int i = 0; i < m; i++) {
             list.add(i + 1);
         }
-
         for (int i = 0; i < queries.length; i++) {
             int index = list.indexOf(queries[i]);
             ans[i] = index;
-
             list.remove(index);
             list.add(0, queries[i]);
         }
-
         return ans;
     }
 }
