352701dedb6ffa9924f206437f559b590666a351
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-26 21:15:58 +0200

    Improved task 1410.

--- src/main/java/g1401_1500/s1410_html_entity_parser/Solution.java
+++ src/main/java/g1401_1500/s1410_html_entity_parser/Solution.java
@@ -1,6 +1,6 @@
 package g1401_1500.s1410_html_entity_parser;
 
-// #Medium #String #Hash_Table #2022_03_26_Time_41_ms_(73.12%)_Space_72.8_MB_(20.43%)
+// #Medium #String #Hash_Table #2022_03_26_Time_22_ms_(97.85%)_Space_54.5_MB_(80.64%)
 
 public class Solution {
     public String entityParser(String text) {
@@ -10,22 +10,22 @@ public class Solution {
             if (text.charAt(i) != '&') {
                 sb.append(text.charAt(i));
             } else {
-                if (i + 7 <= text.length() && text.substring(i, i + 7).equals("&frasl;")) {
+                if (i + 7 <= text.length() && text.startsWith("&frasl;", i)) {
                     sb.append("/");
                     i += 6;
-                } else if (i + 6 <= text.length() && text.substring(i, i + 6).equals("&quot;")) {
+                } else if (i + 6 <= text.length() && text.startsWith("&quot;", i)) {
                     sb.append("\"");
                     i += 5;
-                } else if (i + 6 <= text.length() && text.substring(i, i + 6).equals("&apos;")) {
+                } else if (i + 6 <= text.length() && text.startsWith("&apos;", i)) {
                     sb.append("'");
                     i += 5;
-                } else if (i + 5 <= text.length() && text.substring(i, i + 5).equals("&amp;")) {
+                } else if (i + 5 <= text.length() && text.startsWith("&amp;", i)) {
                     sb.append("&");
                     i += 4;
-                } else if (i + 4 <= text.length() && text.substring(i, i + 4).equals("&gt;")) {
+                } else if (i + 4 <= text.length() && text.startsWith("&gt;", i)) {
                     sb.append(">");
                     i += 3;
-                } else if (i + 4 <= text.length() && text.substring(i, i + 4).equals("&lt;")) {
+                } else if (i + 4 <= text.length() && text.startsWith("&lt;", i)) {
                     sb.append("<");
                     i += 3;
                 } else {
