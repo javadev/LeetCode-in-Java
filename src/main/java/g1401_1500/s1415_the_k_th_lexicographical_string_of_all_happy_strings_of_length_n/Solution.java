c42f326ca147c12cd78cbe3a3972cb4e93daac84
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-27 06:26:39 +0300

    Fixed style.

--- src/main/java/g1401_1500/s1415_the_k_th_lexicographical_string_of_all_happy_strings_of_length_n/Solution.java
+++ src/main/java/g1401_1500/s1415_the_k_th_lexicographical_string_of_all_happy_strings_of_length_n/Solution.java
@@ -3,7 +3,7 @@ package g1401_1500.s1415_the_k_th_lexicographical_string_of_all_happy_strings_of
 // #Medium #String #Backtracking #2022_03_27_Time_2_ms_(90.55%)_Space_42.5_MB_(65.75%)
 
 public class Solution {
-    private char arr[] = new char[] {'a', 'b', 'c'};
+    private char[] arr = new char[] {'a', 'b', 'c'};
     private String res = "";
     private int k;
 
