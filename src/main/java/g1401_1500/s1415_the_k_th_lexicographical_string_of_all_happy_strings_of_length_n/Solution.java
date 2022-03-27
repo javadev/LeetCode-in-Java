51b66d9c772322bd6df70a9f5cacd0250b20e924
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-27 06:24:27 +0300

    Improved task 1415.

--- src/main/java/g1401_1500/s1415_the_k_th_lexicographical_string_of_all_happy_strings_of_length_n/Solution.java
+++ src/main/java/g1401_1500/s1415_the_k_th_lexicographical_string_of_all_happy_strings_of_length_n/Solution.java
@@ -1,40 +1,36 @@
 package g1401_1500.s1415_the_k_th_lexicographical_string_of_all_happy_strings_of_length_n;
 
-// #Medium #String #Backtracking #2022_03_26_Time_2_ms_(89.96%)_Space_40.8_MB_(91.57%)
+// #Medium #String #Backtracking #2022_03_27_Time_2_ms_(90.55%)_Space_42.5_MB_(65.75%)
 
 public class Solution {
-    public String getHappyString(int n, int k) {
-        int finalLength = (int) (3 * Math.pow(2, n - 1));
-        StringBuilder res = new StringBuilder();
-        if (k > finalLength) {
-            return "";
+    private char arr[] = new char[] {'a', 'b', 'c'};
+    private String res = "";
+    private int k;
+
+    private void get(StringBuilder str, int n, int index) {
+        if (k < 1) {
+            return;
         }
-        k--;
-        int previousCall = -1;
-        int calls = 3;
-        for (int i = 0; i < n; i++) {
-            int parts = finalLength / calls;
-            int currentCall = k / parts;
-            if (currentCall == 0) {
-                if (previousCall == 0) {
-                    previousCall = 1;
-                } else {
-                    previousCall = 0;
-                }
-            } else if (currentCall == 1) {
-                if (previousCall == -1 || previousCall == 2) {
-                    previousCall = 1;
-                } else {
-                    previousCall = 2;
-                }
-            } else {
-                previousCall = 2;
+        if (str.length() == n) {
+            if (k == 1) {
+                res = str.toString();
             }
-            calls = 2;
-            res.append((char) ('a' + previousCall));
-            finalLength = parts;
-            k -= (currentCall * parts);
+            k--;
+            return;
         }
-        return res.toString();
+        for (int i = 0; i < 3; i++) {
+            if (i == index) {
+                continue;
+            }
+            str.append(arr[i]);
+            get(str, n, i);
+            str.deleteCharAt(str.length() - 1);
+        }
+    }
+
+    public String getHappyString(int n, int k) {
+        this.k = k;
+        get(new StringBuilder(), n, -1);
+        return res;
     }
 }
