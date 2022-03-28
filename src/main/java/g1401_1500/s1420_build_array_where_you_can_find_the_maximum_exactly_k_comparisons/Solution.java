a6a7257202a15dda409cff5bf4cf73dc2160e8f3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-28 07:50:35 +0300

    Update Solution.java

--- src/main/java/g1401_1500/s1420_build_array_where_you_can_find_the_maximum_exactly_k_comparisons/Solution.java
+++ src/main/java/g1401_1500/s1420_build_array_where_you_can_find_the_maximum_exactly_k_comparisons/Solution.java
@@ -8,21 +8,17 @@ public class Solution {
     public int numOfArrays(int n, int m, int k) {
         long[][] ways = new long[m + 1][k + 1];
         long[][] sums = new long[m + 1][k + 1];
-
         for (int max = 1; max <= m; max++) {
             ways[max][1] = 1;
             sums[max][1] = ways[max][1] + sums[max - 1][1];
         }
-
         for (int count = 2; count <= n; count++) {
             long[][] ways2 = new long[m + 1][k + 1];
             long[][] sums2 = new long[m + 1][k + 1];
             for (int max = 1; max <= m; max++) {
                 for (int cost = 1; cost <= k; cost++) {
-
                     long noCost = max * ways[max][cost] % MOD;
                     long newCost = sums[max - 1][cost - 1];
-
                     ways2[max][cost] = (noCost + newCost) % MOD;
                     sums2[max][cost] = (sums2[max - 1][cost] + ways2[max][cost]) % MOD;
                 }
@@ -30,7 +26,6 @@ public class Solution {
             ways = ways2;
             sums = sums2;
         }
-
         return (int) sums[m][k];
     }
 }
