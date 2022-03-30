b3669ef800b73366010943dd95cbbf93cae5e5cc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-03-30 06:22:49 +0300

    Improved task 1185.

--- src/main/java/g1101_1200/s1185_day_of_the_week/Solution.java
+++ src/main/java/g1101_1200/s1185_day_of_the_week/Solution.java
@@ -1,80 +1,65 @@
 package g1101_1200.s1185_day_of_the_week;
 
-// #Easy #Math #2022_03_03_Time_1_ms_(51.16%)_Space_41_MB_(42.25%)
+// #Easy #Math #2022_03_30_Time_0_ms_(100.00%)_Space_41.7_MB_(31.89%)
 
 public class Solution {
     public String dayOfTheWeek(int day, int month, int year) {
-        int yr = chooseYear((year - 1) % 400) % 7;
-        int m = chooseMonth(month) % 7;
-        int d = day % 7;
-        int sum = 0;
-
-        if ((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) && month > 2) {
-            sum = sum + 1;
+        int counter = 0;
+        for (int i = 1971; i < year; i++) {
+            if (isLeapYear(i)) {
+                counter += 366;
+            } else {
+                counter += 365;
+            }
         }
-
-        sum = sum + yr + m + d;
-        return chooseDay(sum % 7);
-    }
-
-    private int chooseYear(int year) {
-        if (year >= 100 && year < 200) {
-            return (int) Math.floor((year - 100.0) / 4) + year - 100 + 5;
-        } else if (year >= 200 && year < 300) {
-            return (int) Math.floor((year - 200.0) / 4) + year - 200 + 3;
-        } else if (year >= 300 && year < 400) {
-            return (int) Math.floor((year - 300.0) / 4) + year - 300 + 1;
-        } else {
-            return (int) Math.floor(year * 1.0 / 4) + year;
+        for (int i = 1; i < month; i++) {
+            counter += dayOfMonth(i);
         }
-    }
-
-    private int chooseMonth(int month) {
-        switch (month) {
-            case 2:
-            case 3:
-                return 3;
-            case 4:
-                return 6;
-            case 5:
-                return 8;
-            case 6:
-                return 11;
-            case 7:
-                return 13;
-            case 8:
-                return 16;
-            case 9:
-                return 19;
-            case 10:
-                return 21;
-            case 11:
-                return 24;
-            case 12:
-                return 26;
-            default:
-                return 0;
+        for (int i = 1; i <= day; i++) {
+            counter += 1;
         }
-    }
-
-    private String chooseDay(int day) {
-        switch (day) {
-            case 0:
-                return "Sunday";
+        if (isLeapYear(year) && month > 2) {
+            counter++;
+        }
+        switch (counter % 7) {
             case 1:
-                return "Monday";
-            case 2:
-                return "Tuesday";
-            case 3:
-                return "Wednesday";
-            case 4:
-                return "Thursday";
-            case 5:
                 return "Friday";
-            case 6:
+            case 2:
                 return "Saturday";
+            case 3:
+                return "Sunday";
+            case 4:
+                return "Monday";
+            case 5:
+                return "Tuesday";
+            case 6:
+                return "Wednesday";
             default:
-                return "";
+                return "Thursday";
+        }
+    }
+
+    private boolean isLeapYear(int year) {
+        return ((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0;
+    }
+
+    private int dayOfMonth(int month) {
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                return 30;
+            default:
+                return 28;
         }
     }
 }
