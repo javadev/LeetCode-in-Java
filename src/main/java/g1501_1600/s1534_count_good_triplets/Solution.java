b4d483d5418552a79b2dbae79e5203f652c0354c
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-09 17:21:18 +0300

    Update Solution.java

--- src/main/java/g1501_1600/s1534_count_good_triplets/Solution.java
+++ src/main/java/g1501_1600/s1534_count_good_triplets/Solution.java
@@ -4,14 +4,11 @@ package g1501_1600.s1534_count_good_triplets;
 
 public class Solution {
     public int countGoodTriplets(int[] arr, int a, int b, int c) {
-        int i;
-        int j;
-        int k;
         int count = 0;
-        for (i = 0; i < (arr.length) - 2; i++) {
-            for (j = i + 1; j < (arr.length) - 1; j++) {
+        for (int i = 0; i < (arr.length) - 2; i++) {
+            for (int j = i + 1; j < (arr.length) - 1; j++) {
                 if (Math.abs(arr[i] - arr[j]) <= a) {
-                    for (k = j + 1; k < arr.length; k++) {
+                    for (int k = j + 1; k < arr.length; k++) {
                         if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
                             count++;
                         }
