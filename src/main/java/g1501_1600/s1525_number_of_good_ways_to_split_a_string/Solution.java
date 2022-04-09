fa3574f5d445f1ba9e90b9fd5e847036df476bb9
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-04-09 19:47:22 +0700

    Update Solution.java

--- src/main/java/g1501_1600/s1525_number_of_good_ways_to_split_a_string/Solution.java
+++ src/main/java/g1501_1600/s1525_number_of_good_ways_to_split_a_string/Solution.java
@@ -7,22 +7,18 @@ import java.util.HashSet;
 
 public class Solution {
     public int numSplits(String s) {
-        HashSet hs = new HashSet();
+        HashSet<Character> hs = new HashSet<>();
         int[] dp1 = new int[s.length() - 1];
         int[] dp2 = new int[s.length() - 1];
         for (int i = 0; i < s.length() - 1; i++) {
             char ch = s.charAt(i);
-            if (!hs.contains(ch)) {
-                hs.add(ch);
-            }
+            hs.add(ch);
             dp1[i] = hs.size();
         }
-        HashSet hm = new HashSet();
+        HashSet<Character> hm = new HashSet<>();
         for (int i = s.length() - 1; i > 0; i--) {
             char ch = s.charAt(i);
-            if (!hm.contains(ch)) {
-                hm.add(ch);
-            }
+            hm.add(ch);
             dp2[i - 1] = hm.size();
         }
         int count = 0;
