b2b7cc186b83d641462128d6874590ffdb303163
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 01:19:35 +0300

    Update Solution.java

--- src/main/java/g1501_1600/s1540_can_convert_string_in_k_moves/Solution.java
+++ src/main/java/g1501_1600/s1540_can_convert_string_in_k_moves/Solution.java
@@ -2,7 +2,7 @@ package g1501_1600.s1540_can_convert_string_in_k_moves;
 
 // #Medium #String #Hash_Table #2022_04_10_Time_10_ms_(100.00%)_Space_54.6_MB_(53.16%)
 
-class Solution {
+public class Solution {
     public boolean canConvertString(String s, String t, int k) {
         int len1 = s.length();
         int len2 = t.length();
