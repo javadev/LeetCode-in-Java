e01615cb1e91f800a1c142a25d524a21d9e88291
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 11:46:51 +0300

    Improved task 1700.

--- src/main/java/g1601_1700/s1700_number_of_students_unable_to_eat_lunch/Solution.java
+++ src/main/java/g1601_1700/s1700_number_of_students_unable_to_eat_lunch/Solution.java
@@ -1,6 +1,6 @@
 package g1601_1700.s1700_number_of_students_unable_to_eat_lunch;
 
-// #Easy #Array #Stack #Simulation #Queue
+// #Easy #Array #Stack #Simulation #Queue #2022_04_11_Time_2_ms_(63.14%)_Space_42.2_MB_(48.76%)
 
 import java.util.LinkedList;
 import java.util.Objects;
