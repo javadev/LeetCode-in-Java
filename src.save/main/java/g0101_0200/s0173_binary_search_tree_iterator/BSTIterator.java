08858d1d55065d00bdb4593c97a8afe857108188
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-12 02:15:54 +0300

    Fixed javadoc.

--- src.save/main/java/g0101_0200/s0173_binary_search_tree_iterator/BSTIterator.java
+++ src.save/main/java/g0101_0200/s0173_binary_search_tree_iterator/BSTIterator.java
@@ -53,7 +53,9 @@ public class BSTIterator {
     }
 }
 
-/**
- * Your BSTIterator object will be instantiated and called as such: BSTIterator obj = new
- * BSTIterator(root); int param_1 = obj.next(); boolean param_2 = obj.hasNext();
+/*
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
  */
