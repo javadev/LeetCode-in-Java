8626998850927f9c9d7716e32c9ad80442039ca4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 10:48:28 +0300

    Update Solution.java

--- src/main/java/g1501_1600/s1558_minimum_numbers_of_function_calls_to_make_target_array/Solution.java
+++ src/main/java/g1501_1600/s1558_minimum_numbers_of_function_calls_to_make_target_array/Solution.java
@@ -5,23 +5,19 @@ package g1501_1600.s1558_minimum_numbers_of_function_calls_to_make_target_array;
 public class Solution {
     public int minOperations(int[] nums) {
         int ops = 0;
-
         for (int bit = 0; bit < 32; bit++) {
             boolean nonzero = false;
-
             for (int i = 0; i < nums.length; i++) {
                 ops += nums[i] % 2;
                 nums[i] /= 2;
                 nonzero |= nums[i] > 0;
             }
-
             if (nonzero) {
                 ops++;
             } else {
                 break;
             }
         }
-
         return ops;
     }
 }
