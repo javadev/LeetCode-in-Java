5f119fddcb4e147cd282e7d5e1aa0ebed657fa59
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-04-11 10:40:21 +0700

    Add suppresswarnings

--- src/test/java/g1501_1600/s1545_find_kth_bit_in_nth_binary_string/SolutionTest.java
+++ src/test/java/g1501_1600/s1545_find_kth_bit_in_nth_binary_string/SolutionTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     @Test
     void findKthBit() {
-        assertThat(new Solution().findKthBit(1), equalTo('0'));
+        assertThat(new Solution().findKthBit(3, 1), equalTo('0'));
     }
 
     @Test
     void findKthBit2() {
-        assertThat(new Solution().findKthBit(11), equalTo('1'));
+        assertThat(new Solution().findKthBit(4, 11), equalTo('1'));
     }
 }
