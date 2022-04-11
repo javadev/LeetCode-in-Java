4bbb4dadc5278b3f084277ac2bf48b090159fd46
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 10:54:37 +0300

    Improved task 1560.

--- src/main/java/g1501_1600/s1560_most_visited_sector_in_a_circular_track/Solution.java
+++ src/main/java/g1501_1600/s1560_most_visited_sector_in_a_circular_track/Solution.java
@@ -1,51 +1,29 @@
 package g1501_1600.s1560_most_visited_sector_in_a_circular_track;
 
-// #Easy #Array #Simulation #2022_04_11_Time_14_ms_(8.08%)_Space_45_MB_(5.05%)
+// #Easy #Array #Simulation #2022_04_11_Time_1_ms_(88.89%)_Space_43.6_MB_(62.96%)
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Solution {
     public List<Integer> mostVisited(int n, int[] rounds) {
-        List<Integer> result = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < rounds.length - 1; i++) {
-            if (rounds[i] > rounds[i + 1]) {
-                if (i == 0) {
-                    map.put(rounds[i], 1);
-                }
-                for (int j = rounds[i] + 1; j <= n; j++) {
-                    map.put(j, map.getOrDefault(j, 0) + 1);
-                }
-                for (int j = 1; j <= rounds[i + 1]; j++) {
-                    map.put(j, map.getOrDefault(j, 0) + 1);
-                }
-            } else {
-                int j;
-                if (i == 0) {
-                    j = rounds[i];
-                } else {
-                    j = rounds[i] + 1;
-                }
-                for (; j <= rounds[i + 1]; j++) {
-                    map.put(j, map.getOrDefault(j, 0) + 1);
-                }
+        List<Integer> res = new ArrayList<>();
+        int start = rounds[0];
+        int end = rounds[rounds.length - 1];
+        int[] ans = new int[n + 1];
+        while (start != end) {
+            ans[start]++;
+            start++;
+            if (start > n) {
+                start = 1;
             }
         }
-
-        int mostVisitedCount = 0;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            mostVisitedCount = Math.max(mostVisitedCount, entry.getValue());
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() == mostVisitedCount) {
-                result.add(entry.getKey());
+        ans[end]++;
+        for (int i = 1; i <= n; i++) {
+            if (ans[i] != 0) {
+                res.add(i);
             }
         }
-        Collections.sort(result);
-        return result;
+        return res;
     }
 }
