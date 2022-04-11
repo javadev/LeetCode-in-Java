98c656313f2621dd130f0c41707df4473b4f73ef
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-12 01:34:39 +0300

    Added task 1581.

---
+++ src/main/java/g1501_1600/s1581_customer_who_visited_but_did_not_make_any_transactions/script.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# #Easy #Database #2022_04_12_Time_1039_ms_(76.99%)_Space_0B_(100.00%)
+select customer_id, COUNT(*) as count_no_trans
+from visits v left join transactions t
+on v.visit_id = t.visit_id
+where transaction_id is null
+group by customer_id
