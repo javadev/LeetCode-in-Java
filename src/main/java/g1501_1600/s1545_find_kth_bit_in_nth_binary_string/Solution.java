a7d9cacf7a4b9fd077d5d28e526c7c002c98efb3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 06:49:24 +0300

    Update Solution.java

--- src/main/java/g1501_1600/s1545_find_kth_bit_in_nth_binary_string/Solution.java
+++ src/main/java/g1501_1600/s1545_find_kth_bit_in_nth_binary_string/Solution.java
@@ -2,8 +2,9 @@ package g1501_1600.s1545_find_kth_bit_in_nth_binary_string;
 
 // #Medium #String #Recursion #2022_04_11_Time_0_ms_(100.00%)_Space_41.2_MB_(90.34%)
 
+@SuppressWarnings("java:S1172")
 public class Solution {
-    public char findKthBit(@SuppressWarnings("unused") int n, int k) {
+    public char findKthBit(int n, int k) {
         boolean flip = false;
         while (k != 1) {
             int base = floorTwo(k);
