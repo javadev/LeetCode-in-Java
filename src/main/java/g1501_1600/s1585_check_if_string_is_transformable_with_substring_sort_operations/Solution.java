fe3b6597b0ea4e26657488d7e52e44fe1d3ab531
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-11 15:12:58 +0300

    Fixed tags.

--- src/main/java/g1501_1600/s1585_check_if_string_is_transformable_with_substring_sort_operations/Solution.java
+++ src/main/java/g1501_1600/s1585_check_if_string_is_transformable_with_substring_sort_operations/Solution.java
@@ -1,6 +1,6 @@
 package g1501_1600.s1585_check_if_string_is_transformable_with_substring_sort_operations;
 
-// #Hard #String #Sorting #Greedy
+// #Hard #String #Sorting #Greedy #2022_04_11_Time_20_ms_(97.22%)_Space_54.4_MB_(72.22%)
 
 public class Solution {
     public boolean isTransformable(String s, String t) {
