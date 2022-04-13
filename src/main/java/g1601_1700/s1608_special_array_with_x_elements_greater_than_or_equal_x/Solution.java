1d1c6e97a2900fd5c3866aff2be973a5ebe51add
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-13 13:39:17 +0300

    Revert "Improved task 1608."
    This reverts commit c55ae8275d3c256e7de3a0558b6b6eb2b84ea430.

--- src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
+++ src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
@@ -1,17 +1,22 @@
 package g1601_1700.s1608_special_array_with_x_elements_greater_than_or_equal_x;
 
-// #Easy #Array #Sorting #Binary_Search #Binary_Search_I_Day_7
-// #2022_04_13_Time_1_ms_(90.88%)_Space_40.3_MB_(82.63%)
+// #Easy #Array #Sorting #Binary_Search #2022_04_12_Time_4_ms_(8.78%)_Space_42.1_MB_(34.57%)
 
 import java.util.Arrays;
 
 public class Solution {
     public int specialArray(int[] nums) {
         Arrays.sort(nums);
-        for (int i = nums.length; i > 0; i--) {
-            int size = nums.length - i;
-            if ((nums[size] > i || nums[size] == i) && (i == nums.length || nums[size - 1] < i)) {
-                return i;
+        int max = nums[nums.length - 1];
+        for (int x = 1; x <= max; x++) {
+            int found = 0;
+            int i = nums.length - 1;
+            while (i >= 0 && nums[i] >= x) {
+                i--;
+                found++;
+            }
+            if (found == x) {
+                return x;
             }
         }
         return -1;
