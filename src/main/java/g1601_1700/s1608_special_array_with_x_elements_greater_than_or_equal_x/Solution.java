d473d66d12a23146bb6c532d7583ba6c738ceca0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-13 13:32:55 +0300

    Improved task 1608.

--- src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
+++ src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
@@ -1,22 +1,17 @@
 package g1601_1700.s1608_special_array_with_x_elements_greater_than_or_equal_x;
 
-// #Easy #Array #Sorting #Binary_Search #2022_04_12_Time_4_ms_(8.78%)_Space_42.1_MB_(34.57%)
+// #Easy #Array #Sorting #Binary_Search #Binary_Search_I_Day_7
+// #2022_04_13_Time_1_ms_(90.88%)_Space_40.3_MB_(82.63%)
 
 import java.util.Arrays;
 
 public class Solution {
-    public int specialArray(int[] nums) {
-        Arrays.sort(nums);
-        int max = nums[nums.length - 1];
-        for (int x = 1; x <= max; x++) {
-            int found = 0;
-            int i = nums.length - 1;
-            while (i >= 0 && nums[i] >= x) {
-                i--;
-                found++;
-            }
-            if (found == x) {
-                return x;
+    public int specialArray(int[] arr) {
+        Arrays.sort(arr);
+        for (int i = arr.length; i > 0; i--) {
+            int size = arr.length - i;
+            if ((arr[size] > i || arr[size] == i) && (i == arr.length || arr[size - 1] < i)) {
+                return i;
             }
         }
         return -1;
