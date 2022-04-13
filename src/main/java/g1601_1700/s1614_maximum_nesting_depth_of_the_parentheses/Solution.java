5b5788b6b405ae310fe34f4e6345273f9cb2dc32
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-13 04:01:41 +0300

    Update Solution.java

--- src/main/java/g1601_1700/s1614_maximum_nesting_depth_of_the_parentheses/Solution.java
+++ src/main/java/g1601_1700/s1614_maximum_nesting_depth_of_the_parentheses/Solution.java
@@ -2,11 +2,12 @@ package g1601_1700.s1614_maximum_nesting_depth_of_the_parentheses;
 
 // #Easy #String #Stack #2022_04_12_Time_1_ms_(80.44%)_Space_41.4_MB_(78.91%)
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution {
     public int maxDepth(String s) {
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
         int maxDepth = 0;
         for (char c : s.toCharArray()) {
             if (c == '(') {
