0f789b24d7b3713f607d24eb12753b3bc1d815de
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-13 13:57:25 +0300

    Improved task 1608.

--- src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
+++ src/main/java/g1601_1700/s1608_special_array_with_x_elements_greater_than_or_equal_x/Solution.java
@@ -1,6 +1,7 @@
 package g1601_1700.s1608_special_array_with_x_elements_greater_than_or_equal_x;
 
-// #Easy #Array #Sorting #Binary_Search #2022_04_12_Time_4_ms_(8.78%)_Space_42.1_MB_(34.57%)
+// #Easy #Array #Sorting #Binary_Search #Binary_Search_I_Day_7
+// #2022_04_13_Time_2_ms_(61.14%)_Space_40.5_MB_(81.84%)
 
 import java.util.Arrays;
 
