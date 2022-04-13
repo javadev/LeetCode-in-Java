f1f989018c83d6426b22dc4cfbda1816a8a23419
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-13 06:50:24 +0300

    Fixed sonar.

--- src/main/java/g1601_1700/s1694_reformat_phone_number/Solution.java
+++ src/main/java/g1601_1700/s1694_reformat_phone_number/Solution.java
@@ -2,7 +2,7 @@ package g1601_1700.s1694_reformat_phone_number;
 
 // #Easy #String #2022_04_13_Time_1_ms_(99.67%)_Space_40.7_MB_(88.24%)
 
-@SuppressWarnings("java:S127")
+@SuppressWarnings("java:S135")
 public class Solution {
     public String reformatNumber(String number) {
         StringBuilder sb = new StringBuilder();
