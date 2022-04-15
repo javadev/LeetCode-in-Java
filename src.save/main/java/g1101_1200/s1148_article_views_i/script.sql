07925f1bba5d6673601545c9afd5c1985a4db5e4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-15 09:45:49 +0300

    Added task 1148.

---
+++ src.save/main/java/g1101_1200/s1148_article_views_i/script.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# #Easy #Database #SQL_I_Day_5_Union #2022_04_15_Time_459_ms_(44.85%)_Space_0B_(100.00%)
+select distinct author_id as id from Views where author_id = viewer_id order by author_id;
