bf254b12cd7098af1334998f8598f275595492ef
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-15 05:18:16 +0300

    Improved task 1589.

--- src.save/main/java/g1501_1600/s1589_maximum_sum_obtained_of_any_permutation/Solution.java
+++ src.save/main/java/g1501_1600/s1589_maximum_sum_obtained_of_any_permutation/Solution.java
@@ -1,6 +1,7 @@
 package g1501_1600.s1589_maximum_sum_obtained_of_any_permutation;
 
 // #Medium #Array #Sorting #Greedy #Prefix_Sum
+// #2022_04_15_Time_25_ms_(92.45%)_Space_78.4_MB_(83.02%)
 
 import java.util.Arrays;
 
