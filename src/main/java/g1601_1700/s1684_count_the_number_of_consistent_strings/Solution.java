b17509409e8c05c07a91fd8814ca838503fc8b2a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-17 04:25:48 +0300

    Fixed tag.

--- src/main/java/g1601_1700/s1684_count_the_number_of_consistent_strings/Solution.java
+++ src/main/java/g1601_1700/s1684_count_the_number_of_consistent_strings/Solution.java
@@ -1,6 +1,7 @@
 package g1601_1700.s1684_count_the_number_of_consistent_strings;
 
 // #Easy #Array #String #Hash_Table #Bit_Manipulation
+// #2022_04_17_Time_6_ms_(97.04%)_Space_43_MB_(90.20%)
 
 public class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
