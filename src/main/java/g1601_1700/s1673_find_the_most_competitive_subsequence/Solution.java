ab58c600a6a9a4771d4458de404bb9c774d21d38
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-19 15:03:54 +0300

    Improved 1673.

--- src/main/java/g1601_1700/s1673_find_the_most_competitive_subsequence/Solution.java
+++ src/main/java/g1601_1700/s1673_find_the_most_competitive_subsequence/Solution.java
@@ -1,6 +1,7 @@
 package g1601_1700.s1673_find_the_most_competitive_subsequence;
 
 // #Medium #Array #Greedy #Stack #Monotonic_Stack
+// #2022_04_19_Time_7_ms_(96.82%)_Space_54.9_MB_(88.07%)
 
 public class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
