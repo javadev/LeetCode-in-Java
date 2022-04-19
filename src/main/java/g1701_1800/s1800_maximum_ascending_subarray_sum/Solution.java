3be16dffad53db4bf8bad46add48ebfffd127d61
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-19 10:19:08 +0300

    Fixed sonar.

--- src/main/java/g1701_1800/s1800_maximum_ascending_subarray_sum/Solution.java
+++ src/main/java/g1701_1800/s1800_maximum_ascending_subarray_sum/Solution.java
@@ -5,7 +5,9 @@ package g1701_1800.s1800_maximum_ascending_subarray_sum;
 public class Solution {
     public int maxAscendingSum(int[] nums) {
         int maxSum = nums[0];
-        for (int i = 0, j = i + 1; i < nums.length - 1 && j < nums.length; j++) {
+        int i = 0;
+        int j = i + 1;
+        while (i < nums.length - 1 && j < nums.length) {
             int sum = nums[j - 1];
             while (j < nums.length && nums[j] - nums[j - 1] > 0) {
                 sum += nums[j];
@@ -13,6 +15,7 @@ public class Solution {
             }
             i = j;
             maxSum = Math.max(maxSum, sum);
+            j++;
         }
         return maxSum;
     }
