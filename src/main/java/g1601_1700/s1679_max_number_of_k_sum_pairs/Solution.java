8fdefe98985277eba99ca86c7ec85330c27d024d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-21 11:57:59 +0300

    Improved task 1679.

--- src/main/java/g1601_1700/s1679_max_number_of_k_sum_pairs/Solution.java
+++ src/main/java/g1601_1700/s1679_max_number_of_k_sum_pairs/Solution.java
@@ -1,25 +1,28 @@
 package g1601_1700.s1679_max_number_of_k_sum_pairs;
 
 // #Medium #Array #Hash_Table #Sorting #Two_Pointers
+// #2022_04_21_Time_22_ms_(85.31%)_Space_67.1_MB_(70.80%)
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Solution {
     public int maxOperations(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int ops = 0;
-        for (int num : nums) {
-            if (map.containsKey(k - num)) {
-                map.put(k - num, map.get(k - num) - 1);
-                ops++;
-                if (map.get(k - num) == 0) {
-                    map.remove(k - num);
-                }
+        Arrays.sort(nums);
+        int i = 0;
+        int c = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == k) {
+                c++;
+                i++;
+                j--;
+            } else if (sum > k) {
+                j--;
             } else {
-                map.put(num, map.getOrDefault(num, 0) + 1);
+                i++;
             }
         }
-        return ops;
+        return c;
     }
 }
