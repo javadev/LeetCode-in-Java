ca66f00bae189fe53845802eee3571aafad72a92
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-23 08:58:29 +0300

    Added task 1672.

---
+++ src/main/java/g1601_1700/s1672_richest_customer_wealth/Solution.java
@@ -0,0 +1,18 @@
+package g1601_1700.s1672_richest_customer_wealth;
+
+// #Easy #Array #Matrix #Programming_Skills_I_Day_6_Array
+// #2022_04_23_Time_0_ms_(100.00%)_Space_44.1_MB_(7.31%)
+
+public class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
