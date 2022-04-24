6dbec7822715220651bd564a21d662e7f274d9b6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-24 09:52:35 +0300

    Update Solution.java

--- src/main/java/g1701_1800/s1706_where_will_the_ball_fall/Solution.java
+++ src/main/java/g1701_1800/s1706_where_will_the_ball_fall/Solution.java
@@ -3,6 +3,7 @@ package g1701_1800.s1706_where_will_the_ball_fall;
 // #Medium #Array #Dynamic_Programming #Depth_First_Search #Matrix #Simulation
 // #2022_04_24_Time_2_ms_(64.55%)_Space_54.5_MB_(25.38%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int[] findBall(int[][] grid) {
         int m = grid.length;
@@ -28,7 +29,6 @@ public class Solution {
                     }
                 }
             }
-
             if (currentI == m) {
                 res[j] = currentJ;
             } else {
