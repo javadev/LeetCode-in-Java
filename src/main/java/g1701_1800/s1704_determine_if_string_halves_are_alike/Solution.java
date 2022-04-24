20bc95eb8a026a9262051499fc7d58cc972bbae3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-24 23:14:55 +0300

    Improved task 1704.

--- src/main/java/g1701_1800/s1704_determine_if_string_halves_are_alike/Solution.java
+++ src/main/java/g1701_1800/s1704_determine_if_string_halves_are_alike/Solution.java
@@ -1,13 +1,12 @@
 package g1701_1800.s1704_determine_if_string_halves_are_alike;
 
-// #Easy #String #Counting #2022_04_24_Time_2_ms_(97.70%)_Space_42.4_MB_(50.86%)
+// #Easy #String #Counting #2022_04_24_Time_1_ms_(100.00%)_Space_41.9_MB_(69.58%)
 
 public class Solution {
     public boolean halvesAreAlike(String s) {
         if (s.length() < 1) {
             return false;
         }
-        s = s.toLowerCase();
         return countVowel(0, s.length() / 2, s) == countVowel(s.length() / 2, s.length(), s);
     }
 
@@ -15,7 +14,8 @@ public class Solution {
         int c = 0;
         for (int i = start; i < end; i++) {
             char ch = s.charAt(i);
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'
+                    || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                 c++;
             }
         }
