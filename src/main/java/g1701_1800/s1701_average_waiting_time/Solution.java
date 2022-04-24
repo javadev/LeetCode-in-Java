6d1617857329709281fa37764654a45b23e02ca0
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-24 22:52:42 +0300

    Improved task 1701.

--- src/main/java/g1701_1800/s1701_average_waiting_time/Solution.java
+++ src/main/java/g1701_1800/s1701_average_waiting_time/Solution.java
@@ -1,16 +1,20 @@
 package g1701_1800.s1701_average_waiting_time;
 
-// #Medium #Array #Simulation #2022_04_24_Time_4_ms_(54.50%)_Space_89.2_MB_(91.50%)
+// #Medium #Array #Simulation #2022_04_24_Time_2_ms_(100.00%)_Space_94.5_MB_(56.50%)
 
 public class Solution {
     public double averageWaitingTime(int[][] customers) {
-        double sumOfTime = 0;
-        int preFinishTime = 0;
-        for (int[] customer : customers) {
-            preFinishTime = Math.max(customer[0], preFinishTime);
-            sumOfTime += (preFinishTime + customer[1]) - customer[0];
-            preFinishTime += customer[1];
+        int ctime = 0;
+        double ans = 0;
+        for (int i = 0; i < customers.length; i++) {
+            if (customers[i][0] >= ctime) {
+                ctime = customers[i][0] + customers[i][1];
+                ans = ans + (ctime - customers[i][0]);
+            } else {
+                ctime = ctime + customers[i][1];
+                ans = ans + (ctime - customers[i][0]);
+            }
         }
-        return sumOfTime / customers.length;
+        return (double) Math.round(ans / customers.length * 100000d) / 100000d;
     }
 }
