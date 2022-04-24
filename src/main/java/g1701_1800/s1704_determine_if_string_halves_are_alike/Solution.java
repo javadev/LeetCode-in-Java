e43c2631cee17c64e9749dfcef4bd91ff3b72a2f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-24 23:23:52 +0300

    Improved task 1704.

--- src/main/java/g1701_1800/s1704_determine_if_string_halves_are_alike/Solution.java
+++ src/main/java/g1701_1800/s1704_determine_if_string_halves_are_alike/Solution.java
@@ -1,26 +1,24 @@
 package g1701_1800.s1704_determine_if_string_halves_are_alike;
 
-// #Easy #String #Counting #2022_04_24_Time_6_ms_(42.11%)_Space_40.4_MB_(91.25%)
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.IntStream;
+// #Easy #String #Counting #2022_04_24_Time_1_ms_(100.00%)_Space_41.9_MB_(69.58%)
 
 public class Solution {
     public boolean halvesAreAlike(String s) {
-        Set<Character> vowels =
-                new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
-        int firstHalfVowelsCount =
-                (int)
-                        IntStream.range(0, s.length() / 2)
-                                .filter(i -> vowels.contains(s.charAt(i)))
-                                .count();
-        int secondHalfVowelsCount =
-                (int)
-                        IntStream.range(s.length() / 2, s.length())
-                                .filter(i -> vowels.contains(s.charAt(i)))
-                                .count();
-        return firstHalfVowelsCount == secondHalfVowelsCount;
+        if (s.length() < 1) {
+            return false;
+        }
+        return countVowel(0, s.length() / 2, s) == countVowel(s.length() / 2, s.length(), s);
+    }
+
+    private int countVowel(int start, int end, String s) {
+        int c = 0;
+        for (int i = start; i < end; i++) {
+            char ch = s.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A'
+                    || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                c++;
+            }
+        }
+        return c;
     }
 }
