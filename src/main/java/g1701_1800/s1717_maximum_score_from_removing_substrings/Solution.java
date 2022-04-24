b64fbd2786a30f204566ced77ad33fa3971d88cf
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-24 15:30:59 +0300

    Update Solution.java

--- src/main/java/g1701_1800/s1717_maximum_score_from_removing_substrings/Solution.java
+++ src/main/java/g1701_1800/s1717_maximum_score_from_removing_substrings/Solution.java
@@ -2,6 +2,7 @@ package g1701_1800.s1717_maximum_score_from_removing_substrings;
 
 // #Medium #String #Greedy #Stack #2022_04_24_Time_19_ms_(99.25%)_Space_43.2_MB_(91.04%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int maximumGain(String s, int x, int y) {
         char[] v = s.toCharArray();
