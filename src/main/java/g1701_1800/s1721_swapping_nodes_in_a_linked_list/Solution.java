9b53c2664ffcaeaf88632d6b06d840c53b4b1192
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-25 18:35:16 +0300

    Improved task 1721.

--- src/main/java/g1701_1800/s1721_swapping_nodes_in_a_linked_list/Solution.java
+++ src/main/java/g1701_1800/s1721_swapping_nodes_in_a_linked_list/Solution.java
@@ -1,39 +1,38 @@
 package g1701_1800.s1721_swapping_nodes_in_a_linked_list;
 
-// #Medium #Two_Pointers #Linked_List #2022_04_24_Time_5_ms_(27.11%)_Space_163.5_MB_(78.20%)
+// #Medium #Two_Pointers #Linked_List #2022_04_25_Time_2_ms_(100.00%)_Space_182.3_MB_(38.16%)
 
 import com_github_leetcode.ListNode;
 
-@SuppressWarnings("java:S2259")
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 public class Solution {
     public ListNode swapNodes(ListNode head, int k) {
-        int length = 0;
-        int secondIndex;
-        ListNode temp1 = null;
-        ListNode temp2 = null;
-        ListNode temp3 = head;
-        while (temp3 != null) {
-            length++;
-            temp3 = temp3.next;
-        }
-        secondIndex = length - k + 1;
-        temp3 = head;
-        try {
-            for (int i = 1; i <= length; i++) {
-                if (i == k) {
-                    temp1 = temp3;
-                }
-                if (i == secondIndex) {
-                    temp2 = temp3;
-                }
-                temp3 = temp3.next;
+        ListNode beg = null;
+        ListNode end = null;
+        ListNode node = head;
+        while (node != null) {
+            k--;
+            if (k == 0) {
+                beg = node;
+                end = head;
+            } else if (end != null) {
+                end = end.next;
             }
-            int value = temp1.val;
-            temp1.val = temp2.val;
-            temp2.val = value;
-        } catch (Exception ignored) {
-            // ignore
+            node = node.next;
         }
+
+        int tem = beg.val;
+        beg.val = end.val;
+        end.val = tem;
         return head;
     }
 }
