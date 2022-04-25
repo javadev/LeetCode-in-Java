909430748176b5753789a75f7e58853742f4d55e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-25 19:14:11 +0300

    Fixed sonar.

--- src/main/java/g1701_1800/s1799_maximize_score_after_n_operations/Solution.java
+++ src/main/java/g1701_1800/s1799_maximize_score_after_n_operations/Solution.java
@@ -31,7 +31,8 @@ public class Solution {
                 }
             }
         }
-        return memo[mask] = maxScore;
+        memo[mask] = maxScore;
+        return maxScore;
     }
 
     private int gcd(int a, int b) {
