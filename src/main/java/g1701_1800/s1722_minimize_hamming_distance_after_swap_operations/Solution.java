cfce992e52c6f9c4503c053bbe1b3612e3421247
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-25 18:22:41 +0300

    Update Solution.java

--- src/main/java/g1701_1800/s1722_minimize_hamming_distance_after_swap_operations/Solution.java
+++ src/main/java/g1701_1800/s1722_minimize_hamming_distance_after_swap_operations/Solution.java
@@ -17,11 +17,9 @@ public class Solution {
         for (i = 0; i < n; i++) {
             parent[i] = i;
         }
-
         for (int[] swap : allowedSwaps) {
             union(swap[0], swap[1], parent);
         }
-
         HashMap<Integer, List<Integer>> components = new HashMap<>();
         for (i = 0; i < n; i++) {
             find(i, parent);
@@ -29,29 +27,23 @@ public class Solution {
             list.add(i);
             components.put(parent[i], list);
         }
-
         for (Map.Entry<Integer, List<Integer>> entry : components.entrySet()) {
             weight += getHammingDistance(source, target, entry.getValue());
         }
-
         return weight;
     }
 
     private int getHammingDistance(int[] source, int[] target, List<Integer> indices) {
         HashMap<Integer, Integer> list1 = new HashMap<>();
         HashMap<Integer, Integer> list2 = new HashMap<>();
-
         for (int i : indices) {
             list1.put(target[i], 1 + list1.getOrDefault(target[i], 0));
             list2.put(source[i], 1 + list2.getOrDefault(source[i], 0));
         }
-
         int size = indices.size();
-
         for (Map.Entry<Integer, Integer> entry : list1.entrySet()) {
             size -= Math.min(entry.getValue(), list2.getOrDefault(entry.getKey(), 0));
         }
-
         return size;
     }
 
@@ -67,11 +59,9 @@ public class Solution {
 
     private int find(int x, int[] parent) {
         int y = x;
-
         while (y != parent[y]) {
             y = parent[y];
         }
-
         parent[x] = y;
         return y;
     }
