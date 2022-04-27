e867cf28ce8533b91f41a867661cec93f7590a7d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-27 13:29:30 +0300

    Added task 1774.

---
+++ src/main/java/g1701_1800/s1774_closest_dessert_cost/Solution.java
@@ -0,0 +1,30 @@
+package g1701_1800.s1774_closest_dessert_cost;
+
+// #Medium #Array #Dynamic_Programming #Backtracking
+// #2022_04_27_Time_5_ms_(82.32%)_Space_42.3_MB_(20.34%)
+
+public class Solution {
+    private int finalValue = Integer.MAX_VALUE;
+
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        for (int i = 0; i < baseCosts.length; i++) {
+            closestCost(baseCosts[i], toppingCosts, target, 0);
+        }
+        return finalValue;
+    }
+
+    private void closestCost(int curCost, int[] toppingCosts, int target, int index) {
+        if (index >= toppingCosts.length || curCost >= target) {
+            if (Math.abs(target - curCost) < Math.abs(target - finalValue)) {
+                finalValue = curCost;
+            } else if (Math.abs(target - curCost) == Math.abs(target - finalValue)
+                    && target < finalValue) {
+                finalValue = curCost;
+            }
+            return;
+        }
+        closestCost(curCost, toppingCosts, target, index + 1);
+        closestCost(curCost + toppingCosts[index], toppingCosts, target, index + 1);
+        closestCost(curCost + toppingCosts[index] * 2, toppingCosts, target, index + 1);
+    }
+}
