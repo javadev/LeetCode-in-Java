0b1fce205d9f8be2ba90d48af0da3c77429e5d60
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-27 13:33:19 +0300

    Fixed sonar.

--- src/main/java/g1701_1800/s1774_closest_dessert_cost/Solution.java
+++ src/main/java/g1701_1800/s1774_closest_dessert_cost/Solution.java
@@ -3,12 +3,13 @@ package g1701_1800.s1774_closest_dessert_cost;
 // #Medium #Array #Dynamic_Programming #Backtracking
 // #2022_04_27_Time_5_ms_(82.32%)_Space_42.3_MB_(20.34%)
 
+@SuppressWarnings("java:S1871")
 public class Solution {
     private int finalValue = Integer.MAX_VALUE;
 
     public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
-        for (int i = 0; i < baseCosts.length; i++) {
-            closestCost(baseCosts[i], toppingCosts, target, 0);
+        for (int baseCost : baseCosts) {
+            closestCost(baseCost, toppingCosts, target, 0);
         }
         return finalValue;
     }
