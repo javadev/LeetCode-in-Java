09735df3ef272f54565f2d11ebe7594c84b951d6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-28 17:15:14 +0300

    Improved test 101.

--- src/test/java/g0101_0200/s0101_symmetric_tree/SolutionTest.java
+++ src/test/java/g0101_0200/s0101_symmetric_tree/SolutionTest.java
@@ -4,14 +4,20 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import com_github_leetcode.TreeNode;
+import com_github_leetcode.TreeUtils;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void symmetricTree() {
-        TreeNode left = new TreeNode(2, new TreeNode(3), new TreeNode(4));
-        TreeNode right = new TreeNode(2, new TreeNode(4), new TreeNode(3));
-        TreeNode root = new TreeNode(1, left, right);
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 2, 3, 4, 4, 3));
         assertThat(new Solution().isSymmetric(root), equalTo(true));
+    }
+
+    @Test
+    void symmetricTree2() {
+        TreeNode root = TreeUtils.constructBinaryTree(Arrays.asList(1, 2, 2, null, 3, null, 3));
+        assertThat(new Solution().isSymmetric(root), equalTo(false));
     }
 }
