22e875234031c0298c8bfd968603d1f09e986098
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-28 11:27:42 +0300

    Improved task 855.

--- src/main/java/g0801_0900/s0855_exam_room/ExamRoom.java
+++ src/main/java/g0801_0900/s0855_exam_room/ExamRoom.java
@@ -7,6 +7,33 @@ import java.util.Map;
 import java.util.Objects;
 
 public class ExamRoom {
+    private static class Node {
+        Node pre;
+        Node next;
+        int val;
+
+        Node(int val, Map<Integer, Node> map) {
+            this.val = val;
+            map.put(val, this);
+        }
+
+        int insert(Node left) {
+            Node right = left.next;
+            left.next = this;
+            right.pre = this;
+            this.next = right;
+            this.pre = left;
+            return val;
+        }
+
+        void delete() {
+            Node left = this.pre;
+            Node right = this.next;
+            left.next = right;
+            right.pre = left;
+        }
+    }
+
     private Map<Integer, Node> map = new HashMap<>();
     private Node head = new Node(-1, map);
     private Node tail = new Node(-1, map);
@@ -52,3 +79,10 @@ public class ExamRoom {
         map.remove(p);
     }
 }
+
+/*
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom obj = new ExamRoom(n);
+ * int param_1 = obj.seat();
+ * obj.leave(p);
+ */
