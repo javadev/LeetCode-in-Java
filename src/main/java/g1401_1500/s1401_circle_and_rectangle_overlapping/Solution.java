63d5f360823af2ac642b04e0f50dd42eaade250c
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-29 15:30:24 +0300

    Fixed style.

--- src/main/java/g1401_1500/s1401_circle_and_rectangle_overlapping/Solution.java
+++ src/main/java/g1401_1500/s1401_circle_and_rectangle_overlapping/Solution.java
@@ -4,13 +4,13 @@ package g1401_1500.s1401_circle_and_rectangle_overlapping;
 
 public class Solution {
     public boolean checkOverlap(
-            int radius, int x_center, int y_center, int x1, int y1, int x2, int y2) {
+            int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
         // Find the closest point to the circle within the rectangle
-        int closestX = clamp(x_center, x1, x2);
-        int closestY = clamp(y_center, y1, y2);
+        int closestX = clamp(xCenter, x1, x2);
+        int closestY = clamp(yCenter, y1, y2);
         // Calculate the distance between the circle's center and this closest point
-        int distanceX = x_center - closestX;
-        int distanceY = y_center - closestY;
+        int distanceX = xCenter - closestX;
+        int distanceY = yCenter - closestY;
         // If the distance is less than the circle's radius, an intersection occurs
         int distanceSquared = distanceX * distanceX + distanceY * distanceY;
         return distanceSquared <= radius * radius;
