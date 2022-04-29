04d97f70c8a6d98cb44363b2dd23de57e0f6c631
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-29 13:54:35 +0300

    Improved task 1207.

--- src/main/java/g1201_1300/s1207_unique_number_of_occurrences/Solution.java
+++ src/main/java/g1201_1300/s1207_unique_number_of_occurrences/Solution.java
@@ -1,19 +1,30 @@
 package g1201_1300.s1207_unique_number_of_occurrences;
 
-// #Easy #Array #Hash_Table #2022_03_09_Time_8_ms_(5.98%)_Space_42.4_MB_(22.63%)
+// #Easy #Array #Hash_Table #2022_04_29_Time_2_ms_(82.71%)_Space_42.4_MB_(34.08%)
 
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        Arrays.stream(arr)
-                .forEach(num -> map.put(num, map.containsKey(num) ? map.get(num) + 1 : 1));
-        Set<Integer> set = new HashSet<>();
-        return map.keySet().stream().mapToInt(key -> key).allMatch(key -> set.add(map.get(key)));
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (mp.containsKey(arr[i])) {
+                mp.put(arr[i], mp.get(arr[i]) + 1);
+            } else {
+                mp.put(arr[i], 1);
+            }
+        }
+        // map for check unique number of  count
+        Map<Integer, Integer> uni = new HashMap<>();
+        for (Integer val : mp.values()) {
+            if (uni.containsKey(val)) {
+                return false;
+            } else {
+
+                uni.put(val, 1);
+            }
+        }
+        return true;
     }
 }
