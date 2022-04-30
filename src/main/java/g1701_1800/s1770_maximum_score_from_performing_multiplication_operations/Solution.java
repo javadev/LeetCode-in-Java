d380f3b9c1ff3bb13ea9fbf5f4029d4013f7720b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-30 11:06:14 +0300

    Update Solution.java

--- src/main/java/g1701_1800/s1770_maximum_score_from_performing_multiplication_operations/Solution.java
+++ src/main/java/g1701_1800/s1770_maximum_score_from_performing_multiplication_operations/Solution.java
@@ -3,14 +3,12 @@ package g1701_1800.s1770_maximum_score_from_performing_multiplication_operations
 // #Medium #Array #Dynamic_Programming #2022_04_30_Time_31_ms_(92.41%)_Space_53.2_MB_(88.74%)
 
 public class Solution {
-
     public int maximumScore(int[] nums, int[] mult) {
         int n = nums.length;
         int m = mult.length;
         int row = m;
         int[] dp = new int[m];
         int[] prev = new int[m + 1];
-
         while (--row >= 0) {
             for (int i = 0; i <= row; ++i) {
                 dp[i] =
@@ -20,7 +18,6 @@ public class Solution {
             }
             prev = dp;
         }
-
         return dp[0];
     }
 }
