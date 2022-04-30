5507e189bf9ddaadf0be1a4b30e74fb4a367d374
Valentyn Kolesnikov <javadev75@gmail.com>
2022-04-30 11:05:20 +0300

    Update Solution.java

--- src/main/java/g1701_1800/s1769_minimum_number_of_operations_to_move_all_balls_to_each_box/Solution.java
+++ src/main/java/g1701_1800/s1769_minimum_number_of_operations_to_move_all_balls_to_each_box/Solution.java
@@ -4,23 +4,19 @@ package g1701_1800.s1769_minimum_number_of_operations_to_move_all_balls_to_each_
 
 public class Solution {
     public int[] minOperations(String boxes) {
-
         int countFromLeft = 0;
         int countFromRight = 0;
         int moves = 0;
         int[] result = new int[boxes.length()];
-
         for (char c : boxes.toCharArray()) {
             moves += countFromLeft;
             if (c == '1') {
                 countFromLeft++;
             }
         }
-
         for (int i = boxes.length() - 1; i >= 0; i--) {
             char c = boxes.charAt(i);
             result[i] = moves;
-
             if (c == '1') {
                 countFromLeft--;
                 countFromRight++;
@@ -28,7 +24,6 @@ public class Solution {
             moves -= countFromLeft;
             moves += countFromRight;
         }
-
         return result;
     }
 }
