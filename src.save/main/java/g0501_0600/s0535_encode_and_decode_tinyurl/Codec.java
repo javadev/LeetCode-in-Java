2c973208f347dd757f13feab3836f12a91af8d38
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-01 18:19:59 +0300

    Improved task 535.

--- src.save/main/java/g0501_0600/s0535_encode_and_decode_tinyurl/Codec.java
+++ src.save/main/java/g0501_0600/s0535_encode_and_decode_tinyurl/Codec.java
@@ -1,24 +1,25 @@
 package g0501_0600.s0535_encode_and_decode_tinyurl;
 
 // #Medium #String #Hash_Table #Design #Hash_Function
-// #2022_03_20_Time_3_ms_(82.89%)_Space_43.4_MB_(53.75%)
+// #2022_05_01_Time_4_ms_(65.42%)_Space_43.4_MB_(58.94%)
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Codec {
-    private final Map<String, String> map = new HashMap<>();
-    private static final String DIGITS =
-            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private Map<String, String> map = new HashMap<>();
+    private int n = 0;
 
+    // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        long cur = 0;
-        StringBuilder shortUrl = new StringBuilder();
-        shortUrl.append(DIGITS.charAt((int) cur));
-        map.put(shortUrl.toString(), longUrl);
-        return shortUrl.toString();
+        n++;
+        String ans = "http://tinyurl.com/";
+        ans += Integer.toString(n);
+        map.put(ans, longUrl);
+        return ans;
     }
 
+    // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
         return map.get(shortUrl);
     }
