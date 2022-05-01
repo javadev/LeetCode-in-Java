2549d861113ce8278b0f196506bd7e9d63a3da55
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-01 04:29:02 +0300

    Improved task 1758.

--- src/main/java/g1701_1800/s1758_minimum_changes_to_make_alternating_binary_string/Solution.java
+++ src/main/java/g1701_1800/s1758_minimum_changes_to_make_alternating_binary_string/Solution.java
@@ -1,34 +1,20 @@
 package g1701_1800.s1758_minimum_changes_to_make_alternating_binary_string;
 
-// #Easy #String #2022_04_30_Time_10_ms_(9.93%)_Space_42.9_MB_(56.74%)
+// #Easy #String #2022_05_01_Time_2_ms_(98.92%)_Space_43.5_MB_(18.77%)
 
 public class Solution {
     public int minOperations(String s) {
-        int ops1 = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                if (s.charAt(i) != '0') {
-                    ops1++;
-                }
-            } else {
-                if (s.charAt(i) != '1') {
-                    ops1++;
-                }
-            }
-        }
+        return Math.min(countFlips(s, '0'), countFlips(s, '1'));
+    }
 
-        int ops2 = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (i % 2 == 0) {
-                if (s.charAt(i) != '1') {
-                    ops2++;
-                }
-            } else {
-                if (s.charAt(i) != '0') {
-                    ops2++;
-                }
+    private int countFlips(String s, char next) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (next != c) {
+                count++;
             }
+            next = (next == '0') ? '1' : '0';
         }
-        return Math.min(ops1, ops2);
+        return count;
     }
 }
