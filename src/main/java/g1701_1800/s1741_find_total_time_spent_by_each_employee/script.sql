2583d04b0f9d4123f98ebec094046327d78983bb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-03 02:41:20 +0300

    Added task 1741.

---
+++ src/main/java/g1701_1800/s1741_find_total_time_spent_by_each_employee/script.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# #Easy #Database #SQL_I_Day_8_Function #2022_05_03_Time_414_ms_(93.27%)_Space_0B_(100.00%)
+SELECT event_day AS day, emp_id, SUM(out_time) - SUM(in_time) AS total_time 
+FROM Employees
+GROUP BY event_day, emp_id
