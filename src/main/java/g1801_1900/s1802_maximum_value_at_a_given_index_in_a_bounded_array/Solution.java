5a5ab7e5b3c6ca83b7eea4cac0489c15b7c53633
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-02 17:52:50 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1802_maximum_value_at_a_given_index_in_a_bounded_array/Solution.java
+++ src/main/java/g1801_1900/s1802_maximum_value_at_a_given_index_in_a_bounded_array/Solution.java
@@ -7,17 +7,14 @@ public class Solution {
     private boolean isPossible(int n, int index, int maxSum, int value) {
         int leftValue = Math.max(value - index, 0);
         int rightValue = Math.max(value - ((n - 1) - index), 0);
-
         long sumBefore = (long) (value + leftValue) * (value - leftValue + 1) / 2;
         long sumAfter = (long) (value + rightValue) * (value - rightValue + 1) / 2;
-
         return sumBefore + sumAfter - value <= maxSum;
     }
 
     public int maxValue(int n, int index, int maxSum) {
         int left = 0;
         int right = maxSum - n;
-
         while (left < right) {
             int middle = (left + right + 1) / 2;
             if (isPossible(n, index, maxSum - n, middle)) {
