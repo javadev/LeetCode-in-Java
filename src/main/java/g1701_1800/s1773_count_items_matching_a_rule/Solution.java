ded1885f3169f09c314cf33d6f535967365f1166
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-02 11:48:15 +0300

    Improved task 1773.

--- src/main/java/g1701_1800/s1773_count_items_matching_a_rule/Solution.java
+++ src/main/java/g1701_1800/s1773_count_items_matching_a_rule/Solution.java
@@ -1,19 +1,23 @@
 package g1701_1800.s1773_count_items_matching_a_rule;
 
-// #Easy #Array #String #2022_04_30_Time_7_ms_(25.91%)_Space_56.9_MB_(35.49%)
+// #Easy #Array #String #2022_05_02_Time_3_ms_(98.33%)_Space_46.9_MB_(92.77%)
 
 import java.util.List;
 
 public class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int match = 0;
+        int ans = 0;
+        int checkRuleNum = 0;
+        if (ruleKey.equals("color")) {
+            checkRuleNum = 1;
+        } else if (ruleKey.equals("name")) {
+            checkRuleNum = 2;
+        }
         for (List<String> item : items) {
-            if ((ruleKey.equals("type") && item.get(0).equals(ruleValue))
-                    || (ruleKey.equals("color") && item.get(1).equals(ruleValue))
-                    || (ruleKey.equals("name") && item.get(2).equals(ruleValue))) {
-                match++;
+            if (item.get(checkRuleNum).equals(ruleValue)) {
+                ans++;
             }
         }
-        return match;
+        return ans;
     }
 }
