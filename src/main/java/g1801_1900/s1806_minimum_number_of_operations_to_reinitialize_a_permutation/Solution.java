3020d54544b598a9a7d2bc89b0535b24a25585fc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-02 17:55:46 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1806_minimum_number_of_operations_to_reinitialize_a_permutation/Solution.java
+++ src/main/java/g1801_1900/s1806_minimum_number_of_operations_to_reinitialize_a_permutation/Solution.java
@@ -3,7 +3,6 @@ package g1801_1900.s1806_minimum_number_of_operations_to_reinitialize_a_permutat
 // #Medium #Array #Math #Simulation #2022_05_02_Time_0_ms_(100.00%)_Space_38.8_MB_(98.28%)
 
 public class Solution {
-
     public int reinitializePermutation(int n) {
         final int factor = n - 1;
         if (factor < 2) {
