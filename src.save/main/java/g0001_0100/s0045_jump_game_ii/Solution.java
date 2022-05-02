a2e74c25ef5187be53de9e996eb85d8fa14826f7
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-02 14:16:17 +0300

    Update Solution.java

--- src.save/main/java/g0001_0100/s0045_jump_game_ii/Solution.java
+++ src.save/main/java/g0001_0100/s0045_jump_game_ii/Solution.java
@@ -9,23 +9,18 @@ public class Solution {
         int length = 0;
         int maxLength = 0;
         int minJump = 0;
-
         for (int i = 0; i < nums.length - 1; ++i) {
             length--;
             maxLength--;
-
             maxLength = Math.max(maxLength, nums[i]);
-
             if (length <= 0) {
                 length = maxLength;
                 minJump++;
             }
-
             if (length >= nums.length - i - 1) {
                 return minJump;
             }
         }
-
         return minJump;
     }
 }
