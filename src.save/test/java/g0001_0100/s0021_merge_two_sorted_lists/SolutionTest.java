8bd883d90499ccaac46c2e4019111aeaccbaaee6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-04 11:14:31 +0300

    Added tests 21, 23, 24, 25, 26.

--- src.save/test/java/g0001_0100/s0021_merge_two_sorted_lists/SolutionTest.java
+++ src.save/test/java/g0001_0100/s0021_merge_two_sorted_lists/SolutionTest.java
@@ -3,33 +3,23 @@ package g0001_0100.s0021_merge_two_sorted_lists;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com_github_leetcode.LinkedListUtils;
 import com_github_leetcode.ListNode;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void mergeTwoLists() {
-        ListNode head = new ListNode();
-        ListNode node1 = new ListNode();
-        node1.val = 1;
-        head.next = node1;
-        ListNode node2 = new ListNode();
-        node2.val = 2;
-        node1.next = node2;
-        ListNode node3 = new ListNode();
-        node3.val = 4;
-        node2.next = node3;
-        ListNode head2 = new ListNode();
-        ListNode node4 = new ListNode();
-        node4.val = 1;
-        head2.next = node4;
-        ListNode node5 = new ListNode();
-        node5.val = 3;
-        node4.next = node5;
-        ListNode node6 = new ListNode();
-        node6.val = 4;
-        node5.next = node6;
+        ListNode l1 = LinkedListUtils.createSinglyLinkedList(Arrays.asList(1, 2, 4));
+        ListNode l2 = LinkedListUtils.createSinglyLinkedList(Arrays.asList(1, 3, 4));
+        assertThat(new Solution().mergeTwoLists(l1, l2).toString(), equalTo("1, 1, 2, 3, 4, 4"));
+    }
+
+    @Test
+    void mergeTwoLists2() {
         assertThat(
-                new Solution().mergeTwoLists(node1, node4).toString(), equalTo("1, 1, 2, 3, 4, 4"));
+                new Solution().mergeTwoLists(new ListNode(), new ListNode()).toString(),
+                equalTo("0, 0"));
     }
 }
--- src.save/test/java/g0001_0100/s0025_reverse_nodes_in_k_group/SolutionTest.java
+++ src.save/test/java/g0001_0100/s0025_reverse_nodes_in_k_group/SolutionTest.java
@@ -3,28 +3,20 @@ package g0001_0100.s0025_reverse_nodes_in_k_group;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import com_github_leetcode.LinkedListUtils;
 import com_github_leetcode.ListNode;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void reverseKGroup() {
-        ListNode head = new ListNode();
-        ListNode node1 = new ListNode();
-        node1.val = 1;
-        head.next = node1;
-        ListNode node2 = new ListNode();
-        node2.val = 2;
-        node1.next = node2;
-        ListNode node3 = new ListNode();
-        node3.val = 3;
-        node2.next = node3;
-        ListNode node4 = new ListNode();
-        node4.val = 4;
-        node3.next = node4;
-        ListNode node5 = new ListNode();
-        node5.val = 5;
-        node4.next = node5;
-        assertThat(new Solution().reverseKGroup(node1, 2).toString(), equalTo("2, 1, 4, 3, 5"));
+        ListNode head = LinkedListUtils.contructLinkedList(new int[] {1, 2, 3, 4, 5});
+        assertThat(new Solution().reverseKGroup(head, 2).toString(), equalTo("2, 1, 4, 3, 5"));
+    }
+
+    @Test
+    void reverseKGroup2() {
+        ListNode head = LinkedListUtils.contructLinkedList(new int[] {1, 2, 3, 4, 5});
+        assertThat(new Solution().reverseKGroup(head, 3).toString(), equalTo("3, 2, 1, 4, 5"));
     }
 }
