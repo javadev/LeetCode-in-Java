722141b4aafd8ddae16cbef4f11011e1b05bcbee
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-04 17:30:02 +0300

    Fixed names.

--- src/main/java/g1801_1900/s1813_sentence_similarity_iii/Solution.java
+++ src/main/java/g1801_1900/s1813_sentence_similarity_iii/Solution.java
@@ -3,14 +3,14 @@ package g1801_1900.s1813_sentence_similarity_iii;
 // #Medium #Array #String #Two_Pointers #2022_05_04_Time_3_ms_(41.38%)_Space_42.8_MB_(10.34%)
 
 public class Solution {
-    public boolean areSentencesSimilar(String s1, String s2) {
-        String[] words1 = s1.split(" ");
-        String[] words2 = s2.split(" ");
+    public boolean areSentencesSimilar(String sentence1, String sentence2) {
+        String[] words1 = sentence1.split(" ");
+        String[] words2 = sentence2.split(" ");
         int i = 0;
         int n1 = words1.length;
         int n2 = words2.length;
         if (n1 > n2) {
-            return areSentencesSimilar(s2, s1);
+            return areSentencesSimilar(sentence2, sentence1);
         }
         while (i < n1 && words1[i].equals(words2[i])) {
             ++i;
