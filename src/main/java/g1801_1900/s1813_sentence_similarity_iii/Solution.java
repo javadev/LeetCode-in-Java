0a206f688575593207c0387b701d5187cd6b05fd
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-04 17:31:25 +0300

    Fixed sonar.

--- src/main/java/g1801_1900/s1813_sentence_similarity_iii/Solution.java
+++ src/main/java/g1801_1900/s1813_sentence_similarity_iii/Solution.java
@@ -2,6 +2,7 @@ package g1801_1900.s1813_sentence_similarity_iii;
 
 // #Medium #Array #String #Two_Pointers #2022_05_04_Time_3_ms_(41.38%)_Space_42.8_MB_(10.34%)
 
+@SuppressWarnings("java:S2234")
 public class Solution {
     public boolean areSentencesSimilar(String sentence1, String sentence2) {
         String[] words1 = sentence1.split(" ");
