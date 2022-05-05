7e6e09812a633a79623616309bec4d9621f45a64
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-05 15:11:12 +0300

    Added task 1141.

---
+++ src.save/main/java/g1101_1200/s1141_user_activity_for_the_past_30_days_i/script.sql
@@ -0,0 +1,6 @@
+﻿# Write your MySQL query statement below
+# #Easy #Database #SQL_I_Day_7_Function #2022_05_05_Time_459_ms_(54.44%)_Space_0B_(100.00%)
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+GROUP BY day
+HAVING day > (SELECT SUBDATE("2019-07-27", INTERVAL 30 DAY))
