c1e1aa829cc1c128f5791efbd163d249994bb664
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-05 15:23:36 +0300

    Improved task 1156.

--- src.save/main/java/g1101_1200/s1156_swap_for_longest_repeated_character_substring/Solution.java
+++ src.save/main/java/g1101_1200/s1156_swap_for_longest_repeated_character_substring/Solution.java
@@ -1,69 +1,58 @@
 package g1101_1200.s1156_swap_for_longest_repeated_character_substring;
 
-// #Medium #String #Sliding_Window #2022_03_05_Time_22_ms_(15.79%)_Space_44.2_MB_(15.04%)
+// #Medium #String #Sliding_Window #2022_05_05_Time_12_ms_(47.37%)_Space_44.1_MB_(22.11%)
 
 public class Solution {
+    private static class Pair {
+        public char character;
+        public int count;
+
+        public Pair(char c, int count) {
+            this.character = c;
+            this.count = count;
+        }
+    }
+
     public int maxRepOpt1(String text) {
-        int n = text.length();
-        int[] freqTxt = new int[26];
-        for (int i = 0; i < n; i++) {
-            freqTxt[text.charAt(i) - 'a']++;
-        }
-        int ans = 0;
-        int distinct = 0;
-        char cMin;
-        char cMax;
-        int[] window = new int[26];
-        int l = 0;
-        int r = 0;
-        while (r < n) {
-            char cur = text.charAt(r);
-            window[cur - 'a']++;
-            cMin = minFreq(window);
-            cMax = maxFreq(window);
-            if (window[cur - 'a'] == 1) {
-                distinct++;
+        List<Pair> pairs = new ArrayList<>();
+        Map<Character, Integer> map = new HashMap<>();
+        // collect counts for each char-block
+        for (int i = 0; i < text.length(); ) {
+            char c = text.charAt(i);
+            int count = 0;
+            while (i < text.length() && text.charAt(i) == c) {
+                count++;
+                i++;
             }
-            while (distinct > 2
-                    || (distinct == 2
-                            && (window[cMin - 'a'] > 1
-                                    || freqTxt[cMax - 'a'] - window[cMax - 'a'] == 0))) {
-                char lft = text.charAt(l);
-                window[lft - 'a']--;
-                if (window[lft - 'a'] == 0) {
-                    distinct--;
+            pairs.add(new Pair(c, count));
+            map.put(c, map.getOrDefault(c, 0) + count);
+        }
+        int max = 0;
+        // case 1, swap 1 item to the boundary of a consecutive cha-block to achieve possible max
+        // length
+        // we need total count to make sure whether a swap is possible!
+        for (Pair p : pairs) {
+            int totalCount = map.get(p.character);
+            if (totalCount > p.count) {
+                max = Math.max(max, p.count + 1);
+            } else {
+                max = Math.max(max, p.count);
+            }
+        }
+        // case 2, find xxxxYxxxxx pattern
+        // we need total count to make sure whether a swap is possible!
+        for (int i = 1; i < pairs.size() - 1; i++) {
+            if (pairs.get(i - 1).character == pairs.get(i + 1).character
+                    && pairs.get(i).count == 1) {
+                int totalCount = map.get(pairs.get(i - 1).character);
+                int groupSum = pairs.get(i - 1).count + pairs.get(i + 1).count;
+                if (totalCount > groupSum) {
+                    max = Math.max(max, groupSum + 1);
+                } else {
+                    max = Math.max(max, groupSum);
                 }
-                l++;
-                cMax = maxFreq(window);
-                cMin = minFreq(window);
-            }
-            ans = Math.max(ans, r - l + 1);
-            r++;
-        }
-        return ans;
-    }
-
-    private char minFreq(int[] cnt) {
-        int min = Integer.MAX_VALUE;
-        char ans = '#';
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (cnt[c - 'a'] != 0 && cnt[c - 'a'] < min) {
-                min = cnt[c - 'a'];
-                ans = c;
             }
         }
-        return ans;
-    }
-
-    private char maxFreq(int[] cnt) {
-        int max = Integer.MIN_VALUE;
-        char ans = '#';
-        for (char c = 'a'; c <= 'z'; c++) {
-            if (cnt[c - 'a'] != 0 && cnt[c - 'a'] > max) {
-                max = cnt[c - 'a'];
-                ans = c;
-            }
-        }
-        return ans;
+        return max;
     }
 }
