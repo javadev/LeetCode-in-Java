a1e82b3b753ebcf77d0f9a16d2a3dafcd1fef522
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-05 14:52:10 +0300

    Improved task 1140.

--- src.save/main/java/g1101_1200/s1140_stone_game_ii/Solution.java
+++ src.save/main/java/g1101_1200/s1140_stone_game_ii/Solution.java
@@ -1,37 +1,39 @@
 package g1101_1200.s1140_stone_game_ii;
 
 // #Medium #Array #Dynamic_Programming #Math #Game_Theory
-// #2022_03_05_Time_7_ms_(68.62%)_Space_42.3_MB_(48.99%)
+// #2022_05_05_Time_12_ms_(44.22%)_Space_46.7_MB_(21.97%)
 
 import java.util.Arrays;
 
 public class Solution {
-    private int solve(int i, int m, int[] piles, int[][] dp) {
-        if (i >= piles.length) {
-            return 0;
+    private int[][] dp = new int[105][105];
+
+    private int help(int i, int m, int[] p) {
+        if (i >= p.length) {
+            return dp[i][m] = 0;
         }
         if (dp[i][m] != -1) {
             return dp[i][m];
         }
-        int sum = 0;
         int ans = Integer.MIN_VALUE;
-        for (int j = 0; j < 2 * m && i + j < piles.length; j++) {
-            sum += piles[i + j];
-            ans = Math.max(ans, sum - solve(i + j + 1, Math.max(j + 1, m), piles, dp));
+        int total = 0;
+        for (int j = 0; j < 2 * m; j++) {
+            if (i + j < p.length) {
+                total += p[i + j];
+                ans = Math.max(ans, total - help(i + j + 1, Math.max(m, j + 1), p));
+            }
         }
-        return ans;
+        return dp[i][m] = ans;
     }
 
-    public int stoneGameII(int[] piles) {
+    public int stoneGameII(int[] p) {
         int sum = 0;
-        int[][] dp = new int[piles.length][piles.length + 1];
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        for (int[] arr1 : dp) {
+            Arrays.fill(arr1, -1);
         }
-        for (int ele : piles) {
-            sum += ele;
+        for (int z : p) {
+            sum += z;
         }
-        int diff = solve(0, 1, piles, dp);
-        return (sum + diff) / 2;
+        return (sum + help(0, 1, p)) / 2;
     }
 }
