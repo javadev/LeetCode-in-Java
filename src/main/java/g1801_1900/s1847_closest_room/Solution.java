f451c184a00d06e3c3f6d2462b66a4f9c2c0569b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-07 20:20:38 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1847_closest_room/Solution.java
+++ src/main/java/g1801_1900/s1847_closest_room/Solution.java
@@ -9,48 +9,33 @@ public class Solution {
     public int[] closestRoom(int[][] rooms, int[][] queries) {
         int numRoom = rooms.length;
         int numQuery = queries.length;
-
         for (int i = 0; i < numQuery; i++) {
             queries[i] = new int[] {queries[i][0], queries[i][1], i};
         }
-
         Arrays.sort(rooms, (a, b) -> (a[1] != b[1] ? (a[1] - b[1]) : (a[0] - b[0])));
         Arrays.sort(queries, (a, b) -> (a[1] != b[1] ? (a[1] - b[1]) : (a[0] - b[0])));
-
         TreeSet<Integer> roomIds = new TreeSet<>();
         int[] result = new int[numQuery];
-
         int j = numRoom - 1;
         for (int i = numQuery - 1; i >= 0; i--) {
-
             int currRoomId = queries[i][0];
             int currRoomSize = queries[i][1];
             int currQueryIndex = queries[i][2];
-
             while (j >= 0 && rooms[j][1] >= currRoomSize) {
                 roomIds.add(rooms[j--][0]);
             }
-
             if (roomIds.contains(currRoomId)) {
                 result[currQueryIndex] = currRoomId;
                 continue;
             }
-
             Integer nextRoomId = roomIds.higher(currRoomId);
-
             Integer prevRoomId = roomIds.lower(currRoomId);
-
             if (nextRoomId == null && prevRoomId == null) {
                 result[currQueryIndex] = -1;
-
             } else if (nextRoomId == null) {
-
                 result[currQueryIndex] = prevRoomId;
-
             } else if (prevRoomId == null) {
-
                 result[currQueryIndex] = nextRoomId;
-
             } else {
                 if ((currRoomId - prevRoomId) <= (nextRoomId - currRoomId)) {
                     result[currQueryIndex] = prevRoomId;
@@ -59,7 +44,6 @@ public class Solution {
                 }
             }
         }
-
         return result;
     }
 }
