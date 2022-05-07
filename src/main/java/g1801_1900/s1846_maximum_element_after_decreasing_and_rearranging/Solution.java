a1a2d67c81998d50d873415e08d47c9a3ad8c7a9
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-07 20:19:00 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1846_maximum_element_after_decreasing_and_rearranging/Solution.java
+++ src/main/java/g1801_1900/s1846_maximum_element_after_decreasing_and_rearranging/Solution.java
@@ -8,12 +8,10 @@ public class Solution {
         for (int j : arr) {
             count[Math.min(j, arr.length)]++;
         }
-
         int ans = 1;
         for (int i = 1; i < count.length; i++) {
             ans = Math.min(i, ans + count[i]);
         }
-
         return ans;
     }
 }
