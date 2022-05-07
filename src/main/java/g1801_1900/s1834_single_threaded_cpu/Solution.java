09551456d5daacb9079c830efdb84fb0c88496c1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-07 09:47:26 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1834_single_threaded_cpu/Solution.java
+++ src/main/java/g1801_1900/s1834_single_threaded_cpu/Solution.java
@@ -10,13 +10,10 @@ import java.util.PriorityQueue;
 public class Solution {
     public int[] getOrder(int[][] tasks1) {
         int n = tasks1.length;
-
         int[][] tasks = new int[n][3];
-
         for (int i = 0; i < n; i++) {
             tasks[i] = new int[] {tasks1[i][0], tasks1[i][1], i};
         }
-
         Arrays.sort(tasks, Comparator.comparingInt(a -> a[0]));
         PriorityQueue<int[]> minHeap =
                 new PriorityQueue<>(
@@ -27,18 +24,14 @@ public class Solution {
                                 return a[1] - b[1];
                             }
                         });
-
         int time = tasks[0][0];
-
         int[] taskOrderResult = new int[n];
-
         int i = 0;
         int index = 0;
         while (!minHeap.isEmpty() || i < n) {
             while (i < n && time >= tasks[i][0]) {
                 minHeap.add(tasks[i++]);
             }
-
             if (!minHeap.isEmpty()) {
                 int[] task = minHeap.remove();
                 taskOrderResult[index++] = task[2];
@@ -47,7 +40,6 @@ public class Solution {
                 time = tasks[i][0];
             }
         }
-
         return taskOrderResult;
     }
 }
