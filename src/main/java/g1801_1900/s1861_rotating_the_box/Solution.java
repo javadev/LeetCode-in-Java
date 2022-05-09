141f02e33d2dcf86ca4bfee863f075a3be467dcc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-09 05:58:02 +0300

    Fixed style.

--- src/main/java/g1801_1900/s1861_rotating_the_box/Solution.java
+++ src/main/java/g1801_1900/s1861_rotating_the_box/Solution.java
@@ -7,13 +7,13 @@ import java.util.Arrays;
 public class Solution {
     public char[][] rotateTheBox(char[][] box) {
         char[][] result = new char[box[0].length][box.length];
-        for(char[] row : result) {
+        for (char[] row : result) {
             Arrays.fill(row, '#');
         }
-        for(int i = 0; i < box.length; i++) {
+        for (int i = 0; i < box.length; i++) {
             int col = box.length - 1 - i;
             int stones = 0;
-            for(int j = 0; j < box[0].length; j++) {
+            for (int j = 0; j < box[0].length; j++) {
                 if (box[i][j] == '#') {
                     stones++;
                 } else if (box[i][j] == '*') {
