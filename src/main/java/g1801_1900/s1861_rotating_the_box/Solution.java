6fa6b1de2e17c8efe23638d96be42456c0f31d41
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-09 05:51:16 +0300

    Improved task 1861.

--- src/main/java/g1801_1900/s1861_rotating_the_box/Solution.java
+++ src/main/java/g1801_1900/s1861_rotating_the_box/Solution.java
@@ -1,35 +1,29 @@
 package g1801_1900.s1861_rotating_the_box;
 
-// #Medium #Array #Matrix #Two_Pointers #2022_05_08_Time_16_ms_(30.81%)_Space_77.6_MB_(95.11%)
+// #Medium #Array #Matrix #Two_Pointers #2022_05_09_Time_11_ms_(63.70%)_Space_141_MB_(55.18%)
 
 public class Solution {
     public char[][] rotateTheBox(char[][] box) {
         int m = box.length;
         int n = box[0].length;
-        for (int i = 0; i < m; i++) {
+        char[][] result = new char[n][m];
+        for (int i = m - 1; i >= 0; i--) {
+            int free = -1;
             for (int j = n - 1; j >= 0; j--) {
-                if (box[i][j] == '#') {
-                    int empty = j + 1;
-                    while (empty < n && box[i][empty] == '.') {
-                        empty++;
-                    }
-                    if (empty < n && box[i][empty] == '.') {
-                        box[i][empty] = '#';
-                        box[i][j] = '.';
-                    } else if (empty - 1 < n && box[i][empty - 1] == '.') {
-                        box[i][empty - 1] = '#';
-                        box[i][j] = '.';
-                    }
+                if (box[i][j] == '.' && free == -1) {
+                    free = j;
+                } else if (box[i][j] == '*') {
+                    free = -1;
+                } else if (box[i][j] == '#' && free != -1) {
+                    box[i][j] = '.';
+                    box[i][free] = '#';
+                    free = free - 1;
                 }
             }
-        }
-        char[][] result = new char[n][m];
-        int k = m - 1;
-        for (int i = 0; i < m; i++) {
+            // this part can be further optimized by doing this in above for-loop
             for (int j = 0; j < n; j++) {
-                result[j][k] = box[i][j];
+                result[j][m - 1 - i] = box[i][j];
             }
-            k--;
         }
         return result;
     }
