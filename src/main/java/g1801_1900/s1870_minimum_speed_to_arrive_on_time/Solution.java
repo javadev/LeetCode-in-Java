939914a1c145c6c2c7723b13be6751f552996fc5
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-10 16:33:43 +0300

    Fixed sonar.

--- src/main/java/g1801_1900/s1870_minimum_speed_to_arrive_on_time/Solution.java
+++ src/main/java/g1801_1900/s1870_minimum_speed_to_arrive_on_time/Solution.java
@@ -3,17 +3,18 @@ package g1801_1900.s1870_minimum_speed_to_arrive_on_time;
 // #Medium #Array #Binary_Search #Binary_Search_II_Day_6
 // #2022_05_10_Time_86_ms_(88.58%)_Space_100.1_MB_(51.38%)
 
+@SuppressWarnings("java:S2184")
 public class Solution {
     public int minSpeedOnTime(int[] dist, double hour) {
         int n = dist.length;
         return fmin(dist, n, hour);
     }
 
-    private boolean p(int[] dist, int n, double h, int spe) {
+    private boolean check(int[] dist, int n, double h, int spe) {
         double cost = 0;
         for (int i = 0; i < n - 1; i++) {
             // same as ceil(doubleTime/doubleSpeed)
-            cost += (int) ((dist[i] - 1) / spe) + 1;
+            cost += (dist[i] - 1) / spe + 1;
         }
         cost += (double) dist[n - 1] / (double) spe;
         return cost <= h;
@@ -29,7 +30,7 @@ public class Solution {
         while (lo < hi) {
             int mid = (lo + hi) / 2;
             // speed of mid is possible, move to left side
-            if (p(dist, n, h, mid)) {
+            if (check(dist, n, h, mid)) {
                 hi = mid;
             } else {
                 // need higher speed, move to right side
