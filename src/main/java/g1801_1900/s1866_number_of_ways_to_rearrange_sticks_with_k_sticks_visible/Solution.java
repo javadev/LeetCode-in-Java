4e08b797d3efeffdedfb89e2338af3771a2b2e21
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-10 19:28:40 +0300

    Update Solution.java

--- src/main/java/g1801_1900/s1866_number_of_ways_to_rearrange_sticks_with_k_sticks_visible/Solution.java
+++ src/main/java/g1801_1900/s1866_number_of_ways_to_rearrange_sticks_with_k_sticks_visible/Solution.java
@@ -15,19 +15,15 @@ public class Solution {
         if (k == n) {
             return 1;
         }
-
         long[] dp = new long[k + 1];
         Arrays.fill(dp, 1);
-
         for (int i = 1; i + k <= n; i++) {
             long[] dp2 = new long[k + 1];
             for (int j = 1; j <= k; j++) {
                 dp2[j] = (dp2[j - 1] + (i + j - 1) * dp[j]) % MOD;
             }
-
             dp = dp2;
         }
-
         return (int) dp[k];
     }
 }
