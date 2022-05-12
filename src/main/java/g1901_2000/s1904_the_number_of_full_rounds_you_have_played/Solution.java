eaf3dad2930ea997ccc066c8d1e386bf5f01ca8f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-12 15:10:38 +0300

    Fixed style

--- src/main/java/g1901_2000/s1904_the_number_of_full_rounds_you_have_played/Solution.java
+++ src/main/java/g1901_2000/s1904_the_number_of_full_rounds_you_have_played/Solution.java
@@ -3,9 +3,9 @@ package g1901_2000.s1904_the_number_of_full_rounds_you_have_played;
 // #Medium #String #Math #2022_05_12_Time_0_ms_(100.00%)_Space_40_MB_(96.23%)
 
 public class Solution {
-    private final int MID_NIGHT_END = 1440;
-    private final int MID_NIGHT_START = 0;
-    private final int ROUND_INTERVAL = 15;
+    private static final int MID_NIGHT_END = 1440;
+    private static final int MID_NIGHT_START = 0;
+    private static final int ROUND_INTERVAL = 15;
 
     public int numberOfRounds(String loginTime, String logoutTime) {
         int loginSerializeTime = serializeTime(loginTime);
