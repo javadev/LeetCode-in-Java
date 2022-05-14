70af54f1ee5d43feadbe3921de67e1086dba4120
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-14 09:10:51 +0300

    Improved task 1911.

--- src/main/java/g1901_2000/s1911_maximum_alternating_subsequence_sum/Solution.java
+++ src/main/java/g1901_2000/s1911_maximum_alternating_subsequence_sum/Solution.java
@@ -1,6 +1,6 @@
 package g1901_2000.s1911_maximum_alternating_subsequence_sum;
 
-// #Medium #Array #Dynamic_Programming #2022_05_14_Time_13_ms_(49.60%)_Space_104.2_MB_(47.17%)
+// #Medium #Array #Dynamic_Programming #2022_05_14_Time_12_ms_(51.75%)_Space_97.4_MB_(59.03%)
 
 public class Solution {
     public long maxAlternatingSum(int[] nums) {
@@ -11,7 +11,6 @@ public class Solution {
             even = Math.max(even, Math.max(odd + nums[i], nums[i]));
             odd = Math.max(odd, Math.max(even - nums[i], 0));
         }
-
         return Math.max(even, odd);
     }
 }
