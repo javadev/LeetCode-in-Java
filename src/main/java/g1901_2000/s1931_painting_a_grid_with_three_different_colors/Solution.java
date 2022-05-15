1d9e8de70c782b3fb059caa2cf6f4ec4c5730852
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-05-15 21:22:17 +0700

    Update Solution.java

--- src/main/java/g1901_2000/s1931_painting_a_grid_with_three_different_colors/Solution.java
+++ src/main/java/g1901_2000/s1931_painting_a_grid_with_three_different_colors/Solution.java
@@ -4,13 +4,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-    public static final int p = 1_000_000_007;
+    public static final int P = 1_000_000_007;
 
     public int colorTheGrid(int m, int n) {
-        if (m == 1) return (int) (3L * powMod(2, n - 1) % p);
-        if (m == 2) return (int) (6L * powMod(3, n - 1) % p);
-        if (n == 1) return (int) (3L * powMod(2, m - 1) % p);
-        if (n == 2) return (int) (6L * powMod(3, m - 1) % p);
+        if (m == 1) {
+            return (int) (3L * powMod(2, n - 1) % P);
+        }
+        if (m == 2) {
+            return (int) (6L * powMod(3, n - 1) % P);
+        }
+        if (n == 1) {
+            return (int) (3L * powMod(2, m - 1) % P);
+        }
+        if (n == 2) {
+            return (int) (6L * powMod(3, m - 1) % P);
+        }
 
         int totalTemplates = 1 << (m - 2);
         int totalPaintings = binPow(3, m);
@@ -57,7 +65,7 @@ public class Solution {
             }
         }
 
-        long[][] matrixPower = matrixPower(templateEdgeCount, n - 1);
+        long[][] matrixPower = matrixPower(templateEdgeCount, (long) n - 1);
         long ans = 0;
         for (int i = 0; i < totalTemplates; i++) {
             long s = 0;
@@ -67,7 +75,7 @@ public class Solution {
             }
             ans += paintingCountForTemplate[i] * s;
         }
-        return (int) (ans % p);
+        return (int) (ans % P);
     }
 
     private static boolean checkAllowance(int a, int b, int m) {
@@ -108,14 +116,15 @@ public class Solution {
 
     private static int powMod(int a, int b) {
         long res = 1;
-        while (b != 0)
+        while (b != 0) {
             if ((b & 1) != 0) {
-                res = (res * a) % p;
+                res = (res * a) % P;
                 --b;
             } else {
-                a = (int) (((long) a * a) % p);
+                a = (int) (((long) a * a) % P);
                 b >>= 1;
             }
+        }
         return (int) res;
     }
 
@@ -158,7 +167,7 @@ public class Solution {
                 for (int k = 0; k < n; k++) {
                     ans[i][j] += a[i][k] * b[k][j];
                 }
-                ans[i][j] %= p;
+                ans[i][j] %= P;
             }
         }
         return ans;
