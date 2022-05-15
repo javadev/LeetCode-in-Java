73d99f9986a381a64488f45d645cd52eeeb97a75
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-15 14:39:34 +0300

    Improved sql scripts.

--- src.save/main/java/g1501_1600/s1527_patients_with_a_condition/script.sql
+++ src.save/main/java/g1501_1600/s1527_patients_with_a_condition/script.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-# #Easy #Database #2022_04_12_Time_308_ms_(66.93%)_Space_0B_(100.00%)
+# #Easy #Database #SQL_I_Day_3_String_Processing_Functions
+# #2022_04_12_Time_308_ms_(66.93%)_Space_0B_(100.00%)
 Select patient_id,patient_name,conditions from Patients
 where conditions like "% DIAB1%" or conditions like "DIAB1%";
--- src.save/main/java/g1501_1600/s1587_bank_account_summary_ii/script.sql
+++ src.save/main/java/g1501_1600/s1587_bank_account_summary_ii/script.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-# #Easy #Database #2022_04_11_Time_630_ms_(60.32%)_Space_0B_(100.00%)
+# #Easy #Database #SQL_I_Day_10_Where #2022_04_11_Time_630_ms_(60.32%)_Space_0B_(100.00%)
 select u.name, sum(t.amount) as balance from Users as u join Transactions as t on u.account = t.account group by u.name having balance > 10000
