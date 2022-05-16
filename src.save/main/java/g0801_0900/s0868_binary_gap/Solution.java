736d186ac519ab2c75b5e55b92ff57d91b4140a3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-16 11:42:07 +0300

    Improved task 868.

--- src.save/main/java/g0801_0900/s0868_binary_gap/Solution.java
+++ src.save/main/java/g0801_0900/s0868_binary_gap/Solution.java
@@ -1,23 +1,22 @@
 package g0801_0900.s0868_binary_gap;
 
-// #Easy #Math #Bit_Manipulation #2022_03_27_Time_3_ms_(9.45%)_Space_41.4_MB_(24.80%)
-
-import java.util.ArrayList;
-import java.util.List;
+// #Easy #Math #Bit_Manipulation #2022_05_16_Time_1_ms_(82.94%)_Space_41.4_MB_(28.50%)
 
 public class Solution {
     public int binaryGap(int n) {
-        String bin = Integer.toBinaryString(n);
-        List<Integer> oneIndexes = new ArrayList<>();
-        for (int i = 0; i < bin.length(); i++) {
-            if (bin.charAt(i) == '1') {
-                oneIndexes.add(i);
+        int max = 0;
+        int pos = 0;
+        int lastPos = -1;
+        while (n != 0) {
+            pos++;
+            if ((n & 1) == 1) {
+                if (lastPos != -1) {
+                    max = Math.max(max, pos - lastPos);
+                }
+                lastPos = pos;
             }
+            n >>= 1;
         }
-        int maxGap = 0;
-        for (int i = 0; i < oneIndexes.size() - 1; i++) {
-            maxGap = Math.max(oneIndexes.get(i + 1) - oneIndexes.get(i), maxGap);
-        }
-        return maxGap;
+        return max;
     }
 }
