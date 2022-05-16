8aafb8fe5d9d35cc92082ef3dac64c6ad5fc6287
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-16 20:09:30 +0300

    Improved task 1996.

--- src/main/java/g1901_2000/s1996_the_number_of_weak_characters_in_the_game/Solution.java
+++ src/main/java/g1901_2000/s1996_the_number_of_weak_characters_in_the_game/Solution.java
@@ -1,36 +1,20 @@
 package g1901_2000.s1996_the_number_of_weak_characters_in_the_game;
 
 // #Medium #Array #Sorting #Greedy #Stack #Monotonic_Stack
-// #2022_05_16_Time_113_ms_(90.91%)_Space_89.7_MB_(76.18%)
+// #2022_05_16_Time_121_ms_(81.46%)_Space_89.3_MB_(79.64%)
 
 import java.util.Arrays;
 
 public class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
-        // sorting according to decreasing attacking power
-        Arrays.sort(properties, (a, b) -> b[0] - a[0]);
-        int max = 0;
-        int temp;
-        int n = properties.length;
+        Arrays.sort(properties, (a, b) -> (a[0] == b[0]) ? b[1] - a[1] : a[0] - b[0]);
+        int max = properties[properties.length - 1][1];
         int count = 0;
-        int i = 0;
-        while (i < n) {
-            temp = properties[i][1];
+        for (int i = properties.length - 2; i >= 0; i--) {
             if (properties[i][1] < max) {
                 count++;
             }
-            // If many characters are having same attack then max defence seen wont be updated till
-            // we complete all characters with same attack
-            while (i < n - 1 && properties[i + 1][0] == properties[i][0]) {
-                i++;
-                if (properties[i][1] < max) {
-                    count++;
-                }
-                temp = Math.max(temp, properties[i][1]);
-            }
-            // updating max defence seen for characters with lesser attack power
-            max = Math.max(max, temp);
-            i++;
+            max = Math.max(max, properties[i][1]);
         }
         return count;
     }
