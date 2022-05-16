4255a4da52d7625cc62c1cda4e01d15b28722dff
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-16 20:22:39 +0300

    Improved task 1997.

--- src/main/java/g1901_2000/s1997_first_day_where_you_have_been_in_all_the_rooms/Solution.java
+++ src/main/java/g1901_2000/s1997_first_day_where_you_have_been_in_all_the_rooms/Solution.java
@@ -1,30 +1,15 @@
 package g1901_2000.s1997_first_day_where_you_have_been_in_all_the_rooms;
 
-// #Medium #Array #Dynamic_Programming #2022_05_16_Time_12_ms_(92.96%)_Space_98.5_MB_(21.13%)
+// #Medium #Array #Dynamic_Programming #2022_05_16_Time_14_ms_(83.10%)_Space_98.2_MB_(22.53%)
 
 public class Solution {
-    private static final int MOD = 1_000_000_007;
-
     public int firstDayBeenInAllRooms(int[] nextVisit) {
-        int n = nextVisit.length;
-        int[] ps = new int[n];
-        ps[0] = 2;
-        for (int i = 1; i < n - 1; i++) {
-            int j = nextVisit[i];
-            // if I arrive to room i with an odd number of visits, how many days will it take me to
-            // move to i+1
-            int days = 2 + (ps[i - 1] - (j == 0 ? 0 : ps[j - 1]));
-            if (days < 0) {
-                days += MOD;
-            }
-            if (days >= MOD) {
-                days -= MOD;
-            }
-            ps[i] = ps[i - 1] + days;
-            if (ps[i] >= MOD) {
-                ps[i] -= MOD;
-            }
+        int[] dp = new int[nextVisit.length];
+        int m = 1000000007;
+        for (int i = 1; i < dp.length; i++) {
+            int steps = 2 * dp[i - 1] - dp[nextVisit[i - 1]] + 2;
+            dp[i] = steps < 0 ? (steps + m) % m : steps % m;
         }
-        return ps[n - 2];
+        return dp[dp.length - 1];
     }
 }
