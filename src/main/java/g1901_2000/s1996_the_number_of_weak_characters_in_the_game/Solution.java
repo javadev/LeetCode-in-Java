35252d050970d18042acc4f5f9792d78b884b7b3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-16 20:15:04 +0300

    Fixed style.

--- src/main/java/g1901_2000/s1996_the_number_of_weak_characters_in_the_game/Solution.java
+++ src/main/java/g1901_2000/s1996_the_number_of_weak_characters_in_the_game/Solution.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 
 public class Solution {
     public int numberOfWeakCharacters(int[][] properties) {
-        Arrays.sort(properties, (a, b) -> (a[0] == b[0]) ? b[1] - a[1] : a[0] - b[0]);
+        Arrays.sort(properties, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
         int max = properties[properties.length - 1][1];
         int count = 0;
         for (int i = properties.length - 2; i >= 0; i--) {
