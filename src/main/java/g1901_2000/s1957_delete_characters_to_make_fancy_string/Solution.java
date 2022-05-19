4ec70deea03bbcb7e9a42298c7c9b0dcdb2ac41f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-19 09:15:20 +0300

    Improved task 1957.

--- src/main/java/g1901_2000/s1957_delete_characters_to_make_fancy_string/Solution.java
+++ src/main/java/g1901_2000/s1957_delete_characters_to_make_fancy_string/Solution.java
@@ -1,22 +1,21 @@
 package g1901_2000.s1957_delete_characters_to_make_fancy_string;
 
-// #Easy #String #2022_05_18_Time_54_ms_(52.94%)_Space_67.8_MB_(72.35%)
+// #Easy #String #2022_05_19_Time_34_ms_(93.79%)_Space_42.9_MB_(97.04%)
 
 public class Solution {
     public String makeFancyString(String s) {
-        StringBuilder ans = new StringBuilder();
-        int c = 1;
-        ans.append(s.charAt(0));
-        for (int i = 1; i < s.length(); i++) {
-            if (s.charAt(i) == s.charAt(i - 1)) {
-                c++;
-            } else {
-                c = 1;
-            }
-            if (c < 3) {
-                ans.append(s.charAt(i));
+        char[] ar = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ar.length - 2; i++) {
+            if ((ar[i] != ar[i + 1]) || (ar[i + 1] != ar[i + 2])) {
+                sb.append(ar[i]);
             }
         }
-        return ans.toString();
+        if (ar.length >= 2) {
+            sb.append(ar[ar.length - 2]).append(ar[ar.length - 1]);
+        } else if (ar.length == 1) {
+            sb.append(ar[ar.length - 1]);
+        }
+        return sb.toString();
     }
 }
