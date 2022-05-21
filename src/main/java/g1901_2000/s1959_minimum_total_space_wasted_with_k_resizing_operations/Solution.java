1366e2905d2d4d3c10f09bd14699d766cdd25efa
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-21 08:53:14 +0300

    Update Solution.java

--- src/main/java/g1901_2000/s1959_minimum_total_space_wasted_with_k_resizing_operations/Solution.java
+++ src/main/java/g1901_2000/s1959_minimum_total_space_wasted_with_k_resizing_operations/Solution.java
@@ -2,6 +2,7 @@ package g1901_2000.s1959_minimum_total_space_wasted_with_k_resizing_operations;
 
 // #Medium #Array #Dynamic_Programming #2022_05_20_Time_42_ms_(95.45%)_Space_43_MB_(66.67%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int minSpaceWastedKResizing(int[] arr, int k) {
         int n = arr.length;
