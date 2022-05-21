b6eb3db38e30914b7fc2584c5796d279bf33001d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-21 08:48:10 +0300

    Update Solution.java

--- src/main/java/g1901_2000/s1958_check_if_move_is_legal/Solution.java
+++ src/main/java/g1901_2000/s1958_check_if_move_is_legal/Solution.java
@@ -2,6 +2,7 @@ package g1901_2000.s1958_check_if_move_is_legal;
 
 // #Medium #Array #Matrix #Enumeration #2022_05_20_Time_0_ms_(100.00%)_Space_43.9_MB_(11.63%)
 
+@SuppressWarnings("java:S3358")
 public class Solution {
 
     private static final int[][] DIRS =
