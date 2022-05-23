c5141d89de9c5b898b24788f153276154f3d5652
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-23 09:35:35 +0530

    Added Task 2090

---
+++ src/main/java/g2001_2100/s2090_k_radius_subarray_averages/Solution.java
@@ -0,0 +1,30 @@
+package g2001_2100.s2090_k_radius_subarray_averages;
+
+// #Array #Sliding Window
+// #2022_05_20_Time_12ms_(83.19%)_Space_59.9_MB_(93.58%)
+
+public class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        if (k >= nums.length) {
+            int[] ans = new int[nums.length];
+            Arrays.fill(ans,-1);
+            return ans;
+        }
+        long[] prefix = new long[nums.length];
+        int[] ans = new int[nums.length];
+        prefix[0] = nums[0];
+        for (int i=1;i<nums.length;i++)
+            prefix[i] = nums[i] + prefix[i-1];
+        for (int i=0;i<k;i++)
+            ans[i] = -1;
+        int n = 2*k+1;
+        for (int i=k;i<nums.length-k;i++) {
+            long left_sum = prefix[i] - nums[i] - prefix[i-k] + nums[i-k];
+            long right_sum = prefix[i+k] - prefix[i];
+            ans[i] = (int)((right_sum + left_sum + nums[i])/n);
+        }
+        for (int i=nums.length-k;i<nums.length;i++)
+            ans[i] = -1;
+        return ans;
+    }
+}
