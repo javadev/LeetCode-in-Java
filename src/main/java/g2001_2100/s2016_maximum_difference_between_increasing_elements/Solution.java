4dfaad16e54af4ad554d477c1d3794080b0b43cd
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-23 10:54:15 +0530

    commit

--- src/main/java/g2001_2100/s2016_maximum_difference_between_increasing_elements/Solution.java
+++ src/main/java/g2001_2100/s2016_maximum_difference_between_increasing_elements/Solution.java
@@ -3,21 +3,6 @@ package g2001_2100.s2016_maximum_difference_between_increasing_elements;
 // #Array
 // #2022_05_23_Time_0ms_(100%)_Space_42.9_MB_(20.20%)
 
-<<<<<<< HEAD
-import java.util.*;
-
-public class Solution {
-    public int maximumDifference(int[] nums) {
-        int mini = nums[0]; // we keep the track of minimum element while traversing to maximise
-        // nums[j] - nums[i]
-        // if arr[i] is minimum arr[j] - arr[i] will be maximum
-        int ans = -1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] < mini) mini = nums[i];
-            if (nums[i + 1] - mini > ans) ans = nums[i + 1] - mini;
-        }
-        if (ans <= 0) return -1; // if ans is not increased we simply return -1
-=======
 public class Solution {
     public int maximumDifference(int[] nums) {
         int mini = nums[0];
@@ -33,7 +18,6 @@ public class Solution {
         if (ans <= 0) {
             return -1;
         }
->>>>>>> f9fa3338840c8d2b3211936da3db24212b7cddec
         return ans;
     }
 }
