40ce7f67f4ff676957423fc12fdda6a7150d8464
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-23 16:33:25 +0530

    Added task 2095.

---
+++ src/main/java/g2001_2100/s2095_delete_the_middle_node_of_a_linked_list/Solution.java
@@ -0,0 +1,31 @@
+package g2001_2100.s2095_delete_the_middle_node_of_a_linked_list;
+
+// #Medium #Linked List #Two Pointers
+// 2022_05_23_Time_4_ms_(95.33%)_Space_220.1_MB_(38.17%)
+
+import com_github_leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+public class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
