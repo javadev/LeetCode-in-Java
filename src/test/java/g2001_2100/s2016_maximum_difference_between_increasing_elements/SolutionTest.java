6d19e1104e43918160addfa0fb09f11307934ad4
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-23 10:08:25 +0530

    files format fixed

--- src/test/java/g2001_2100/s2016_maximum_difference_between_increasing_elements/SolutionTest.java
+++ src/test/java/g2001_2100/s2016_maximum_difference_between_increasing_elements/SolutionTest.java
@@ -8,28 +8,16 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     @Test
     void maximumDifference() {
-        assertThat(
-            new Solution()
-                .maximumDifference(
-                    new int[] {7,1,5,4}),
-            equalTo(4));
+        assertThat(new Solution().maximumDifference(new int[] {7, 1, 5, 4}), equalTo(4));
     }
 
     @Test
     void maximumDifference2() {
-        assertThat(
-            new Solution()
-                .maximumDifference(
-                    new int[] {9,4,3,2}),
-            equalTo(-1));
+        assertThat(new Solution().maximumDifference(new int[] {9, 4, 3, 2}), equalTo(-1));
     }
 
     @Test
     void maximumDifference3() {
-        assertThat(
-            new Solution()
-                .maximumDifference(
-                    new int[] {1,5,2,10}),
-            equalTo(9));
+        assertThat(new Solution().maximumDifference(new int[] {1, 5, 2, 10}), equalTo(9));
     }
 }
