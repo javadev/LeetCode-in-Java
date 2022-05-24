d9740c68b820df941cb70f916e4238e872aa6ca4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-24 22:07:54 +0300

    Update SolutionTest.java

--- src/test/java/g2001_2100/s2054_two_best_non_overlapping_events/SolutionTest.java
+++ src/test/java/g2001_2100/s2054_two_best_non_overlapping_events/SolutionTest.java
@@ -7,27 +7,25 @@ import com_github_leetcode.CommonUtils;
 import org.junit.jupiter.api.Test;
 
 public class SolutionTest {
-    private static int[][] events;
-
     @Test
-    public void maxTwoEvents1() {
-        events =
+    void maxTwoEvents1() {
+        int[][] events =
                 CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray(
                         "[1,3,2],[4,5,2],[2,4,3]");
         assertThat(new Solution().maxTwoEvents(events), equalTo(4));
     }
 
     @Test
-    public void maxTwoEvents2() {
-        events =
+    void maxTwoEvents2() {
+        int[][] events =
                 CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray(
                         "[1,3,2],[4,5,2],[1,5,5]");
         assertThat(new Solution().maxTwoEvents(events), equalTo(5));
     }
 
     @Test
-    public void maxTwoEvents3() {
-        events =
+    void maxTwoEvents3() {
+        int[][] events =
                 CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray(
                         "[1,5,3],[1,5,1],[6,6,5]");
         assertThat(new Solution().maxTwoEvents(events), equalTo(8));
