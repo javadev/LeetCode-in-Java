9ac9244c247cbe288d8db3daf574d6a12773883a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-24 22:23:07 +0300

    Improved task 2054.

--- src/main/java/g2001_2100/s2054_two_best_non_overlapping_events/Solution.java
+++ src/main/java/g2001_2100/s2054_two_best_non_overlapping_events/Solution.java
@@ -1,39 +1,29 @@
 package g2001_2100.s2054_two_best_non_overlapping_events;
 
 // #Medium #Array #Dynamic_Programming #Sorting #Binary_Search #Heap_Priority_Queue
-// #2022_05_24_Time_58_ms(70.59%)_Space_109.2_MB_(88.24%)
+// #2022_05_24_Time_45_ms_(89.71%)_Space_87.9_MB_(93.63%)
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
     public int maxTwoEvents(int[][] events) {
-        Arrays.sort(events, (a, b) -> a[0] - b[0]);
-        int[] max = new int[events.length];
-        for (int i = events.length - 1; i >= 0; i--) {
-            if (i == events.length - 1) {
-                max[i] = events[i][2];
-            } else {
-                max[i] = Math.max(events[i][2], max[i + 1]);
-            }
-        }
+        int n = events.length;
+        Arrays.sort(events, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
+        int maxVal = 0;
         int ans = 0;
-        for (int i = 0; i < events.length; i++) {
-            int end = events[i][1];
-            int left = i + 1;
-            int right = events.length;
-            while (left < right) {
-                int mid = left + (right - left) / 2;
-                if (events[mid][0] <= end) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
+        for (int[] e : events) {
+            int start = e[0];
+            while (!queue.isEmpty()) {
+                if (queue.peek()[0] >= start) {
+                    break;
                 }
+                int[] eve = queue.remove();
+                maxVal = Math.max(maxVal, eve[1]);
             }
-            int value = events[i][2];
-            if (right >= 0 && right < events.length) {
-                value += max[right];
-            }
-            ans = Math.max(ans, value);
+            ans = Math.max(ans, e[2] + maxVal);
+            queue.add(new int[] {e[1], e[2]});
         }
         return ans;
     }
