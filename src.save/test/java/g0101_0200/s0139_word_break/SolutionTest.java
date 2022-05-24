8e25c62910cf951d72262d8aa5740658b7b348b8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-24 17:46:28 +0300

    Added tests 139-142.

--- src.save/test/java/g0101_0200/s0139_word_break/SolutionTest.java
+++ src.save/test/java/g0101_0200/s0139_word_break/SolutionTest.java
@@ -11,9 +11,17 @@ class SolutionTest {
     void wordBreak() {
         assertThat(
                 new Solution().wordBreak("leetcode", Arrays.asList("leet", "code")), equalTo(true));
+    }
+
+    @Test
+    void wordBreak2() {
         assertThat(
                 new Solution().wordBreak("applepenapple", Arrays.asList("apple", "pen")),
                 equalTo(true));
+    }
+
+    @Test
+    void wordBreak3() {
         assertThat(
                 new Solution()
                         .wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")),
--- src.save/test/java/g0101_0200/s0140_word_break_ii/SolutionTest.java
+++ src.save/test/java/g0101_0200/s0140_word_break_ii/SolutionTest.java
@@ -4,15 +4,32 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
     void wordBreak() {
+        List<String> wordDict = Arrays.asList("cat", "cats", "and", "sand", "dog");
         assertThat(
-                new Solution()
-                        .wordBreak(
-                                "catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")),
+                new Solution().wordBreak("catsanddog", wordDict),
                 equalTo(Arrays.asList("cat sand dog", "cats and dog")));
+    }
+
+    @Test
+    void wordBreak2() {
+        List<String> wordDict = Arrays.asList("apple","pen","applepen","pine","pineapple");
+        assertThat(
+                new Solution().wordBreak("pineapplepenapple", wordDict),
+                equalTo(Arrays.asList("pine apple pen apple", "pine applepen apple", "pineapple pen apple")));
+    }
+
+    @Test
+    void wordBreak3() {
+        List<String> wordDict = Arrays.asList("cats","dog","sand","and","cat");
+        assertThat(
+                new Solution().wordBreak("catsandog", wordDict),
+                equalTo(Collections.emptyList()));
     }
 }
