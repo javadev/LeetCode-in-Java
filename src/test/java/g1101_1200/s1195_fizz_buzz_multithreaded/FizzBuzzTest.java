2b56f407ff0e71be7f8869adb295962e23514a57
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-26 01:46:47 +0300

    Improved unit tests

--- src/test/java/g1101_1200/s1195_fizz_buzz_multithreaded/FizzBuzzTest.java
+++ src/test/java/g1101_1200/s1195_fizz_buzz_multithreaded/FizzBuzzTest.java
@@ -46,7 +46,7 @@ class FizzBuzzTest {
                             }
                         })
                 .start();
-        TimeUnit.MILLISECONDS.sleep(400);
+        TimeUnit.MILLISECONDS.sleep(450);
         assertThat(fizz[0], equalTo(15));
     }
 
@@ -89,7 +89,7 @@ class FizzBuzzTest {
                             }
                         })
                 .start();
-        TimeUnit.MILLISECONDS.sleep(400);
+        TimeUnit.MILLISECONDS.sleep(450);
         assertThat(fizz[0], equalTo(5));
     }
 }
