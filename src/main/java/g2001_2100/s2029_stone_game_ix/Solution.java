cc3c9670e8a1b15a3b31fb18850132dc1d271a7a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-25 19:44:57 +0300

    Fixed style.

--- src/main/java/g2001_2100/s2029_stone_game_ix/Solution.java
+++ src/main/java/g2001_2100/s2029_stone_game_ix/Solution.java
@@ -6,8 +6,12 @@ package g2001_2100.s2029_stone_game_ix;
 public class Solution {
     public boolean stoneGameIX(int[] stones) {
         int[] cnt = new int[3];
-        for (int a : stones) cnt[a % 3]++;
-        if (Math.min(cnt[1], cnt[2]) == 0) return Math.max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 > 0;
+        for (int a : stones) {
+            cnt[a % 3]++;
+        }
+        if (Math.min(cnt[1], cnt[2]) == 0) {
+            return Math.max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 > 0;
+        }
         return Math.abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
     }
 }
