b7a623558b1378804a476cbbb5cc5555a9d5982b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-26 10:34:10 +0300

    Update Solution.java

--- src/main/java/g2001_2100/s2057_smallest_index_with_equal_value/Solution.java
+++ src/main/java/g2001_2100/s2057_smallest_index_with_equal_value/Solution.java
@@ -1,7 +1,6 @@
 package g2001_2100.s2057_smallest_index_with_equal_value;
 
-// #Easy #Array
-// #2022_05_26_Time_1_ms_(94.39%)_Space_46.9_MB_(19.23%)
+// #Easy #Array #2022_05_26_Time_1_ms_(94.39%)_Space_46.9_MB_(19.23%)
 
 public class Solution {
     public int smallestEqual(int[] nums) {
