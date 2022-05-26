1fab306714f13803db846c714e8a62b3d0ba6ea2
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-05-26 22:58:18 +0700

    Update Solution.java

--- src/main/java/g2001_2100/s2040_kth_smallest_product_of_two_sorted_arrays/Solution.java
+++ src/main/java/g2001_2100/s2040_kth_smallest_product_of_two_sorted_arrays/Solution.java
@@ -3,12 +3,12 @@ package g2001_2100.s2040_kth_smallest_product_of_two_sorted_arrays;
 // #Hard #Array #Binary_Search #2022_05_26_Time_635_ms_(75.24%)_Space_50.6_MB_(92.26%)
 
 public class Solution {
-    static long INF = (long) 1e10;
+    static long inf = (long) 1e10;
 
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
         int n = nums2.length;
-        long lo = -INF - 1;
-        long hi = INF + 1;
+        long lo = -inf - 1;
+        long hi = inf + 1;
         while (lo < hi) {
             long mid = lo + ((hi - lo) >> 1);
             long cnt = 0;
