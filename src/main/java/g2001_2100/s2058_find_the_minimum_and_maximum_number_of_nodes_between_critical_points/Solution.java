1f5b9ff036fc701f4a1646f2d124438860e67de9
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-27 13:19:20 +0300

    Update Solution.java

--- src/main/java/g2001_2100/s2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points/Solution.java
+++ src/main/java/g2001_2100/s2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points/Solution.java
@@ -1,7 +1,6 @@
 package g2001_2100.s2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points;
 
-// #Medium #Linked_List
-// 2022_05_27_Time_4_ms(100%)_Space_128.7_MB_(83.63)
+// #Medium #Linked_List #2022_05_27_Time_4_ms_(100.00%)_Space_63.7_MB_(88.92%)
 
 import com_github_leetcode.ListNode;
 
