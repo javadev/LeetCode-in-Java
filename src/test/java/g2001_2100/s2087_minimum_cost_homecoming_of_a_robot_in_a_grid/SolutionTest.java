57615be255551196078f782d079a28eb23afdc19
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-27 13:17:04 +0300

    Fixed test.

--- src/test/java/g2001_2100/s2087_minimum_cost_homecoming_of_a_robot_in_a_grid/SolutionTest.java
+++ src/test/java/g2001_2100/s2087_minimum_cost_homecoming_of_a_robot_in_a_grid/SolutionTest.java
@@ -7,7 +7,22 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
     @Test
-    void test() {
-        assertThat(new Solution().decode(""), equalTo(""));
+    void minCost() {
+        assertThat(
+                new Solution()
+                        .minCost(
+                                new int[] {1, 0},
+                                new int[] {2, 3},
+                                new int[] {5, 4, 3},
+                                new int[] {8, 2, 6, 7}),
+                equalTo(18));
+    }
+
+    @Test
+    void minCost2() {
+        assertThat(
+                new Solution()
+                        .minCost(new int[] {0, 0}, new int[] {0, 0}, new int[] {5}, new int[] {26}),
+                equalTo(0));
     }
 }
