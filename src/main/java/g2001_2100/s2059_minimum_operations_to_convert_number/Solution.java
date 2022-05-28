4b1b27d424d5dac4b5716b24630d34f5e238ff76
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-28 11:11:51 +0530

    updated solution of task 2059

--- src/main/java/g2001_2100/s2059_minimum_operations_to_convert_number/Solution.java
+++ src/main/java/g2001_2100/s2059_minimum_operations_to_convert_number/Solution.java
@@ -2,7 +2,9 @@ package g2001_2100.s2059_minimum_operations_to_convert_number;
 
 // #Medium #Array #Breadth_First_Search #2022_05_28_Time_97_ms(64.14%)_Space_116.9_MB(51.03%)
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Solution {
     public int minimumOperations(int[] nums, int start, int goal) {
@@ -12,9 +14,9 @@ class Solution {
         while (!q.isEmpty()) {
             ++cnt;
             List<Integer> q1 = new ArrayList<>();
-            for (int x : q)
+            for (int x : q) {
                 for (int n : nums) {
-                    for (int xn : new int[] {x + n, x - n, x ^ n})
+                    for (int xn : new int[] {x + n, x - n, x ^ n}) {
                         if (xn >= 0 && xn <= 1000 && !seen[xn]) {
                             if (xn == start) {
                                 return cnt;
@@ -22,8 +24,10 @@ class Solution {
                             seen[xn] = true;
                             q1.add(xn);
                         }
+                    }
                 }
-            q = q1;
+                q = q1;
+            }
         }
         return -1;
     }
