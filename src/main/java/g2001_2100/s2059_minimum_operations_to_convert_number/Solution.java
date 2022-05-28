a99cc72e78b1934a73f2d40b0ca576224572276a
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-28 13:27:38 +0530

    updated task 2068

--- src/main/java/g2001_2100/s2059_minimum_operations_to_convert_number/Solution.java
+++ src/main/java/g2001_2100/s2059_minimum_operations_to_convert_number/Solution.java
@@ -1,20 +1,12 @@
 package g2001_2100.s2059_minimum_operations_to_convert_number;
 
-<<<<<<< HEAD
-// #Medium #Array #Breadth_First_Search #2022_05_28_Time_97_ms(64.14%)_Space_116.9_MB(51.03%)
-=======
 // #Medium #Array #Breadth_First_Search #2022_05_28_Time_97_ms_(64.14%)_Space_116.9_MB_(51.03%)
->>>>>>> 5ff28ef1427714e338539132a38c0a32860317b3
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-<<<<<<< HEAD
-class Solution {
-=======
 public class Solution {
->>>>>>> 5ff28ef1427714e338539132a38c0a32860317b3
     public int minimumOperations(int[] nums, int start, int goal) {
         boolean[] seen = new boolean[1001];
         List<Integer> q = Arrays.asList(goal);
