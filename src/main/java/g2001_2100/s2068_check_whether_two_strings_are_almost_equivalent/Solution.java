f38583892badcaaa15dc7d0e0fc973d259f5e2d4
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-29 16:49:11 +0300

    Updated tags.

--- src/main/java/g2001_2100/s2068_check_whether_two_strings_are_almost_equivalent/Solution.java
+++ src/main/java/g2001_2100/s2068_check_whether_two_strings_are_almost_equivalent/Solution.java
@@ -1,6 +1,6 @@
 package g2001_2100.s2068_check_whether_two_strings_are_almost_equivalent;
 
-// #Easy #String #Hash_Table #Counting #2022_05_28_Time_1_ms_(94.83%)_Space_42.7_MB_(23.62%)
+// #Easy #String #Hash_Table #Counting #2022_05_29_Time_1_ms_(95.21%)_Space_42.6_MB_(35.36%)
 
 public class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
