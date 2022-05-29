57313b3eb449cb93b5999d4b924ce87e1df9f545
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-29 14:32:19 +0530

    Added task 2078

---
+++ src/main/java/g2001_2100/s2078_tow_furthest_house_with_different_colors/Solution.java
@@ -0,0 +1,29 @@
+package g2001_2100.s2078_two_furthest_houses_with_different_colors;
+
+// #Easy #Array #Greedy #2022_05_29_Time_0_ms(100%)_Space_43_MB(5.49%)
+
+public class Solution {
+    public int maxDistance(int[] colors) {
+        int left = 0;
+        int right = colors.length - 1;
+        int max = 0;
+        while (left < right) {
+            if (colors[left] != colors[right]) {
+                max = Math.max(max, right - left);
+                break;
+            } else {
+                left++;
+            }
+        }
+        left = 0;
+        while (left < right) {
+            if (colors[left] != colors[right]) {
+                max = Math.max(max, right - left);
+                break;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
