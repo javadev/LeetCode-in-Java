21147c5e37c0bd6f794e6967a7959cebbe602888
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-29 16:50:51 +0300

    Updated tags.

--- src/main/java/g2001_2100/s2078_two_furthest_houses_with_different_colors/Solution.java
+++ src/main/java/g2001_2100/s2078_two_furthest_houses_with_different_colors/Solution.java
@@ -1,6 +1,6 @@
 package g2001_2100.s2078_two_furthest_houses_with_different_colors;
 
-// #Easy #Array #Greedy #2022_05_29_Time_0_ms_(100%)_Space_43_MB_(5.49%)
+// #Easy #Array #Greedy #2022_05_29_Time_0_ms_(100.00%)_Space_41.3_MB_(72.67%)
 
 public class Solution {
     public int maxDistance(int[] colors) {
