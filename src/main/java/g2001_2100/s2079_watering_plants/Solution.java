6d98959efeb7cd591764e39c2f4688ef777cba54
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-29 20:54:16 +0300

    Improved task 2079.

--- src/main/java/g2001_2100/s2079_watering_plants/Solution.java
+++ src/main/java/g2001_2100/s2079_watering_plants/Solution.java
@@ -1,6 +1,6 @@
 package g2001_2100.s2079_watering_plants;
 
-// #Medium #Array #2022_23_05_Time_0_ms(100%)_Space_43.1_MB_(30.50%)
+// #Medium #Array #2022_05_29_Time_0_ms_(100.00%)_Space_43.3_MB_(26.31%)
 
 public class Solution {
     public int wateringPlants(int[] plants, int capacity) {
