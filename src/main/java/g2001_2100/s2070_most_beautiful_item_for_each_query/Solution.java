8b91059ec5adbe88e69f1344a81903c8073c5e99
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-30 10:18:55 +0530

    updated solution of task 2070

--- src/main/java/g2001_2100/s2070_most_beautiful_item_for_each_query/Solution.java
+++ src/main/java/g2001_2100/s2070_most_beautiful_item_for_each_query/Solution.java
@@ -3,6 +3,9 @@ package g2001_2100.s2070_most_beautiful_item_for_each_query;
 // #Medium #Array #Sorting #Binary_Search
 // #2022_05_30_Time_53_ms(96.04%)_Space_119.9_MB(48.51%)
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
         int[] res = new int[queries.length];
