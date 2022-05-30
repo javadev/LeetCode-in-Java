20eb9f1a83aa406ebb3dfb37f4a09c0f7b1507b5
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-05-30 10:29:58 +0530

    Added task 2070.

---
+++ src/main/java/g2001_2100/s2070_most_beautiful_item_for_each_query/Solution.java
@@ -0,0 +1,29 @@
+package g2001_2100.s2070_most_beautiful_item_for_each_query;
+
+// #Medium #Array #Sorting #Binary_Search
+// #2022_05_30_Time_53_ms_(96.04%)_Space_119.9_MB_(48.51%)
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        int[] res = new int[queries.length];
+        Arrays.sort(items, Comparator.comparingInt((a) -> a[1]));
+        for (int i = 0; i < res.length; i++) {
+            res[i] = maxBeauty(items, queries[i]);
+        }
+        return res;
+    }
+
+    private int maxBeauty(int[][] items, int query) {
+        for (int i = items.length - 1; i >= 0; i--) {
+            int price = items[i][0];
+            int beauty = items[i][1];
+            if (price <= query) {
+                return beauty;
+            }
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/g2001_2100/s2070_most_beautiful_item_for_each_query/SolutionTest.java
@@ -0,0 +1,38 @@
+package g2001_2100.s2070_most_beautiful_item_for_each_query;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com_github_leetcode.CommonUtils;
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    public void maximumBeauty() {
+        int[][] items =
+                CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray(
+                        "[1,2],[3,2],[2,4],[5,6],[3,5]");
+        int[] queries = new int[] {1, 2, 3, 4, 5, 6};
+        int[] expected = new int[] {2, 4, 5, 5, 6, 6};
+        assertThat(new Solution().maximumBeauty(items, queries), equalTo(expected));
+    }
+
+    @Test
+    public void maximumBeauty2() {
+        int[][] items =
+                CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray(
+                        "[1,2],[1,2],[1,3],[1,4]");
+        int[] queries = new int[] {1};
+        int[] expected = new int[] {4};
+        assertThat(new Solution().maximumBeauty(items, queries), equalTo(expected));
+    }
+
+    @Test
+    public void maximumBeauty3() {
+        int[][] items =
+                CommonUtils.convertLeetCodeIrregularLengths2DArrayInputIntoJavaArray("[10,1000]");
+        int[] queries = new int[] {5};
+        int[] expected = new int[] {0};
+        assertThat(new Solution().maximumBeauty(items, queries), equalTo(expected));
+    }
+}
