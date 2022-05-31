2047c0364d59071c71f5b04ec7fa9a8cea439beb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-31 14:47:12 +0300

    Improved task 2106.

--- src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
+++ src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
@@ -6,26 +6,28 @@ package g2101_2200.s2106_maximum_fruits_harvested_after_at_most_k_steps;
 public class Solution {
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
         int res = 0;
-        for (int left = 0, right = 0, sum = 0; right < fruits.length; right++) {
+        int sum = 0;
+        for (int left = 0, right = 0; right < fruits.length; right++) {
             sum += fruits[right][1];
             while (left <= right && !isValidRange(fruits[left][0], fruits[right][0], startPos, k)) {
                 sum -= fruits[left++][1];
             }
             res = Math.max(sum, res);
         }
-
         return res;
     }
 
     private boolean isValidRange(int leftPos, int rightPos, int startPos, int k) {
+        final boolean result;
         if (rightPos <= startPos) {
-            return startPos - leftPos <= k;
+            result = startPos - leftPos <= k;
         } else if (leftPos >= startPos) {
-            return rightPos - startPos <= k;
+            result = rightPos - startPos <= k;
         } else {
             int left = startPos - leftPos;
             int right = rightPos - startPos;
-            return left <= right ? left * 2 + right <= k : right * 2 + left <= k;
+            result = left <= right ? left * 2 + right <= k : right * 2 + left <= k;
         }
+        return result;
     }
 }
