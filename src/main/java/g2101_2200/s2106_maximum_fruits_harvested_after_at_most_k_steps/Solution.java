d16afb2ff6b678924d5731422c5929c323aef4f0
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-05-31 18:31:08 +0700

    Update Solution.java

--- src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
+++ src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
@@ -3,75 +3,28 @@ package g2101_2200.s2106_maximum_fruits_harvested_after_at_most_k_steps;
 // #Hard #Array #Binary_Search #Prefix_Sum #Sliding_Window
 // #2022_05_31_Time_27_ms_(48.57%)_Space_128.7_MB_(44.76%)
 
-class Solution {
+public class Solution {
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
-        int n = fruits.length;
-        int posOfLastFruit = fruits[n - 1][0];
-        int[] prefixArr = new int[posOfLastFruit + 1];
-        int start = Math.max(startPos - k, 0);
-        int end = Math.min(startPos + k, prefixArr.length - 1);
+        int res = 0;
 
-        if (startPos > posOfLastFruit) {
-            int diff = startPos - posOfLastFruit;
-            startPos = posOfLastFruit;
-            k = k - diff;
-            if (k == 0) {
-                return fruits[posOfLastFruit][1];
-            } else if (k < 0) {
-                return 0;
-            }
+        for (int left = 0, right = 0, sum = 0; right < fruits.length; right++) {
+            sum += fruits[right][1];
+            while (left <= right && !isValidRange(fruits[left][0], fruits[right][0], startPos, k))
+                sum -= fruits[left++][1];
+            res = Math.max(sum, res);
         }
 
-        for (int[] fruit : fruits) {
-            prefixArr[fruit[0]] = fruit[1];
+        return res;
+    }
+
+    private boolean isValidRange(int leftPos, int rightPos, int startPos, int k) {
+        if (rightPos <= startPos) {
+            return startPos - leftPos <= k;
+        } else if (leftPos >= startPos) {
+            return rightPos - startPos <= k;
+        } else { // leftPos < startPos < rightPos
+            int left = startPos - leftPos, right = rightPos - startPos;
+            return left <= right ? left * 2 + right <= k : right * 2 + left <= k;
         }
-
-        int curr = 0;
-        for (int i = startPos - 1; i >= start; i--) {
-            curr += prefixArr[i];
-            prefixArr[i] = curr;
-        }
-
-        curr = 0;
-        for (int i = startPos + 1; i <= end; i++) {
-            curr += prefixArr[i];
-            prefixArr[i] = curr;
-        }
-
-        int minimum = prefixArr[startPos];
-        prefixArr[startPos] = 0;
-        int ans = 0;
-
-        for (int i = start; i < startPos; i++) {
-            int maxCurrPossible = prefixArr[i];
-            int stepsAlreadyWalked = startPos - i;
-            int stepsRemaining = k - stepsAlreadyWalked;
-            int endIndex = i + stepsRemaining;
-
-            if (endIndex > startPos && endIndex < prefixArr.length) {
-                maxCurrPossible += prefixArr[endIndex];
-            } else if (endIndex >= prefixArr.length) {
-                maxCurrPossible += prefixArr[prefixArr.length - 1];
-            }
-
-            ans = Math.max(ans, maxCurrPossible);
-        }
-
-        for (int i = startPos + 1; i <= end; i++) {
-            int maxCurrPossible = prefixArr[i];
-            int stepsAlreadyWalked = i - startPos;
-            int stepsRemaining = k - stepsAlreadyWalked;
-            int endIndex = i - stepsRemaining;
-
-            if (endIndex < startPos && endIndex >= 0) {
-                maxCurrPossible += prefixArr[endIndex];
-            } else if (endIndex < 0) {
-                maxCurrPossible += prefixArr[0];
-            }
-
-            ans = Math.max(ans, maxCurrPossible);
-        }
-
-        return ans + minimum;
     }
 }
