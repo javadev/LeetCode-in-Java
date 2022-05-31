a4ddbb820a74e5241d6f5fe469161b5bb80b07ee
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-31 15:11:51 +0300

    Updated readme.

--- src/main/java/g1001_1100/s1095_find_in_mountain_array/Solution.java
+++ src/main/java/g1001_1100/s1095_find_in_mountain_array/Solution.java
@@ -2,6 +2,14 @@ package g1001_1100.s1095_find_in_mountain_array;
 
 // #Hard #Array #Binary_Search #Interactive #2022_02_22_Time_0_ms_(100.00%)_Space_42.1_MB_(37.47%)
 
+/*
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
 public class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int peakIndex = findPeak(mountainArr);
