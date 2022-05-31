131c76f0b0487f4f2c82c18f153f8f532b3a8de3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-05-31 14:50:02 +0300

    Improved task 2106.

--- src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
+++ src/main/java/g2101_2200/s2106_maximum_fruits_harvested_after_at_most_k_steps/Solution.java
@@ -1,7 +1,7 @@
 package g2101_2200.s2106_maximum_fruits_harvested_after_at_most_k_steps;
 
 // #Hard #Array #Binary_Search #Prefix_Sum #Sliding_Window
-// #2022_05_31_Time_27_ms_(48.57%)_Space_128.7_MB_(44.76%)
+// #2022_05_31_Time_4_ms_(86.67%)_Space_90.5_MB_(84.76%)
 
 public class Solution {
     public int maxTotalFruits(int[][] fruits, int startPos, int k) {
