61755c0acac80e0e0e763fe7f4d38e524817ce13
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-01 08:10:13 +0300

    Update Solution.java

--- src/main/java/g2101_2200/s2116_check_if_a_parentheses_string_can_be_valid/Solution.java
+++ src/main/java/g2101_2200/s2116_check_if_a_parentheses_string_can_be_valid/Solution.java
@@ -7,27 +7,22 @@ public class Solution {
         if (s == null || s.isEmpty()) {
             return true;
         }
-
         if ((s.length() & 1) > 0) {
             return false;
         }
-
         if (locked == null || locked.isEmpty()) {
             return true;
         }
-
         int numOfLockedClose = 0;
         int numOfLockedOpen = 0;
         for (int i = 0; i < s.length(); i++) {
             int countOfChars = i + 1;
             if (s.charAt(i) == ')' && locked.charAt(i) == '1') {
                 numOfLockedClose++;
-
                 if (numOfLockedClose * 2 > countOfChars) {
                     return false;
                 }
             }
-
             int j = s.length() - 1 - i;
             if (s.charAt(j) == '(' && locked.charAt(j) == '1') {
                 numOfLockedOpen++;
@@ -37,7 +32,6 @@ public class Solution {
                 }
             }
         }
-
         return true;
     }
 }
