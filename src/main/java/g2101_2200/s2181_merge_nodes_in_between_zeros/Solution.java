8853f41fdfb141d2e4f50c1e73d30229318fdb64
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-02 01:06:38 +0300

    Added task 2181.

---
+++ src/main/java/g2101_2200/s2181_merge_nodes_in_between_zeros/Solution.java
@@ -0,0 +1,38 @@
+package g2101_2200.s2181_merge_nodes_in_between_zeros;
+
+// #Medium #Simulation #Linked_List
+
+import com_github_leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode temp = head.next;
+        ListNode slow = head;
+        int sum = 0;
+        ListNode fast = temp;
+        while (temp != null) {
+            if (temp.val == 0) {
+                temp.val = sum;
+                sum = 0;
+                slow.next = fast.next;
+                slow = temp;
+                fast = fast.next;
+            } else {
+                sum += temp.val;
+                fast = temp;
+            }
+            temp = temp.next;
+        }
+        return head.next;
+    }
+}
