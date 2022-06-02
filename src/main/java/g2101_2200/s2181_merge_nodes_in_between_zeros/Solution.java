efd82c719f4e1caefa1b40ef603b0f2ba3bcaacb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-02 03:54:40 +0300

    Improved task 2181.

--- src/main/java/g2101_2200/s2181_merge_nodes_in_between_zeros/Solution.java
+++ src/main/java/g2101_2200/s2181_merge_nodes_in_between_zeros/Solution.java
@@ -1,6 +1,6 @@
 package g2101_2200.s2181_merge_nodes_in_between_zeros;
 
-// #Medium #Simulation #Linked_List
+// #Medium #Simulation #Linked_List #2022_06_02_Time_6_ms_(96.26%)_Space_71.2_MB_(97.95%)
 
 import com_github_leetcode.ListNode;
 
