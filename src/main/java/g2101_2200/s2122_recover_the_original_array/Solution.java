3a279db50be08ae5df5c7681f8689b19150b644d
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-02 09:45:27 +0300

    Improved task 2122.

--- src/main/java/g2101_2200/s2122_recover_the_original_array/Solution.java
+++ src/main/java/g2101_2200/s2122_recover_the_original_array/Solution.java
@@ -1,60 +1,65 @@
 package g2101_2200.s2122_recover_the_original_array;
 
 // #Hard #Array #Hash_Table #Sorting #Enumeration
-// #2022_06_02_Time_30_ms_(71.23%)_Space_55.2_MB_(35.62%)
+// #2022_06_02_Time_19_ms_(89.04%)_Space_54.9_MB_(39.73%)
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-@SuppressWarnings("java:S135")
 public class Solution {
+    private int[] res;
+
     public int[] recoverArray(int[] nums) {
-        int n2 = nums.length;
-        int n = n2 / 2;
-        int[] res = new int[n];
-        boolean[] visited = new boolean[n2];
-        Map<Integer, int[]> map = new HashMap<>();
+        int n = nums.length;
         Arrays.sort(nums);
-        map.put(nums[0], new int[] {0, 1});
-        for (int i = 1; i < n2; i++) {
-            if (nums[i] != nums[i - 1]) {
-                map.get(nums[i - 1])[1] = i;
-                map.put(nums[i], new int[] {i, i + 1});
+        ArrayList<Integer> diffs = new ArrayList<>();
+        int smallest = nums[0];
+        for (int i = 1; i < n; i++) {
+            int k = (nums[i] - smallest) / 2;
+            if ((nums[i] - smallest) % 2 == 0 && k != 0) {
+                diffs.add(k);
             }
         }
-        map.get(nums[n2 - 1])[1] = n2;
-        for (int i = 1; i <= n; i++) {
-            int k2 = nums[i] - nums[0];
-            int k = k2 / 2;
-            int idx = 0;
-            if (k2 == 0 || k2 % 2 != 0) {
-                continue;
-            }
-            Arrays.fill(visited, false);
-            int itr;
-            for (int j = 0; j < n2; j++) {
-                if (visited[j]) {
-                    continue;
-                }
-                if (!map.containsKey(nums[j] + k2)) {
-                    break;
-                }
-                int[] pos = map.get(nums[j] + k2);
-                itr = pos[0];
-                while (itr < pos[1] && visited[itr]) {
-                    itr++;
-                }
-                if (itr == pos[1]) {
-                    break;
-                }
-                visited[j] = visited[itr] = true;
-                res[idx++] = nums[j] + k;
-            }
-            if (idx == n) {
+        for (int k : diffs) {
+            if (check(n, k, nums)) {
                 break;
             }
         }
         return res;
+    }
+
+    private boolean check(int n, int k, int[] nums) {
+        res = new int[n / 2];
+        boolean[] visited = new boolean[n];
+        int lower = 0;
+        int higher = 1;
+        int count = 0;
+        while (lower < n) {
+            if (visited[lower]) {
+                lower++;
+                continue;
+            }
+            int lowerVal = nums[lower];
+            int higherVal = lowerVal + (2 * k);
+            while (higher < n) {
+                if (nums[higher] == higherVal && !visited[higher]) {
+                    break;
+                }
+                higher++;
+            }
+            if (higher == n) {
+                return false;
+            }
+            visited[lower] = true;
+            visited[higher] = true;
+            res[count] = lowerVal + k;
+            count++;
+            if (count == n / 2) {
+                return true;
+            }
+            lower++;
+            higher++;
+        }
+        return false;
     }
 }
