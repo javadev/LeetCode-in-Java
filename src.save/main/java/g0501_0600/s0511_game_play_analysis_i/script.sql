0eda5f23a00a7bbe09f96480360c28a1f4f4cd54
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-03 19:15:52 +0300

    Added tasks 511, 586.

---
+++ src.save/main/java/g0501_0600/s0511_game_play_analysis_i/script.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+# #Easy #Database #SQL_I_Day_8_Function #2022_06_03_Time_620_ms_(35.70%)_Space_0B_(100.00%)
+select distinct(player_id), min(event_date) as first_login
+from activity
+group by player_id
