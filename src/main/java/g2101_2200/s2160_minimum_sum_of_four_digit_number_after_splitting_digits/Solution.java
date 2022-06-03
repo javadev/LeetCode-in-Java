c2a800819e103f0cdaa0fac5f583e44ce92e9818
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-06-03 10:22:27 +0530

    updated solution of task 2160

--- src/main/java/g2101_2200/s2160_minimum_sum_of_four_digit_number_after_splitting_digits/Solution.java
+++ src/main/java/g2101_2200/s2160_minimum_sum_of_four_digit_number_after_splitting_digits/Solution.java
@@ -6,15 +6,15 @@ import java.util.Arrays;
 
 public class Solution {
     public int minimumSum(int num) {
-        int[] dig = new int[4];
+        int[] digit = new int[4];
         int cur = 0;
         while (num > 0) {
-            dig[cur++] = num % 10;
+            digit[cur++] = num % 10;
             num /= 10;
         }
-        Arrays.sort(dig);
-        int num1 = dig[0] * 10 + dig[2];
-        int num2 = dig[1] * 10 + dig[3];
+        Arrays.sort(digit);
+        int num1 = digit[0] * 10 + digit[2];
+        int num2 = digit[1] * 10 + digit[3];
         return num1 + num2;
     }
 }
