d2b7d743979311453bf39195009d81722b48362a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-04 17:07:33 +0300

    Improved task 1.

--- src.save/main/java/g0001_0100/s0001_two_sum/Solution.java
+++ src.save/main/java/g0001_0100/s0001_two_sum/Solution.java
@@ -1,46 +1,21 @@
 package g0001_0100.s0001_two_sum;
 
 // #Easy #Top_100_Liked_Questions #Top_Interview_Questions #Array #Hash_Table
-// #Data_Structure_I_Day_2_Array #2022_02_17_Time_3_ms_(80.08%)_Space_45.6_MB_(13.25%)
+// #Data_Structure_I_Day_2_Array #2022_06_04_Time_1_ms_(99.68%)_Space_42.5_MB_(83.07%)
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        if (nums == null || nums.length <= 1) {
-            return new int[0];
-        }
-        int[] result = new int[2];
-        int left = 0;
-        int right = nums.length - 1;
-        int[] nums1 = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(nums1);
-        while (left < right) {
-            if (nums1[left] + nums1[right] == target) {
-                break;
-            } else if (nums1[left] + nums1[right] > target) {
-                right--;
-            } else if (nums1[left] + nums1[right] < target) {
-                left++;
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < numbers.length; i++) {
+            Integer requiredNum = (Integer) (target - numbers[i]);
+            if (indexMap.containsKey(requiredNum)) {
+                return new int[] {indexMap.get(requiredNum), i};
             }
+            indexMap.put(numbers[i], i);
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (nums1[left] == nums[i]) {
-                result[0] = i;
-                break;
-            }
-        }
-        for (int j = nums.length - 1; j >= 0; j--) {
-            if (nums1[right] == nums[j]) {
-                result[1] = j;
-                break;
-            }
-        }
-
-        int tmp = result[0];
-        result[0] = Math.min(result[0], result[1]);
-        result[1] = Math.max(tmp, result[1]);
-        return result;
+        return new int[] {-1, -1};
     }
 }
