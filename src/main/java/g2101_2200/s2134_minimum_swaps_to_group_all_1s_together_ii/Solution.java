79812693f50c4eabe4dbe53fe5df2bb8027722b1
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-04 12:57:26 +0300

    Update Solution.java

--- src/main/java/g2101_2200/s2134_minimum_swaps_to_group_all_1s_together_ii/Solution.java
+++ src/main/java/g2101_2200/s2134_minimum_swaps_to_group_all_1s_together_ii/Solution.java
@@ -14,34 +14,27 @@ public class Solution {
                 ones[i] = ones[i - 1];
             }
         }
-
         if (ones[l - 1] == l || ones[l - 1] == 0) {
             return 0;
         }
-
         int ws = ones[l - 1];
         int minSwaps = Integer.MAX_VALUE;
         int si = 0;
         int ei;
-
         while (si < nums.length) {
             ei = (si + ws - 1) % l;
             int totalones;
-
             if (ei >= si) {
                 totalones = ones[ei] - (si == 0 ? 0 : ones[si - 1]);
             } else {
                 totalones = ones[ei] + (ones[l - 1] - ones[si - 1]);
             }
-
             int swapsreq = ws - totalones;
             if (swapsreq < minSwaps) {
                 minSwaps = swapsreq;
             }
-
             si++;
         }
-
         return minSwaps;
     }
 }
