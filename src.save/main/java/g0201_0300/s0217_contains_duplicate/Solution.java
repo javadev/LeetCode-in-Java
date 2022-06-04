1dcc6038fa4d9a69990250312609bef63c1ed808
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-04 08:41:06 +0300

    Improved task 217.

--- src.save/main/java/g0201_0300/s0217_contains_duplicate/Solution.java
+++ src.save/main/java/g0201_0300/s0217_contains_duplicate/Solution.java
@@ -2,16 +2,19 @@ package g0201_0300.s0217_contains_duplicate;
 
 // #Easy #Top_Interview_Questions #Array #Hash_Table #Sorting #Data_Structure_I_Day_1_Array
 // #Programming_Skills_I_Day_11_Containers_and_Libraries
-// #2022_03_05_Time_27_ms_(16.94%)_Space_70.7_MB_(39.19%)
+// #2022_06_04_Time_10_ms_(67.20%)_Space_68.1_MB_(64.97%)
 
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) {
                 return true;
+            } else {
+                set.add(n);
             }
         }
         return false;
