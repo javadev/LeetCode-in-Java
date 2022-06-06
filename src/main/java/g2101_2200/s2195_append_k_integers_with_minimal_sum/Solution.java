e388c1f0db1908a351aeae001f7f0c48c738f880
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-06 14:34:37 +0300

    Updated readme.

--- src/main/java/g2101_2200/s2195_append_k_integers_with_minimal_sum/Solution.java
+++ src/main/java/g2101_2200/s2195_append_k_integers_with_minimal_sum/Solution.java
@@ -1,6 +1,6 @@
 package g2101_2200.s2195_append_k_integers_with_minimal_sum;
 
-// #Medium #Array #Math #Sorting #Greedy
+// #Medium #Array #Math #Sorting #Greedy #2022_06_06_Time_19_ms_(96.88%)_Space_60_MB_(78.44%)
 
 import java.util.Arrays;
 
