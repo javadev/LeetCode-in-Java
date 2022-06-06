892321c7b3b14abed483baf72e7106a480a17a27
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-06 18:36:30 +0300

    Added test

--- src/test/java/g2101_2200/s2197_replace_non_coprime_numbers_in_array/SolutionTest.java
+++ src/test/java/g2101_2200/s2197_replace_non_coprime_numbers_in_array/SolutionTest.java
@@ -20,4 +20,13 @@ class SolutionTest {
                 new Solution().replaceNonCoprimes(new int[] {2, 2, 1, 1, 3, 3, 3}),
                 equalTo(Arrays.asList(2, 1, 1, 3)));
     }
+
+    @Test
+    void replaceNonCoprimes3() {
+        assertThat(
+                new Solution()
+                        .replaceNonCoprimes(
+                                new int[] {287, 41, 49, 287, 899, 23, 23, 20677, 5, 825}),
+                equalTo(Arrays.asList(2009, 20677, 825)));
+    }
 }
