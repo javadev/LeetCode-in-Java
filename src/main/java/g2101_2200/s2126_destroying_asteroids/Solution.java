22073c6d28ada87168daf3dad9b3d9d5a5c2fc88
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-08 16:38:31 +0300

    Improved task 2126.

--- src/main/java/g2101_2200/s2126_destroying_asteroids/Solution.java
+++ src/main/java/g2101_2200/s2126_destroying_asteroids/Solution.java
@@ -1,19 +1,41 @@
 package g2101_2200.s2126_destroying_asteroids;
 
-// #Medium #Array #Sorting #Greedy #2022_06_03_Time_25_ms_(83.82%)_Space_54.6_MB_(93.38%)
-
-import java.util.Arrays;
+// #Medium #Array #Sorting #Greedy #2022_06_08_Time_6_ms_(99.27%)_Space_54.1_MB_(97.81%)
 
 public class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-        Arrays.sort(asteroids);
-        long m = mass;
-        for (int ele : asteroids) {
-            if (m < ele) {
-                return false;
-            }
-            m += ele;
+        return helper(mass, 0, asteroids);
+    }
+
+    private boolean helper(long mass, int startIndex, int[] asteroids) {
+        int smallOrEqualIndex = partition(mass, startIndex, asteroids);
+        if (smallOrEqualIndex < startIndex) {
+            return false;
         }
-        return true;
+        if (smallOrEqualIndex >= asteroids.length - 1) {
+            return true;
+        }
+        for (int i = startIndex; i <= smallOrEqualIndex; ++i) {
+            mass += asteroids[i];
+        }
+        return helper(mass, ++smallOrEqualIndex, asteroids);
+    }
+
+    private int partition(long mass, int startIndex, int[] asteroids) {
+        int length = asteroids.length;
+        int smallOrEqualIndex = startIndex - 1;
+        for (int i = startIndex; i < length; ++i) {
+            if (asteroids[i] <= mass && i != smallOrEqualIndex) {
+                smallOrEqualIndex++;
+                swap(asteroids, i, smallOrEqualIndex);
+            }
+        }
+        return smallOrEqualIndex;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int tmp = array[i];
+        array[i] = array[j];
+        array[j] = tmp;
     }
 }
