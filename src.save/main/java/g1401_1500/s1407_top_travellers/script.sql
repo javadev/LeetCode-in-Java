29fadde0112b102c134a2bb778e1402b489d5e40
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-09 16:59:04 +0300

    Removed bom from sql scripts.

--- src.save/main/java/g1401_1500/s1407_top_travellers/script.sql
+++ src.save/main/java/g1401_1500/s1407_top_travellers/script.sql
@@ -1,4 +1,4 @@
-﻿# Write your MySQL query statement below
+# Write your MySQL query statement below
 # #Easy #Database #SQL_I_Day_9_Control_of_Flow #2022_03_28_Time_682_ms_(70.16%)_Space_0B_(100.00%)
 SELECT U.NAME , COALESCE(SUM(R.DISTANCE),0) AS travelled_distance
 FROM USERS U LEFT JOIN RIDES R ON U.ID=R.USER_ID
