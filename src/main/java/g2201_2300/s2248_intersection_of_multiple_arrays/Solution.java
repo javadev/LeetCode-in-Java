46e8926662fa8f9a69594e3a651519d462d9d334
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-10 01:50:20 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2248_intersection_of_multiple_arrays/Solution.java
+++ src/main/java/g2201_2300/s2248_intersection_of_multiple_arrays/Solution.java
@@ -1,6 +1,6 @@
 package g2201_2300.s2248_intersection_of_multiple_arrays;
 
-// #2022_06_09_Time_2_ms_(99.46%)_Space_42.6_MB_(92.53%)
+// #Easy #2022_06_09_Time_2_ms_(99.46%)_Space_42.6_MB_(92.53%)
 
 import java.util.ArrayList;
 import java.util.List;
