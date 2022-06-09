8d65e6d1901421387d789507c9d6cd21cacc5c4a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-09 08:24:47 +0300

    Fixed style.

--- src/main/java/g2101_2200/s2170_minimum_operations_to_make_the_array_alternating/Solution.java
+++ src/main/java/g2101_2200/s2170_minimum_operations_to_make_the_array_alternating/Solution.java
@@ -5,16 +5,24 @@ package g2101_2200.s2170_minimum_operations_to_make_the_array_alternating;
 
 public class Solution {
     public int minimumOperations(int[] nums) {
-        int maxOdd = 0, maxEven = 0, max = 0;
+        int maxOdd = 0;
+        int maxEven = 0;
+        int max = 0;
         int n = nums.length;
-        for (int i = 0; i < n; i++) max = Math.max(max, nums[i]);
-        int even[] = new int[max + 1];
-        int odd[] = new int[max + 1];
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) even[nums[i]]++;
-            else odd[nums[i]]++;
+        for (int num : nums) {
+            max = Math.max(max, num);
         }
-        int t1 = 0, t2 = 0;
+        int[] even = new int[max + 1];
+        int[] odd = new int[max + 1];
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                even[nums[i]]++;
+            } else {
+                odd[nums[i]]++;
+            }
+        }
+        int t1 = 0;
+        int t2 = 0;
         for (int i = 0; i < max + 1; i++) {
             if (even[i] > maxEven) {
                 maxEven = even[i];
@@ -25,19 +33,24 @@ public class Solution {
                 t2 = i;
             }
         }
-        int ans = 0;
+        int ans;
         if (t1 == t2) {
-            int secondEven = 0, secondOdd = 0;
+            int secondEven = 0;
+            int secondOdd = 0;
             for (int i = 0; i < max + 1; i++) {
-                if (i != t1 && even[i] > secondEven) secondEven = even[i];
-                if (i != t2 && odd[i] > secondOdd) secondOdd = odd[i];
+                if (i != t1 && even[i] > secondEven) {
+                    secondEven = even[i];
+                }
+                if (i != t2 && odd[i] > secondOdd) {
+                    secondOdd = odd[i];
+                }
             }
             ans =
                     Math.min(
                             (n / 2 + n % 2 - maxEven) + (n / 2 - secondOdd),
                             (n / 2 + n % 2 - secondEven) + (n / 2 - maxOdd));
         } else {
-            ans = ((n / 2 + n % 2) - maxEven) + ((n / 2) - maxOdd);
+            ans = n / 2 + n % 2 - maxEven + n / 2 - maxOdd;
         }
         return ans;
     }
