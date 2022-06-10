398bede04138609d2d97f636f09303d24233152a
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-06-10 22:14:16 +0530

    updated solution of task 2239"

--- src/main/java/g2201_2300/s2239_find_closest_number_to_zero/Solution.java
+++ src/main/java/g2201_2300/s2239_find_closest_number_to_zero/Solution.java
@@ -5,15 +5,15 @@ package g2201_2300.s2239_find_closest_number_to_zero;
 public class Solution {
     public int findClosestNumber(int[] nums) {
         int mini = Integer.MAX_VALUE;
-        int closest_num = 0;
+        int closestNum = 0;
         for (int n : nums) {
             if (mini > Math.abs(n)) {
                 mini = Math.abs(n);
-                closest_num = n;
-            } else if (mini == Math.abs(n) && closest_num < n) {
-                closest_num = n;
+                closestNum = n;
+            } else if (mini == Math.abs(n) && closestNum < n) {
+                closestNum = n;
             }
         }
-        return closest_num;
+        return closestNum;
     }
 }
