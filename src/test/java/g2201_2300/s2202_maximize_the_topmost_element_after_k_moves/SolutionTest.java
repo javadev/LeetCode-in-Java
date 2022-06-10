67d0b44828b3c6a04ca85de8c52c2b6411a3dab3
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-10 08:56:59 +0300

    Added task 2202.

---
+++ src/test/java/g2201_2300/s2202_maximize_the_topmost_element_after_k_moves/SolutionTest.java
@@ -0,0 +1,18 @@
+package g2201_2300.s2202_maximize_the_topmost_element_after_k_moves;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void maximumTop() {
+        assertThat(new Solution().maximumTop(new int[] {5, 2, 2, 4, 0, 6}, 4), equalTo(5));
+    }
+
+    @Test
+    void maximumTop2() {
+        assertThat(new Solution().maximumTop(new int[] {2}, 1), equalTo(-1));
+    }
+}
