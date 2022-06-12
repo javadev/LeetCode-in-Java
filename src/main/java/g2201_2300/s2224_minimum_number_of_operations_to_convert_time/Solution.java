22f4a5c4df680de7f9b29e4ec47acc02c7f01ec8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 20:13:36 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2224_minimum_number_of_operations_to_convert_time/Solution.java
+++ src/main/java/g2201_2300/s2224_minimum_number_of_operations_to_convert_time/Solution.java
@@ -3,7 +3,7 @@ package g2201_2300.s2224_minimum_number_of_operations_to_convert_time;
 // #Easy #String #Greedy #2022_06_12_Time_1_ms_(97.78%)_Space_42.7_MB_(18.92%)
 
 public class Solution {
-    private int duration[] = {60, 15, 5, 1};
+    private int[] duration = {60, 15, 5, 1};
     private int c = 0;
 
     public int convertTime(String current, String correct) {
