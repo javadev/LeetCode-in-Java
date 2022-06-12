da322a2c30bccc93066d5f1ca0a747cd99014742
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 12:14:35 +0300

    Improved task 2203.

--- src/main/java/g2201_2300/s2203_minimum_weighted_subgraph_with_the_required_paths/Solution.java
+++ src/main/java/g2201_2300/s2203_minimum_weighted_subgraph_with_the_required_paths/Solution.java
@@ -1,72 +1,73 @@
 package g2201_2300.s2203_minimum_weighted_subgraph_with_the_required_paths;
 
-// #Hard #Graph #Shortest_Path #2022_06_12_Time_241_ms_(50.34%)_Space_205.1_MB_(10.20%)
+// #Hard #Graph #Shortest_Path #2022_06_12_Time_94_ms_(97.26%)_Space_90.7_MB_(99.32%)
 
+import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     public long minimumWeight(int n, int[][] edges, int src1, int src2, int dest) {
-        Map<Integer, Integer>[] g1 = new Map[n];
-        Map<Integer, Integer>[] g2 = new Map[n];
-        for (int[] edge : edges) {
-            int s1 = edge[0];
-            int s2 = edge[1];
-            int w = edge[2];
-            if (g1[s1] == null) {
-                g1[s1] = new HashMap<>();
+        List<int[]>[] graph = new List[n];
+        long[][] weight = new long[3][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                weight[j][i] = Long.MAX_VALUE;
             }
-            g1[s1].put(s2, Math.min(w, g1[s1].getOrDefault(s2, Integer.MAX_VALUE)));
-            if (g2[s2] == null) {
-                g2[s2] = new HashMap<>();
-            }
-            g2[s2].put(s1, Math.min(w, g2[s2].getOrDefault(s1, Integer.MAX_VALUE)));
+            graph[i] = new ArrayList<>();
         }
-        Long[] dist1 = dijkstra(g1, src1);
-        Long[] dist2 = dijkstra(g1, src2);
-        Long[] dist3 = dijkstra(g2, dest);
-        Long res = null;
-        for (int i = 0; i < n; i += 1) {
-            if (dist1[i] != null && dist2[i] != null && dist3[i] != null) {
-                long sum = dist1[i] + dist2[i] + dist3[i];
-                if (res == null || res > sum) {
-                    res = sum;
-                }
-            }
+        for (int[] e : edges) {
+            graph[e[0]].add(new int[] {e[1], e[2]});
         }
-        return res == null ? -1 : res;
-    }
-
-    private Long[] dijkstra(Map<Integer, Integer>[] graph, int start) {
-        Long[] res = new Long[graph.length];
-        PriorityQueue<State> q = new PriorityQueue<>(Comparator.comparingLong(a -> a.w));
-        q.offer(new State(start, 0L));
-        while (!q.isEmpty()) {
-            State cur = q.poll();
-            if (res[cur.id] != null) {
+        Queue<Node> queue =
+                new PriorityQueue(
+                        (Comparator<Node>)
+                                (node1, node2) -> Long.compare(node1.weight, node2.weight));
+        queue.offer(new Node(0, src1, 0));
+        weight[0][src1] = 0;
+        queue.offer(new Node(1, src2, 0));
+        weight[1][src2] = 0;
+        while (!queue.isEmpty()) {
+            Node curr = queue.poll();
+            if (curr.weight > weight[curr.index][curr.vertex]
+                    || curr.weight > weight[2][curr.vertex]) {
                 continue;
             }
-            res[cur.id] = (long) cur.w;
-            if (graph[cur.id] != null) {
-                for (int next : graph[cur.id].keySet()) {
-                    if (res[next] == null) {
-                        q.offer(new State(next, cur.w + graph[cur.id].get(next)));
+            if (curr.vertex == dest && curr.index == 2) {
+                return curr.weight;
+            }
+            for (int[] next : graph[curr.vertex]) {
+                if (curr.index == 2 && weight[curr.index][next[0]] > curr.weight + next[1]) {
+                    weight[curr.index][next[0]] = curr.weight + next[1];
+                    queue.offer(new Node(curr.index, next[0], weight[curr.index][next[0]]));
+                } else if (weight[curr.index][next[0]] > curr.weight + next[1]) {
+                    weight[curr.index][next[0]] = curr.weight + next[1];
+                    queue.offer(new Node(curr.index, next[0], weight[curr.index][next[0]]));
+                    if (weight[curr.index ^ 1][next[0]] != Long.MAX_VALUE
+                            && weight[curr.index][next[0]] + weight[curr.index ^ 1][next[0]]
+                                    < weight[2][next[0]]) {
+                        weight[2][next[0]] =
+                                weight[curr.index][next[0]] + weight[curr.index ^ 1][next[0]];
+                        queue.offer(new Node(2, next[0], weight[2][next[0]]));
                     }
                 }
             }
         }
-        return res;
+        return -1;
     }
 
-    private static class State {
-        long w;
-        int id;
+    private static class Node {
+        int index;
+        int vertex;
+        long weight;
 
-        State(int id, long w) {
-            this.id = id;
-            this.w = w;
+        Node(int index, int vertex, long weight) {
+            this.index = index;
+            this.vertex = vertex;
+            this.weight = weight;
         }
     }
 }
