624e77a293ac8e3c3f0b1f61a7010c0618626b1b
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 14:38:49 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2211_count_collisions_on_a_road/Solution.java
+++ src/main/java/g2201_2300/s2211_count_collisions_on_a_road/Solution.java
@@ -2,18 +2,18 @@ package g2201_2300.s2211_count_collisions_on_a_road;
 
 // #Medium #String #Stack #2022_06_12_Time_113_ms_(45.96%)_Space_54.3_MB_(57.28%)
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Solution {
     public int countCollisions(String directions) {
         if (directions == null || directions.length() == 1) {
             return 0;
         }
-        Stack<Character> stack = new Stack<>();
+        Deque <Character> stack = new ArrayDeque<>();
         char[] direction = directions.toCharArray();
         char prevc = '0';
         int collision = 0;
-
         for (int i = 0; i < direction.length; i++) {
             if (direction[i] == 'R') {
                 stack.push(direction[i]);
@@ -29,7 +29,6 @@ public class Solution {
                         stack.pop();
                     }
                 }
-
                 if ((direction[i] == 'L' && prevc == 'R')) {
                     stack.pop();
                     collision += 2;
