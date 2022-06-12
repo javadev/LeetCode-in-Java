581beb241deaa815c99f1191fcbd86bdd6f23895
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 18:43:45 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2217_find_palindrome_with_fixed_length/Solution.java
+++ src/main/java/g2201_2300/s2217_find_palindrome_with_fixed_length/Solution.java
@@ -2,10 +2,11 @@ package g2201_2300.s2217_find_palindrome_with_fixed_length;
 
 // #Medium #Array #Math #2022_06_12_Time_37_ms_(88.60%)_Space_53.7_MB_(77.19%)
 
+@SuppressWarnings("java:S2184")
 public class Solution {
     public long[] kthPalindrome(int[] queries, int intLength) {
-        long minHalf = (long) Math.pow(10, (intLength - 1) / 2D);
-        long maxIndex = (long) Math.pow(10, (intLength + 1) / 2D) - minHalf;
+        long minHalf = (long) Math.pow(10, (intLength - 1) / 2);
+        long maxIndex = (long) Math.pow(10, (intLength + 1) / 2) - minHalf;
         boolean isOdd = intLength % 2 == 1;
         long[] res = new long[queries.length];
         for (int i = 0; i < res.length; i++) {
