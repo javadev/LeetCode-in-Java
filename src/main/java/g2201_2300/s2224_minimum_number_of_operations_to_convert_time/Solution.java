7db04dc0f89153e4ddb9c007b3d16ae5f671ce7a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 20:12:08 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2224_minimum_number_of_operations_to_convert_time/Solution.java
+++ src/main/java/g2201_2300/s2224_minimum_number_of_operations_to_convert_time/Solution.java
@@ -1,43 +1,34 @@
 package g2201_2300.s2224_minimum_number_of_operations_to_convert_time;
 
-// #Easy #String #Greedy #2022_06_12_Time_2_ms_(68.74%)_Space_42.3_MB_(47.60%)
+// #Easy #String #Greedy #2022_06_12_Time_1_ms_(97.78%)_Space_42.7_MB_(18.92%)
 
 public class Solution {
+    private int duration[] = {60, 15, 5, 1};
+    private int c = 0;
+
     public int convertTime(String current, String correct) {
-        int curr = Integer.parseInt(current.substring(0, 2));
-        curr = curr * 60;
-        curr += Integer.parseInt(current.substring(3));
-        int corr =
-                (Integer.parseInt(correct.substring(0, 2)) * 60)
-                        + (Integer.parseInt(correct.substring(3)));
-        int diff = corr - curr;
-        int step = 0;
-        if (diff % 60 == 0) {
-            step = diff / 60;
-            return step;
+        int dmin = Integer.parseInt(correct.substring(3)) - Integer.parseInt(current.substring(3));
+        int dhour =
+                Integer.parseInt(correct.substring(0, 2))
+                        - Integer.parseInt(current.substring(0, 2));
+        int min = dhour * 60 + dmin;
+        dfs(0, min);
+        return c;
+    }
+
+    private void dfs(int i, int amount) {
+        if (i == 4) {
+            return;
+        }
+        if (amount == 0) {
+            return;
+        }
+        if ((amount - duration[i]) >= 0) {
+            c++;
+            dfs(i, amount - duration[i]);
         } else {
-            step = diff / 60;
-            diff = diff % 60;
+            dfs(i + 1, amount);
         }
-        if (diff >= 15) {
-            if (diff % 15 == 0) {
-                step += diff / 15;
-                return step;
-            } else {
-                step += diff / 15;
-                diff = diff % 15;
-            }
-        }
-        if (diff >= 5) {
-            if (diff % 5 == 0) {
-                step += diff / 5;
-                return step;
-            } else {
-                step += diff / 5;
-                diff = diff % 5;
-            }
-        }
-        step += diff;
-        return step;
     }
 }
+
