27464ac1ee6593fd07d159a14ebb35ed3af68a98
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-12 14:48:02 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2210_count_hills_and_valleys_in_an_array/Solution.java
+++ src/main/java/g2201_2300/s2210_count_hills_and_valleys_in_an_array/Solution.java
@@ -3,10 +3,7 @@ package g2201_2300.s2210_count_hills_and_valleys_in_an_array;
 // #Easy #Array #2022_06_12_Time_0_ms_(100.00%)_Space_41.8_MB_(60.16%)
 
 public class Solution {
-    public int countHillValley(int[] nums) {
-        if (nums == null || nums.length <= 2) {
-            return 0;
-        }
+    public int countHillValley(int[] nums) {       
         int left = nums[0];
         int count = 0;
         for (int i = 1; i < nums.length - 1; i++) {
