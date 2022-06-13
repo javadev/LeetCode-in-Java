65369bf0a55a56c9cc81b6d78b11eb95e7e65d9f
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-13 19:05:56 +0300

    Improved task 1175.

--- src.save/main/java/g1101_1200/s1175_prime_arrangements/Solution.java
+++ src.save/main/java/g1101_1200/s1175_prime_arrangements/Solution.java
@@ -1,44 +1,29 @@
 package g1101_1200.s1175_prime_arrangements;
 
-// #Easy #Math #2022_03_05_Time_6_ms_(10.00%)_Space_42.1_MB_(5.71%)
-
-import java.math.BigInteger;
-import java.util.Arrays;
+// #Easy #Math #2022_06_13_Time_0_ms_(100.00%)_Space_40.2_MB_(64.71%)
 
 public class Solution {
-    private static int mod = 1000000007;
-
     public int numPrimeArrangements(int n) {
-        int numberOfPrimes = generatePrimes(n);
-        BigInteger x = factorial(numberOfPrimes);
-        BigInteger y = factorial(n - numberOfPrimes);
-        return x.multiply(y).mod(BigInteger.valueOf(mod)).intValue();
-    }
-
-    private BigInteger factorial(int n) {
-        BigInteger fac = BigInteger.ONE;
-        for (int i = 2; i <= n; i++) {
-            fac = fac.multiply(BigInteger.valueOf(i));
+        int[] a = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83,
+            89, 97
+        };
+        int c = 0;
+        while (c < 25 && n >= a[c]) {
+            c++;
         }
-        return fac.mod(BigInteger.valueOf(mod));
-    }
-
-    private int generatePrimes(int n) {
-        boolean[] prime = new boolean[n + 1];
-        Arrays.fill(prime, 2, n + 1, true);
-        for (int i = 2; i * i <= n; i++) {
-            if (prime[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    prime[j] = false;
-                }
-            }
+        int m = 1000000007;
+        long res = 1l;
+        while ((n - c) > 0) {
+            res *= (n - c);
+            res %= m;
+            n--;
         }
-        int count = 0;
-        for (boolean b : prime) {
-            if (b) {
-                count++;
-            }
+        while (c > 0) {
+            res *= c;
+            res %= m;
+            c--;
         }
-        return count;
+        return (int) res;
     }
 }
