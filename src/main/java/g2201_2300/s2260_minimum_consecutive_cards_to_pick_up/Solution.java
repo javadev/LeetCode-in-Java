b3291b5d2a093d94cf9ff4f7f69d62fe98fcacf2
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2022-06-13 22:25:47 +0530

    updated solution of task 2260"

--- src/main/java/g2201_2300/s2260_minimum_consecutive_cards_to_pick_up/Solution.java
+++ src/main/java/g2201_2300/s2260_minimum_consecutive_cards_to_pick_up/Solution.java
@@ -2,6 +2,10 @@ package g2201_2300.s2260_minimum_consecutive_cards_to_pick_up;
 
 // #Array #Hash_Table #Sliding_Window #2022_06_13_Time_50_ms_(97.04%)_Space_52.5_MB_(98.34%)
 
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.Integer;
+
 public class Solution {
     public int minimumCardPickup(int[] cards) {
         int mindiff = Integer.MAX_VALUE;
