0da5d8854aff079aa3ee659754da1152acebab22
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-13 21:04:46 +0300

    Added tags for tasks 2248, 2249, 2250, 2251, 2255, 2256, 2257.

--- src/main/java/g2201_2300/s2257_count_unguarded_cells_in_the_grid/Solution.java
+++ src/main/java/g2201_2300/s2257_count_unguarded_cells_in_the_grid/Solution.java
@@ -1,6 +1,6 @@
 package g2201_2300.s2257_count_unguarded_cells_in_the_grid;
 
-// #Medium #2022_06_13_Time_32_ms_(70.28%)_Space_128.9_MB_(30.77%)
+// #Medium #Array #Matrix #Simulation #2022_06_13_Time_32_ms_(70.28%)_Space_128.9_MB_(30.77%)
 
 public class Solution {
     public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
