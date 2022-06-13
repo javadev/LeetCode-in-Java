44906864754a331f24e35ced099f6aca84950914
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-13 17:05:06 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2250_count_number_of_rectangles_containing_each_point/Solution.java
+++ src/main/java/g2201_2300/s2250_count_number_of_rectangles_containing_each_point/Solution.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     public int[] countRectangles(int[][] rectangles, int[][] points) {
         List<Integer>[] bucket = new ArrayList[101];
