ec4156edba0435a1e1a6f6c517c9c2ae39acf9b6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-13 20:18:06 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2260_minimum_consecutive_cards_to_pick_up/Solution.java
+++ src/main/java/g2201_2300/s2260_minimum_consecutive_cards_to_pick_up/Solution.java
@@ -1,6 +1,6 @@
 package g2201_2300.s2260_minimum_consecutive_cards_to_pick_up;
 
-// #Array #Hash_Table #Sliding_Window #2022_06_13_Time_50_ms_(97.04%)_Space_52.5_MB_(98.34%)
+// #Medium #Array #Hash_Table #Sliding_Window #2022_06_13_Time_50_ms_(97.04%)_Space_52.5_MB_(98.34%)
 
 import java.util.HashMap;
 import java.util.Map;
