e41da76b9e66c4867debdc3822d263066e26ba39
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-14 21:05:51 +0300

    Fixed style.

--- src/main/java/g2201_2300/s2300_successful_pairs_of_spells_and_potions/Solution.java
+++ src/main/java/g2201_2300/s2300_successful_pairs_of_spells_and_potions/Solution.java
@@ -9,7 +9,8 @@ public class Solution {
     public int[] successfulPairs(int[] spells, int[] potions, long success) {
         Arrays.sort(potions);
         for (int i = 0; i < spells.length; i++) {
-            int l = 0, r = potions.length;
+            int l = 0;
+            int r = potions.length;
             while (l < r) {
                 int m = l + (r - l) / 2;
                 if ((long) spells[i] * potions[m] >= success) {
