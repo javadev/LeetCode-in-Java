e39336586222e58155cf3de55935580664a9c6ac
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-15 16:43:56 +0300

    Added task 2262.

---
+++ src/main/java/g2201_2300/s2262_total_appeal_of_a_string/Solution.java
@@ -0,0 +1,21 @@
+package g2201_2300.s2262_total_appeal_of_a_string;
+
+// #Hard #Hash_Table #String #Dynamic_Programming
+// #2022_06_15_Time_6_ms_(97.92%)_Space_48.3_MB_(80.72%)
+
+import java.util.Arrays;
+
+public class Solution {
+    public long appealSum(String s) {
+        int len = s.length();
+        int[] lastPos = new int[26];
+        Arrays.fill(lastPos, -1);
+        long res = 0;
+        for (int i = 0; i < len; i++) {
+            int idx = s.charAt(i) - 'a';
+            res += (i - lastPos[idx]) * (len - i);
+            lastPos[idx] = i;
+        }
+        return res;
+    }
+}
