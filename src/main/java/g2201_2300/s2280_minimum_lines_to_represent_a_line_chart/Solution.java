307bd5e8f30552030d582effb055acced0b4dfd7
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-15 09:05:27 +0300

    Updated tags.

--- src/main/java/g2201_2300/s2280_minimum_lines_to_represent_a_line_chart/Solution.java
+++ src/main/java/g2201_2300/s2280_minimum_lines_to_represent_a_line_chart/Solution.java
@@ -1,5 +1,6 @@
 package g2201_2300.s2280_minimum_lines_to_represent_a_line_chart;
 
+// #Medium #Array #Math #Geometry #Sorting #Number_Theory
 // #2022_06_15_Time_40_ms_(96.09%)_Space_89.1_MB_(92.63%)
 
 import java.util.Arrays;
