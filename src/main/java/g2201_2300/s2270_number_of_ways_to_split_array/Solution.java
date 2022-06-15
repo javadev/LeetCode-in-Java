8ed193b3a255a924e8f39e494ffee22d9195d5bb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-15 20:21:58 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2270_number_of_ways_to_split_array/Solution.java
+++ src/main/java/g2201_2300/s2270_number_of_ways_to_split_array/Solution.java
@@ -1,6 +1,6 @@
 package g2201_2300.s2270_number_of_ways_to_split_array;
 
-// #2022_06_15_Time_4_ms_(77.55%)_Space_91.9_MB_(18.27%)
+// #Medium #Array #Prefix_Sum #2022_06_15_Time_4_ms_(77.55%)_Space_91.9_MB_(18.27%)
 
 public class Solution {
     public int waysToSplitArray(int[] nums) {
