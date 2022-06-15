da8a5249a969164af13663eb20382fede11a377a
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-15 20:19:21 +0300

    Update Solution.java

--- src/main/java/g2201_2300/s2266_count_number_of_texts/Solution.java
+++ src/main/java/g2201_2300/s2266_count_number_of_texts/Solution.java
@@ -1,7 +1,9 @@
 package g2201_2300.s2266_count_number_of_texts;
 
+// #Medium #Hash_Table #Math #String #Dynamic_Programming
 // #2022_06_15_Time_38_ms_(81.43%)_Space_51.2_MB_(80.48%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int countTexts(String pressedKeys) {
         int len = pressedKeys.length();
