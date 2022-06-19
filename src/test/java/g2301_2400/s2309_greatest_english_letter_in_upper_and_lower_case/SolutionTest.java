72a32f11246b7b3b5c101f58f6fbe2b4ab13f9f5
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-19 21:35:30 +0300

    Fixed sonar

--- src/test/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/SolutionTest.java
+++ src/test/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/SolutionTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("java:S5778")
 class SolutionTest {
     @Test
     void greatestLetter() {
