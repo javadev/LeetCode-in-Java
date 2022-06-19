715f4b5eb10a8b5d1de447816df497b9e3c05a39
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-19 21:24:42 +0300

    Added tests

--- src/test/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/SolutionTest.java
+++ src/test/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/SolutionTest.java
@@ -2,6 +2,7 @@ package g2301_2400.s2309_greatest_english_letter_in_upper_and_lower_case;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +15,16 @@ class SolutionTest {
     @Test
     void greatestLetter2() {
         assertThat(new Solution().greatestLetter("arRAzFif"), equalTo("R"));
+    }
+
+    @Test
+    void greatestLetter3() {
+        assertThat(new Solution().greatestLetter(""), equalTo(""));
+    }
+
+    @Test
+    void greatestLetter4() {
+        assertThrows(
+                ArrayIndexOutOfBoundsException.class, () -> new Solution().greatestLetter("0|"));
     }
 }
