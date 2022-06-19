ed858983441c9cf895367d22ae005f117de487cc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-19 23:54:27 +0300

    Update Solution.java

--- src/main/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/Solution.java
+++ src/main/java/g2301_2400/s2309_greatest_english_letter_in_upper_and_lower_case/Solution.java
@@ -1,25 +1,28 @@
 package g2301_2400.s2309_greatest_english_letter_in_upper_and_lower_case;
 
-// #Easy #Array #2022_06_19_Time_5_ms_(75.00%)_Space_42.3_MB_(100.00%)
+// #Easy #Array #2022_06_19_Time_4_ms_(75.00%)_Space_42.9_MB_(50.00%)
 
 public class Solution {
     public String greatestLetter(String s) {
-        char gt = ' ';
-        boolean[] sA = new boolean[26];
-        boolean[] uA = new boolean[26];
-        for (char ch : s.toCharArray()) {
-            int i = 0;
-            if (ch <= 'Z' && ch >= 'A') {
-                i = ch - 'A';
-                uA[i] = true;
+        boolean[] lowers = new boolean[26];
+        boolean[] uppers = new boolean[26];
+        for (char c : s.toCharArray()) {
+            if ('a' <= c && c <= 'z') {
+                lowers[c - 'a'] = true;
             } else {
-                i = ch - 'a';
-                sA[i] = true;
-            }
-            if (uA[i] == sA[i] && gt < ('A' + i)) {
-                gt = (char) ('A' + i);
+                uppers[c - 'A'] = true;
             }
         }
-        return gt == ' ' ? "" : gt + "";
+        char res = ' ';
+        for (int i = 0; i < 26; ++i) {
+            if (lowers[i] && uppers[i]) {
+                res = (char) ('A' + i);
+            }
+        }
+        if (res != ' ') {
+            return "" + res;
+        } else {
+            return "";
+        }
     }
 }
