a4fbe97980ade237b14bf00590d772d2cd71ab36
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-20 10:34:59 +0300

    Updated tags.

--- src/main/java/g2301_2400/s2312_selling_pieces_of_wood/Solution.java
+++ src/main/java/g2301_2400/s2312_selling_pieces_of_wood/Solution.java
@@ -1,6 +1,6 @@
 package g2301_2400.s2312_selling_pieces_of_wood;
 
-// #2022_06_20_Time_78_ms_(63.64%)_Space_53.1_MB_(63.64%)
+// #Hard #Backtracking #2022_06_20_Time_78_ms_(63.64%)_Space_53.1_MB_(63.64%)
 
 public class Solution {
     public long sellingWood(int m, int n, int[][] prices) {
