f2f01bd0acb8d9fd1881cfba50d82488a65fc5eb
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-21 18:39:30 +0300

    Improved task 2029.

--- src.save/main/java/g2001_2100/s2029_stone_game_ix/Solution.java
+++ src.save/main/java/g2001_2100/s2029_stone_game_ix/Solution.java
@@ -1,84 +1,38 @@
 package g2001_2100.s2029_stone_game_ix;
 
 // #Medium #Array #Math #Greedy #Counting #Game_Theory
-// #2022_05_25_Time_31_ms_(10.87%)_Space_114.5_MB_(65.22%)
+// #2022_06_21_Time_14_ms_(53.97%)_Space_126.8_MB_(25.40%)
 
 public class Solution {
-    private int[] stones;
-
     public boolean stoneGameIX(int[] stones) {
-        this.stones = stones;
-        int[] freq = new int[3];
+        int zero = 0;
+        int one = 0;
+        int two = 0;
         for (int i : stones) {
             if (i % 3 == 0) {
-                freq[0]++;
+                zero++;
             } else if (i % 3 == 1) {
-                freq[1]++;
-            } else {
-                freq[2]++;
+                one++;
+            } else if (i % 3 == 2) {
+                two++;
             }
         }
-        boolean b1 = false;
-        boolean b2 = false;
-        int[] a = freq.clone();
-        int[] b = freq.clone();
-        if (a[1] > 0) {
-            a[1]--;
-            b1 = fun(a, 1);
+        if (one == 0 && two == 0) {
+            return false;
         }
-        if (b[2] > 0) {
-            b[2]--;
-            b2 = fun(b, 2);
-        }
-        return b1 || b2;
-    }
-
-    private boolean fun(int[] freq, int sum) {
-        int n = stones.length;
-        int i = 1;
-        while (i < n) {
-            if (i % 2 == 0) {
-                if (sum % 3 == 1) {
-                    if (freq[0] > 0) {
-                        freq[0]--;
-                    } else if (freq[1] > 0) {
-                        freq[1]--;
-                        sum += 1;
-                    } else {
-                        return false;
-                    }
-                } else if (sum % 3 == 2) {
-                    if (freq[0] > 0) {
-                        freq[0]--;
-                    } else if (freq[2] > 0) {
-                        freq[2]--;
-                        sum += 2;
-                    } else {
-                        return false;
-                    }
-                }
-            } else {
-                if (sum % 3 == 2) {
-                    if (freq[0] > 0) {
-                        freq[0]--;
-                    } else if (freq[2] > 0) {
-                        freq[2]--;
-                        sum += 2;
-                    } else {
-                        return true;
-                    }
-                } else if (sum % 3 == 1) {
-                    if (freq[0] > 0) {
-                        freq[0]--;
-                    } else if (freq[1] > 0) {
-                        freq[1]--;
-                        sum += 1;
-                    } else {
-                        return true;
-                    }
-                }
+        int max = Math.max(one, two);
+        int min = Math.min(one, two);
+        if (zero % 2 == 0) {
+            if (min == 0) {
+                return false;
             }
-            i++;
+            return true;
+        }
+        if (zero % 2 == 1) {
+            if (max - 2 > min) {
+                return true;
+            }
+            return false;
         }
         return false;
     }
