937f5d2d40089c612cea3611721d52d59b327916
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-23 01:48:15 +0300

    Update script.sql

--- src.save/main/java/g0501_0600/s0584_find_customer_referee/script.sql
+++ src.save/main/java/g0501_0600/s0584_find_customer_referee/script.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
 # #Easy #Database #SQL_I_Day_1_Select #2022_04_13_Time_505_ms_(43.87%)_Space_0B_(100.00%)
-SELECT  name FROM customer where referee_id  != 2 or referee_id is null;
+SELECT name FROM customer where referee_id != 2 or referee_id is null;
