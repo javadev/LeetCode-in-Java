da87d8b4cc2cf922de9070d061abf536a2ce9683
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-22 09:37:26 +0300

    Improved task 1551.

--- src.save/main/java/g1501_1600/s1551_minimum_operations_to_make_array_equal/Solution.java
+++ src.save/main/java/g1501_1600/s1551_minimum_operations_to_make_array_equal/Solution.java
@@ -1,16 +1,9 @@
 package g1501_1600.s1551_minimum_operations_to_make_array_equal;
 
-// #Medium #Math #2022_04_11_Time_7_ms_(12.93%)_Space_40.9_MB_(68.22%)
+// #Medium #Math #2022_06_22_Time_0_ms_(100.00%)_Space_41.3_MB_(35.47%)
 
 public class Solution {
     public int minOperations(int n) {
-        int min = 1;
-        int max = 2 * (n - 1) + 1;
-        int equalNumber = (max + min) / 2;
-        int ops = 0;
-        for (int i = 0; i < n / 2; i++) {
-            ops += equalNumber - (2 * i + 1);
-        }
-        return ops;
+        return (n * n) / 4;
     }
 }
