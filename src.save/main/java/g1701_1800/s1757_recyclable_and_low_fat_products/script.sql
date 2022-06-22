ea06994a4cd3f4a2a364fbe60387ed13b455a75c
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-23 01:47:27 +0300

    Update script.sql

--- src.save/main/java/g1701_1800/s1757_recyclable_and_low_fat_products/script.sql
+++ src.save/main/java/g1701_1800/s1757_recyclable_and_low_fat_products/script.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 # #Easy #Database #SQL_I_Day_1_Select #2022_05_03_Time_475_ms_(71.54%)_Space_0B_(100.00%)
-select product_id  from Products 
+select product_id from Products 
 where low_fats='Y' and recyclable='Y'
