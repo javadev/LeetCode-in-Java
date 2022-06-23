4cf3991ef19cba98b71037a3c971f72a579e3e91
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-23 15:16:03 +0300

    Improved task 1460.

--- src.save/main/java/g1401_1500/s1460_make_two_arrays_equal_by_reversing_sub_arrays/Solution.java
+++ src.save/main/java/g1401_1500/s1460_make_two_arrays_equal_by_reversing_sub_arrays/Solution.java
@@ -1,28 +1,20 @@
 package g1401_1500.s1460_make_two_arrays_equal_by_reversing_sub_arrays;
 
-// #Easy #Array #Hash_Table #Sorting #2022_03_29_Time_13_ms_(14.65%)_Space_47.6_MB_(22.39%)
+// #Easy #Array #Hash_Table #Sorting #2022_06_23_Time_4_ms_(74.61%)_Space_45.8_MB_(56.42%)
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int num : target) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
-        for (int num : arr) {
-            if (!map.containsKey(num)) {
-                return false;
-            } else {
-                map.put(num, map.get(num) - 1);
+        int n = target.length;
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < target.length; i++) {
+            if (target[i] == arr[i]) {
+                count++;
             }
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() != 0) {
-                return false;
-            }
-        }
-        return true;
+        return count == n;
     }
 }
