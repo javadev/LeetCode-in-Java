63d4a053b053f79dbb61e1a61adf6148c99c0868
Valentyn Kolesnikov <javadev75@gmail.com>
2022-06-28 16:52:42 +0300

    Improved task 2319

--- src/main/java/g2301_2400/s2319_check_if_matrix_is_x_matrix/Solution.java
+++ src/main/java/g2301_2400/s2319_check_if_matrix_is_x_matrix/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
     public boolean checkXMatrix(int[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if ((i == j) || (i + j) == (grid.length - 1)) {
+                if (i == j || i + j == grid.length - 1) {
                     if (grid[i][j] == 0) {
                         return false;
                     }
