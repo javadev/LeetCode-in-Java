8c70f18ff19b2ea5f2966fe3680642ab13815738
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-04 05:07:01 +0300

    Added tasks 2327, 2328.

---
+++ src/main/java/g2301_2400/s2327_number_of_people_aware_of_a_secret/Solution.java
@@ -0,0 +1,25 @@
+package g2301_2400.s2327_number_of_people_aware_of_a_secret;
+
+// #Medium #2022_07_04_Time_7_ms_(80.00%)_Space_44_MB_(10.00%)
+
+public class Solution {
+    public int peopleAwareOfSecret(int n, int delay, int forget) {
+        long[][] dp = new long[n + forget][3];
+        // 0: people who currently know the secret (includes [1] below)
+        // 1: people who start sharing the secret on this day
+        // 2: people who forget on this day
+        long mod = (long) 1e9 + 7;
+        dp[0][0] = dp[delay][1] = dp[forget][2] = 1;
+        for (int i = 1; i < n; i++) {
+            // dp[i][1] was originally just the i - delay newcomers
+            dp[i][1] = (dp[i][1] + dp[i - 1][1] - dp[i][2] + mod) % mod;
+            // these people forget on i + forget day
+            dp[i + forget][2] = dp[i][1];
+            // these people start sharing on i + delay day
+            dp[i + delay][1] = dp[i][1];
+            // today's total people who know the secret
+            dp[i][0] = (dp[i - 1][0] + dp[i][1] - dp[i][2] + mod) % mod;
+        }
+        return (int) dp[n - 1][0];
+    }
+}
---
+++ src/main/java/g2301_2400/s2328_number_of_increasing_paths_in_a_grid/Solution.java
@@ -0,0 +1,42 @@
+package g2301_2400.s2328_number_of_increasing_paths_in_a_grid;
+
+// #Hard #2022_07_04_Time_43_ms_(100.00%)_Space_89.1_MB_(63.64%)
+
+public class Solution {
+    private int help(int a[][], int i, int j, int n, int m, int dp[][]) {
+        if (i < 0 || i >= n || j >= m || j < 0) {
+            return 0;
+        }
+        if (dp[i][j] != 0) {
+            return dp[i][j];
+        }
+        long res = 0;
+        if (i < n - 1 && a[i + 1][j] > a[i][j]) {
+            res += 1 + help(a, i + 1, j, n, m, dp);
+        }
+        if (i > 0 && a[i - 1][j] > a[i][j]) {
+            res += 1 + help(a, i - 1, j, n, m, dp);
+        }
+        if (j > 0 && a[i][j - 1] > a[i][j]) {
+            res += 1 + help(a, i, j - 1, n, m, dp);
+        }
+        if (j < m - 1 && a[i][j + 1] > a[i][j]) {
+            res += 1 + help(a, i, j + 1, n, m, dp);
+        }
+        return dp[i][j] = (int) res % 1000000007;
+    }
+
+    public int countPaths(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        long ans = n * m;
+        int dp[][] = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans += help(grid, i, j, n, m, dp) % 1000000007;
+            }
+        }
+        ans = ans % 1000000007;
+        return (int) ans;
+    }
+}
