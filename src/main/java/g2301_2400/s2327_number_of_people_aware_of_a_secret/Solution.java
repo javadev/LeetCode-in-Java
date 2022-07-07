1c9a3c34136891819c345e930ec47a296510d686
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-07 15:50:25 +0300

    Added tags.

--- src/main/java/g2301_2400/s2327_number_of_people_aware_of_a_secret/Solution.java
+++ src/main/java/g2301_2400/s2327_number_of_people_aware_of_a_secret/Solution.java
@@ -1,6 +1,7 @@
 package g2301_2400.s2327_number_of_people_aware_of_a_secret;
 
-// #Medium #2022_07_04_Time_7_ms_(80.00%)_Space_44_MB_(10.00%)
+// #Medium #Dynamic_Programming #Queue #Simulation
+// #2022_07_04_Time_7_ms_(80.00%)_Space_44_MB_(10.00%)
 
 public class Solution {
     public int peopleAwareOfSecret(int n, int delay, int forget) {
