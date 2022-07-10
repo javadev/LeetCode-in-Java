3b8845767020182316f2ab52dd9ea12ec3986cca
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-10 12:32:36 +0300

    Improved task 176.

--- src.save/main/java/g0101_0200/s0176_second_highest_salary/script.sql
+++ src.save/main/java/g0101_0200/s0176_second_highest_salary/script.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 # #Medium #Database #SQL_I_Day_4_Union_and_Select
-# #2022_06_26_Time_405_ms_(13.88%)_Space_0B_(100.00%)
+# #2022_07_10_Time_225_ms_(73.10%)_Space_0B_(100.00%)
 SELECT ifnull(
                 (SELECT distinct(Salary)
                  FROM Employee
