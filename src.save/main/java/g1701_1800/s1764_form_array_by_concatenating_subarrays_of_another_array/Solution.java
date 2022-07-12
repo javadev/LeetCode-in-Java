f54b094ab94811f2b9cc2451f89f96444bf04603
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-12 08:24:15 +0300

    Improved task 1764.

--- src.save/main/java/g1701_1800/s1764_form_array_by_concatenating_subarrays_of_another_array/Solution.java
+++ src.save/main/java/g1701_1800/s1764_form_array_by_concatenating_subarrays_of_another_array/Solution.java
@@ -1,29 +1,39 @@
 package g1701_1800.s1764_form_array_by_concatenating_subarrays_of_another_array;
 
-// #Medium #Array #Greedy #String_Matching #2022_04_30_Time_7_ms_(16.90%)_Space_42.4_MB_(85.92%)
+// #Medium #Array #Greedy #String_Matching #2022_07_12_Time_3_ms_(43.69%)_Space_45.5_MB_(69.90%)
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Solution {
     public boolean canChoose(int[][] groups, int[] nums) {
-        List<Integer> numsInt = new ArrayList<>();
-        for (int num : nums) {
-            numsInt.add(num);
-        }
-        int prevIndex = 0;
-        for (int[] group : groups) {
-            List<Integer> groupInt = new ArrayList<>();
-            for (int num : group) {
-                groupInt.add(num);
+        int prev = 0;
+        for (int i = 0; i < groups.length; i++) {
+            int[] temp = new int[groups[i].length];
+            if (prev + groups[i].length > nums.length) {
+                return false;
             }
-            int index =
-                    Collections.indexOfSubList(
-                            numsInt.subList(prevIndex, numsInt.size()), groupInt);
-            if (index != -1) {
-                prevIndex = index + group.length;
-            } else {
+            int index = 0;
+            int j;
+            for (j = prev; j < prev + groups[i].length; j++) {
+                temp[index++] = nums[j];
+            }
+            if (Arrays.equals(temp, groups[i])) {
+                prev = j;
+                continue;
+            }
+            int k;
+            for (k = j; k < nums.length; k++) {
+                int l;
+                for (l = 0; l < temp.length - 1; l++) {
+                    temp[l] = temp[l + 1];
+                }
+                temp[l] = nums[k];
+                if (Arrays.equals(temp, groups[i])) {
+                    prev = k + 1;
+                    break;
+                }
+            }
+            if (k == nums.length) {
                 return false;
             }
         }
