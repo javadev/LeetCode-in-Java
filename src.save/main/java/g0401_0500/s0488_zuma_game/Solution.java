c95d5e2e03e38aa8286e848c49875ba1390fdafa
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-13 18:46:33 +0300

    Improved task 488.

--- src.save/main/java/g0401_0500/s0488_zuma_game/Solution.java
+++ src.save/main/java/g0401_0500/s0488_zuma_game/Solution.java
@@ -1,164 +1,79 @@
 package g0401_0500.s0488_zuma_game;
 
 // #Hard #String #Dynamic_Programming #Breadth_First_Search #Memoization
-// #2022_04_15_Time_2077_ms_(7.41%)_Space_42.5_MB_(77.78%)
+// #2022_07_13_Time_1989_ms_(32.79%)_Space_56.5_MB_(81.97%)
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Stack;
 
-@SuppressWarnings("java:S135")
 public class Solution {
-    public int findMinStep(String board, String hand) {
-        List<Character> boardList = new ArrayList<>();
-        for (char c : board.toCharArray()) {
-            boardList.add(c);
-        }
-        List<Character> handList = new ArrayList<>();
-        for (char c : hand.toCharArray()) {
-            handList.add(c);
-        }
-        return findMinStep(boardList, handList);
-    }
+    private HashMap<String, Integer> map = new HashMap<>();
 
-    private int findMinStep(List<Character> boardList, List<Character> handList) {
-        if (boardList.isEmpty()) {
-            return 0;
-        }
-        if (handList.isEmpty()) {
-            return -1;
-        }
-        if (!isValid(boardList, handList)) {
-            return -1;
-        }
-        int res = Integer.MAX_VALUE;
-        for (int boardListIndex = 0; boardListIndex <= boardList.size(); boardListIndex++) {
-            Set<Character> duplicate = new HashSet<>();
-            for (int handListIndex = 0; handListIndex < handList.size(); handListIndex++) {
-                if (boardListIndex > 0
-                        && boardList.get(boardListIndex - 1).equals(handList.get(handListIndex))) {
-                    continue;
-                }
-                if (duplicate.contains(handList.get(handListIndex))) {
-                    continue;
+    private String removeConsecutiveThreeOrMoreBalls(String board) {
+        Stack<Character> st = new Stack<>();
+        int n = board.length();
+        for (int i = 0; i < n; i++) {
+            char ch = board.charAt(i);
+            st.push(ch);
+            if (st.size() >= 3 && (i == n - 1 || ch != board.charAt(i + 1))) {
+                char a = st.pop();
+                char b = st.pop();
+                char c = st.pop();
+                if (a == b && b == c) {
+                    while (!st.isEmpty() && st.peek() == a) {
+                        st.pop();
+                    }
                 } else {
-                    duplicate.add(handList.get(handListIndex));
-                }
-                boolean goodCase1 =
-                        (boardListIndex < boardList.size()
-                                && boardList
-                                        .get(boardListIndex)
-                                        .equals(handList.get(handListIndex)));
-                boolean goodCase2 =
-                        (boardListIndex > 0
-                                && boardListIndex < boardList.size()
-                                && boardList
-                                        .get(boardListIndex - 1)
-                                        .equals(boardList.get(boardListIndex))
-                                && !boardList
-                                        .get(boardListIndex - 1)
-                                        .equals(handList.get(handListIndex)));
-                if (!goodCase1 && !goodCase2) {
-                    continue;
-                }
-                List<Character> boardListClone = new ArrayList<>(boardList);
-                List<Character> handListClone = new ArrayList<>(handList);
-                boardListClone.add(boardListIndex, handListClone.remove(handListIndex));
-                cleanup(boardListClone);
-                int preRes = findMinStep(boardListClone, handListClone);
-                if (preRes != -1 && preRes < res) {
-                    res = preRes;
+                    st.push(c);
+                    st.push(b);
+                    st.push(a);
                 }
             }
         }
-
-        if (res == Integer.MAX_VALUE) {
-            return -1;
+        StringBuilder res = new StringBuilder();
+        while (!st.isEmpty()) {
+            res.append(st.pop());
         }
-        return res + 1;
+        return res.reverse().toString();
     }
 
-    public void cleanup(List<Character> boardList) {
-        boolean isCleanup = false;
-        while (!isCleanup) {
-            isCleanup = true;
-            for (int i = 0; i < boardList.size() - 2; i++) {
-                int repeatLen = 1;
-                for (int j = i + 1; j < boardList.size(); j++) {
-                    if (boardList.get(j).equals(boardList.get(j - 1))) {
-                        repeatLen++;
-                    } else {
-                        break;
-                    }
-                }
-                if (repeatLen >= 3) {
-                    isCleanup = false;
-                    for (; repeatLen > 0; repeatLen--) {
-                        boardList.remove(i);
-                    }
-                }
-            }
-        }
+    private String ss(String s, int i, int j) {
+        return s.substring(i, j);
     }
 
-    public boolean isValid(List<Character> boardList, List<Character> handList) {
-        int boardR = 0;
-        int boardY = 0;
-        int boardB = 0;
-        int boardG = 0;
-        int boardW = 0;
-        int handR = 0;
-        int handY = 0;
-        int handB = 0;
-        int handG = 0;
-        int handW = 0;
-        for (char c : boardList) {
-            if (c == 'R') {
-                boardR++;
-            }
-            if (c == 'Y') {
-                boardY++;
-            }
-            if (c == 'B') {
-                boardB++;
-            }
-            if (c == 'G') {
-                boardG++;
-            }
-            if (c == 'W') {
-                boardW++;
+    private int solve(String board, String hand) {
+        String key = board + "#" + hand;
+        if (map.containsKey(key)) {
+            return map.get(key);
+        }
+        board = removeConsecutiveThreeOrMoreBalls(board);
+        int ans = 100, n = board.length(), m = hand.length();
+        if (n == 0 || m == 0) {
+            map.put(key, n == 0 ? 0 : 100);
+            return n == 0 ? 0 : 100;
+        }
+        for (int i = 0; i < hand.length(); i++) {
+            for (int j = 0; j < n; j++) {
+                if (board.charAt(j) == hand.charAt(i)
+                        || (j < n - 1 && board.charAt(j) == board.charAt(j + 1))) {
+                    ans =
+                            Math.min(
+                                    ans,
+                                    1
+                                            + solve(
+                                                    ss(board, 0, j + 1)
+                                                            + hand.charAt(i)
+                                                            + ss(board, j + 1, n),
+                                                    ss(hand, 0, i) + ss(hand, i + 1, m)));
+                }
             }
         }
-        for (char c : handList) {
-            if (c == 'R') {
-                handR++;
-            }
-            if (c == 'Y') {
-                handY++;
-            }
-            if (c == 'B') {
-                handB++;
-            }
-            if (c == 'G') {
-                handG++;
-            }
-            if (c == 'W') {
-                handW++;
-            }
-        }
-        if (boardR < 3 && boardR > 0 && boardR + handR < 3) {
-            return false;
-        }
-        if (boardY < 3 && boardY > 0 && boardY + handY < 3) {
-            return false;
-        }
-        if (boardB < 3 && boardB > 0 && boardB + handB < 3) {
-            return false;
-        }
-        if (boardG < 3 && boardG > 0 && boardG + handG < 3) {
-            return false;
-        }
-        return boardW >= 3 || boardW <= 0 || boardW + handW >= 3;
+        map.put(key, ans);
+        return ans;
+    }
+
+    public int findMinStep(String board, String hand) {
+        int ans = solve(board, hand);
+        return ans >= 100 ? -1 : ans;
     }
 }
