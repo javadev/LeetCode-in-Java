f2fc2ce1a42b3b75aaef9fe6191d5a51b573e235
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-13 09:54:28 +0300

    Added tags.

--- src/main/java/g2301_2400/s2331_evaluate_boolean_binary_tree/Solution.java
+++ src/main/java/g2301_2400/s2331_evaluate_boolean_binary_tree/Solution.java
@@ -1,6 +1,7 @@
 package g2301_2400.s2331_evaluate_boolean_binary_tree;
 
-// #Easy #2022_07_10_Time_0_ms_(100.00%)_Space_46.1_MB_(85.71%)
+// #Easy #Binary_Search #Tree #Depth_First_Search
+// #2022_07_10_Time_0_ms_(100.00%)_Space_46.1_MB_(85.71%)
 
 import com_github_leetcode.TreeNode;
 
--- src/main/java/g2301_2400/s2333_minimum_sum_of_squared_difference/Solution.java
+++ src/main/java/g2301_2400/s2333_minimum_sum_of_squared_difference/Solution.java
@@ -1,6 +1,7 @@
 package g2301_2400.s2333_minimum_sum_of_squared_difference;
 
-// #Medium #2022_07_12_Time_15_ms_(95.13%)_Space_106.4_MB_(46.91%)
+// #Medium #Array #Math #Sorting #Heap_Priority_Queue
+// #2022_07_12_Time_15_ms_(95.13%)_Space_106.4_MB_(46.91%)
 
 public class Solution {
     public long minSumSquareDiff(int[] nums1, int[] nums2, int k1, int k2) {
