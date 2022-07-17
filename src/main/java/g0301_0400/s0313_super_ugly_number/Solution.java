cf55901cc84b000736cb9324b6e0a668f1f62ac2
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-17 14:06:35 +0300

    Fixed sonar warnings, updated readme.

--- src/main/java/g0301_0400/s0313_super_ugly_number/Solution.java
+++ src/main/java/g0301_0400/s0313_super_ugly_number/Solution.java
@@ -2,6 +2,7 @@ package g0301_0400.s0313_super_ugly_number;
 
 // #Medium #Array #Dynamic_Programming #Math #2022_07_10_Time_16_ms_(96.57%)_Space_42.3_MB_(64.43%)
 
+@SuppressWarnings("java:S3012")
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
         long[] primes1 = new long[primes.length];
