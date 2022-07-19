ca9adae8bb961cd7d85d138ffc5dc1f5a33c6391
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-19 17:20:17 +0300

    Added tasks 2343, 2344.

---
+++ src/main/java/g2301_2400/s2344_minimum_deletions_to_make_array_divisible/Solution.java
@@ -0,0 +1,34 @@
+package g2301_2400.s2344_minimum_deletions_to_make_array_divisible;
+
+// #Hard #2022_07_19_Time_13_ms_(88.89%)_Space_77.2_MB_(77.78%)
+
+public class Solution {
+    public int minOperations(int[] nums, int[] numsDivide) {
+        int g = numsDivide[0];
+        for (int i : numsDivide) {
+            g = gcd(g, i);
+        }
+        int minOp = 0;
+        int smallest = Integer.MAX_VALUE;
+        for (int num : nums) {
+            if (g % num == 0) {
+                smallest = Math.min(smallest, num);
+            }
+        }
+        for (int num : nums) {
+            if (num < smallest) {
+                ++minOp;
+            }
+        }
+        return smallest == Integer.MAX_VALUE ? -1 : minOp;
+    }
+
+    private int gcd(int a, int b) {
+        while (b > 0) {
+            int tmp = a;
+            a = b;
+            b = tmp % b;
+        }
+        return a;
+    }
+}
