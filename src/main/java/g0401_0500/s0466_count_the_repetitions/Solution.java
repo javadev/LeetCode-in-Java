ad79e96c3ba1bb7ea0dfc0aa8f128e2303224b85
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-19 17:32:44 +0300

    Updated exec time 459-470.

--- src/main/java/g0401_0500/s0466_count_the_repetitions/Solution.java
+++ src/main/java/g0401_0500/s0466_count_the_repetitions/Solution.java
@@ -1,6 +1,6 @@
 package g0401_0500.s0466_count_the_repetitions;
 
-// #Hard #String #Dynamic_Programming #2022_03_18_Time_1_ms_(100.00%)_Space_42_MB_(50.63%)
+// #Hard #String #Dynamic_Programming #2022_07_19_Time_1_ms_(100.00%)_Space_42.4_MB_(23.19%)
 
 public class Solution {
     public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
--- src/main/java/g0401_0500/s0467_unique_substrings_in_wraparound_string/Solution.java
+++ src/main/java/g0401_0500/s0467_unique_substrings_in_wraparound_string/Solution.java
@@ -1,6 +1,6 @@
 package g0401_0500.s0467_unique_substrings_in_wraparound_string;
 
-// #Medium #String #Dynamic_Programming #2022_03_18_Time_5_ms_(88.61%)_Space_43.3_MB_(34.18%)
+// #Medium #String #Dynamic_Programming #2022_07_19_Time_4_ms_(98.79%)_Space_44_MB_(5.26%)
 
 public class Solution {
     public int findSubstringInWraproundString(String p) {
