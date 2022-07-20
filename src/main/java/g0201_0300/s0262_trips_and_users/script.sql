0111d6811cc32ccc1decb906d463d1463c0ac650
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-20 15:33:08 +0300

    Added test 262.

--- src/main/java/g0201_0300/s0262_trips_and_users/script.sql
+++ src/main/java/g0201_0300/s0262_trips_and_users/script.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 # #Hard #Database #2022_07_05_Time_420_ms_(95.80%)_Space_0B_(100.00%)
 SELECT request_at AS "Day",
-       ROUND(SUM(CASE
-                     WHEN t.status!= 'completed' THEN 1
+       ROUND(cast(SUM(CASE
+                     WHEN t.status != 'completed' THEN 1
                      ELSE 0
-                 END)/count(*), 2) AS "Cancellation Rate"
+                 END) as decimal(7,2))/count(*), 2) AS "Cancellation Rate"
 FROM Trips t
 INNER JOIN Users a ON (a.users_id = t.client_id)
-AND (a.banned="No")
+AND (a.banned='No')
 INNER JOIN Users b ON (b.users_id = t.driver_id)
-AND (b.banned="No")
-WHERE request_at BETWEEN "2013-10-01" AND "2013-10-03"
+AND (b.banned='No')
+WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY request_at;
