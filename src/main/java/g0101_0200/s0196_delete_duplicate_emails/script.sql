3a2c6aa9a39ad1d53d989ac8b0442316f8c2a1c6
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-20 13:09:21 +0300

    Fixed script

--- src/main/java/g0101_0200/s0196_delete_duplicate_emails/script.sql
+++ src/main/java/g0101_0200/s0196_delete_duplicate_emails/script.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 # #Easy #Database #SQL_I_Day_2_Select_and_Order #2022_06_28_Time_903_ms_(48.10%)_Space_0B_(100.00%)
 DELETE FROM Person
-WHERE Id NOT IN (SELECT id FROM (SELECT Email, MIN(Id) AS 'id' FROM Person GROUP BY Email) t)
+WHERE Id NOT IN (SELECT id FROM (SELECT Email, MIN(Id) AS id FROM Person GROUP BY Email) t)
