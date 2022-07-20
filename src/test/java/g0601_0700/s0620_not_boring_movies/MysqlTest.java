c31d5814b8d2551ad86f868b4ca8d32c0b8bf77a
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2022-07-21 01:07:43 +0700

    Added tests for tasks 620, 626, 1050.

---
+++ src/test/java/g0601_0700/s0620_not_boring_movies/MysqlTest.java
@@ -0,0 +1,64 @@
+package g0601_0700.s0620_not_boring_movies;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.zapodot.junit.db.annotations.EmbeddedDatabase;
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest;
+import org.zapodot.junit.db.common.CompatibilityMode;
+
+@EmbeddedDatabaseTest(
+        compatibilityMode = CompatibilityMode.MySQL,
+        initialSqls =
+                "CREATE TABLE cinema(id INTEGER PRIMARY KEY, movie VARCHAR(512)"
+                        + ", description VARCHAR(512), rating FLOAT); "
+                        + "INSERT INTO cinema(id, movie, description, rating) "
+                        + "VALUES (1, 'War', 'great 3D', 8.9); "
+                        + "INSERT INTO cinema(id, movie, description, rating) "
+                        + "VALUES (2, 'Science', 'fiction', 8.5); "
+                        + "INSERT INTO cinema(id, movie, description, rating) "
+                        + "VALUES (3, 'irish', 'boring', 6.2); "
+                        + "INSERT INTO cinema(id, movie, description, rating) "
+                        + "VALUES (4, 'Ice song', 'Fantacy', 8.6);"
+                        + "INSERT INTO cinema(id, movie, description, rating) "
+                        + "VALUES (5, 'House card', 'Interesting', 9.1);")
+class MysqlTest {
+    @Test
+    void testScript(@EmbeddedDatabase DataSource dataSource)
+            throws SQLException, FileNotFoundException {
+        try (final Connection connection = dataSource.getConnection()) {
+            try (final Statement statement = connection.createStatement();
+                    final ResultSet resultSet =
+                            statement.executeQuery(
+                                    new BufferedReader(
+                                                    new FileReader(
+                                                            "src/main/java/g0601_0700/"
+                                                                    + "s0620_not_boring_movies/script.sql"))
+                                            .lines()
+                                            .collect(Collectors.joining("\n"))
+                                            .replaceAll("#.*?\\r?\\n", ""))) {
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getInt(1), equalTo(5));
+                assertThat(resultSet.getNString(2), equalTo("House card"));
+                assertThat(resultSet.getNString(3), equalTo("Interesting"));
+                assertThat(resultSet.getNString(4), equalTo("9.1"));
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getInt(1), equalTo(1));
+                assertThat(resultSet.getNString(2), equalTo("War"));
+                assertThat(resultSet.getNString(3), equalTo("great 3D"));
+                assertThat(resultSet.getNString(4), equalTo("8.9"));
+                assertThat(resultSet.next(), equalTo(false));
+            }
+        }
+    }
+}
---
+++ src/test/java/g1001_1100/s1050_actors_and_directors_who_cooperated_at_least_three_times/MysqlTest.java
@@ -0,0 +1,61 @@
+package g1001_1100.s1050_actors_and_directors_who_cooperated_at_least_three_times;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.zapodot.junit.db.annotations.EmbeddedDatabase;
+import org.zapodot.junit.db.annotations.EmbeddedDatabaseTest;
+import org.zapodot.junit.db.common.CompatibilityMode;
+
+@EmbeddedDatabaseTest(
+        compatibilityMode = CompatibilityMode.MySQL,
+        initialSqls =
+                "CREATE TABLE ACTORDIRECTOR(actor_id INTEGER, director_id INTEGER, timestamp INTEGER PRIMARY KEY); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 1,0); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 1,1); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 1,2); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 2,3); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 2,4); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (1, 1,5); "
+                        + "INSERT INTO ACTORDIRECTOR(actor_id, director_id, timestamp) "
+                        + "VALUES (2, 1,6); ")
+class MysqlTest {
+    @Test
+    void testScript(@EmbeddedDatabase DataSource dataSource)
+            throws SQLException, FileNotFoundException {
+        try (final Connection connection = dataSource.getConnection()) {
+            try (final Statement statement = connection.createStatement();
+                    final ResultSet resultSet =
+                            statement.executeQuery(
+                                    new BufferedReader(
+                                                    new FileReader(
+                                                            "src/main/java/g1001_1100/s1050_actors"
+                                                                    + "_and_directors_who_cooperated_at_least"
+                                                                    + "_three_times/script.sql"))
+                                            .lines()
+                                            .collect(Collectors.joining("\n"))
+                                            .replaceAll("#.*?\\r?\\n", ""))) {
+                assertThat(resultSet.next(), equalTo(true));
+                assertThat(resultSet.getNString(1), equalTo("1"));
+                assertThat(resultSet.getNString(2), equalTo("1"));
+                assertThat(resultSet.next(), equalTo(false));
+            }
+        }
+    }
+}
