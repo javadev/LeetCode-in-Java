43c758682334aa784fec54618b2b6345da2944c2
ThanhNIT <93962044+ThanhNIT@users.noreply.github.com>
2022-07-26 02:31:42 +0700

    Added tests for tasks 1407, 1484, 1527, 1581.

--- src/main/java/g1501_1600/s1527_patients_with_a_condition/script.sql
+++ src/main/java/g1501_1600/s1527_patients_with_a_condition/script.sql
@@ -2,4 +2,4 @@
 # #Easy #Database #SQL_I_Day_3_String_Processing_Functions
 # #2022_04_12_Time_308_ms_(66.93%)_Space_0B_(100.00%)
 Select patient_id,patient_name,conditions from Patients
-where conditions like "% DIAB1%" or conditions like "DIAB1%";
+where conditions like '% DIAB1%' or conditions like 'DIAB1%'; -- NOSONAR
