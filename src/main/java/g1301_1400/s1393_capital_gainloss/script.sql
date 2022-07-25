06d83aa4ab7e06d8161a1cf5df577f6723121d4c
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-26 00:37:01 +0300

    Added test 1393.

--- src/main/java/g1301_1400/s1393_capital_gainloss/script.sql
+++ src/main/java/g1301_1400/s1393_capital_gainloss/script.sql
@@ -1,12 +1,5 @@
 # Write your MySQL query statement below
 # #Medium #LeetCode_Curated_SQL_70 #Database #SQL_I_Day_9_Control_of_Flow
-# #2022_05_24_Time_457_ms_(79.39%)_Space_0B_(100.00%)
-SELECT
-    stock_name, 
-    SUM(IF (operation = 1, price, -1 * price)) AS capital_gain_loss
-FROM
-    Stocks
-GROUP BY
-    stock_name
-ORDER BY
-    operation_day asc;
+# #2022_07_26_Time_428_ms_(92.01%)_Space_0B_(100.00%)
+SELECT stock_name, SUM(CASE WHEN operation='Sell' THEN price ELSE -price END) AS capital_gain_loss
+FROM Stocks GROUP BY stock_name;
