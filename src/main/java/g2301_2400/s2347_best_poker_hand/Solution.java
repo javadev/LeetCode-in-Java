2d7de49522e0242f1f19fe93bdf0993df7268761
Valentyn Kolesnikov <javadev75@gmail.com>
2022-07-26 18:09:05 +0300

    Added task 2347.

---
+++ src/main/java/g2301_2400/s2347_best_poker_hand/Solution.java
@@ -0,0 +1,37 @@
+package g2301_2400.s2347_best_poker_hand;
+
+// #Easy #2022_07_26_Time_1_ms_(76.92%)_Space_41.6_MB_(30.77%)
+
+import java.util.HashMap;
+
+public class Solution {
+    public String bestHand(int[] ranks, char[] suits) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char suit : suits) {
+            if (map.containsKey(suit)) {
+                map.put(suit, map.get(suit) + 1);
+                if (map.get(suit) == 5) {
+                    return "Flush";
+                }
+            } else {
+                map.put(suit, 1);
+            }
+        }
+        String s = "";
+        HashMap<Integer, Integer> map2 = new HashMap<>();
+        for (int rank : ranks) {
+            if (map2.containsKey(rank)) {
+                map2.put(rank, map2.get(rank) + 1);
+                if (map2.get(rank) == 2) {
+                    s = "Pair";
+                } else if (map2.get(rank) == 3) {
+                    s = "Three of a Kind";
+                    return s;
+                }
+            } else {
+                map2.put(rank, 1);
+            }
+        }
+        return s.isEmpty() ? "High Card" : s;
+    }
+}
---
+++ src/test/java/g2301_2400/s2347_best_poker_hand/SolutionTest.java
@@ -0,0 +1,33 @@
+package g2301_2400.s2347_best_poker_hand;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+    @Test
+    void bestHand() {
+        assertThat(
+                new Solution()
+                        .bestHand(new int[] {13, 2, 3, 1, 9}, new char[] {'a', 'a', 'a', 'a', 'a'}),
+                equalTo("Flush"));
+    }
+
+    @Test
+    void bestHand2() {
+        assertThat(
+                new Solution()
+                        .bestHand(new int[] {4, 4, 2, 4, 4}, new char[] {'d', 'a', 'a', 'b', 'c'}),
+                equalTo("Three of a Kind"));
+    }
+
+    @Test
+    void bestHand3() {
+        assertThat(
+                new Solution()
+                        .bestHand(
+                                new int[] {10, 10, 2, 12, 9}, new char[] {'a', 'b', 'c', 'a', 'd'}),
+                equalTo("Pair"));
+    }
+}
