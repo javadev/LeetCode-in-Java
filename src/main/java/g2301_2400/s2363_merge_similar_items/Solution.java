d8bd300c2a304e8cb7ba94762e7d5408b49e18c1
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-08-14 21:09:30 +0700

    Added task 2363, 2364, 2365, 2366, 2367

---
+++ src/main/java/g2301_2400/s2363_merge_similar_items/Solution.java
@@ -0,0 +1,29 @@
+package g2301_2400.s2363_merge_similar_items;
+
+// #Easy #Array #Sorting #Hash_Table #Ordered_Set
+// #2022_08_14_Time_3_ms_(100.00%)_Space_43_MB_(100.00%)
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] arr1, int[][] arr2) {
+        int[] cache = new int[1001];
+        for (int[] num : arr1) {
+            cache[num[0]] += num[1];
+        }
+        for (int[] num : arr2) {
+            cache[num[0]] += num[1];
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < cache.length; i++) {
+            int weight = cache[i];
+            if (weight > 0) {
+                int value = i;
+                result.add(Arrays.asList(value, weight));
+            }
+        }
+        return result;
+    }
+}
---
+++ src/main/java/g2301_2400/s2365_task_scheduler_ii/Solution.java
@@ -0,0 +1,25 @@
+package g2301_2400.s2365_task_scheduler_ii;
+
+// #Medium #Array #Simulation #Hash_Table #2022_08_14_Time_70_ms_(55.56%)_Space_123.3_MB_(11.11%)
+
+import java.util.HashMap;
+
+public class Solution {
+    public long taskSchedulerII(int[] tasks, int space) {
+        long days = 0;
+        space++;
+        HashMap<Integer, Long> lastOccurence = new HashMap<>();
+        for (int i = 0; i < tasks.length; i++) {
+            if (lastOccurence.containsKey(tasks[i])) {
+                long lastTimeOccurred = lastOccurence.get(tasks[i]);
+                long daysDifference = days - lastTimeOccurred;
+                if (daysDifference < space) {
+                    days += (space - daysDifference);
+                }
+            }
+            lastOccurence.put(tasks[i], days);
+            days++;
+        }
+        return days;
+    }
+}
