a592eb646fd884289d2fb45f4019d291fc5a0f48
Valentyn Kolesnikov <javadev75@gmail.com>
2022-08-14 18:36:18 +0300

    Update Solution.java

--- src/main/java/g2301_2400/s2366_minimum_replacements_to_sort_the_array/Solution.java
+++ src/main/java/g2301_2400/s2366_minimum_replacements_to_sort_the_array/Solution.java
@@ -6,9 +6,7 @@ public class Solution {
     public long minimumReplacement(int[] nums) {
         int limit = nums[nums.length - 1];
         long ans = 0;
-
         for (int i = nums.length - 2; i >= 0; i--) {
-
             int replacements = nums[i] / limit - 1;
             if (nums[i] % limit != 0) {
                 replacements++;
@@ -16,7 +14,6 @@ public class Solution {
             ans += replacements;
             limit = nums[i] / (replacements + 1);
         }
-
         return ans;
     }
 }
