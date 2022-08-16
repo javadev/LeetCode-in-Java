1bf6cfe1c80f07574c37940d16231f62744b3cf8
Valentyn Kolesnikov <javadev75@gmail.com>
2022-08-16 08:49:25 +0300

    Improved task 2368.

--- src/main/java/g2301_2400/s2368_reachable_nodes_with_restrictions/Solution.java
+++ src/main/java/g2301_2400/s2368_reachable_nodes_with_restrictions/Solution.java
@@ -1,46 +1,44 @@
 package g2301_2400.s2368_reachable_nodes_with_restrictions;
 
 // #Medium #Array #Tree #Graph #Hash_Table #Depth_First_Search #Breadth_First_Search
-// #2022_08_15_Time_88_ms_(42.86%)_Space_201.1_MB_(14.29%)
+// #2022_08_16_Time_59_ms_(85.71%)_Space_89.6_MB_(85.71%)
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     public int reachableNodes(int n, int[][] edges, int[] restricted) {
-        LinkedList<Integer>[] adj = new LinkedList[n];
-        for (int i = 0; i < n - 1; i++) {
-            if (adj[edges[i][0]] == null) {
-                LinkedList<Integer> source = new LinkedList<>();
-                source.add(edges[i][1]);
-                adj[edges[i][0]] = source;
-            } else {
-                adj[edges[i][0]].add(edges[i][1]);
-            }
-            if (adj[edges[i][1]] == null) {
-                LinkedList<Integer> dest = new LinkedList<>();
-                dest.add(edges[i][0]);
-                adj[edges[i][1]] = dest;
-            } else {
-                adj[edges[i][1]].add(edges[i][0]);
-            }
+        List<Integer>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
+        for (int[] edge : edges) {
+            int src = edge[0];
+            int dest = edge[1];
+            graph[src].add(dest);
+            graph[dest].add(src);
+        }
+        Queue<Integer> q = new ArrayDeque<>();
         boolean[] visited = new boolean[n];
-        for (int res : restricted) {
-            visited[res] = true;
-        }
-        int count = 1;
+        q.offer(0);
         visited[0] = true;
-        return countReachableNodes(0, adj, visited, count);
-    }
-
-    private int countReachableNodes(
-            int source, LinkedList<Integer>[] adj, boolean[] visited, int count) {
-        for (int neighbour : adj[source]) {
-            if (!visited[neighbour]) {
-                visited[neighbour] = true;
-                count = countReachableNodes(neighbour, adj, visited, ++count);
+        for (int node : restricted) {
+            visited[node] = true;
+        }
+        int ans = 0;
+        while (!q.isEmpty()) {
+            int vertex = q.poll();
+            ans++;
+            for (int neighbour : graph[vertex]) {
+                if (!visited[neighbour]) {
+                    q.offer(neighbour);
+                    visited[neighbour] = true;
+                }
             }
         }
-        return count;
+        return ans;
     }
 }
