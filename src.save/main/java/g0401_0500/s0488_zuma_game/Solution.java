ae37025f4ddc2c41de6d0b58800eeb9090bdd224
Valentyn Kolesnikov <javadev75@gmail.com>
2022-08-19 06:55:52 +0300

    Improved task

--- src.save/main/java/g0401_0500/s0488_zuma_game/Solution.java
+++ src.save/main/java/g0401_0500/s0488_zuma_game/Solution.java
@@ -5,36 +5,8 @@ package g0401_0500.s0488_zuma_game;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 public class Solution {
-    private static class Pair {
-        String board;
-        String hand;
-
-        public Pair(String board, String hand) {
-            this.board = board;
-            this.hand = hand;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Pair pair = (Pair) o;
-            return Objects.equals(board, pair.board) && Objects.equals(hand, pair.hand);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(board, hand);
-        }
-    }
-
     public int findMinStep(String board, String hand) {
         return dfs(board, hand);
     }
