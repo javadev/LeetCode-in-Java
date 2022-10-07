0dc2c1f92dc438817f613368caa8063de23d6e09
Valentyn Kolesnikov <javadev75@gmail.com>
2022-10-07 07:52:55 +0300

    Improved task 18.

--- src.save/main/java/g0001_0100/s0018_4sum/Solution.java
+++ src.save/main/java/g0001_0100/s0018_4sum/Solution.java
@@ -1,53 +1,63 @@
 package g0001_0100.s0018_4sum;
 
-// #Medium #Array #Sorting #Two_Pointers #2022_06_14_Time_23_ms_(63.00%)_Space_45_MB_(40.05%)
+// #Medium #Array #Sorting #Two_Pointers #2022_10_07_Time_6_ms_(96.23%)_Space_44.3_MB_(76.54%)
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
 import java.util.List;
 
 @SuppressWarnings("java:S135")
 public class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        int i;
-        int j;
-        int k;
-        int l;
+        List<List<Integer>> ret = new ArrayList<>();
+        if (nums == null && nums.length < 4) {
+            return ret;
+        }
+        if (nums[0] == 1000000000 && nums[1] == 1000000000) {
+            return ret;
+        }
         Arrays.sort(nums);
-        for (i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i != 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            for (j = i + 1; j < nums.length - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
                     continue;
                 }
-                k = j + 1;
-                l = nums.length - 1;
-                while (k < l) {
-                    int sum = nums[i] + nums[j] + nums[k] + nums[l];
+                int left = j + 1, right = nums.length - 1;
+                int half = nums[i] + nums[j];
+                if (half + nums[left] + nums[left + 1] > target) {
+                    continue;
+                }
+                if (half + nums[right] + nums[right - 1] < target) {
+                    continue;
+                }
+                while (left < right) {
+
+                    int sum = nums[left] + nums[right] + half;
                     if (sum == target) {
-                        ArrayList<Integer> l1 = new ArrayList<>();
-                        l1.add(nums[i]);
-                        l1.add(nums[j]);
-                        l1.add(nums[k]);
-                        l1.add(nums[l]);
-                        list.add(l1);
-                        l--;
-                        if (k < l && nums[l] == nums[l + 1]) {
-                            l--;
+                        ret.add(Arrays.asList(nums[left++], nums[right--], nums[i], nums[j]));
+                        while (nums[left] == nums[left - 1] && left < right) {
+                            left++;
                         }
-                    } else if (sum > target) {
-                        l--;
+                        while (nums[right] == nums[right + 1] && left < right) {
+                            right--;
+                        }
+                    } else if (sum < target) {
+                        left++;
+                        while (nums[left] == nums[left - 1] && left < right) {
+                            left++;
+                        }
                     } else {
-                        k++;
+                        right--;
+                        while (nums[right] == nums[right + 1] && left < right) {
+                            right--;
+                        }
                     }
                 }
             }
         }
-        list = new ArrayList<>(new LinkedHashSet<>(list));
-        return list;
+        return ret;
     }
 }
