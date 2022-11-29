13178be24c95a70f7fe6622c2a6bc2ef47fdb8dc
Valentyn Kolesnikov <javadev75@gmail.com>
2022-11-29 08:57:38 +0200

    Added test 328.

--- src.save/test/java/g0301_0400/s0328_odd_even_linked_list/SolutionTest.java
+++ src.save/test/java/g0301_0400/s0328_odd_even_linked_list/SolutionTest.java
@@ -22,4 +22,22 @@ class SolutionTest {
         expected.next.next.next.next = new ListNode(4);
         assertThat(new Solution().oddEvenList(node).toString(), equalTo(expected.toString()));
     }
+
+    @Test
+    void oddEvenList2() {
+        ListNode node = new ListNode(2);
+        node.next = new ListNode(1);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(5);
+        node.next.next.next.next = new ListNode(6);
+        node.next.next.next.next.next = new ListNode(4);
+        node.next.next.next.next.next.next = new ListNode(7);
+        ListNode expected = new ListNode(2);
+        expected.next = new ListNode(3);
+        expected.next.next = new ListNode(6);
+        expected.next.next.next = new ListNode(7);
+        expected.next.next.next.next = new ListNode(1);
+        expected.next.next.next.next.next = new ListNode(5);
+        expected.next.next.next.next.next.next = new ListNode(4);
+        assertThat(new Solution().oddEvenList(node).toString(), equalTo(expected.toString()))
 }
