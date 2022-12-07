a3d6781c7e3b8b1c4c7f0973677fa1871472dc4e
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-07 18:00:16 +0200

    Update Solution.java

--- src/main/java/g2401_2500/s2435_paths_in_matrix_whose_sum_is_divisible_by_k/Solution.java
+++ src/main/java/g2401_2500/s2435_paths_in_matrix_whose_sum_is_divisible_by_k/Solution.java
@@ -1,8 +1,8 @@
 package g2401_2500.s2435_paths_in_matrix_whose_sum_is_divisible_by_k;
 
-import java.util.Arrays;
-
 // #Hard #Array #Dynamic_Programming #Matrix #2022_12_07_Time_70_ms_(99.20%)_Space_84.5_MB_(87.93%)
+
+import java.util.Arrays;
 
 public class Solution {
     private int[][] p = new int[50005][50];
