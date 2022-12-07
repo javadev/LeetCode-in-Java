189ae178ed7f7413bbe9b116b169a37da5eb514c
ThanhNIT <thanhnguyen.it.work@gmail.com>
2022-12-07 22:53:25 +0700

    Update Solution.java

--- src/main/java/g2401_2500/s2435_paths_in_matrix_whose_sum_is_divisible_by_k/Solution.java
+++ src/main/java/g2401_2500/s2435_paths_in_matrix_whose_sum_is_divisible_by_k/Solution.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 // #Hard #Array #Dynamic_Programming #Matrix #2022_12_07_Time_70_ms_(99.20%)_Space_84.5_MB_(87.93%)
 
 public class Solution {
-    private static int[][] p = new int[50005][50];
-    private static int[][] r = new int[50005][50];
+    private int[][] p = new int[50005][50];
+    private int[][] r = new int[50005][50];
 
     public int numberOfPaths(int[][] grid, int k) {
         int m = grid.length;
