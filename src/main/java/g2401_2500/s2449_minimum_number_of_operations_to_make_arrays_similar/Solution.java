e14a4e92435f79ffac3677a8cbd8a8a47b3c4714
Valentyn Kolesnikov <javadev75@gmail.com>
2022-12-14 20:39:24 +0200

    Update Solution.java

--- src/main/java/g2401_2500/s2449_minimum_number_of_operations_to_make_arrays_similar/Solution.java
+++ src/main/java/g2401_2500/s2449_minimum_number_of_operations_to_make_arrays_similar/Solution.java
@@ -5,6 +5,7 @@ package g2401_2500.s2449_minimum_number_of_operations_to_make_arrays_similar;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+@SuppressWarnings("java:S2184")
 public class Solution {
     public long makeSimilar(int[] nums, int[] target) {
         ArrayList<Integer> evenNums = new ArrayList<>();
@@ -13,24 +14,20 @@ public class Solution {
         ArrayList<Integer> oddTar = new ArrayList<>();
         Arrays.sort(nums);
         Arrays.sort(target);
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
                 evenNums.add(nums[i]);
             } else {
                 oddNums.add(nums[i]);
             }
-
             if (target[i] % 2 == 0) {
                 evenTar.add(target[i]);
             } else {
                 oddTar.add(target[i]);
             }
         }
-
         long countPositiveIteration = 0;
         long countNegativeIteration = 0;
-
         for (int i = 0; i < evenNums.size(); i++) {
             int num = evenNums.get(i);
             int tar = evenTar.get(i);
@@ -42,7 +39,6 @@ public class Solution {
                 countPositiveIteration += Math.abs(iteration);
             }
         }
-
         for (int i = 0; i < oddNums.size(); i++) {
             int num = oddNums.get(i);
             int tar = oddTar.get(i);
@@ -54,9 +50,7 @@ public class Solution {
                 countPositiveIteration += Math.abs(iteration);
             }
         }
-
         long totalDifference = countPositiveIteration - countNegativeIteration;
-
         return totalDifference == 0
                 ? countPositiveIteration
                 : countPositiveIteration + Math.abs(totalDifference);
