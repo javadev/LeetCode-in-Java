4c8fe33d51205ee68e267fa6ad91dfce52cd92e8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-01-25 17:13:05 +0200

    Update Solution.java

--- src/main/java/g2401_2500/s2478_number_of_beautiful_partitions/Solution.java
+++ src/main/java/g2401_2500/s2478_number_of_beautiful_partitions/Solution.java
@@ -2,6 +2,7 @@ package g2401_2500.s2478_number_of_beautiful_partitions;
 
 // #Hard #String #Dynamic_Programming #2023_01_25_Time_44_ms_(90.08%)_Space_55.1_MB_(32.23%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int beautifulPartitions(String s, int k, int l) {
         char[] cs = s.toCharArray();
