3bbd5dd023930fb52f8f60deb7ac7c599a89ef33
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-30 18:51:31 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2516_take_k_of_each_character_from_left_and_right/Solution.java
+++ src/main/java/g2501_2600/s2516_take_k_of_each_character_from_left_and_right/Solution.java
@@ -3,17 +3,22 @@ package g2501_2600.s2516_take_k_of_each_character_from_left_and_right;
 // #Medium #String #Hash_Table #Sliding_Window #2023_03_30_Time_6_ms_(94.24%)_Space_43.5_MB_(46.60%)
 
 public class Solution {
-    public int takeCharacters(String s, int k) {      
-        if(s.length() < 3 * k) {return -1;}
+    public int takeCharacters(String s, int k) {
+        if (s.length() < 3 * k) {
+            return -1;
+        }
         int ans = Integer.MAX_VALUE;
         int cnt[] = new int[3];
         char arr[] = s.toCharArray();
-        for(char ch : arr){
+        for(char ch : arr) {
             cnt[ch - 'a']++;
         }
-        if(cnt[0] < k || cnt[1] < k || cnt[2] < k) {return -1;}
+        if (cnt[0] < k || cnt[1] < k || cnt[2] < k) {
+            return -1;
+        }
         ans = arr.length;
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
         while(j < arr.length) {
             cnt[arr[j] - 'a']--;
             if(cnt[0] >= k && cnt[1] >= k && cnt[2] >= k) {
