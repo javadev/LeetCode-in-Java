b9ca7e708b55ac41c87244fb7f9edd08ce07ea35
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-15 09:32:55 +0300

    Improved tasks 385, 398

--- src/main/java/g0301_0400/s0385_mini_parser/Solution.java
+++ src/main/java/g0301_0400/s0385_mini_parser/Solution.java
@@ -4,6 +4,34 @@ package g0301_0400.s0385_mini_parser;
 
 import com_github_leetcode.NestedInteger;
 
+/*
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
 public class Solution {
     private int i = 0;
 
