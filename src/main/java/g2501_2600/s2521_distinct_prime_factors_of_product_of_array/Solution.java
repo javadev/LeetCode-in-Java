02067461806f285bdc5cb0f6dfd55986d9ac9b10
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-18 06:55:46 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2521_distinct_prime_factors_of_product_of_array/Solution.java
+++ src/main/java/g2501_2600/s2521_distinct_prime_factors_of_product_of_array/Solution.java
@@ -3,6 +3,7 @@ package g2501_2600.s2521_distinct_prime_factors_of_product_of_array;
 // #Medium #Array #Hash_Table #Math #Number_Theory
 // #2023_04_18_Time_2_ms_(100.00%)_Space_44.1_MB_(18.47%)
 
+@SuppressWarnings("java:S1119")
 public class Solution {
     static final int[] PRIMES = new int[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
 
