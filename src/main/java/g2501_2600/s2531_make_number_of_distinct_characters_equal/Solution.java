4a63cfa2a91ceec54d8459ccd5f520e06cce6020
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-21 06:16:12 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2531_make_number_of_distinct_characters_equal/Solution.java
+++ src/main/java/g2501_2600/s2531_make_number_of_distinct_characters_equal/Solution.java
@@ -2,6 +2,7 @@ package g2501_2600.s2531_make_number_of_distinct_characters_equal;
 
 // #Medium #String #Hash_Table #Counting #2023_04_20_Time_7_ms_(100.00%)_Space_43_MB_(96.82%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public boolean isItPossible(String word1, String word2) {
         int[] count1 = count(word1);
