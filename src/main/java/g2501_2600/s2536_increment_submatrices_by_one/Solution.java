06a816293e802b5255384e67120f9b97c07b4784
Valentyn Kolesnikov <javadev75@gmail.com>
2023-04-26 09:39:00 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2536_increment_submatrices_by_one/Solution.java
+++ src/main/java/g2501_2600/s2536_increment_submatrices_by_one/Solution.java
@@ -21,7 +21,6 @@ public class Solution {
                 g[r2 + 1][c2 + 1]++;
             }
         }
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i > 0) {
@@ -35,7 +34,6 @@ public class Solution {
                 }
             }
         }
-
         return g;
     }
 }
