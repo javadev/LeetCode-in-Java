80b22e96975ec41fd98b1b7215ee58b1cd65cab6
Valentyn Kolesnikov <javadev75@gmail.com>
2023-05-03 07:06:08 +0300

    Improved task 2538

--- src/main/java/g2501_2600/s2538_difference_between_maximum_and_minimum_price_sum/Solution.java
+++ src/main/java/g2501_2600/s2538_difference_between_maximum_and_minimum_price_sum/Solution.java
@@ -1,6 +1,6 @@
 package g2501_2600.s2538_difference_between_maximum_and_minimum_price_sum;
 
-// #Hard #Array #Dynamic_Programming #Tree #Depth_First_Search
+// #Hard #Array #Dynamic_Programming #Depth_First_Search #Tree
 // #2023_04_22_Time_43_ms_(95.19%)_Space_86.2_MB_(57.69%)
 
 import java.util.ArrayList;
