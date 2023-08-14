6a8004c0461b4a0d178631475d3f924df8dfd503
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-14 05:59:51 +0300

    Update script.sql

--- src/main/java/g1301_1400/s1321_restaurant_growth/script.sql
+++ src/main/java/g1301_1400/s1321_restaurant_growth/script.sql
@@ -4,7 +4,6 @@ with cteX as
 (select visited_on, sum(amount) as amount from customer
 group by visited_on)
 
-
 SELECT visited_on, SUM(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
 round(AVG(amount) OVER(ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) as average_amount FROM cteX
 order by visited_on
