16a867bdb0c0773b7a2668d74bb24c5ec6f0d56b
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-08-16 10:07:10 +0700

    update

--- src/main/java/g1401_1500/s1460_make_two_arrays_equal_by_reversing_subarrays/Solution.java
+++
@@ -1,9 +0,0 @@
-package g1401_1500.s1460_make_two_arrays_equal_by_reversing_subarrays;
-
-// #Easy #Array #Hash_Table #Sorting
-
-public class Solution {
-    public String decode(String value) {
-        return value;
-    }
-}
--- src/main/java/g1501_1600/s1529_minimum_suffix_flips/Solution.java
+++
@@ -1,9 +0,0 @@
-package g1501_1600.s1529_minimum_suffix_flips;
-
-// #Medium #String #Greedy
-
-public class Solution {
-    public String decode(String value) {
-        return value;
-    }
-}
