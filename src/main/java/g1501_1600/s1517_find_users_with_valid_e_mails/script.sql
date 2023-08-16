3a581e999defab1dde635a92f47ff8d19a1eed5c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-16 06:16:46 +0300

    Revert "Added tasks 1327, 1341, 1478, 1517, 1633"
    This reverts commit 4806998c57eef7de1dd30bcafba244995fabd2f4.

--- src/main/java/g1501_1600/s1517_find_users_with_valid_e_mails/script.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-# #Easy #Database #2023_08_16_Time_1356_ms_(75.95%)_Space_0B_(100.00%)
-SELECT *
-FROM Users
-WHERE mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode[.]com'
