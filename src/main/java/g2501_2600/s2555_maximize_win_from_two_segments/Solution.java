de8940560eb02386c7210d1c311387adf2fee057
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-19 05:43:24 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2555_maximize_win_from_two_segments/Solution.java
+++ src/main/java/g2501_2600/s2555_maximize_win_from_two_segments/Solution.java
@@ -3,7 +3,7 @@ package g2501_2600.s2555_maximize_win_from_two_segments;
 // #Medium #Array #Binary_Search #Sliding_Window
 // #2023_08_19_Time_5_ms_(90.10%)_Space_55.4_MB_(45.54%)
 
-class Solution {
+public class Solution {
     public int maximizeWin(int[] a, int k) {
         int j = 0;
         int i;
