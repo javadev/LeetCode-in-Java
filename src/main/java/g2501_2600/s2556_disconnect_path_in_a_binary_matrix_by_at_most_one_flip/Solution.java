3e79da9f14ff5b6ee4a5922802b35b1c22e93ae8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-19 05:43:44 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2556_disconnect_path_in_a_binary_matrix_by_at_most_one_flip/Solution.java
+++ src/main/java/g2501_2600/s2556_disconnect_path_in_a_binary_matrix_by_at_most_one_flip/Solution.java
@@ -3,7 +3,7 @@ package g2501_2600.s2556_disconnect_path_in_a_binary_matrix_by_at_most_one_flip;
 // #Medium #Array #Dynamic_Programming #Depth_First_Search #Breadth_First_Search #Matrix
 // #2023_08_19_Time_0_ms_(100.00%)_Space_54.4_MB_(97.73%)
 
-class Solution {
+public class Solution {
     private int n;
     private int m;
 
