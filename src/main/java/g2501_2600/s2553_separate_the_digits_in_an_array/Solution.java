2e7759d56956b86c8c79232c4278d614a8f56307
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-19 05:45:45 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2553_separate_the_digits_in_an_array/Solution.java
+++ src/main/java/g2501_2600/s2553_separate_the_digits_in_an_array/Solution.java
@@ -2,7 +2,7 @@ package g2501_2600.s2553_separate_the_digits_in_an_array;
 
 // #Easy #Array #Simulation #2023_08_18_Time_3_ms_(92.00%)_Space_43.7_MB_(93.70%)
 
-class Solution {
+public class Solution {
     public int[] separateDigits(int[] nums) {
         StringBuilder str = new StringBuilder();
         for (int num : nums) {
