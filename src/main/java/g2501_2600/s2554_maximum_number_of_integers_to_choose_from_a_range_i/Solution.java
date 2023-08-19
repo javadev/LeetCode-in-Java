d4abdf382c49697e13dc81e1ed04d520418ee248
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-19 05:43:02 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2554_maximum_number_of_integers_to_choose_from_a_range_i/Solution.java
+++ src/main/java/g2501_2600/s2554_maximum_number_of_integers_to_choose_from_a_range_i/Solution.java
@@ -3,7 +3,7 @@ package g2501_2600.s2554_maximum_number_of_integers_to_choose_from_a_range_i;
 // #Medium #Array #Hash_Table #Sorting #Greedy #Binary_Search
 // #2023_08_19_Time_4_ms_(100.00%)_Space_44.9_MB_(17.88%)
 
-class Solution {
+public class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
         boolean[] arr = new boolean[10002];
         for (int j : banned) {
