cc1d5d970f282f1dbe7acd9605748380d61ad0a8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-21 07:29:24 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2564_substring_xor_queries/Solution.java
+++ src/main/java/g2501_2600/s2564_substring_xor_queries/Solution.java
@@ -1,6 +1,7 @@
 package g2501_2600.s2564_substring_xor_queries;
 
 // #Medium #Array #String #Hash_Table #Bit_Manipulation
+// #2023_08_21_Time_26_ms_(95.83%)_Space_84.1_MB_(80.56%)
 
 import java.util.HashMap;
 import java.util.Map;
