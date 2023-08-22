47de10914811c8152519c073272ab4ba8a024333
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-08-22 22:15:47 +0700

    Update Solution.java

--- src/main/java/g2501_2600/s2587_rearrange_array_to_maximize_prefix_score/Solution.java
+++ src/main/java/g2501_2600/s2587_rearrange_array_to_maximize_prefix_score/Solution.java
@@ -12,7 +12,9 @@ public class Solution {
         long sum = 0;
         for (int i = nums.length - 1; i >= 0; i--) {
             sum += nums[i];
-            if (sum > 0) count++;
+            if (sum > 0) {
+                count++;
+            }
         }
         return count;
     }
