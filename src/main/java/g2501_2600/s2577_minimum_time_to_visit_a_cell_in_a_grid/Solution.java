0acd420a6fa61f28dc43b94aa0dba2c170c9d014
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-22 05:30:45 +0300

    Update Solution.java

--- src/main/java/g2501_2600/s2577_minimum_time_to_visit_a_cell_in_a_grid/Solution.java
+++ src/main/java/g2501_2600/s2577_minimum_time_to_visit_a_cell_in_a_grid/Solution.java
@@ -1,6 +1,7 @@
 package g2501_2600.s2577_minimum_time_to_visit_a_cell_in_a_grid;
 
 // #Hard #Array #Breadth_First_Search #Matrix #Heap_Priority_Queue #Graph #Shortest_Path
+// #2023_08_22_Time_132_ms_(85.82%)_Space_56_MB_(57.46%)
 
 import java.util.Arrays;
 import java.util.Comparator;
