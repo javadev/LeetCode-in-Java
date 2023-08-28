31cf680f8a75dd1aab1454eb92850f842361e8ea
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-28 03:01:15 +0300

    Improve task 2584

--- src/main/java/g2501_2600/s2584_split_the_array_to_make_coprime_products/Solution.java
+++ src/main/java/g2501_2600/s2584_split_the_array_to_make_coprime_products/Solution.java
@@ -1,6 +1,6 @@
 package g2501_2600.s2584_split_the_array_to_make_coprime_products;
 
-// #Medium #Breadth_First_Search #Tree #Binary_Search
+// #Hard #Array #Hash_Table #Math #Number_Theory
 // #2023_08_22_Time_13_ms_(99.83%)_Space_60.3_MB_(92.18%)
 
 import java.util.ArrayList;
