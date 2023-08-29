348b7856414fadce58aaa84337397cbeddfcccf3
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-29 04:48:20 +0300

    Improved tasks 2597, 2600, 2601

--- src/main/java/g2501_2600/s2600_k_items_with_the_maximum_sum/Solution.java
+++ src/main/java/g2501_2600/s2600_k_items_with_the_maximum_sum/Solution.java
@@ -2,6 +2,7 @@ package g2501_2600.s2600_k_items_with_the_maximum_sum;
 
 // #Easy #Math #Greedy #2023_08_29_Time_1_ms_(100.00%)_Space_40.3_MB_(19.10%)
 
+@SuppressWarnings("java:S1172")
 public class Solution {
     public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
         if (k <= numOnes) {
