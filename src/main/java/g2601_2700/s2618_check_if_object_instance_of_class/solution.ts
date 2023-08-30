4a0fcb040dd96be9544d7a93538c08b0ec033895
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-30 11:39:02 +0300

    Update solution.ts

--- src/main/java/g2601_2700/s2618_check_if_object_instance_of_class/solution.ts
+++ src/main/java/g2601_2700/s2618_check_if_object_instance_of_class/solution.ts
@@ -1,11 +1,11 @@
 // #Medium #2023_08_30_Time_82_ms_(99.55%)_Space_52.1_MB_(79.46%)
 
 function checkIfInstanceOf(obj: any, classFunction: any): boolean {
-    if(obj===null || obj===undefined || typeof classFunction!=="function") return false
+    if (obj===null || obj===undefined || typeof classFunction!=="function") return false
 
     let proto = Object.getPrototypeOf(obj)
-    while(proto !== null) {
-        if(proto === classFunction.prototype) return true
+    while (proto !== null) {
+        if (proto === classFunction.prototype) return true
         proto = Object.getPrototypeOf(proto)
     }
     return false
