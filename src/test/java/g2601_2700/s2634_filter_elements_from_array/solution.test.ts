5baba1e1837f01ced45f3570bdf62e1e76edc478
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-31 17:11:15 +0300

    Improved tasks 2630-2637

--- src/test/java/g2601_2700/s2634_filter_elements_from_array/solution.test.ts
+++ src/test/java/g2601_2700/s2634_filter_elements_from_array/solution.test.ts
@@ -3,19 +3,25 @@ import { filter } from 'src/main/java/g2601_2700/s2634_filter_elements_from_arra
 import { expect, test } from 'vitest'
 
 test('filter', () => {
-    let arr = [0,10,20,30]
-    let fn = function greaterThan10(n) { return n > 10; }
-    expect(filter(arr,fn)).toEqual([20, 30])
+    let arr = [0, 10, 20, 30]
+    let fn = function greaterThan10(n) {
+        return n > 10
+    }
+    expect(filter(arr, fn)).toEqual([20, 30])
 })
 
 test('filter2', () => {
-    let arr = [-2,-1,0,1,2]
-    let fn = function plusOne(n) { return n + 1 }
-    expect(filter(arr,fn)).toEqual([-2,0,1,2])
+    let arr = [-2, -1, 0, 1, 2]
+    let fn = function plusOne(n) {
+        return n + 1
+    }
+    expect(filter(arr, fn)).toEqual([-2, 0, 1, 2])
 })
 
 test('filter3', () => {
-    let arr = [1,2,3]
-    let fn = function firstIndex(n, i) { return i === 0; }
-    expect(filter(arr,fn)).toEqual([1])
+    let arr = [1, 2, 3]
+    let fn = function firstIndex(n, i) {
+        return i === 0
+    }
+    expect(filter(arr, fn)).toEqual([1])
 })
--- src/test/java/g2601_2700/s2637_promise_time_limit/solution.test.ts
+++ src/test/java/g2601_2700/s2637_promise_time_limit/solution.test.ts
@@ -2,81 +2,81 @@
 import { timeLimit } from 'src/main/java/g2601_2700/s2637_promise_time_limit/solution'
 import { expect, test } from 'vitest'
 
-test('timeLimit',async () => {
+test('timeLimit', async () => {
     let fn = async (n) => {
-      await new Promise(res => setTimeout(res, 100));
-      return n * n;
+        await new Promise((res) => setTimeout(res, 100))
+        return n * n
     }
     let inputs = [5]
     let t = 50
 
     const limited = timeLimit(fn, t)
     const start = Date.now()
-    let result;
+    let result
     try {
-       const res = await limited(...inputs)
-       result = {"resolved": res,};
+        const res = await limited(...inputs)
+        result = { resolved: res }
     } catch (err) {
-       result = {"rejected": err};
+        result = { rejected: err }
     }
-    expect(result).toEqual({"rejected":new Error('Time Limit Exceeded')})
+    expect(result).toEqual({ rejected: new Error('Time Limit Exceeded') })
 })
 
-test('timeLimit2',async () => {
+test('timeLimit2', async () => {
     let fn = async (n) => {
-          await new Promise(res => setTimeout(res, 100));
-          return n * n;
-        }
+        await new Promise((res) => setTimeout(res, 100))
+        return n * n
+    }
     let inputs = [5]
     let t = 150
 
     const limited = timeLimit(fn, t)
     const start = Date.now()
-    let result;
+    let result
     try {
-       const res = await limited(...inputs)
-       result = {"resolved": res,};
+        const res = await limited(...inputs)
+        result = { resolved: res }
     } catch (err) {
-       result = {"rejected": err};
+        result = { rejected: err }
     }
-    expect(result).toEqual({"resolved":25})
+    expect(result).toEqual({ resolved: 25 })
 })
 
-test('timeLimit3',async () => {
+test('timeLimit3', async () => {
     let fn = async (a, b) => {
-          await new Promise(res => setTimeout(res, 120));
-          return a + b;
-        }
-    let inputs = [5,10]
+        await new Promise((res) => setTimeout(res, 120))
+        return a + b
+    }
+    let inputs = [5, 10]
     let t = 150
 
     const limited = timeLimit(fn, t)
     const start = Date.now()
-    let result;
+    let result
     try {
-       const res = await limited(...inputs)
-       result = {"resolved": res};
+        const res = await limited(...inputs)
+        result = { resolved: res }
     } catch (err) {
-       result = {"rejected": err};
+        result = { rejected: err }
     }
-    expect(result).toEqual({"resolved":15})
+    expect(result).toEqual({ resolved: 15 })
 })
 
 test('timeLimit4', async () => {
     let fn = async () => {
-               throw "Error";
-             }
+        throw 'Error'
+    }
     let inputs = []
     let t = 1000
 
     const limited = timeLimit(fn, t)
     const start = Date.now()
-    let result;
+    let result
     try {
-       const res = await limited(...inputs)
-       result = {"resolved": res, "time": Math.floor(Date.now() - start)};
+        const res = await limited(...inputs)
+        result = { resolved: res, time: Math.floor(Date.now() - start) }
     } catch (err) {
-       result = {"rejected": err, "time": Math.floor(Date.now() - start)};
+        result = { rejected: err, time: Math.floor(Date.now() - start) }
     }
-    expect(result).toEqual({"rejected":"Error","time":0})
+    expect(result).toEqual({ rejected: 'Error', time: 0 })
 })
