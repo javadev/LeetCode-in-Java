313dfb51266acefeba8dc17496f55c387d9283d8
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-31 10:13:35 +0300

    Update solution.ts

--- src/main/java/g2601_2700/s2625_flatten_deeply_nested_array/solution.ts
+++ src/main/java/g2601_2700/s2625_flatten_deeply_nested_array/solution.ts
@@ -4,8 +4,7 @@ type MultiDimensionalArray = (number | MultiDimensionalArray)[];
 
 const flat = (arr: MultiDimensionalArray, n: number): MultiDimensionalArray => {
   const result: MultiDimensionalArray = [];
-
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) { //NOSONAR
     const el = arr[i];
     if (Array.isArray(el) && n > 0) {
       const flattened = flat(el, n - 1);
