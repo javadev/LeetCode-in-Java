c6655906453cb101978cd8879077f807390b22ad
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-03 11:14:04 +0300

    Fixed sonar

--- src/main/java/g1301_1400/s1376_time_needed_to_inform_all_employees/Solution.java
+++ src/main/java/g1301_1400/s1376_time_needed_to_inform_all_employees/Solution.java
@@ -3,8 +3,6 @@ package g1301_1400.s1376_time_needed_to_inform_all_employees;
 // #Medium #Depth_First_Search #Breadth_First_Search #Tree #Programming_Skills_II_Day_11
 // #Graph_Theory_I_Day_9_Standard_Traversal #2023_09_03_Time_8_ms_(99.85%)_Space_58_MB_(89.47%)
 
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
