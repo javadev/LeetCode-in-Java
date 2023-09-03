e36b0ed77894c36133deb7c65d6367721d0cedfd
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-03 11:15:47 +0300

    Improved tasks 1105, 1158, 1376, 1502, 1572, 1629

--- src/main/java/g1601_1700/s1629_slowest_key/Solution.java
+++ src/main/java/g1601_1700/s1629_slowest_key/Solution.java
@@ -1,49 +1,21 @@
 package g1601_1700.s1629_slowest_key;
 
-// #Easy #Array #String #2022_04_18_Time_4_ms_(14.60%)_Space_42.9_MB_(72.62%)
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+// #Easy #Array #String #2023_09_03_Time_1_ms_(94.07%)_Space_43.9_MB_(9.16%)
 
 @SuppressWarnings("java:S3824")
 public class Solution {
     public char slowestKey(int[] releaseTimes, String keysPressed) {
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < releaseTimes.length; i++) {
-            char c = keysPressed.charAt(i);
-            int duration;
-            if (i == 0) {
-                duration = releaseTimes[i];
-            } else {
-                duration = releaseTimes[i] - releaseTimes[i - 1];
-            }
-            if (!map.containsKey(c)) {
-                map.put(c, duration);
-            } else {
-                int val = map.get(c);
-                if (duration > val) {
-                    map.put(c, duration);
-                }
+        int maxIndex = 0;
+        int maxValue = releaseTimes[0];
+        for (int i = 1; i < releaseTimes.length; i++) {
+            final int newVal = releaseTimes[i] - releaseTimes[i - 1];
+            if (newVal > maxValue) {
+                maxValue = newVal;
+                maxIndex = i;
+            } else if (newVal == maxValue && keysPressed.charAt(i) > keysPressed.charAt(maxIndex)) {
+                maxIndex = i;
             }
         }
-        Map<Integer, List<Character>> map2 = new HashMap<>();
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            int duration = entry.getValue();
-            if (!map2.containsKey(duration)) {
-                map2.put(duration, new ArrayList<>());
-            }
-            map2.get(duration).add(entry.getKey());
-        }
-        int max = -1;
-        for (Map.Entry<Integer, List<Character>> entry : map2.entrySet()) {
-            List<Character> chars = entry.getValue();
-            Collections.sort(chars);
-            map2.put(entry.getKey(), chars);
-            max = Math.max(max, entry.getKey());
-        }
-        return map2.get(max).get(map2.get(max).size() - 1);
+        return keysPressed.charAt(maxIndex);
     }
 }
