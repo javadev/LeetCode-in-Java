d774fb978f21c1d55c960524be692fbd224a2605
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-05 06:03:07 +0300

    Update Solution.java

--- src.save/main/java/g1501_1600/s1572_matrix_diagonal_sum/Solution.java
+++ src.save/main/java/g1501_1600/s1572_matrix_diagonal_sum/Solution.java
@@ -3,7 +3,7 @@ package g1501_1600.s1572_matrix_diagonal_sum;
 // #Easy #Array #Matrix #Programming_Skills_I_Day_7_Array #Udemy_2D_Arrays/Matrix
 // #2023_09_03_Time_0_ms_(100.00%)_Space_43.9_MB_(57.91%)
 
-class Solution {
+public class Solution {
     public int diagonalSum(int[][] mat) {
         int len = mat.length;
         int sum = 0;
