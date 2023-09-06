fe63bfac98e629ae2ef6f87fc173b44bb04402fa
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-06 06:12:17 +0300

    Update solution.ts

--- src/main/java/g2601_2700/s2649_nested_array_generator/solution.ts
+++ src/main/java/g2601_2700/s2649_nested_array_generator/solution.ts
@@ -4,7 +4,7 @@ type MultidimensionalArray = (MultidimensionalArray | number)[]
 
 function* inorderTraversal(arr: MultidimensionalArray): Generator<number, void, unknown> {
     for (const item of arr)
-        if (Array.isArray(item)) yield* inorderTraversal(item as MultidimensionalArray)
+        if (Array.isArray(item)) yield* inorderTraversal(item)
         else yield item
 };
 
