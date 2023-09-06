a6d5cc9d7da5e5387470e54655473a1b02118acb
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-06 15:27:39 +0300

    Update Solution.java

--- src/main/java/g2601_2700/s2653_sliding_subarray_beauty/Solution.java
+++ src/main/java/g2601_2700/s2653_sliding_subarray_beauty/Solution.java
@@ -2,6 +2,7 @@ package g2601_2700.s2653_sliding_subarray_beauty;
 
 // #Medium #Array #Hash_Table #Sliding_Window #2023_09_06_Time_33_ms_(86.67%)_Space_60.4_MB_(44.44%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
         int[] arr = new int[101];
