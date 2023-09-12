a8a1228910dfe8b8814aa271514bbfd47a1ffece
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-12 06:19:47 +0300

    Update solution.test.ts

--- src/test/java/g2601_2700/s2693_call_function_with_custom_context/solution.test.ts
+++ src/test/java/g2601_2700/s2693_call_function_with_custom_context/solution.test.ts
@@ -4,7 +4,7 @@ import { expect, test } from 'vitest'
 
 test('callPolyfill', () => {
     const fn = function add(b) {
-      return this.a + b;
+      return this.a + b
     }
     const args = [{"a": 5}, 7]
     expect(fn.callPolyfill(...args)).toEqual(12)
@@ -12,7 +12,7 @@ test('callPolyfill', () => {
 
 test('callPolyfill2', () => {
     const fn =  function tax(price, taxRate) {
-        return `The cost of the ${this.item} is ${price * taxRate}`;
+        return `The cost of the ${this.item} is ${price * taxRate}`
     }
     const args = [{"item": "burger"}, 10, 1.1]
     expect(fn.callPolyfill(...args)).toEqual('The cost of the burger is 11')
