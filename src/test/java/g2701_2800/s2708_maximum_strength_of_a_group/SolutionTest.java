e427437ed36b4a3f05c9b818113b945ad5144223
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-15 11:27:42 +0300

    Update SolutionTest.java

--- src/test/java/g2701_2800/s2708_maximum_strength_of_a_group/SolutionTest.java
+++ src/test/java/g2701_2800/s2708_maximum_strength_of_a_group/SolutionTest.java
@@ -15,4 +15,10 @@ class SolutionTest {
     void maxStrength2() {
         assertThat(new Solution().maxStrength(new int[] {-4, -5, -4}), equalTo(20L));
     }
+
+    @Test
+    void maxStrength3() {
+        assertThat(new Solution().maxStrength(
+            new int[] {8, 6, 0, 5, -4, -8, -4, 9, -1, 6, -4, 8, -5}), equalTo(265420800L));
+    }
 }
