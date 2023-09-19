ac0c9289b3a79bfde73afe1e0092c2984d6c68e0
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-19 20:45:49 +0300

    Fixed compile

--- src/main/java/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.java
+++ src/main/java/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.java
@@ -2,6 +2,8 @@ package g2601_2700.s2684_maximum_number_of_moves_in_a_grid;
 
 // #Medium #Array #Dynamic_Programming #Matrix #2023_09_19_Time_3_ms_(99.53%)_Space_55.4_MB_(15.49%)
 
+import java.util.Arrays;
+
 public class Solution {
     public int maxMoves(int[][] grid) {
         int h = grid.length;
