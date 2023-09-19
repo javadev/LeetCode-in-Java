ee3a8ab541be1a470d9589ae20f3671712319b19
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-19 20:43:20 +0300

    Improved task 2684

--- src/main/java/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.java
+++ src/main/java/g2601_2700/s2684_maximum_number_of_moves_in_a_grid/Solution.java
@@ -1,25 +1,42 @@
 package g2601_2700.s2684_maximum_number_of_moves_in_a_grid;
 
-// #Medium #Array #Dynamic_Programming #Matrix #2023_09_12_Time_4_ms_(97.95%)_Space_54.4_MB_(68.34%)
+// #Medium #Array #Dynamic_Programming #Matrix #2023_09_19_Time_3_ms_(99.53%)_Space_55.4_MB_(15.49%)
 
 public class Solution {
     public int maxMoves(int[][] grid) {
-        int m = Integer.MIN_VALUE;
-        int[][] vis = new int[grid.length][grid[0].length];
-        for (int i = 0; i < grid.length; i++) {
-            m = Math.max(m, mov(i, 0, grid, Integer.MIN_VALUE, vis));
+        int h = grid.length;
+        boolean[] dp1 = new boolean[h];
+        boolean[] dp2 = new boolean[h];
+        int rtn = 0;
+        Arrays.fill(dp1, true);
+        for (int col = 1; col < grid[0].length; col++) {
+            boolean f = false;
+            for (int row = 0; row < h; row++) {
+                int pr = row - 1;
+                int nr = row + 1;
+                dp2[row] = false;
+                if (pr >= 0 && dp1[pr] && grid[pr][col - 1] < grid[row][col]) {
+                    dp2[row] = true;
+                    f = true;
+                }
+                if (nr < h && dp1[nr] && grid[nr][col - 1] < grid[row][col]) {
+                    dp2[row] = true;
+                    f = true;
+                }
+                if (dp1[row] && grid[row][col - 1] < grid[row][col]) {
+                    dp2[row] = true;
+                    f = true;
+                }
+            }
+            boolean[] t = dp1;
+            dp1 = dp2;
+            dp2 = t;
+            if (f) {
+                rtn++;
+            } else {
+                break;
+            }
         }
-        return m - 1;
-    }
-
-    private int mov(int i, int j, int[][] g, int p, int[][] vis) {
-        if (i < 0 || j < 0 || i >= g.length || j >= g[0].length || g[i][j] <= p || vis[i][j] == 1) {
-            return 0;
-        }
-        vis[i][j] = 1;
-        int ur = 1 + mov(i - 1, j + 1, g, g[i][j], vis);
-        int dr = 1 + mov(i + 1, j + 1, g, g[i][j], vis);
-        int r = 1 + mov(i, j + 1, g, g[i][j], vis);
-        return Math.max(ur, Math.max(dr, r));
+        return rtn;
     }
 }
