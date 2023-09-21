2658e59b8f198b8b3639b367d2221947ad1af162
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-21 11:25:36 +0300

    Update Solution.java

--- src/main/java/g2701_2800/s2772_apply_operations_to_make_all_array_elements_equal_to_zero/Solution.java
+++ src/main/java/g2701_2800/s2772_apply_operations_to_make_all_array_elements_equal_to_zero/Solution.java
@@ -2,7 +2,7 @@ package g2701_2800.s2772_apply_operations_to_make_all_array_elements_equal_to_ze
 
 // #Medium #Array #Prefix_Sum #2023_09_21_Time_1_ms_(100.00%)_Space_58.1_MB_(54.11%)
 
-class Solution {
+public class Solution {
     public boolean checkArray(int[] nums, int k) {
         int cur = 0;
         int n = nums.length;
