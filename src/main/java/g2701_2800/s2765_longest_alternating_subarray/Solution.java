7f1069e09a9d014c033eace8c64c9a2b3deb71bc
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-24 14:58:43 +0300

    Fixed sonar

--- src/main/java/g2701_2800/s2765_longest_alternating_subarray/Solution.java
+++ src/main/java/g2701_2800/s2765_longest_alternating_subarray/Solution.java
@@ -2,6 +2,7 @@ package g2701_2800.s2765_longest_alternating_subarray;
 
 // #Easy #Array #Enumeration #2023_09_24_Time_2_ms_(82.60%)_Space_43_MB_(69.16%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     public int alternatingSubarray(int[] nums) {
         int result = -1;
