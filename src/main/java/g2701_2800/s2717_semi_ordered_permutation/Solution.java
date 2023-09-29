d364f16de88ab21043a5ece8342fda8a2b2f71fb
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-30 02:56:49 +0300

    Improved task 2717

--- src/main/java/g2701_2800/s2717_semi_ordered_permutation/Solution.java
+++ src/main/java/g2701_2800/s2717_semi_ordered_permutation/Solution.java
@@ -1,7 +1,6 @@
 package g2701_2800.s2717_semi_ordered_permutation;
 
-// #Easy #Array #String #Hash_Table #Simulation
-// #2023_09_15_Time_3_ms_(100.00%)_Space_43.3_MB_(98.28%)
+// #Easy #Array #Simulation #2023_09_15_Time_3_ms_(100.00%)_Space_43.3_MB_(98.28%)
 
 public class Solution {
     public int semiOrderedPermutation(int[] nums) {
