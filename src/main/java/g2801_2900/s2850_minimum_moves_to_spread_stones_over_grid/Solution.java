76551a4c03397134fe93587e224fffc9f959a6ae
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-21 04:48:07 +0200

    Improved tags

--- src/main/java/g2801_2900/s2850_minimum_moves_to_spread_stones_over_grid/Solution.java
+++ src/main/java/g2801_2900/s2850_minimum_moves_to_spread_stones_over_grid/Solution.java
@@ -1,6 +1,6 @@
 package g2801_2900.s2850_minimum_moves_to_spread_stones_over_grid;
 
-// #Medium #Array #Dynamic_Programming #Matrix #Breadth_First_Search
+// #Medium #Array #Dynamic_Programming #Breadth_First_Search #Matrix
 // #2023_12_15_Time_1_ms_(100.00%)_Space_40.5_MB_(89.16%)
 
 public class Solution {
--- src/main/java/g2801_2900/s2858_minimum_edge_reversals_so_every_node_is_reachable/Solution.java
+++ src/main/java/g2801_2900/s2858_minimum_edge_reversals_so_every_node_is_reachable/Solution.java
@@ -1,6 +1,6 @@
 package g2801_2900.s2858_minimum_edge_reversals_so_every_node_is_reachable;
 
-// #Hard #Dynamic_Programming #Graph #Depth_First_Search #Breadth_First_Search
+// #Hard #Dynamic_Programming #Depth_First_Search #Breadth_First_Search #Graph
 // #2023_12_19_Time_52_ms_(92.31%)_Space_119.5_MB_(75.38%)
 
 import java.util.ArrayList;
