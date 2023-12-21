19f0ce49bf7dbe00c737af206d086881d071f0c5
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-21 04:50:43 +0200

    Fixed compile

--- src/main/java/g2801_2900/s2858_minimum_edge_reversals_so_every_node_is_reachable/Solution.java
+++ src/main/java/g2801_2900/s2858_minimum_edge_reversals_so_every_node_is_reachable/Solution.java
@@ -8,6 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     public int[] minEdgeReversals(int n, int[][] edges) {
         List<int[]>[] nexts = new List[n];
