f4fb81b8dcb7c7ece755e8e7e20317f3766fdd9b
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-12-22 21:29:35 +0700

    update

--- src/main/java/g2801_2900/s2876_count_visited_nodes_in_a_directed_graph/Solution.java
+++ src/main/java/g2801_2900/s2876_count_visited_nodes_in_a_directed_graph/Solution.java
@@ -11,27 +11,27 @@ public class Solution {
         boolean[] visited = new boolean[n];
         int[] ans = new int[n];
         int[] level = new int[n];
-        for(int i = 0; i<n; i++){
-            if(!visited[i]){
-                visit(edges,0,i,ans,visited,level);
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visit(edges, 0, i, ans, visited, level);
             }
         }
         return ans;
     }
-    
-    private int[] visit(List<Integer> edges, int count, int curr, int[] ans,boolean[] visited,int[] level){
-        if(ans[curr]!=0){
-            return new int[]{-1,ans[curr]};
+
+    private int[] visit(
+            List<Integer> edges, int count, int curr, int[] ans, boolean[] visited, int[] level) {
+        if (ans[curr] != 0) {
+            return new int[] {-1, ans[curr]};
         }
-        if(visited[curr]){
-            return new int[]{level[curr],count-level[curr]};
+        if (visited[curr]) {
+            return new int[] {level[curr], count - level[curr]};
         }
         level[curr] = count;
         visited[curr] = true;
-        int[] ret = visit(edges,count+1,edges.get(curr),ans,visited,level);
-        if(ret[0] == -1 || count<ret[0]){
+        int[] ret = visit(edges, count + 1, edges.get(curr), ans, visited, level);
+        if (ret[0] == -1 || count < ret[0]) {
             ret[1]++;
-            
         }
         ans[curr] = ret[1];
         return ret;
