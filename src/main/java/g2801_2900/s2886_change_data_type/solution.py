275c82ad5d7d324bd2d8dfa13fc327816efb2767
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-23 13:15:43 +0200

    Update solution.py

--- src/main/java/g2801_2900/s2886_change_data_type/solution.py
+++ src/main/java/g2801_2900/s2886_change_data_type/solution.py
@@ -4,4 +4,4 @@ import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     students['grade'] = students['grade'].astype(int) 
-    return students   
+    return students
