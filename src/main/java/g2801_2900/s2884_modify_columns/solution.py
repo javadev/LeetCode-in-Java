8e7d7998c34e19999e6a9f0db932032c877a763a
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-23 13:15:00 +0200

    Update solution.py

--- src/main/java/g2801_2900/s2884_modify_columns/solution.py
+++ src/main/java/g2801_2900/s2884_modify_columns/solution.py
@@ -3,5 +3,5 @@
 import pandas as pd
 
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['salary'] = employees['salary']*2
+    employees['salary'] = employees['salary'] * 2
     return employees
