f58bea01ee51dce69c469ce7a47511de1b79ce57
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-26 12:01:03 +0200

    Improved tasks 577, 585

--- src/test/java/g0501_0600/s0585_investments_in_2016/MysqlTest.java
+++ src/test/java/g0501_0600/s0585_investments_in_2016/MysqlTest.java
@@ -20,20 +20,20 @@ import org.zapodot.junit.db.common.CompatibilityMode;
 @EmbeddedDatabaseTest(
         compatibilityMode = CompatibilityMode.MySQL,
         initialSqls =
-        "CREATE TABLE Insurance(pid INTEGER, tiv_2015 DECIMAL,  tiv_2016 DECIMAL"
-            + ", lat DECIMAL, lon DECIMAL); "
-            + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
-            + ", lat, lon)"
-            + " VALUES (1, 10, 5, 10, 10); "
-            + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
-            + ", lat, lon)"
-            + " VALUES (2, 20, 20, 20, 20); "
-            + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
-            + ", lat, lon)"
-            + " VALUES (3, 10, 30, 20, 20); "
-            + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
-            + ", lat, lon)"
-            + " VALUES (4, 10, 40, 40, 40); ")
+                "CREATE TABLE Insurance(pid INTEGER, tiv_2015 DECIMAL,  tiv_2016 DECIMAL"
+                        + ", lat DECIMAL, lon DECIMAL); "
+                        + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
+                        + ", lat, lon)"
+                        + " VALUES (1, 10, 5, 10, 10); "
+                        + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
+                        + ", lat, lon)"
+                        + " VALUES (2, 20, 20, 20, 20); "
+                        + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
+                        + ", lat, lon)"
+                        + " VALUES (3, 10, 30, 20, 20); "
+                        + "INSERT INTO Insurance(pid, tiv_2015, tiv_2016"
+                        + ", lat, lon)"
+                        + " VALUES (4, 10, 40, 40, 40); ")
 class MysqlTest {
     @Test
     void testScript(@EmbeddedDatabase DataSource dataSource)
