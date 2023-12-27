c391205a58aa8aaae6f249595a1bb5002c95debe
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-27 10:12:40 +0200

    Update Solution.java

--- src/main/java/g2901_3000/s2910_minimum_number_of_groups_to_create_a_valid_assignment/Solution.java
+++ src/main/java/g2901_3000/s2910_minimum_number_of_groups_to_create_a_valid_assignment/Solution.java
@@ -1,52 +1,63 @@
 package g2901_3000.s2910_minimum_number_of_groups_to_create_a_valid_assignment;
 
-// #Medium #Array #Hash_Table #Greedy #2023_12_27_Time_33_ms_(93.62%)_Space_60.9_MB_(34.20%)
+// #Medium #Array #Hash_Table #Greedy #2023_12_27_Time_36_ms_(68.99%)_Space_61_MB_(33.33%)
 
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings("java:S1172")
 public class Solution {
-    private int groupBySize(Map<Integer, Integer> countFreq, int size, int n) {
-        int group = 0;
-        for (Map.Entry<Integer, Integer> e : countFreq.entrySet()) {
-            int len = e.getKey();
-            int rem = len % (size + 1);
-            int g = len / (size + 1);
-            if (rem == 0) {
-                group += g * e.getValue();
-                continue;
-            }
-            int need = size - rem;
-            if (g >= need) {
-                group += (g + 1) * e.getValue();
-                continue;
-            }
-            return -1;
-        }
-        return group;
-    }
-
     public int minGroupsForValidAssignment(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int num : nums) {
-            count.merge(num, 1, Integer::sum);
-        }
-        int min = Integer.MAX_VALUE;
-        Map<Integer, Integer> countFreq = new HashMap<>();
-        for (int c : count.values()) {
-            countFreq.merge(c, 1, Integer::sum);
-        }
-        for (int c : countFreq.keySet()) {
-            min = Math.min(min, c);
-        }
-        for (int size = min; size >= 1; size--) {
-            int group = groupBySize(countFreq, size, n);
+        Map<Integer, Integer> count = getCountMap(nums);
+        Map<Integer, Integer> countFreq = getCountFrequencyMap(count);
+        int minFrequency = getMinFrequency(countFreq);
+        for (int size = minFrequency; size >= 1; size--) {
+            int group = calculateGroups(countFreq, size);
             if (group > 0) {
                 return group;
             }
         }
         return -1;
+    }
+
+    private Map<Integer, Integer> getCountMap(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.merge(num, 1, Integer::sum);
+        }
+        return count;
+    }
+
+    private Map<Integer, Integer> getCountFrequencyMap(Map<Integer, Integer> count) {
+        Map<Integer, Integer> countFreq = new HashMap<>();
+        for (int c : count.values()) {
+            countFreq.merge(c, 1, Integer::sum);
+        }
+        return countFreq;
+    }
+
+    private int getMinFrequency(Map<Integer, Integer> countFreq) {
+        return countFreq.keySet().stream()
+                .min(Integer::compareTo)
+                .orElseThrow(() -> new IllegalStateException("Count frequency map is empty"));
+    }
+
+    private int calculateGroups(Map<Integer, Integer> countFreq, int size) {
+        int group = 0;
+        for (Map.Entry<Integer, Integer> entry : countFreq.entrySet()) {
+            int len = entry.getKey();
+            int rem = len % (size + 1);
+            int g = len / (size + 1);
+            if (rem == 0) {
+                group += g * entry.getValue();
+            } else {
+                int need = size - rem;
+                if (g >= need) {
+                    group += (g + 1) * entry.getValue();
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return group;
     }
 }
