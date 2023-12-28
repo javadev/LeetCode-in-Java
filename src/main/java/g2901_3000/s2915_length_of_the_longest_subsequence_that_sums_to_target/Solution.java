bdeec5e31d7545915ef044f53c9113646ea53bce
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-28 14:06:10 +0200

    Improved task 2915

--- src/main/java/g2901_3000/s2915_length_of_the_longest_subsequence_that_sums_to_target/Solution.java
+++ src/main/java/g2901_3000/s2915_length_of_the_longest_subsequence_that_sums_to_target/Solution.java
@@ -1,8 +1,8 @@
 package g2901_3000.s2915_length_of_the_longest_subsequence_that_sums_to_target;
 
-import java.util.List;
-
 // #Medium #Array #Dynamic_Programming #2023_12_28_Time_23_ms_(91.30%)_Space_44.5_MB_(66.47%)
+
+import java.util.List;
 
 public class Solution {
     public int lengthOfLongestSubsequence(List<Integer> nums, int target) {
