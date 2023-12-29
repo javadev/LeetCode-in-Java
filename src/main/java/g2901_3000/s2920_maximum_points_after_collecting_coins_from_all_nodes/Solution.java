2db4ad4b5c78ef88a05a325a9d93b0228e67d759
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-29 14:56:27 +0200

    Improved tasks 2920, 2925

--- src/main/java/g2901_3000/s2920_maximum_points_after_collecting_coins_from_all_nodes/Solution.java
+++ src/main/java/g2901_3000/s2920_maximum_points_after_collecting_coins_from_all_nodes/Solution.java
@@ -1,6 +1,6 @@
 package g2901_3000.s2920_maximum_points_after_collecting_coins_from_all_nodes;
 
-// #Hard #Array #Dynamic_Programming #Tree #Bit_Manipulation #Depth_First_Search
+// #Hard #Array #Dynamic_Programming #Depth_First_Search #Tree #Bit_Manipulation
 // #2023_12_29_Time_113_ms_(77.94%)_Space_111.3_MB_(91.50%)
 
 import java.util.ArrayList;
