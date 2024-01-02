02979d8b19e6fee3364a236ecda0ee0b743fbc84
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-02 09:39:59 +0200

    Update Solution.java

--- src/main/java/g2901_3000/s2930_number_of_strings_which_can_be_rearranged_to_contain_substring/Solution.java
+++ src/main/java/g2901_3000/s2930_number_of_strings_which_can_be_rearranged_to_contain_substring/Solution.java
@@ -18,12 +18,12 @@ public class Solution {
     }
 
     public int stringCount(int n) {
-        long mod = (int) 1e9 + 7;
+        long mod = (int) 1e9 + 7L;
         return (int)
                 (((+pow(26, n, mod)
-                                                - (n + 75) * pow(25, n - 1, mod)
-                                                + (2 * n + 72) * pow(24, n - 1, mod)
-                                                - (n + 23) * pow(23, n - 1, mod))
+                                                - (n + 75) * pow(25, n - 1L, mod)
+                                                + (2 * n + 72) * pow(24, n - 1L, mod)
+                                                - (n + 23) * pow(23, n - 1L, mod))
                                         % mod
                                 + mod)
                         % mod);
