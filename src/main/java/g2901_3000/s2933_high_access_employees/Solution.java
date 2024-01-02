19ee11329ffe60b6f9a3e85eb96608538616838f
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-02 09:51:47 +0200

    Update Solution.java

--- src/main/java/g2901_3000/s2933_high_access_employees/Solution.java
+++ src/main/java/g2901_3000/s2933_high_access_employees/Solution.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
     private boolean isPossible(int a, int b) {
