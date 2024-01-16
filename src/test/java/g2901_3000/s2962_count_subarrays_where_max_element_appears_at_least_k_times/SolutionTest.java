c6736841bf82d8a8ef1a76a5988bfe40c09be3af
ThanhNIT <thanhnguyen.it.work@gmail.com>
2024-01-16 10:42:57 +0700

    Update SolutionTest.java

--- src/test/java/g2901_3000/s2962_count_subarrays_where_max_element_appears_at_least_k_times/SolutionTest.java
+++ src/test/java/g2901_3000/s2962_count_subarrays_where_max_element_appears_at_least_k_times/SolutionTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     @Test
     void countSubarrays() {
-        assertThat(new Solution().countSubarrays(new int[] {1, 3, 2, 3, 3}, 2), equalTo(3));
+        assertThat(new Solution().countSubarrays(new int[] {1, 3, 2, 3, 3}, 2), equalTo(6L));
     }
 
     @Test
     void countSubarrays2() {
-        assertThat(new Solution().countSubarrays(new int[] {1, 4, 2, 1}, 3), equalTo(0));
+        assertThat(new Solution().countSubarrays(new int[] {1, 4, 2, 1}, 3), equalTo(0L));
     }
 }
