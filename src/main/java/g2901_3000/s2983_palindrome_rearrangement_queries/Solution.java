96df59523832322a34e1aa027a69fa167829325b
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-18 17:28:03 +0200

    Update Solution.java

--- src/main/java/g2901_3000/s2983_palindrome_rearrangement_queries/Solution.java
+++ src/main/java/g2901_3000/s2983_palindrome_rearrangement_queries/Solution.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-@SuppressWarnings({"java:S6541", "java:S135"})
+@SuppressWarnings("java:S6541")
 public class Solution {
     private int n;
 
