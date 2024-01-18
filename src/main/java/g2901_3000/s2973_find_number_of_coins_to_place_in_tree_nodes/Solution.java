0d682d48f6c1f6276ba1cc9136445db80909a430
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-19 01:35:04 +0200

    Improved tasks 2973, 2998

--- src/main/java/g2901_3000/s2973_find_number_of_coins_to_place_in_tree_nodes/Solution.java
+++ src/main/java/g2901_3000/s2973_find_number_of_coins_to_place_in_tree_nodes/Solution.java
@@ -1,6 +1,6 @@
 package g2901_3000.s2973_find_number_of_coins_to_place_in_tree_nodes;
 
-// #Hard #Dynamic_Programming #Sorting #Tree #Heap_Priority_Queue #Depth_First_Search
+// #Hard #Dynamic_Programming #Sorting #Depth_First_Search #Tree #Heap_Priority_Queue
 // #2024_01_16_Time_93_ms_(72.11%)_Space_63.4_MB_(33.51%)
 
 import java.util.ArrayList;
--- src/main/java/g2901_3000/s2998_minimum_number_of_operations_to_make_x_and_y_equal/Solution.java
+++ src/main/java/g2901_3000/s2998_minimum_number_of_operations_to_make_x_and_y_equal/Solution.java
@@ -1,6 +1,6 @@
 package g2901_3000.s2998_minimum_number_of_operations_to_make_x_and_y_equal;
 
-// #Medium #Dynamic_Programming #Memoization #Breadth_First_Search
+// #Medium #Dynamic_Programming #Breadth_First_Search #Memoization
 // #2024_01_17_Time_0_ms_(100.00%)_Space_41.2_MB_(92.39%)
 
 public class Solution {
