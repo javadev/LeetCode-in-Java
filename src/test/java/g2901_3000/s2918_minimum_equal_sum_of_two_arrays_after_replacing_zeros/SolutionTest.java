3cea18c0ebc8698ef03e5ce8a6f831842baa3607
Valentyn Kolesnikov <javadev75@gmail.com>
2024-01-19 01:54:28 +0200

    Added tests

--- src/test/java/g2901_3000/s2918_minimum_equal_sum_of_two_arrays_after_replacing_zeros/SolutionTest.java
+++ src/test/java/g2901_3000/s2918_minimum_equal_sum_of_two_arrays_after_replacing_zeros/SolutionTest.java
@@ -17,4 +17,23 @@ class SolutionTest {
     void minSum2() {
         assertThat(new Solution().minSum(new int[] {2, 0, 2, 0}, new int[] {1, 4}), equalTo(-1L));
     }
+
+    @Test
+    void minSum3() {
+        assertThat(
+                new Solution().minSum(new int[] {17, 1, 13, 12, 3, 13}, new int[] {2, 25}),
+                equalTo(-1L));
+    }
+
+    @Test
+    void minSum4() {
+        assertThat(
+                new Solution()
+                        .minSum(
+                                new int[] {9, 5},
+                                new int[] {
+                                    15, 12, 5, 21, 4, 26, 27, 9, 6, 29, 0, 18, 16, 0, 0, 0, 20
+                                }),
+                equalTo(-1L));
+    }
 }
