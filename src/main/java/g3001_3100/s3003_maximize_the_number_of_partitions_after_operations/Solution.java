8047f12934ed8401dd61a91320b5de1ed381b46b
Valentyn Kolesnikov <javadev75@gmail.com>
2024-02-26 07:35:11 +0200

    Update Solution.java

--- src/main/java/g3001_3100/s3003_maximize_the_number_of_partitions_after_operations/Solution.java
+++ src/main/java/g3001_3100/s3003_maximize_the_number_of_partitions_after_operations/Solution.java
@@ -3,6 +3,7 @@ package g3001_3100.s3003_maximize_the_number_of_partitions_after_operations;
 // #Hard #String #Dynamic_Programming #Bit_Manipulation #Bitmask
 // #2024_02_26_Time_1_ms_(100.00%)_Space_42.1_MB_(99.44%)
 
+@SuppressWarnings("java:S6541")
 public class Solution {
     private static final int ALPHABET_SIZE = 'z' - 'a' + 1;
 
