e0df5cd773ce466154fcebe081aaa7c739d034e2
ThanhNIT <thanhnguyen.it.work@gmail.com>
2024-02-27 17:12:59 +0700

    Update Solution.java

--- src/main/java/g3001_3100/s3013_divide_an_array_into_subarrays_with_minimum_cost_ii/Solution.java
+++ src/main/java/g3001_3100/s3013_divide_an_array_into_subarrays_with_minimum_cost_ii/Solution.java
@@ -1,5 +1,8 @@
 package g3001_3100.s3013_divide_an_array_into_subarrays_with_minimum_cost_ii;
 
+// #Hard #Array #Hash_Table #Heap_Priority_Queue #Sliding_Window
+// #2024_02_27_Time_276_ms_(90.86%)_Space_81.5_MB_(5.19%)
+
 import java.util.Comparator;
 import java.util.TreeSet;
 
