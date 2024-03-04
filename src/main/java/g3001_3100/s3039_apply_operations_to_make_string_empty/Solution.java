56ac442858686cb5621812c96be295404b5de20f
ThanhNIT <thanhnguyen.it.work@gmail.com>
2024-03-04 21:53:52 +0700

    Update Solution.java

--- src/main/java/g3001_3100/s3039_apply_operations_to_make_string_empty/Solution.java
+++ src/main/java/g3001_3100/s3039_apply_operations_to_make_string_empty/Solution.java
@@ -5,22 +5,22 @@ package g3001_3100.s3039_apply_operations_to_make_string_empty;
 
 public class Solution {
     public String lastNonEmptyString(String s) {
-        int[] freq=new int[26];          
-        char[] ar=s.toCharArray();
-        int n=ar.length;
-        int max=1;
-        StringBuilder sb=new StringBuilder();
+        int[] freq = new int[26];
+        char[] ar = s.toCharArray();
+        int n = ar.length;
+        int max = 1;
+        StringBuilder sb = new StringBuilder();
         for (char c : ar) {
             freq[c - 'a']++;
             max = Math.max(freq[c - 'a'], max);
         }
-        for(int i=n-1;i>=0;i--){
-            if(freq[ar[i]-'a']==max){
+        for (int i = n - 1; i >= 0; i--) {
+            if (freq[ar[i] - 'a'] == max) {
                 sb.append(ar[i]);
-                freq[ar[i]-'a']=0;
+                freq[ar[i] - 'a'] = 0;
             }
         }
         return sb.reverse().toString();
-        
+
     }
 }
