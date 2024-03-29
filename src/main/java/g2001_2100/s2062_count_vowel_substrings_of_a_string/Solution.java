8e563f3247865b0e01e917174f7fa54e11dcccff
Valentyn Kolesnikov <valentyn.kolesnikov@cantire.com>
2024-03-29 14:31:47 +0200

    Improved tasks 1903, 1993, 2062, 2111, 2272

--- src/main/java/g2001_2100/s2062_count_vowel_substrings_of_a_string/Solution.java
+++ src/main/java/g2001_2100/s2062_count_vowel_substrings_of_a_string/Solution.java
@@ -1,32 +1,45 @@
 package g2001_2100.s2062_count_vowel_substrings_of_a_string;
 
-// #Easy #String #Hash_Table #2022_05_29_Time_34_ms_(23.83%)_Space_41.9_MB_(71.28%)
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+// #Easy #String #Hash_Table #2024_03_29_Time_1_ms_(99.82%)_Space_41.5_MB_(72.24%)
 
 public class Solution {
     public int countVowelSubstrings(String word) {
-        int count = 0;
-        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-        Set<Character> window = new HashSet<>();
-        for (int i = 0; i < word.length(); i++) {
-            window.clear();
-            if (vowels.contains(word.charAt(i))) {
-                window.add(word.charAt(i));
-                for (int j = i + 1; j < word.length(); j++) {
-                    if (!vowels.contains(word.charAt(j))) {
-                        break;
-                    } else {
-                        window.add(word.charAt(j));
-                        if (window.size() == 5) {
-                            count++;
-                        }
-                    }
+        final int length = word.length();
+        boolean[] vows = new boolean[128];
+        vows['a'] = true;
+        vows['e'] = true;
+        vows['i'] = true;
+        vows['o'] = true;
+        vows['u'] = true;
+        int[] counts = new int[128];
+        int uniqVows = 0;
+        int originalBegin = 0;
+        int begin = 0;
+        int result = 0;
+        for (int i = 0; i < length; i++) {
+            char ch = word.charAt(i);
+            if (vows[ch]) {
+                counts[ch]++;
+                if (counts[ch] == 1) {
+                    uniqVows++;
                 }
+                while (uniqVows == 5) {
+                    uniqVows -= --counts[word.charAt(begin)] == 0 ? 1 : 0;
+                    begin++;
+                }
+                result += begin - originalBegin;
+            } else {
+                if (uniqVows != 0) {
+                    uniqVows = 0;
+                    counts['a'] = 0;
+                    counts['e'] = 0;
+                    counts['i'] = 0;
+                    counts['o'] = 0;
+                    counts['u'] = 0;
+                }
+                originalBegin = begin = i + 1;
             }
         }
-        return count;
+        return result;
     }
 }
