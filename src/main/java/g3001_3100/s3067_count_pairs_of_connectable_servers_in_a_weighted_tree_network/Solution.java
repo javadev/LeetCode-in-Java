fc3daf418266cae40b215f1fa507f5d7066fb1b5
Valentyn Kolesnikov <javadev75@gmail.com>
2024-03-31 05:22:21 -0400

    Updated tags

--- src/main/java/g3001_3100/s3067_count_pairs_of_connectable_servers_in_a_weighted_tree_network/Solution.java
+++ src/main/java/g3001_3100/s3067_count_pairs_of_connectable_servers_in_a_weighted_tree_network/Solution.java
@@ -1,6 +1,6 @@
 package g3001_3100.s3067_count_pairs_of_connectable_servers_in_a_weighted_tree_network;
 
-// #Medium #Array #Tree #Depth_First_Search #2024_03_31_Time_69_ms_(99.83%)_Space_45.5_MB_(81.49%)
+// #Medium #Array #Depth_First_Search #Tree #2024_03_31_Time_69_ms_(99.83%)_Space_45.5_MB_(81.49%)
 
 import java.util.ArrayList;
 
