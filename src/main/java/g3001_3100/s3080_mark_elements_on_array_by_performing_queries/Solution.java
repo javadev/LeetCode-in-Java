ce6368c9cc2600e88999be3005b0b4f3ba680b86
Valentyn Kolesnikov <javadev75@gmail.com>
2024-04-16 18:35:31 +0300

    Update Solution.java

--- src/main/java/g3001_3100/s3080_mark_elements_on_array_by_performing_queries/Solution.java
+++ src/main/java/g3001_3100/s3080_mark_elements_on_array_by_performing_queries/Solution.java
@@ -3,7 +3,7 @@ package g3001_3100.s3080_mark_elements_on_array_by_performing_queries;
 // #Medium #Array #Hash_Table #Sorting #Heap_Priority_Queue #Simulation
 // #2024_04_16_Time_50_ms_(99.96%)_Space_77.2_MB_(15.35%)
 
-@SuppressWarnings("java:S6541")
+@SuppressWarnings({"java:S1871", "java:S6541"})
 public class Solution {
     public long[] unmarkedSumArray(int[] nums, int[][] queries) {
         int l = nums.length;
