351cb3794337db5fa02039f5883e000c474ecf1e
Valentyn Kolesnikov <javadev75@gmail.com>
2024-06-09 05:12:01 +0300

    Improved task 208

--- src/main/java/g0201_0300/s0208_implement_trie_prefix_tree/Trie.java
+++ src/main/java/g0201_0300/s0208_implement_trie_prefix_tree/Trie.java
@@ -66,3 +66,11 @@ public class Trie {
         return startWith;
     }
 }
+
+/*
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
