6ff3a2ec7696fdde329361dc6b6b200674565c37
Valentyn Kolesnikov <javadev75@gmail.com>
2024-06-14 09:27:57 +0300

    Fixed sonar

--- src/main/java/g3101_3200/s3180_maximum_total_reward_using_operations_i/Solution.java
+++ src/main/java/g3101_3200/s3180_maximum_total_reward_using_operations_i/Solution.java
@@ -2,6 +2,7 @@ package g3101_3200.s3180_maximum_total_reward_using_operations_i;
 
 // #Medium #Array #Dynamic_Programming #2024_06_14_Time_1_ms_(100.00%)_Space_43.3_MB_(97.85%)
 
+@SuppressWarnings("java:S135")
 public class Solution {
     private int[] sortedSet(int[] values) {
         int max = 0;
@@ -34,7 +35,8 @@ public class Solution {
         boolean[] isSumPossible = new boolean[max];
         isSumPossible[0] = true;
         int maxSum = 0;
-        for (int sum = rewardValues[0], last = 1; sum < max; sum++) {
+        int last = 1;
+        for (int sum = rewardValues[0]; sum < max; sum++) {
             while (last < n && rewardValues[last] <= sum) {
                 last++;
             }
@@ -50,6 +52,7 @@ public class Solution {
                     break;
                 }
             }
+            last++;
         }
         return maxSum + max;
     }
