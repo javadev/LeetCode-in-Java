8ce41cb23199ec30c8cfd49e0bf9d65f245a9dc1
Valentyn Kolesnikov <javadev75@gmail.com>
2024-07-03 09:37:48 +0300

    Improved task 152

--- src/main/java/g0101_0200/s0152_maximum_product_subarray/Solution.java
+++ src/main/java/g0101_0200/s0152_maximum_product_subarray/Solution.java
@@ -2,27 +2,23 @@ package g0101_0200.s0152_maximum_product_subarray;
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Array #Dynamic_Programming
 // #Dynamic_Programming_I_Day_6 #Level_2_Day_13_Dynamic_Programming #Udemy_Dynamic_Programming
-// #Big_O_Time_O(N)_Space_O(1) #2022_06_25_Time_0_ms_(100.00%)_Space_42.7_MB_(82.46%)
+// #Big_O_Time_O(N)_Space_O(1) #2024_07_03_Time_1_ms_(92.31%)_Space_44.6_MB_(75.65%)
 
 public class Solution {
-    public int maxProduct(int[] arr) {
-        int ans = Integer.MIN_VALUE;
-        int cprod = 1;
-        for (int j : arr) {
-            cprod = cprod * j;
-            ans = Math.max(ans, cprod);
-            if (cprod == 0) {
-                cprod = 1;
+    public int maxProduct(int[] nums) {
+        int currentMaxProd = nums[0];
+        int currentMinProd = nums[0];
+        int overAllMaxProd = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = currentMaxProd;
+                currentMaxProd = currentMinProd;
+                currentMinProd = temp;
             }
+            currentMaxProd = Math.max(nums[i], nums[i] * currentMaxProd);
+            currentMinProd = Math.min(nums[i], nums[i] * currentMinProd);
+            overAllMaxProd = Math.max(overAllMaxProd, currentMaxProd);
         }
-        cprod = 1;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            cprod = cprod * arr[i];
-            ans = Math.max(ans, cprod);
-            if (cprod == 0) {
-                cprod = 1;
-            }
-        }
-        return ans;
+        return overAllMaxProd;
     }
 }
