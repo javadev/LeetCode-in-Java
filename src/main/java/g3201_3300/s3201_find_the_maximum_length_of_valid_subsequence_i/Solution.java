d8ac3a8743b9945d44020ff52dbb11da6d306ec2
Valentyn Kolesnikov <javadev75@gmail.com>
2024-07-06 12:41:48 +0300

    Improved task 3201

--- src/main/java/g3201_3300/s3201_find_the_maximum_length_of_valid_subsequence_i/Solution.java
+++ src/main/java/g3201_3300/s3201_find_the_maximum_length_of_valid_subsequence_i/Solution.java
@@ -2,7 +2,7 @@ package g3201_3300.s3201_find_the_maximum_length_of_valid_subsequence_i;
 
 // #Medium #Array #Dynamic_Programming #2024_07_04_Time_5_ms_(82.23%)_Space_61.7_MB_(91.46%)
 
-class Solution {
+public class Solution {
     public int maximumLength(int[] nums) {
         int n = nums.length;
         int alter = 1;
