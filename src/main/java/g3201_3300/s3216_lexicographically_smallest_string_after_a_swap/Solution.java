b46254ddda2cc35182b1ba49b04d92a360419dd0
Valentyn Kolesnikov <javadev75@gmail.com>
2024-07-18 09:18:27 +0300

    Fixed style

--- src/main/java/g3201_3300/s3216_lexicographically_smallest_string_after_a_swap/Solution.java
+++ src/main/java/g3201_3300/s3216_lexicographically_smallest_string_after_a_swap/Solution.java
@@ -5,13 +5,14 @@ package g3201_3300.s3216_lexicographically_smallest_string_after_a_swap;
 public class Solution {
     public String getSmallestString(String s) {
         final char[] arr = s.toCharArray();
-        for (int i = 1; i < arr.length; i++)
+        for (int i = 1; i < arr.length; i++) {
             if (arr[i - 1] % 2 == arr[i] % 2 && arr[i - 1] > arr[i]) {
                 final char temp = arr[i];
                 arr[i] = arr[i - 1];
                 arr[i - 1] = temp;
                 break;
             }
+        }
         return String.valueOf(arr);
     }
 }
