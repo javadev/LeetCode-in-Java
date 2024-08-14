c8128651fa4bd25c1f146b4dde596639ede4dae9
Valentyn Kolesnikov <javadev75@gmail.com>
2024-08-14 05:26:29 +0300

    Improved tags

--- src/main/java/g3201_3300/s3217_delete_nodes_from_linked_list_present_in_array/Solution.java
+++ src/main/java/g3201_3300/s3217_delete_nodes_from_linked_list_present_in_array/Solution.java
@@ -1,13 +1,18 @@
 package g3201_3300.s3217_delete_nodes_from_linked_list_present_in_array;
 
-import com_github_leetcode.ListNode;
-
 // #Medium #Array #Hash_Table #Linked_List #2024_07_18_Time_3_ms_(100.00%)_Space_63.9_MB_(93.81%)
 
-/**
- * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {}
- * ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
- * this.next = next; } }
+import com_github_leetcode.ListNode;
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 public class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
