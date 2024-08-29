d8b916dd7d4cec8ed86642669fc732ff26cf90c1
Valentyn Kolesnikov <javadev75@gmail.com>
2024-08-29 03:23:58 +0300

    Improved tasks 3264, 3265, 3266, 3267

--- src/main/java/g3201_3300/s3264_final_array_state_after_k_multiplication_operations_i/Solution.java
+++ src/main/java/g3201_3300/s3264_final_array_state_after_k_multiplication_operations_i/Solution.java
@@ -1,6 +1,7 @@
 package g3201_3300.s3264_final_array_state_after_k_multiplication_operations_i;
 
-// #Easy #2024_08_28_Time_1_ms_(100.00%)_Space_44.9_MB_(31.20%)
+// #Easy #Array #Math #Heap_Priority_Queue #Simulation
+// #2024_08_28_Time_1_ms_(100.00%)_Space_44.9_MB_(31.20%)
 
 public class Solution {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
