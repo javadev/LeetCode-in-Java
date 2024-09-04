e5194decac4c393386d31ac2efb9078d2ee53b31
Valentyn Kolesnikov <javadev75@gmail.com>
2024-09-04 04:50:23 +0300

    Improved 1616

--- src/main/java/g1601_1700/s1616_split_two_strings_to_make_palindrome/Solution.java
+++ src/main/java/g1601_1700/s1616_split_two_strings_to_make_palindrome/Solution.java
@@ -1,28 +1,32 @@
 package g1601_1700.s1616_split_two_strings_to_make_palindrome;
 
-// #Medium #String #Greedy #Two_Pointers #2022_04_13_Time_4_ms_(89.77%)_Space_43.3_MB_(85.58%)
+// #Medium #String #Greedy #Two_Pointers #2024_09_04_Time_2_ms_(100.00%)_Space_45.1_MB_(97.99%)
 
 @SuppressWarnings("java:S2234")
 public class Solution {
     public boolean checkPalindromeFormation(String a, String b) {
-        return check(a, b) || check(b, a);
+        int n = a.length();
+        int s = 0;
+        int e = n - 1;
+        if (isPalindrome(a, b, s, e, true)) {
+            return true;
+        } else {
+            return isPalindrome(b, a, s, e, true);
+        }
     }
 
-    private boolean check(String a, String b) {
-        int i = 0;
-        int j = b.length() - 1;
-        while (j > i && a.charAt(i) == b.charAt(j)) {
-            ++i;
-            --j;
+    private boolean isPalindrome(String a, String b, int s, int e, boolean check) {
+        if (s == e) {
+            return true;
         }
-        return isPalindrome(a, i, j) || isPalindrome(b, i, j);
-    }
-
-    private boolean isPalindrome(String s, int i, int j) {
-        while (j > i && s.charAt(i) == s.charAt(j)) {
-            ++i;
-            --j;
+        while (s < e) {
+            if (a.charAt(s) != b.charAt(e)) {
+                return check
+                        && (isPalindrome(a, a, s, e, false) || isPalindrome(b, b, s, e, false));
+            }
+            s++;
+            e--;
         }
-        return i >= j;
+        return true;
     }
 }
