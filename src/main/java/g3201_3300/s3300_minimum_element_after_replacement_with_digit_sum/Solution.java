7699ae3aff5bf3de760f716540204f6e37166faa
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-01 06:16:39 +0300

    Improved 3300

--- src/main/java/g3201_3300/s3300_minimum_element_after_replacement_with_digit_sum/Solution.java
+++ src/main/java/g3201_3300/s3300_minimum_element_after_replacement_with_digit_sum/Solution.java
@@ -2,7 +2,7 @@ package g3201_3300.s3300_minimum_element_after_replacement_with_digit_sum;
 
 // #Easy #Array #Math #2024_10_01_Time_1_ms_(100.00%)_Space_42.9_MB_(75.97%)
 
-class Solution {
+public class Solution {
     public int minElement(int[] nums) {
         int min = Integer.MAX_VALUE;
         for (int x : nums) {
