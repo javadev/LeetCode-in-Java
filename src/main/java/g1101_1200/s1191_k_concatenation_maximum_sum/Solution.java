13e0b7b9426457ffd0ce5986b01f3cf9ab644960
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-08 12:15:38 +0300

    Update Solution.java

--- src/main/java/g1101_1200/s1191_k_concatenation_maximum_sum/Solution.java
+++ src/main/java/g1101_1200/s1191_k_concatenation_maximum_sum/Solution.java
@@ -3,7 +3,7 @@ package g1101_1200.s1191_k_concatenation_maximum_sum;
 // #Medium #Array #Dynamic_Programming #2022_03_03_Time_6_ms_(73.85%)_Space_59.8_MB_(30.38%)
 
 public class Solution {
-    private final long MOD = 1000000007;
+    private static final long MOD = 1000000007;
 
     public int kConcatenationMaxSum(int[] arr, int k) {
         // int kadane = Kadane(arr);
