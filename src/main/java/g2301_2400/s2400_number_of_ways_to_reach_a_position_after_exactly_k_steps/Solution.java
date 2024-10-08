31234cec36b2c405e0c118bbb1e0b3e6a7a544fa
Valentyn Kolesnikov <javadev75@gmail.com>
2024-10-08 12:22:17 +0300

    Update Solution.java

--- src/main/java/g2301_2400/s2400_number_of_ways_to_reach_a_position_after_exactly_k_steps/Solution.java
+++ src/main/java/g2301_2400/s2400_number_of_ways_to_reach_a_position_after_exactly_k_steps/Solution.java
@@ -4,7 +4,7 @@ package g2301_2400.s2400_number_of_ways_to_reach_a_position_after_exactly_k_step
 // #2022_09_19_Time_1_ms_(99.66%)_Space_41.7_MB_(91.83%)
 
 public class Solution {
-    private final int MOD = 1000000007;
+    private static final int MOD = 1000000007;
 
     public int numberOfWays(int startPos, int endPos, int k) {
         if (Math.abs(endPos - startPos) > k) {
