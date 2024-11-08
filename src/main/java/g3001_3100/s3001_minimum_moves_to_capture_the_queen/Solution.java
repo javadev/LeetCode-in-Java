b855aff4d7a8c169110bff997574d86dd8c04ca0
Valentyn Kolesnikov <javadev75@gmail.com>
2024-11-08 06:44:47 +0200

    Fixed style

--- src/main/java/g3001_3100/s3001_minimum_moves_to_capture_the_queen/Solution.java
+++ src/main/java/g3001_3100/s3001_minimum_moves_to_capture_the_queen/Solution.java
@@ -5,16 +5,16 @@ package g3001_3100.s3001_minimum_moves_to_capture_the_queen;
 public class Solution {
     public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
         if (a == e || b == f) {
-            if (a == e && a == c && ((d - b) * (d - f) < 0)) {
+            if (a == e && a == c && (d - b) * (d - f) < 0) {
                 return 2;
             }
-            if (b == f && b == d && ((c - a) * (c - e) < 0)) {
+            if (b == f && b == d && (c - a) * (c - e) < 0) {
                 return 2;
             }
             return 1;
         }
         if (Math.abs(c - e) == Math.abs(d - f)) {
-            if (Math.abs(c - a) == Math.abs(d - b) && ((b - f) * (b - d) < 0)) {
+            if (Math.abs(c - a) == Math.abs(d - b) && (b - f) * (b - d) < 0) {
                 return 2;
             }
             return 1;
