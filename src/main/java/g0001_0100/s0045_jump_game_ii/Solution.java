bd7079b055be9e8ffdb1aa9a09566daa3da880ae
Valentyn Kolesnikov <javadev75@gmail.com>
2024-11-10 06:55:27 +0200

    Update Solution.java

--- src/main/java/g0001_0100/s0045_jump_game_ii/Solution.java
+++ src/main/java/g0001_0100/s0045_jump_game_ii/Solution.java
@@ -2,29 +2,29 @@ package g0001_0100.s0045_jump_game_ii;
 
 // #Medium #Top_100_Liked_Questions #Array #Dynamic_Programming #Greedy
 // #Algorithm_II_Day_13_Dynamic_Programming #Dynamic_Programming_I_Day_4
-// #Big_O_Time_O(n)_Space_O(1) #2024_11_10_Time_0_ms_(100.00%)_Space_44.9_MB_(75.73%)
+// #Big_O_Time_O(n)_Space_O(1) #2024_11_10_Time_0_ms_(100.00%)_Space_45_MB_(64.44%)
 
 public class Solution {
-    public int jump(int[] nums) {
-        if (nums.length == 1) {
-            return 0;
+    private int getMax(int[] nums, int l, int r) {
+        int max = -1;
+        int curr = -1;
+        for (int i = l; i <= r; i++) {
+            curr = i + nums[i];
+            max = Math.max(max, curr);
         }
-        return minJumps(nums, 0, 0);
+        return max;
     }
 
-    private int minJumps(int[] nums, int currIndex, int jumpStatus) {
-        if (currIndex + nums[currIndex] >= nums.length - 1) {
-            return jumpStatus + 1;
+    public int jump(int[] nums) {
+        int l = 0;
+        int r = 0;
+        int jumps = 0;
+        while (r < nums.length - 1) {
+            int prev = r;
+            r = getMax(nums, l, r);
+            l = prev + 1;
+            jumps++;
         }
-        int nextIndex = currIndex + 1;
-        int distanceLeft = (nums.length - 1 - (currIndex + 1)) - nums[currIndex + 1];
-        for (int i = currIndex + 2; i <= (currIndex + nums[currIndex]); i++) {
-            int tempDistanceLeft = (nums.length - 1 - i) - nums[i];
-            if (distanceLeft > tempDistanceLeft) {
-                distanceLeft = tempDistanceLeft;
-                nextIndex = i;
-            }
-        }
-        return minJumps(nums, nextIndex, jumpStatus + 1);
+        return jumps;
     }
 }
