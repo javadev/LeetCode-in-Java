7f09298d5626f93775d2b154c96d5998556de2ab
Valentyn Kolesnikov <javadev75@gmail.com>
2024-11-27 11:23:35 +0200

    Fixed compile warning

--- src/main/java/g3301_3400/s3367_maximize_sum_of_weights_after_edge_removals/Solution.java
+++ src/main/java/g3301_3400/s3367_maximize_sum_of_weights_after_edge_removals/Solution.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
 
+@SuppressWarnings("unchecked")
 public class Solution {
     private List<int[]>[] adj;
     private int k;
