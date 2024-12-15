a27df2e587ef5286a199a72a8a73cfef8a6be1e7
Valentyn Kolesnikov <javadev75@gmail.com>
2024-12-15 19:32:33 +0200

    Fixed sonar

--- src/main/java/g3301_3400/s3387_maximize_amount_after_two_days_of_conversions/Solution.java
+++ src/main/java/g3301_3400/s3387_maximize_amount_after_two_days_of_conversions/Solution.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@SuppressWarnings("java:S3824")
 public class Solution {
     private double res;
     private Map<String, List<Pair>> map1;
