b1f355b5759e59116c8a598228a3c1565a70d5f6
Valentyn Kolesnikov <javadev75@gmail.com>
2025-01-30 05:02:00 +0200

    Improved tags

--- src/main/java/g0001_0100/s0053_maximum_subarray/Solution.java
+++ src/main/java/g0001_0100/s0053_maximum_subarray/Solution.java
@@ -2,7 +2,7 @@ package g0001_0100.s0053_maximum_subarray;
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Array #Dynamic_Programming
 // #Divide_and_Conquer #Data_Structure_I_Day_1_Array #Dynamic_Programming_I_Day_5
-// #Udemy_Famous_Algorithm #Big_O_Time_O(n)_Space_O(1)
+// #Udemy_Famous_Algorithm #Top_Interview_150_Kadane's_Algorithm #Big_O_Time_O(n)_Space_O(1)
 // #2024_11_11_Time_1_ms_(99.32%)_Space_56.9_MB_(54.82%)
 
 public class Solution {
