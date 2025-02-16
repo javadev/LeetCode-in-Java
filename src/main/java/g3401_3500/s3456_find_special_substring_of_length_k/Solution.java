e99ac35069941cc424d0117e5436f9620fe30a9e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-16 22:30:09 +0200

    Fixed sonar

--- src/main/java/g3401_3500/s3456_find_special_substring_of_length_k/Solution.java
+++ src/main/java/g3401_3500/s3456_find_special_substring_of_length_k/Solution.java
@@ -1,21 +1,10 @@
 package g3401_3500.s3456_find_special_substring_of_length_k;
 
-// #Easy #2025_02_16_Time_0_ms_(100.00%)_Space_42.05_MB_(100.00%)
+// #Easy #2025_02_16_Time_1_ms_(100.00%)_Space_42.25_MB_(100.00%)
 
 @SuppressWarnings("java:S1871")
 public class Solution {
     public boolean hasSpecialSubstring(String s, int k) {
-        if (s.length() == k) {
-            if (k == 1) {
-                return true;
-            }
-            for (int i = 0; i < k - 1; i++) {
-                if (s.charAt(i) != s.charAt(i + 1)) {
-                    return false;
-                }
-            }
-            return true;
-        }
         int start = 0;
         int end = k;
         while (end <= s.length()) {
