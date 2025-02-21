17dbb1e5a169c69e03bcb953856544b12c4b57b0
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-21 06:39:50 +0200

    Fixed sonar

--- src/main/java/g3401_3500/s3459_length_of_longest_v_shaped_diagonal_segment/Solution.java
+++ src/main/java/g3401_3500/s3459_length_of_longest_v_shaped_diagonal_segment/Solution.java
@@ -4,14 +4,15 @@ package g3401_3500.s3459_length_of_longest_v_shaped_diagonal_segment;
 // #2025_02_21_Time_51_ms_(77.23%)_Space_75.55_MB_(90.55%)
 
 public class Solution {
-    private int[][] directions = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+    private final int[][] directions = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
 
-    public int lenOfVDiagonal(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] bottomLeft = new int[m][n];
-        int[][] bottomRight = new int[m][n];
-        int[][] topLeft = new int[m][n];
-        int[][] topRight = new int[m][n];
+    private void initializeArrays(
+            int[][] bottomLeft,
+            int[][] bottomRight,
+            int[][] topLeft,
+            int[][] topRight,
+            int m,
+            int n) {
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 bottomLeft[i][j] = 1;
@@ -20,6 +21,10 @@ public class Solution {
                 topRight[i][j] = 1;
             }
         }
+    }
+
+    private int processBottomDirections(
+            int[][] grid, int[][] bottomLeft, int[][] bottomRight, int m, int n) {
         int ans = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -36,6 +41,11 @@ public class Solution {
                 }
             }
         }
+        return ans;
+    }
+
+    private void processTopDirections(
+            int[][] grid, int[][] topLeft, int[][] topRight, int m, int n) {
         for (int i = m - 1; i >= 0; --i) {
             for (int j = n - 1; j >= 0; --j) {
                 int x = grid[i][j];
@@ -50,7 +60,10 @@ public class Solution {
                 }
             }
         }
-        int[][][] memo = {topLeft, topRight, bottomRight, bottomLeft};
+    }
+
+    private int findMaxDiagonal(int[][] grid, int[][][] memo, int m, int n, int initialAns) {
+        int ans = initialAns;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 int x = grid[i][j];
@@ -63,17 +76,31 @@ public class Solution {
                     if ((v & 1) != x) {
                         continue;
                     }
-                    if (v + memo[k + 3 & 3][i][j] <= ans) {
-                        continue;
-                    }
-                    int[] d = directions[k];
-                    int ni = i - d[0] * v, nj = j - d[1] * v;
-                    if (ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == 1) {
-                        ans = Math.max(ans, v + memo[k + 3 & 3][i][j]);
+                    if (v + memo[k + 3 & 3][i][j] > ans) {
+                        int[] d = directions[k];
+                        int ni = i - d[0] * v;
+                        int nj = j - d[1] * v;
+                        if (ni >= 0 && nj >= 0 && ni < m && nj < n && grid[ni][nj] == 1) {
+                            ans = Math.max(ans, v + memo[k + 3 & 3][i][j]);
+                        }
                     }
                 }
             }
         }
         return ans;
+    }
+
+    public int lenOfVDiagonal(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] bottomLeft = new int[m][n];
+        int[][] bottomRight = new int[m][n];
+        int[][] topLeft = new int[m][n];
+        int[][] topRight = new int[m][n];
+        initializeArrays(bottomLeft, bottomRight, topLeft, topRight, m, n);
+        int ans = processBottomDirections(grid, bottomLeft, bottomRight, m, n);
+        processTopDirections(grid, topLeft, topRight, m, n);
+        int[][][] memo = {topLeft, topRight, bottomRight, bottomLeft};
+        return findMaxDiagonal(grid, memo, m, n, ans);
     }
 }
