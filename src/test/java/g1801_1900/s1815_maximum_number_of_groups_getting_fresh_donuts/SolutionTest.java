ffd7784796972850564edc56a77d4f706621d170
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-21 02:34:07 +0200

    Improved task 1815

--- src/test/java/g1801_1900/s1815_maximum_number_of_groups_getting_fresh_donuts/SolutionTest.java
+++ src/test/java/g1801_1900/s1815_maximum_number_of_groups_getting_fresh_donuts/SolutionTest.java
@@ -16,4 +16,36 @@ class SolutionTest {
         assertThat(
                 new Solution().maxHappyGroups(4, new int[] {1, 3, 2, 5, 2, 2, 1, 6}), equalTo(4));
     }
+
+    @Test
+    void maxHappyGroups3() {
+        assertThat(
+                new Solution()
+                        .maxHappyGroups(
+                                7,
+                                new int[] {
+                                    287773481, 815094798, 356732984, 644469322, 543193620,
+                                    903158817, 274116865, 395252956, 363839119, 365378492,
+                                    122313059, 312690039, 252532812
+                                }),
+                equalTo(9));
+    }
+
+    @Test
+    void maxHappyGroups4() {
+        assertThat(
+                new Solution()
+                        .maxHappyGroups(
+                                9,
+                                new int[] {
+                                    3, 1, 3, 3, 5, 6, 1, 1, 9, 10, 3, 3, 3, 1, 1, 3, 3, 3, 19, 20,
+                                    1, 3, 3, 3, 3, 1, 1, 3, 3, 30
+                                }),
+                equalTo(9));
+    }
+
+    @Test
+    void maxHappyGroups5() {
+        assertThat(new Solution().maxHappyGroups(1, new int[] {1, 2, 3}), equalTo(3));
+    }
 }
