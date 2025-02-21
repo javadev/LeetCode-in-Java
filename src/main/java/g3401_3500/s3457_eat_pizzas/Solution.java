ec63b966049e36509964d5769184e2b61af0ddf7
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-21 06:56:23 +0200

    Improved 3457

--- src/main/java/g3401_3500/s3457_eat_pizzas/Solution.java
+++ src/main/java/g3401_3500/s3457_eat_pizzas/Solution.java
@@ -1,26 +1,40 @@
 package g3401_3500.s3457_eat_pizzas;
 
-// #Medium #Array #Sorting #Greedy #2025_02_18_Time_63_ms_(40.14%)_Space_81.02_MB_(36.94%)
+// #Medium #Array #Sorting #Greedy #2025_02_21_Time_16_ms_(100.00%)_Space_75.98_MB_(97.29%)
 
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public long maxWeight(int[] pizzas) {
-        int n = pizzas.length;
-        int m = n / 4;
-        int z = (m + 1) / 2;
-        int y = m / 2;
-        int j = 0;
-        Arrays.sort(pizzas);
-        long res = 0;
-        for (int i = 0; i < z; ++i) {
-            res += pizzas[n - 1 - j];
-            j += 1;
+        int max = 0;
+        for (int x : pizzas) {
+            max = Math.max(max, x);
         }
-        for (int i = 0; i < y; ++i) {
-            res += pizzas[n - 1 - j - 1];
-            j += 2;
+        int[] count = new int[max + 1];
+        for (int x : pizzas) {
+            count[x]++;
         }
-        return res;
+        int m = pizzas.length;
+        int n = m / 4;
+        int index = 0;
+        for (int x = max; x > 0; --x) {
+            if (count[x] == 0) {
+                continue;
+            }
+            int c = count[x];
+            while (c-- > 0) {
+                pizzas[index++] = x;
+            }
+            if (index >= m / 2) {
+                break;
+            }
+        }
+        long ans = 0;
+        for (int i = 0; i < (n + 1) / 2; ++i) {
+            ans += pizzas[i];
+        }
+        int k = n - (n + 1) / 2;
+        for (int i = (n + 1) / 2 + 1; k > 0; i += 2, k--) {
+            ans += pizzas[i];
+        }
+        return ans;
     }
 }
