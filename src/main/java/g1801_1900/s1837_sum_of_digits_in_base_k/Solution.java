5bf632a19ed455fc7a67f9f94c1d977fd730267c
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-23 15:56:21 +0200

    Improved task 1837

--- src/main/java/g1801_1900/s1837_sum_of_digits_in_base_k/Solution.java
+++ src/main/java/g1801_1900/s1837_sum_of_digits_in_base_k/Solution.java
@@ -1,13 +1,17 @@
 package g1801_1900.s1837_sum_of_digits_in_base_k;
 
-// #Easy #Math #2022_05_07_Time_1_ms_(10.42%)_Space_38.9_MB_(91.55%)
+// #Easy #Math #2025_02_23_Time_0_ms_(100.00%)_Space_40.80_MB_(21.87%)
 
 public class Solution {
     public int sumBase(int n, int k) {
-        String str = Integer.toString(Integer.parseInt(n + "", 10), k);
+        int a = 0;
         int sum = 0;
-        for (char c : str.toCharArray()) {
-            sum += Character.getNumericValue(c);
+        int b = 0;
+        while (n != 0) {
+            a = n % k;
+            b = n / k;
+            sum += a;
+            n = b;
         }
         return sum;
     }
