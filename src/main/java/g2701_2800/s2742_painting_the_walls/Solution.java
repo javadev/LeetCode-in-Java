60634fc2b2055b71adc1a4a9ae101fe255dedc7b
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-25 07:47:52 +0200

    Improved task 2732

--- src/main/java/g2701_2800/s2742_painting_the_walls/Solution.java
+++ src/main/java/g2701_2800/s2742_painting_the_walls/Solution.java
@@ -10,13 +10,11 @@ public class Solution {
         int[] dp = new int[n + 1];
         Arrays.fill(dp, (int) 1e9);
         dp[0] = 0;
-
         for (int i = 0; i < n; ++i) {
             for (int j = n; j > 0; --j) {
                 dp[j] = Math.min(dp[j], dp[Math.max(j - time[i] - 1, 0)] + cost[i]);
             }
         }
-
         return dp[n];
     }
 }
