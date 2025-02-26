2536be40bf44610bed0f51a0f91adc2d6811de71
Valentyn Kolesnikov <javadev75@gmail.com>
2025-02-26 05:24:09 +0200

    Improved 2678

--- src/main/java/g2601_2700/s2678_number_of_senior_citizens/Solution.java
+++ src/main/java/g2601_2700/s2678_number_of_senior_citizens/Solution.java
@@ -1,16 +1,16 @@
 package g2601_2700.s2678_number_of_senior_citizens;
 
-// #Easy #Array #String #2023_09_11_Time_0_ms_(100.00%)_Space_40.7_MB_(97.65%)
+// #Easy #Array #String #2025_02_26_Time_0_ms_(100.00%)_Space_42.10_MB_(95.99%)
 
 public class Solution {
     public int countSeniors(String[] details) {
-        int count = 0;
+        int seniorCitizen = 0;
         for (String detail : details) {
-            if (detail.charAt(11) - '0' == 6 && detail.charAt(12) - '0' > 0
-                || detail.charAt(11) - '0' > 6) {
-                count++;
+            int age = (detail.charAt(11) - '0') * 10 + detail.charAt(12) - '0';
+            if (age > 60) {
+                seniorCitizen++;
             }
         }
-        return count;
+        return seniorCitizen;
     }
 }
