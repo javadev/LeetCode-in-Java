b31f3054a80d67db39f532e7e88460cbc1ccb02e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-03-13 04:07:49 +0200

    Improved tasks 999, 1825, 3425

--- src/main/java/g1801_1900/s1825_finding_mk_average/MKAverage.java
+++ src/main/java/g1801_1900/s1825_finding_mk_average/MKAverage.java
@@ -1,141 +1,70 @@
 package g1801_1900.s1825_finding_mk_average;
 
 // #Hard #Design #Heap_Priority_Queue #Ordered_Set #Queue
-// #2022_05_06_Time_83_ms_(60.59%)_Space_96.3_MB_(77.83%)
+// #2025_03_13_Time_37_ms_(100.00%)_Space_100.84_MB_(46.09%)
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.TreeMap;
+import java.util.LinkedList;
+import java.util.TreeSet;
 
-@SuppressWarnings("java:S2184")
 public class MKAverage {
-    private final double m;
-    private final double k;
-    private final double c;
-    private double avg;
-    private final Bst middle;
-    private final Bst min;
-    private final Bst max;
-    private final Deque<Integer> q;
+    private final int capacity;
+    private final int boundary;
+    private final int[] nums;
+    private final TreeSet<Integer> numSet;
+    private final LinkedList<Integer> order;
 
     public MKAverage(int m, int k) {
-        this.m = m;
-        this.k = k;
-        this.c = m - k * 2;
-        this.avg = 0;
-        this.middle = new Bst();
-        this.min = new Bst();
-        this.max = new Bst();
-        this.q = new ArrayDeque<>();
+        this.capacity = m;
+        this.boundary = k;
+        nums = new int[100001];
+        numSet = new TreeSet<>();
+        order = new LinkedList<>();
     }
 
     public void addElement(int num) {
-        if (min.size < k) {
-            min.add(num);
-            q.offer(num);
-            return;
-        }
-        if (max.size < k) {
-            min.add(num);
-            max.add(min.removeMax());
-            q.offer(num);
-            return;
-        }
-
-        if (num >= min.lastKey() && num <= max.firstKey()) {
-            middle.add(num);
-            avg += num / c;
-        } else if (num < min.lastKey()) {
-            min.add(num);
-            int val = min.removeMax();
-            middle.add(val);
-            avg += val / c;
-        } else if (num > max.firstKey()) {
-            max.add(num);
-            int val = max.removeMin();
-            middle.add(val);
-            avg += val / c;
-        }
-
-        q.offer(num);
-
-        if (q.size() > m) {
-            num = q.poll();
-            if (middle.containsKey(num)) {
-                avg -= num / c;
-                middle.remove(num);
-            } else if (min.containsKey(num)) {
-                min.remove(num);
-                int val = middle.removeMin();
-                avg -= val / c;
-                min.add(val);
-            } else if (max.containsKey(num)) {
-                max.remove(num);
-                int val = middle.removeMax();
-                avg -= val / c;
-                max.add(val);
+        if (order.size() == capacity) {
+            int numToDelete = order.removeFirst();
+            nums[numToDelete] = nums[numToDelete] - 1;
+            if (nums[numToDelete] == 0) {
+                numSet.remove(numToDelete);
             }
         }
+        nums[num]++;
+        numSet.add(num);
+        order.add(num);
     }
 
     public int calculateMKAverage() {
-        if (q.size() < m) {
-            return -1;
-        }
-        return (int) avg;
-    }
-
-    static class Bst {
-        TreeMap<Integer, Integer> map;
-        int size;
-
-        public Bst() {
-            this.map = new TreeMap<>();
-            this.size = 0;
-        }
-
-        void add(int num) {
-            int count = map.getOrDefault(num, 0) + 1;
-            map.put(num, count);
-            size++;
-        }
-
-        void remove(int num) {
-            int count = map.getOrDefault(num, 1) - 1;
-            if (count > 0) {
-                map.put(num, count);
-            } else {
-                map.remove(num);
+        if (order.size() == capacity) {
+            int skipCount = boundary;
+            int numsCount = capacity - 2 * boundary;
+            int freq = capacity - 2 * boundary;
+            int sum = 0;
+            for (int num : numSet) {
+                int count = nums[num];
+                if (skipCount < 0) {
+                    sum += num * Math.min(count, numsCount);
+                    numsCount -= Math.min(count, numsCount);
+                } else {
+                    skipCount -= count;
+                    if (skipCount < 0) {
+                        sum += num * Math.min(Math.abs(skipCount), numsCount);
+                        numsCount -= Math.min(Math.abs(skipCount), numsCount);
+                    }
+                }
+                if (numsCount == 0) {
+                    break;
+                }
             }
-            size--;
+            return sum / freq;
         }
-
-        int removeMin() {
-            int key = map.firstKey();
-
-            remove(key);
-
-            return key;
-        }
-
-        int removeMax() {
-            int key = map.lastKey();
-
-            remove(key);
-
-            return key;
-        }
-
-        boolean containsKey(int key) {
-            return map.containsKey(key);
-        }
-
-        int firstKey() {
-            return map.firstKey();
-        }
-
-        int lastKey() {
-            return map.lastKey();
-        }
+        return -1;
     }
 }
+
+/*
+ * Your MKAverage object will be instantiated and called as such:
+ * MKAverage obj = new MKAverage(m, k);
+ * obj.addElement(num);
+ * int param_2 = obj.calculateMKAverage();
+ */
