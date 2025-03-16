7ed62458d6f861d23f0384abf391ae963e75cd0d
Valentyn Kolesnikov <javadev75@gmail.com>
2025-03-16 03:16:23 +0200

    Improved tasks 3161, 3327

--- src/main/java/g3101_3200/s3161_block_placement_queries/Solution.java
+++ src/main/java/g3101_3200/s3161_block_placement_queries/Solution.java
@@ -1,107 +1,112 @@
 package g3101_3200.s3161_block_placement_queries;
 
 // #Hard #Array #Binary_Search #Segment_Tree #Binary_Indexed_Tree
-// #2024_05_30_Time_137_ms_(99.38%)_Space_143.7_MB_(54.52%)
+// #2025_03_16_Time_47_ms_(100.00%)_Space_144.38_MB_(56.41%)
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
-    private static class Seg {
-        private final int start;
-        private final int end;
-        private int min;
-        private int max;
-        private int len;
-        private boolean obstacle;
-        private Seg left;
-        private Seg right;
-
-        public static Seg init(int n) {
-            return new Seg(0, n);
-        }
-
-        private Seg(int start, int end) {
-            this.start = start;
-            this.end = end;
-            if (start >= end) {
-                return;
-            }
-            int mid = start + ((end - start) >> 1);
-            left = new Seg(start, mid);
-            right = new Seg(mid + 1, end);
-            refresh();
-        }
-
-        public void set(int i) {
-            if (i < start || i > end) {
-                return;
-            } else if (i == start && i == end) {
-                obstacle = true;
-                min = max = start;
-                return;
-            }
-            left.set(i);
-            right.set(i);
-            refresh();
-        }
-
-        private void refresh() {
-            if (left.obstacle) {
-                min = left.min;
-                if (right.obstacle) {
-                    max = right.max;
-                    len = Math.max(right.min - left.max, Math.max(left.len, right.len));
-                } else {
-                    max = left.max;
-                    len = Math.max(left.len, right.end - left.max);
-                }
-                obstacle = true;
-            } else if (right.obstacle) {
-                min = right.min;
-                max = right.max;
-                len = Math.max(right.len, right.min - left.start);
-                obstacle = true;
-            } else {
-                len = end - start;
+    public List<Boolean> getResults(int[][] queries) {
+        int m = queries.length;
+        int[] pos = new int[m + 1];
+        int size = 0;
+        pos[size++] = 0;
+        int max = 0;
+        for (int[] q : queries) {
+            max = Math.max(max, q[1]);
+            if (q[0] == 1) {
+                pos[size++] = q[1];
             }
         }
+        Arrays.sort(pos, 0, size);
+        max++;
+        UnionFind left = new UnionFind(max + 1);
+        UnionFind right = new UnionFind(max + 1);
+        BIT bit = new BIT(max);
+        initializePositions(size, pos, bit, left, right, max);
+        return List.of(getBooleans(queries, m, size, left, right, bit));
+    }
 
-        public void max(int n, int[] t) {
-            if (end <= n) {
-                t[0] = Math.max(t[0], len);
-                if (obstacle) {
-                    t[1] = max;
-                }
-                return;
+    private void initializePositions(
+            int size, int[] pos, BIT bit, UnionFind left, UnionFind right, int max) {
+        for (int i = 1; i < size; i++) {
+            int pre = pos[i - 1];
+            int cur = pos[i];
+            bit.update(cur, cur - pre);
+            for (int j = pre + 1; j < cur; j++) {
+                left.parent[j] = pre;
+                right.parent[j] = cur;
             }
-            left.max(n, t);
-            if (!right.obstacle || right.min >= n) {
-                return;
-            }
-            t[0] = Math.max(t[0], right.min - t[1]);
-            right.max(n, t);
+        }
+        for (int j = pos[size - 1] + 1; j < max; j++) {
+            left.parent[j] = pos[size - 1];
+            right.parent[j] = max;
         }
     }
 
-    public List<Boolean> getResults(int[][] queries) {
-        int max = 0;
-        for (int[] i : queries) {
-            max = Math.max(max, i[1]);
-        }
-        Seg root = Seg.init(max);
-        root.set(0);
-
-        List<Boolean> res = new ArrayList<>(queries.length);
-        for (int[] i : queries) {
-            if (i[0] == 1) {
-                root.set(i[1]);
+    private Boolean[] getBooleans(
+            int[][] queries, int m, int size, UnionFind left, UnionFind right, BIT bit) {
+        Boolean[] ans = new Boolean[m - size + 1];
+        int index = ans.length - 1;
+        for (int i = m - 1; i >= 0; i--) {
+            int[] q = queries[i];
+            int x = q[1];
+            int pre = left.find(x - 1);
+            if (q[0] == 1) {
+                int next = right.find(x + 1);
+                left.parent[x] = pre;
+                right.parent[x] = next;
+                bit.update(next, next - pre);
             } else {
-                int[] t = new int[2];
-                root.max(i[1], t);
-                res.add(Math.max(t[0], i[1] - t[1]) >= i[2]);
+                int maxGap = Math.max(bit.query(pre), x - pre);
+                ans[index--] = maxGap >= q[2];
             }
         }
-        return res;
+        return ans;
+    }
+
+    private static final class BIT {
+        int n;
+        int[] tree;
+
+        public BIT(int n) {
+            this.n = n;
+            tree = new int[n];
+        }
+
+        public void update(int i, int v) {
+            while (i < n) {
+                tree[i] = Math.max(tree[i], v);
+                i += i & -i;
+            }
+        }
+
+        public int query(int i) {
+            int result = 0;
+            while (i > 0) {
+                result = Math.max(result, tree[i]);
+                i &= i - 1;
+            }
+            return result;
+        }
+    }
+
+    private static final class UnionFind {
+        private final int[] parent;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 1; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
     }
 }
