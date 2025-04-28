21e734b76e3841a7e23db6f79a4693d8a42fc6f9
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-28 19:04:47 +0300

    Improved 3528

--- src/main/java/g3501_3600/s3528_unit_conversion_i/Solution.java
+++ src/main/java/g3501_3600/s3528_unit_conversion_i/Solution.java
@@ -1,6 +1,6 @@
 package g3501_3600.s3528_unit_conversion_i;
 
-// #Medium #Graph #Depth_First_Search #Breadth_First_Search
+// #Medium #Depth_First_Search #Breadth_First_Search #Graph
 // #2025_04_28_Time_3_ms_(99.90%)_Space_127.84_MB_(26.65%)
 
 public class Solution {
