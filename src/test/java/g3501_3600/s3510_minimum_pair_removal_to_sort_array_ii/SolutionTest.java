f6f87cf3a19fc670e549fef864b17926078581f8
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-29 07:45:09 +0300

    Added tests

--- src/test/java/g3501_3600/s3510_minimum_pair_removal_to_sort_array_ii/SolutionTest.java
+++ src/test/java/g3501_3600/s3510_minimum_pair_removal_to_sort_array_ii/SolutionTest.java
@@ -15,4 +15,16 @@ class SolutionTest {
     void minimumPairRemoval2() {
         assertThat(new Solution().minimumPairRemoval(new int[] {1, 2, 2}), equalTo(0));
     }
+
+    @Test
+    void minimumPairRemoval3() {
+        assertThat(new Solution().minimumPairRemoval(new int[] {5, 2, 3, 1}), equalTo(2));
+    }
+
+    @Test
+    void minimumPairRemoval4() {
+        assertThat(
+                new Solution().minimumPairRemoval(new int[] {2, 2, -1, 3, -2, 2, 1, 1, 1, 0, -1}),
+                equalTo(9));
+    }
 }
