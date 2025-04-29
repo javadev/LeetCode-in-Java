7c52280572b09a2a57f4069e04fc4c3648e321d9
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-29 07:46:43 +0300

    Improved task

--- src/main/java/g3501_3600/s3510_minimum_pair_removal_to_sort_array_ii/Solution.java
+++ src/main/java/g3501_3600/s3510_minimum_pair_removal_to_sort_array_ii/Solution.java
@@ -1,7 +1,7 @@
 package g3501_3600.s3510_minimum_pair_removal_to_sort_array_ii;
 
 // #Hard #Array #Hash_Table #Heap_Priority_Queue #Simulation #Linked_List #Ordered_Set
-// #Doubly_Linked_List #2025_04_29_Time_274_ms_(98.94%)_Space_70.73_MB_(69.15%)
+// #Doubly_Linked_List #2025_04_29_Time_278_ms_(98.94%)_Space_70.90_MB_(68.88%)
 
 import java.util.Arrays;
 
@@ -98,9 +98,6 @@ public class Solution {
     }
 
     private void modify(long[] segment, int idx, long num) {
-        if (segment[idx] == num) {
-            return;
-        }
         segment[idx] = num;
         while (idx != 0) {
             idx = (idx - 1) / 2;
