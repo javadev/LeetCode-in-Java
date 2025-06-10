130c2ac69f6f0d2c651a61572362014e58a055c7
Valentyn Kolesnikov <javadev75@gmail.com>
2025-06-10 04:50:00 +0300

    Fixed compile

--- src/main/java/g3501_3600/s3574_maximize_subarray_gcd_score/Solution.java
+++ src/main/java/g3501_3600/s3574_maximize_subarray_gcd_score/Solution.java
@@ -3,6 +3,8 @@ package g3501_3600.s3574_maximize_subarray_gcd_score;
 // #Hard #Array #Math #Enumeration #Number_Theory
 // #2025_06_10_Time_13_ms_(100.00%)_Space_45.07_MB_(78.08%)
 
+import java.util.Arrays;
+
 public class Solution {
     public long maxGCDScore(int[] nums, int k) {
         int mx = 0;
