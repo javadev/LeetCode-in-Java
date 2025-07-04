cf4d9362958c6082ba37e3aa114c12c52e74fea0
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-04 06:36:53 +0300

    Improved task 138

--- src/main/java/com_github_leetcode/random/Node.java
+++ src/main/java/com_github_leetcode/random/Node.java
@@ -8,18 +8,8 @@ public class Node {
     public Node next;
     public Node random;
 
-    public Node() {
-        this.val = 0;
-    }
-
     public Node(int val) {
         this.val = val;
-    }
-
-    public Node(int val, Node next, Node random) {
-        this.val = val;
-        this.next = next;
-        this.random = random;
     }
 
     public String toString() {
--- src/main/java/g0101_0200/s0138_copy_list_with_random_pointer/Solution.java
+++ src/main/java/g0101_0200/s0138_copy_list_with_random_pointer/Solution.java
@@ -2,70 +2,41 @@ package g0101_0200.s0138_copy_list_with_random_pointer;
 
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #Hash_Table #Linked_List
 // #Programming_Skills_II_Day_14 #Udemy_Linked_List #Top_Interview_150_Linked_List
-// #Big_O_Time_O(N)_Space_O(N) #2024_11_13_Time_0_ms_(100.00%)_Space_44.1_MB_(92.12%)
+// #Big_O_Time_O(N)_Space_O(N) #2025_07_04_Time_0_ms_(100.00%)_Space_43.96_MB_(99.29%)
 
 import com_github_leetcode.random.Node;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 // Definition for a Node.
 class Node {
-    public int val;
-    public Node next;
-    public Node random;
+    int val;
+    Node next;
+    Node random;
 
-    public Node() {}
-
-    public Node(int _val,Node _next,Node _random) {
-        val = _val;
-        next = _next;
-        random = _random;
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
-};
+}
 */
 public class Solution {
     public Node copyRandomList(Node head) {
-        if (head == null) {
-            return null;
+        Map<Node, Node> hashMap = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            hashMap.put(cur, new Node(cur.val));
+            cur = cur.next;
         }
-        // first pass to have a clone node point to the next node. ie A->B becomes A->clonedNode->B
-        Node curr = head;
-        while (curr != null) {
-            Node clonedNode = new Node(curr.val);
-            clonedNode.next = curr.next;
-            curr.next = clonedNode;
-            curr = clonedNode.next;
+        cur = head;
+        while (cur != null) {
+            Node copy = hashMap.get(cur);
+            copy.next = hashMap.get(cur.next);
+            copy.random = hashMap.get(cur.random);
+            cur = cur.next;
         }
-        curr = head;
-        // second pass to make the cloned node's random pointer point to the orginal node's randome
-        // pointer.
-        // ie. A's random pointer becomes ClonedNode's random pointer
-        while (curr != null) {
-            if (curr.random != null) {
-                curr.next.random = curr.random.next;
-            } else {
-                curr.next.random = null;
-            }
-            curr = curr.next.next;
-        }
-        curr = head;
-        // third pass to restore the links and return the head of the cloned nodes' list.
-        Node newHead = null;
-        while (curr != null) {
-            Node clonedNode;
-            if (newHead == null) {
-                clonedNode = curr.next;
-                newHead = clonedNode;
-            } else {
-                clonedNode = curr.next;
-            }
-            curr.next = clonedNode.next;
-            if (curr.next != null) {
-                clonedNode.next = curr.next.next;
-            } else {
-                clonedNode.next = null;
-            }
-            curr = curr.next;
-        }
-        return newHead;
+        return hashMap.get(head);
     }
 }
