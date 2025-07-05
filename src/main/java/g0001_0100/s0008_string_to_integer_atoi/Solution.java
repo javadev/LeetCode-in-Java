0bbc5fec0cf35bfaf2e62b2a9d6ea1d616eaf178
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-05 07:15:09 +0300

    Improved tasks 6-14

--- src/main/java/g0001_0100/s0008_string_to_integer_atoi/Solution.java
+++ src/main/java/g0001_0100/s0008_string_to_integer_atoi/Solution.java
@@ -1,6 +1,7 @@
 package g0001_0100.s0008_string_to_integer_atoi;
 
-// #Medium #Top_Interview_Questions #String #2024_11_09_Time_1_ms_(100.00%)_Space_42_MB_(95.40%)
+// #Medium #Top_Interview_Questions #String #Big_O_Time_O(n)_Space_O(n)
+// #2024_11_09_Time_1_ms_(100.00%)_Space_42_MB_(95.40%)
 
 public class Solution {
     public int myAtoi(String str) {
--- src/main/java/g0001_0100/s0014_longest_common_prefix/Solution.java
+++ src/main/java/g0001_0100/s0014_longest_common_prefix/Solution.java
@@ -1,7 +1,8 @@
 package g0001_0100.s0014_longest_common_prefix;
 
 // #Easy #Top_Interview_Questions #String #Level_2_Day_2_String #Udemy_Strings
-// #Top_Interview_150_Array/String #2025_03_04_Time_0_ms_(100.00%)_Space_41.35_MB_(87.42%)
+// #Top_Interview_150_Array/String #Big_O_Time_O(n*m)_Space_O(m)
+// #2025_03_04_Time_0_ms_(100.00%)_Space_41.35_MB_(87.42%)
 
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
