4e477b36db45cd88d447255222a78b1b9450e325
Valentyn Kolesnikov <javadev75@gmail.com>
2025-07-31 11:39:46 +0300

    Updated tag

--- src/main/java/g3601_3700/s3630_partition_array_for_maximum_xor_and_and/Solution.java
+++ src/main/java/g3601_3700/s3630_partition_array_for_maximum_xor_and_and/Solution.java
@@ -1,6 +1,7 @@
 package g3601_3700.s3630_partition_array_for_maximum_xor_and_and;
 
-// #Hard #Weekly_Contest_460 #2025_07_27_Time_77_ms_(100.00%)_Space_51.20_MB_(30.86%)
+// #Hard #Array #Math #Greedy #Enumeration #Weekly_Contest_460
+// #2025_07_31_Time_82_ms_(96.35%)_Space_50.76_MB_(39.58%)
 
 public class Solution {
     public long maximizeXorAndXor(int[] nums) {
