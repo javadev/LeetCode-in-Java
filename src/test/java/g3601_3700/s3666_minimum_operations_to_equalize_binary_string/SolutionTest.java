fe6d0831bb74cf00bad4c132554ca95d19468c20
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-06 13:48:25 +0300

    Fixed sonar

--- src/test/java/g3601_3700/s3666_minimum_operations_to_equalize_binary_string/SolutionTest.java
+++ src/test/java/g3601_3700/s3666_minimum_operations_to_equalize_binary_string/SolutionTest.java
@@ -23,81 +23,61 @@ class SolutionTest {
 
     @Test
     void minOperations4() {
-        String s = "111111";
         int k = 3;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(0));
+        assertThat(new Solution().minOperations("111111", k), equalTo(0));
     }
 
     @Test
     void minOperations5() {
-        String s = "000000";
         int k = 6;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(1));
+        assertThat(new Solution().minOperations("000000", k), equalTo(1));
     }
 
     @Test
     void minOperations6() {
-        String s = "000111";
         int k = 6;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(-1));
+        assertThat(new Solution().minOperations("000111", k), equalTo(-1));
     }
 
     @Test
     void minOperations7() {
-        String s = "0011";
         int k = 3;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(2));
+        assertThat(new Solution().minOperations("0011", k), equalTo(2));
     }
 
     @Test
     void minOperations8() {
-        String s = "000011";
         int k = 4;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(1));
+        assertThat(new Solution().minOperations("000011", k), equalTo(1));
     }
 
     @Test
     void minOperations9() {
-        String s = "000111";
         int k = 2;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(-1));
+        assertThat(new Solution().minOperations("000111", k), equalTo(-1));
     }
 
     @Test
     void minOperations10() {
-        String s = "001100";
         int k = 4;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(1));
+        assertThat(new Solution().minOperations("001100", k), equalTo(1));
     }
 
     @Test
     void minOperations11() {
-        String s = "000100";
         int k = 3;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(3));
+        assertThat(new Solution().minOperations("000100", k), equalTo(3));
     }
 
     @Test
     void minOperations12() {
-        String s = "111111";
         int k = 4;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(0));
+        assertThat(new Solution().minOperations("111111", k), equalTo(0));
     }
 
     @Test
     void minOperations13() {
-        String s = "001001";
         int k = 4;
-        int result = new Solution().minOperations(s, k);
-        assertThat(result, equalTo(1));
+        assertThat(new Solution().minOperations("001001", k), equalTo(1));
     }
 }
