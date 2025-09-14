85b6f0e5fc75bcadec3c4aa58ac6ceb9ffdc07be
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-14 08:11:24 +0300

    Improved tasks 3675, 3676

--- src/main/java/g3601_3700/s3675_minimum_operations_to_transform_string/Solution.java
+++ src/main/java/g3601_3700/s3675_minimum_operations_to_transform_string/Solution.java
@@ -1,25 +1,23 @@
 package g3601_3700.s3675_minimum_operations_to_transform_string;
 
-// #Medium #Weekly_Contest_466 #2025_09_07_Time_137_ms_(100.00%)_Space_51.85_MB_(100.00%)
-
-import java.util.HashSet;
-import java.util.Set;
+// #Medium #Weekly_Contest_466 #2025_09_14_Time_5_ms_(100.00%)_Space_47.93_MB_(95.06%)
 
 public class Solution {
     public int minOperations(String s) {
-        Set<Character> set = new HashSet<>();
-        for (char ch : s.toCharArray()) {
-            set.add(ch);
-        }
-        if (set.size() == 1 && set.contains('a')) {
-            return 0;
-        }
-        char minCh = 'z';
-        for (char ch : s.toCharArray()) {
-            if (ch != 'a' && ch < minCh) {
-                minCh = ch;
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            final char c = s.charAt(i);
+            if (c != 'a') {
+                int ops = 'z' - c + 1;
+                if (ops > ans) {
+                    ans = ops;
+                }
+                if (ops == 25) {
+                    break;
+                }
             }
         }
-        return ('z' - minCh) + 1;
+        return ans;
     }
 }
--- src/main/java/g3601_3700/s3676_count_bowl_subarrays/Solution.java
+++ src/main/java/g3601_3700/s3676_count_bowl_subarrays/Solution.java
@@ -1,27 +1,25 @@
 package g3601_3700.s3676_count_bowl_subarrays;
 
-// #Medium #Weekly_Contest_466 #2025_09_07_Time_19_ms_(100.00%)_Space_59.30_MB_(100.00%)
-
-import java.util.ArrayDeque;
-import java.util.Deque;
+// #Medium #Weekly_Contest_466 #2025_09_14_Time_2_ms_(100.00%)_Space_58.91_MB_(69.85%)
 
 public class Solution {
     public long bowlSubarrays(int[] nums) {
-        int l = nums.length;
-        int ans = 0;
-        Deque<Integer> stack = new ArrayDeque<>();
-        for (int i = 0; i < l; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i]) {
-                int mid = stack.pop();
-                if (!stack.isEmpty()) {
-                    int left = stack.peek();
-                    if (Math.min(nums[left], nums[i]) > nums[mid]) {
-                        ++ans;
-                    }
-                }
+        int n = nums.length;
+        int res = n;
+        int pre = 0;
+        for (int a : nums) {
+            if (a > pre) {
+                res--;
+                pre = a;
             }
-            stack.push(i);
         }
-        return ans;
+        pre = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] > pre) {
+                res--;
+                pre = nums[i];
+            }
+        }
+        return res + 1;
     }
 }
