98ffd0626a64d9b91664f17c8e311a384bc5edd5
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-26 06:39:15 +0300

    Improved 3690

--- src/main/java/g3601_3700/s3690_split_and_merge_array_transformation/Solution.java
+++ src/main/java/g3601_3700/s3690_split_and_merge_array_transformation/Solution.java
@@ -1,7 +1,7 @@
 package g3601_3700.s3690_split_and_merge_array_transformation;
 
 // #Medium #Array #Hash_Table #Breadth_First_Search #Weekly_Contest_468
-// #2025_09_25_Time_11_ms_(99.81%)_Space_45.46_MB_(45.98%)
+// #2025_09_26_Time_9_ms_(100.00%)_Space_45.05_MB_(91.32%)
 
 import java.util.Deque;
 import java.util.HashMap;
@@ -13,9 +13,9 @@ public class Solution {
         int n = nums1.length;
         int id = 0;
         Map<Integer, Integer> map = new HashMap<>(n << 1);
-        for (int i = 0; i < n; i++) {
-            if (!map.containsKey(nums1[i])) {
-                map.put(nums1[i], id++);
+        for (int value : nums1) {
+            if (!map.containsKey(value)) {
+                map.put(value, id++);
             }
         }
         int source = 0;
@@ -39,30 +39,6 @@ public class Solution {
             for (int i = 0; i < n; i++) {
                 for (int j = i; j < n; j++) {
                     for (int k = -1; k < n; k++) {
-                        if (k < i - 1) {
-                            int[] ncur = new int[n];
-                            int t1 = 0;
-                            for (int t = 0; t <= k; t++) {
-                                ncur[t1++] = cur[t];
-                            }
-                            for (int t = i; t <= j; t++) {
-                                ncur[t1++] = cur[t];
-                            }
-                            for (int t = k + 1; t < i; t++) {
-                                ncur[t1++] = cur[t];
-                            }
-                            for (int t = j + 1; t < n; t++) {
-                                ncur[t1++] = cur[t];
-                            }
-                            int t2 = hash(ncur);
-                            if (distances[t2] == 0) {
-                                distances[t2] = distances[x] + 1;
-                                if (t2 == target) {
-                                    return distances[x];
-                                }
-                                que.add(t2);
-                            }
-                        }
                         if (k > j) {
                             int[] ncur = new int[n];
                             int t1 = 0;
