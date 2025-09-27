b0138ca2d912038c46ddbdc0d4a673f7c072a359
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-27 04:32:03 +0300

    Improved task 3644

--- src/main/java/g3601_3700/s3666_minimum_operations_to_equalize_binary_string/Solution.java
+++ src/main/java/g3601_3700/s3666_minimum_operations_to_equalize_binary_string/Solution.java
@@ -1,6 +1,6 @@
 package g3601_3700.s3666_minimum_operations_to_equalize_binary_string;
 
-// #Hard #String #Hash_Table #Math #Biweekly_Contest_164 #Breadth_First_Search
+// #Hard #String #Hash_Table #Math #Breadth_First_Search #Biweekly_Contest_164
 // #2025_09_26_Time_6_ms_(85.17%)_Space_45.57_MB_(88.76%)
 
 public class Solution {
