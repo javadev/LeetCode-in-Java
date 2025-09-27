e35472ba874eb96ee42a7044b19566862948e423
Valentyn Kolesnikov <javadev75@gmail.com>
2025-09-27 04:19:33 +0300

    Improved task 3644

--- src/main/java/g3601_3700/s3644_maximum_k_to_sort_a_permutation/Solution.java
+++ src/main/java/g3601_3700/s3644_maximum_k_to_sort_a_permutation/Solution.java
@@ -1,6 +1,7 @@
 package g3601_3700.s3644_maximum_k_to_sort_a_permutation;
 
-// #Medium #Weekly_Contest_462 #2025_08_14_Time_1_ms_(100.00%)_Space_61.65_MB_(71.89%)
+// #Medium #Array #Bit_Manipulation #Weekly_Contest_462
+// #2025_09_27_Time_1_ms_(100.00%)_Space_62.64_MB_(34.63%)
 
 public class Solution {
     public int sortPermutation(int[] nums) {
