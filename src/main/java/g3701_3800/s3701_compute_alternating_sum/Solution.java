cc41c089fb559c6dc83faf4f5277d34b14a67d6e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-10-05 22:43:10 +0300

    Improved 3701

--- src/main/java/g3701_3800/s3701_compute_alternating_sum/Solution.java
+++ src/main/java/g3701_3800/s3701_compute_alternating_sum/Solution.java
@@ -7,8 +7,11 @@ public class Solution {
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            if (i % 2 == 0) sum += num;
-            else sum -= num;
+            if (i % 2 == 0) {
+                sum += num;
+            } else {
+                sum -= num;
+            }
         }
         return sum;
     }
