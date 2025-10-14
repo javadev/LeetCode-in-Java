eac826c5aa3f973f6e92b493f2d39d5c0831a545
Valentyn Kolesnikov <javadev75@gmail.com>
2025-10-14 09:43:11 +0300

    Improved tasks 3701-3704, 3710, 3715

--- src/main/java/g3701_3800/s3703_remove_k_balanced_substrings/Solution.java
+++ src/main/java/g3701_3800/s3703_remove_k_balanced_substrings/Solution.java
@@ -1,6 +1,7 @@
 package g3701_3800.s3703_remove_k_balanced_substrings;
 
-// #Medium #Weekly_Contest_470 #2025_10_05_Time_191_ms_(100.00%)_Space_45.86_MB_(100.00%)
+// #Medium #String #Stack #Simulation #Weekly_Contest_470
+// #2025_10_05_Time_191_ms_(100.00%)_Space_45.86_MB_(100.00%)
 
 public class Solution {
     public String removeSubstring(String s, int k) {
