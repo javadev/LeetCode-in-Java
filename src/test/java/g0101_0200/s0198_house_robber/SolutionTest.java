c172a7afc16f81650e35bd513076369293c7d4f2
ThanhNIT <thanhnguyen.it.work@gmail.com>
2025-11-16 11:01:11 +0700

    Added tests for tasks 101-2929

--- src/test/java/g0101_0200/s0198_house_robber/SolutionTest.java
+++ src/test/java/g0101_0200/s0198_house_robber/SolutionTest.java
@@ -15,4 +15,34 @@ class SolutionTest {
     void rob2() {
         assertThat(new Solution().rob(new int[] {2, 7, 9, 3, 1}), equalTo(12));
     }
+
+    @Test
+    void rob3() {
+        assertThat(new Solution().rob(new int[] {5}), equalTo(5));
+    }
+
+    @Test
+    void rob4() {
+        assertThat(new Solution().rob(new int[] {5, 10}), equalTo(10));
+    }
+
+    @Test
+    void rob5() {
+        assertThat(new Solution().rob(new int[] {2, 1, 1, 2}), equalTo(4));
+    }
+
+    @Test
+    void rob6() {
+        assertThat(new Solution().rob(new int[] {3, 3, 3, 3, 3}), equalTo(9));
+    }
+
+    @Test
+    void rob7() {
+        assertThat(new Solution().rob(new int[] {100, 1, 100, 1, 100}), equalTo(300));
+    }
+
+    @Test
+    void rob8() {
+        assertThat(new Solution().rob(new int[] {}), equalTo(0));
+    }
 }
--- src/test/java/g2901_3000/s2929_distribute_candies_among_children_ii/SolutionTest.java
+++ src/test/java/g2901_3000/s2929_distribute_candies_among_children_ii/SolutionTest.java
@@ -15,4 +15,39 @@ class SolutionTest {
     void distributeCandies2() {
         assertThat(new Solution().distributeCandies(3, 3), equalTo(10L));
     }
+
+    @Test
+    void distributeCandies3() {
+        assertThat(new Solution().distributeCandies(2, 5), equalTo(6L));
+    }
+
+    @Test
+    void distributeCandies4() {
+        assertThat(new Solution().distributeCandies(4, 4), equalTo(15L));
+    }
+
+    @Test
+    void distributeCandies5() {
+        assertThat(new Solution().distributeCandies(7, 5), equalTo(27L));
+    }
+
+    @Test
+    void distributeCandies6() {
+        assertThat(new Solution().distributeCandies(12, 5), equalTo(10L));
+    }
+
+    @Test
+    void distributeCandies7() {
+        assertThat(new Solution().distributeCandies(20, 5), equalTo(0L));
+    }
+
+    @Test
+    void distributeCandies8() {
+        assertThat(new Solution().distributeCandies(10, 5), equalTo(21L));
+    }
+
+    @Test
+    void distributeCandies9() {
+        assertThat(new Solution().distributeCandies(15, 5), equalTo(1L));
+    }
 }
