c4412530bb07e9441536e132a3575b46aa37326c
ThanhNIT <thanhnguyen.it.work@gmail.com>
2025-11-16 10:36:16 +0700

    Added tests for tasks 84-2425

--- src/test/java/g0001_0100/s0084_largest_rectangle_in_histogram/SolutionTest.java
+++ src/test/java/g0001_0100/s0084_largest_rectangle_in_histogram/SolutionTest.java
@@ -15,4 +15,44 @@ class SolutionTest {
     void largestRectangleArea2() {
         assertThat(new Solution().largestRectangleArea(new int[] {2, 4}), equalTo(4));
     }
+
+    @Test
+    void largestRectangleArea3() {
+        assertThat(new Solution().largestRectangleArea(new int[] {}), equalTo(0));
+    }
+
+    @Test
+    void largestRectangleArea4() {
+        assertThat(new Solution().largestRectangleArea(new int[] {5}), equalTo(5));
+    }
+
+    @Test
+    void largestRectangleArea5() {
+        assertThat(new Solution().largestRectangleArea(new int[] {3, 6}), equalTo(6));
+    }
+
+    @Test
+    void largestRectangleArea6() {
+        assertThat(new Solution().largestRectangleArea(new int[] {2, 2, 2, 2}), equalTo(8));
+    }
+
+    @Test
+    void largestRectangleArea7() {
+        assertThat(new Solution().largestRectangleArea(new int[] {1, 2, 3, 4, 5}), equalTo(9));
+    }
+
+    @Test
+    void largestRectangleArea8() {
+        assertThat(new Solution().largestRectangleArea(new int[] {5, 4, 3, 2, 1}), equalTo(9));
+    }
+
+    @Test
+    void largestRectangleArea9() {
+        assertThat(new Solution().largestRectangleArea(new int[] {2, 1, 5, 6, 2, 3}), equalTo(10));
+    }
+
+    @Test
+    void largestRectangleArea10() {
+        assertThat(new Solution().largestRectangleArea(new int[] {1, 1, 1, 1}), equalTo(4));
+    }
 }
--- src/test/java/g0801_0900/s0858_mirror_reflection/SolutionTest.java
+++ src/test/java/g0801_0900/s0858_mirror_reflection/SolutionTest.java
@@ -15,4 +15,39 @@ class SolutionTest {
     void mirrorReflection2() {
         assertThat(new Solution().mirrorReflection(3, 1), equalTo(1));
     }
+
+    @Test
+    void mirrorReflection3() {
+        assertThat(new Solution().mirrorReflection(4, 2), equalTo(2));
+    }
+
+    @Test
+    void mirrorReflection4() {
+        assertThat(new Solution().mirrorReflection(6, 3), equalTo(2));
+    }
+
+    @Test
+    void mirrorReflection5() {
+        assertThat(new Solution().mirrorReflection(5, 2), equalTo(0));
+    }
+
+    @Test
+    void mirrorReflection6() {
+        assertThat(new Solution().mirrorReflection(7, 3), equalTo(1));
+    }
+
+    @Test
+    void mirrorReflection7() {
+        assertThat(new Solution().mirrorReflection(1000, 250), equalTo(2));
+    }
+
+    @Test
+    void mirrorReflection8() {
+        assertThat(new Solution().mirrorReflection(4, 4), equalTo(1));
+    }
+
+    @Test
+    void mirrorReflection9() {
+        assertThat(new Solution().mirrorReflection(1, 1), equalTo(1));
+    }
 }
