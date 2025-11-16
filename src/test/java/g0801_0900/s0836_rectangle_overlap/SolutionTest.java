43a80330aa455c280e1de51c58d5a840f0a93100
ThanhNIT <thanhnguyen.it.work@gmail.com>
2025-11-16 10:01:15 +0700

    Added tests for tasks 441-3040

--- src/test/java/g0801_0900/s0836_rectangle_overlap/SolutionTest.java
+++ src/test/java/g0801_0900/s0836_rectangle_overlap/SolutionTest.java
@@ -26,4 +26,47 @@ class SolutionTest {
                 new Solution().isRectangleOverlap(new int[] {0, 0, 1, 1}, new int[] {2, 2, 3, 3}),
                 equalTo(false));
     }
+
+    @Test
+    void isRectangleOverlap4() {
+        assertThat(
+                new Solution().isRectangleOverlap(new int[] {0, 0, 2, 2}, new int[] {0, 2, 2, 4}),
+                equalTo(false));
+    }
+
+    @Test
+    void isRectangleOverlap5() {
+        assertThat(
+                new Solution().isRectangleOverlap(new int[] {1, 1, 3, 3}, new int[] {1, 0, 3, 1}),
+                equalTo(false));
+    }
+
+    @Test
+    void isRectangleOverlap6() {
+        assertThat(
+                new Solution()
+                        .isRectangleOverlap(new int[] {-3, -3, -1, -1}, new int[] {-2, -2, 0, 0}),
+                equalTo(true));
+    }
+
+    @Test
+    void isRectangleOverlap7() {
+        assertThat(
+                new Solution().isRectangleOverlap(new int[] {0, 0, 4, 4}, new int[] {1, 1, 3, 3}),
+                equalTo(true));
+    }
+
+    @Test
+    void isRectangleOverlap8() {
+        assertThat(
+                new Solution().isRectangleOverlap(new int[] {0, 0, 2, 2}, new int[] {0, 0, 2, 2}),
+                equalTo(true));
+    }
+
+    @Test
+    void isRectangleOverlap9() {
+        assertThat(
+                new Solution().isRectangleOverlap(new int[] {0, 0, 1, 1}, new int[] {1, 1, 2, 2}),
+                equalTo(false));
+    }
 }
--- src/test/java/g3001_3100/s3040_maximum_number_of_operations_with_the_same_score_ii/SolutionTest.java
+++ src/test/java/g3001_3100/s3040_maximum_number_of_operations_with_the_same_score_ii/SolutionTest.java
@@ -15,4 +15,44 @@ class SolutionTest {
     void maxOperations2() {
         assertThat(new Solution().maxOperations(new int[] {3, 2, 6, 1, 4}), equalTo(2));
     }
+
+    @Test
+    void maxOperations3() {
+        assertThat(new Solution().maxOperations(new int[] {1, 2}), equalTo(1));
+    }
+
+    @Test
+    void maxOperations4() {
+        assertThat(new Solution().maxOperations(new int[] {1, 1, 1}), equalTo(1));
+    }
+
+    @Test
+    void maxOperations5() {
+        assertThat(new Solution().maxOperations(new int[] {2, 2, 2, 2, 2, 2}), equalTo(3));
+    }
+
+    @Test
+    void maxOperations6() {
+        assertThat(new Solution().maxOperations(new int[] {1, 2, 3, 4, 5, 6}), equalTo(3));
+    }
+
+    @Test
+    void maxOperations7() {
+        assertThat(new Solution().maxOperations(new int[] {6, 5, 4, 3, 2, 1}), equalTo(3));
+    }
+
+    @Test
+    void maxOperations8() {
+        assertThat(new Solution().maxOperations(new int[] {1, 3, 2, 4, 1, 3}), equalTo(2));
+    }
+
+    @Test
+    void maxOperations9() {
+        assertThat(new Solution().maxOperations(new int[] {1, 2, 4, 5}), equalTo(2));
+    }
+
+    @Test
+    void maxOperations10() {
+        assertThat(new Solution().maxOperations(new int[] {5, 5}), equalTo(1));
+    }
 }
