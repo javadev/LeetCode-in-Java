8ec48030ee6893ff2b12de88ad56a1275fa6941a
Valentyn Kolesnikov <javadev75@gmail.com>
2026-04-15 21:33:29 +0300

    Added time tag

--- src/main/java/g3701_3800/s3736_minimum_moves_to_equal_array_elements_iii/Solution.java
+++ src/main/java/g3701_3800/s3736_minimum_moves_to_equal_array_elements_iii/Solution.java
@@ -1,6 +1,6 @@
 package g3701_3800.s3736_minimum_moves_to_equal_array_elements_iii;
 
-// #Easy #Array #Math #Biweekly_Contest_169
+// #Easy #Array #Math #Biweekly_Contest_169 #2026_04_15_Time_1_ms_(99.59%)_Space_46.18_MB_(89.25%)
 
 public class Solution {
     public int minMoves(int[] nums) {
