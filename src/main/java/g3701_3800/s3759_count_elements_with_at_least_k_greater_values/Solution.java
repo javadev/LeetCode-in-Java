2c7d65d5b96d52ffc805ed16b326f8d69f100755
Valentyn Kolesnikov <javadev75@gmail.com>
2026-04-25 14:26:41 +0300

    Fixed compile

--- src/main/java/g3701_3800/s3759_count_elements_with_at_least_k_greater_values/Solution.java
+++ src/main/java/g3701_3800/s3759_count_elements_with_at_least_k_greater_values/Solution.java
@@ -3,6 +3,8 @@ package g3701_3800.s3759_count_elements_with_at_least_k_greater_values;
 // #Medium #Array #Sorting #Binary_Search #Divide_and_Conquer #Quickselect #Senior
 // #Weekly_Contest_478 #2026_04_25_Time_40_ms_(96.15%)_Space_134.18_MB_(73.08%)
 
+import java.util.Arrays;
+
 public class Solution {
     public int countElements(int[] nums, int k) {
         if (k == 0) {
