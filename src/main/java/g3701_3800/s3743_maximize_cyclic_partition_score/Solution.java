839a8ec8f2fa839ed1182e6dbc8b6233edf93966
Valentyn Kolesnikov <javadev75@gmail.com>
2026-04-26 14:25:10 +0300

    Improve readability and efficiency in maximumScore method

--- src/main/java/g3701_3800/s3743_maximize_cyclic_partition_score/Solution.java
+++ src/main/java/g3701_3800/s3743_maximize_cyclic_partition_score/Solution.java
@@ -8,23 +8,22 @@ public class Solution {
         // Find index of minimum element
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < nums[j]) j = i;
+            if (nums[i] < nums[j]) {
+                j = i;
+            }
         }
-
         // Build array 'a'
         int n = nums.length;
         int[] a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = nums[(j + i) % n];
         }
-
         // Build array 'b' (rotated and reversed)
         int[] b = new int[n];
         for (int i = 0; i < n; i++) {
             b[i] = nums[(j + 1 + i) % n];
         }
         reverse(b);
-
         // Compute and return max of f(a, k) and f(b, k)
         return Math.max(f(a, k), f(b, k));
     }
@@ -32,21 +31,18 @@ public class Solution {
     private long f(int[] a, int k) {
         int n = a.length;
         long[][] dp = new long[k + 1][n + 1];
-
         long mn = Long.MAX_VALUE;
         long mx = Long.MIN_VALUE;
-
         // Initialize dp[1][j+1]
         for (int j = 0; j < n; j++) {
             mn = Math.min(mn, a[j]);
             mx = Math.max(mx, a[j]);
             dp[1][j + 1] = mx - mn;
         }
-
         long res = dp[1][n];
-
         for (int i = 2; i <= k; i++) {
-            long x = Long.MIN_VALUE, y = Long.MIN_VALUE;
+            long x = Long.MIN_VALUE;
+            long y = Long.MIN_VALUE;
             for (int j = i - 1; j < n; j++) {
                 x = Math.max(x, dp[i - 1][j] - a[j]);
                 y = Math.max(y, dp[i - 1][j] + a[j]);
@@ -54,7 +50,6 @@ public class Solution {
             }
             res = Math.max(res, dp[i][n]);
         }
-
         return res;
     }
 
