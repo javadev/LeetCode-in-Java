453b3ed5d3b0627d1cffef77a4fa838f5dcf3297
Valentyn Kolesnikov <javadev75@gmail.com>
2026-04-26 14:26:20 +0300

    Refactor condition for short array handling

--- src/main/java/g3701_3800/s3738_longest_non_decreasing_subarray_after_replacing_at_most_one_element/Solution.java
+++ src/main/java/g3701_3800/s3738_longest_non_decreasing_subarray_after_replacing_at_most_one_element/Solution.java
@@ -6,7 +6,9 @@ package g3701_3800.s3738_longest_non_decreasing_subarray_after_replacing_at_most
 public class Solution {
     public int longestSubarray(int[] nums) {
         int n = nums.length;
-        if (n < 3) return n;
+        if (n < 3) {
+            return n;
+        }
         int i = 0;
         int len1 = 0;
         int len2 = 0;
@@ -25,8 +27,9 @@ public class Solution {
                 }
                 len2 = j - i + 1;
                 ans = Math.max(len2 + 1, ans);
-                if (l <= r || (len1 > 1 && nums[i - 2] <= nums[i]))
+                if (l <= r || (len1 > 1 && nums[i - 2] <= nums[i])) {
                     ans = Math.max(len1 + len2, ans);
+                }
                 i = j;
             } else {
                 len2 = 0;
