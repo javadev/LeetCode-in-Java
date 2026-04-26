77de15083748e4c23b82f4092aa0d7b5ced0e15f
Valentyn Kolesnikov <javadev75@gmail.com>
2026-04-26 14:21:39 +0300

    Refactor minimumDistance method for clarity

--- src/main/java/g3701_3800/s3740_minimum_distance_between_three_equal_elements_i/Solution.java
+++ src/main/java/g3701_3800/s3740_minimum_distance_between_three_equal_elements_i/Solution.java
@@ -8,17 +8,17 @@ public class Solution {
         int len = nums.length;
         int[] last2 = new int[len];
         int res = 200;
-
         for (int i = 0; i < len; i++) {
             int val = nums[i] - 1;
-            int pos = i + 1, pack = last2[val];
-            int old = pack & 255, cur = pack >> 8;
-
+            int pos = i + 1;
+            int pack = last2[val];
+            int old = pack & 255;
+            int cur = pack >> 8;
             last2[val] = cur | (pos << 8);
-
-            if (old > 0) res = Math.min(res, (pos - old) << 1);
+            if (old > 0) {
+                res = Math.min(res, (pos - old) << 1);
+            }
         }
-
         return res == 200 ? -1 : res;
     }
 }
