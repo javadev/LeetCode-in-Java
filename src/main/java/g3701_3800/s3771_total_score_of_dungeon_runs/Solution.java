ca6d275b1bc2af8b468db155cd5ba3485db9d760
Valentyn Kolesnikov <javadev75@gmail.com>
2026-05-07 16:29:50 +0300

    Fixed sonar

--- src/main/java/g3701_3800/s3771_total_score_of_dungeon_runs/Solution.java
+++ src/main/java/g3701_3800/s3771_total_score_of_dungeon_runs/Solution.java
@@ -6,19 +6,14 @@ package g3701_3800.s3771_total_score_of_dungeon_runs;
 public class Solution {
     public long totalScore(int hp, int[] damage, int[] requirement) {
         int n = damage.length;
-        long soln = 0;
         int[] cumulative = new int[n + 1];
-        // Set cumulative
         for (int i = n - 1; i >= 0; i--) {
             cumulative[i] = cumulative[i + 1] + damage[i];
         }
+        long soln = 0;
         for (int i = n - 1; i >= 0; i--) {
             int adjHP = hp - requirement[i] + cumulative[i + 1];
-            if (adjHP < cumulative[i]) {
-                continue;
-            } else if (adjHP > cumulative[0]) {
-                soln += i + 1;
-            } else {
+            if (adjHP >= cumulative[i]) {
                 soln += i - binSearch(cumulative, adjHP, i) + 1;
             }
         }
@@ -28,7 +23,7 @@ public class Solution {
     private int binSearch(int[] arr, int hp, int l) {
         int h = 0;
         while (h < l) {
-            int mid = (h + l) / 2;
+            int mid = (h + l) >>> 1;
             if (arr[mid] > hp) {
                 h = mid + 1;
             } else {
