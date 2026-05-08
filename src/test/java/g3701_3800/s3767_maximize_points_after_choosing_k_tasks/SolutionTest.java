b7cd1f2f594ea1914c1a86ff7a9df06e8e707112
Valentyn Kolesnikov <javadev75@gmail.com>
2026-05-08 20:14:27 +0300

    Add additional test cases for maxPoints method

--- src/test/java/g3701_3800/s3767_maximize_points_after_choosing_k_tasks/SolutionTest.java
+++ src/test/java/g3701_3800/s3767_maximize_points_after_choosing_k_tasks/SolutionTest.java
@@ -24,4 +24,39 @@ class SolutionTest {
                 new Solution().maxPoints(new int[] {7, 2, 5}, new int[] {1, 8, 4}, 3),
                 equalTo(14L));
     }
+
+    @Test
+    void maxPoints4() {
+        assertThat(
+                new Solution().maxPoints(new int[] {5, 4, 3}, new int[] {3, 4, 1}, 3),
+                equalTo(12L));
+    }
+
+    @Test
+    void maxPoints5() {
+        assertThat(
+                new Solution().maxPoints(new int[] {1, 2, 3}, new int[] {5, 5, 1}, 1),
+                equalTo(13L));
+    }
+
+    @Test
+    void maxPoints6() {
+        assertThat(
+                new Solution().maxPoints(new int[] {1, 1, 1, 1}, new int[] {3, 4, 2, 6}, 2),
+                equalTo(12L));
+    }
+
+    @Test
+    void maxPoints7() {
+        assertThat(
+                new Solution().maxPoints(new int[] {1, 1, 1, 1}, new int[] {6, 5, 4, 0}, 2),
+                equalTo(13L));
+    }
+
+    @Test
+    void maxPoints8() {
+        assertThat(
+                new Solution().maxPoints(new int[] {7, 8, 9}, new int[] {100, 100, 100}, 3),
+                equalTo(24L));
+    }
 }
