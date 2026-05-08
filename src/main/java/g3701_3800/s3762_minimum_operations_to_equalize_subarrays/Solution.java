af2912b651bc39f89c126883572be6f8287fa70e
Valentyn Kolesnikov <javadev75@gmail.com>
2026-05-08 19:50:30 +0300

    Change static to private for inner classes and methods

--- src/main/java/g3701_3800/s3762_minimum_operations_to_equalize_subarrays/Solution.java
+++ src/main/java/g3701_3800/s3762_minimum_operations_to_equalize_subarrays/Solution.java
@@ -1,7 +1,7 @@
 package g3701_3800.s3762_minimum_operations_to_equalize_subarrays;
 
 // #Hard #Array #Math #Binary_Search #Segment_Tree #Weekly_Contest_478 #Principal
-// #2026_05_08_Time_540_ms_(100.00%)_Space_267.97_MB_(76.92%)
+// #2026_05_08_Time_528_ms_(100.00%)_Space_267.97_MB_(76.92%)
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -9,8 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
-
-    static class MNode {
+    private static class MNode {
         int l;
         int r;
         int[] vals;
@@ -24,7 +23,7 @@ public class Solution {
         }
     }
 
-    static class Group {
+    private static class Group {
         int[] pos;
         int[] val;
         long[] prefPos;
@@ -33,7 +32,7 @@ public class Solution {
         int maxv;
     }
 
-    static int lowerBound(int[] a, int x) {
+    private static int lowerBound(int[] a, int x) {
         int l = 0;
         int r = a.length;
         while (l < r) {
@@ -47,7 +46,7 @@ public class Solution {
         return l;
     }
 
-    static int upperBound(int[] a, int x) {
+    private int upperBound(int[] a, int x) {
         int l = 0;
         int r = a.length;
         while (l < r) {
@@ -61,7 +60,7 @@ public class Solution {
         return l;
     }
 
-    static MNode buildMerge(int[] arr, int l, int r) {
+    private MNode buildMerge(int[] arr, int l, int r) {
         MNode node = new MNode(l, r);
         if (l == r) {
             node.vals = new int[] {arr[l]};
@@ -102,7 +101,7 @@ public class Solution {
         return node;
     }
 
-    static int countLE(MNode node, int ql, int qr, int x) {
+    private int countLE(MNode node, int ql, int qr, int x) {
         if (node == null || ql > node.r || qr < node.l) {
             return 0;
         }
@@ -113,7 +112,7 @@ public class Solution {
         return countLE(node.left, ql, qr, x) + countLE(node.right, ql, qr, x);
     }
 
-    static long sumLE(MNode node, int ql, int qr, int x) {
+    private long sumLE(MNode node, int ql, int qr, int x) {
         if (node == null || ql > node.r || qr < node.l) {
             return 0L;
         }
@@ -126,90 +125,67 @@ public class Solution {
 
     public long[] minOperations(int[] nums, int k, int[][] queries) {
         Map<Integer, Group> groupHashMap = buildGroups(nums, k);
-
         long[] ans = new long[queries.length];
-
         for (int qi = 0; qi < queries.length; qi++) {
             ans[qi] = processQuery(nums, queries[qi], groupHashMap, k);
         }
-
         return ans;
     }
 
     private Map<Integer, Group> buildGroups(int[] nums, int k) {
         Map<Integer, ArrayList<int[]>> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
             int rem = nums[i] % k;
             int value = nums[i] / k;
-
             map.computeIfAbsent(rem, z -> new ArrayList<>()).add(new int[] {i, value});
         }
-
         Map<Integer, Group> groupHashMap = new HashMap<>();
-
         for (Map.Entry<Integer, ArrayList<int[]>> entry : map.entrySet()) {
             groupHashMap.put(entry.getKey(), createGroup(entry.getValue()));
         }
-
         return groupHashMap;
     }
 
     private Group createGroup(ArrayList<int[]> arr) {
         arr.sort(Comparator.comparingInt(a -> a[0]));
-
         int size = arr.size();
-
         int[] pos = new int[size];
         int[] val = new int[size];
         long[] prefPos = new long[size];
-
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-
         for (int i = 0; i < size; i++) {
             pos[i] = arr.get(i)[0];
             val[i] = arr.get(i)[1];
-
             min = Math.min(min, val[i]);
             max = Math.max(max, val[i]);
-
             prefPos[i] = i == 0 ? val[i] : prefPos[i - 1] + val[i];
         }
-
         Group group = new Group();
         group.pos = pos;
         group.val = val;
         group.prefPos = prefPos;
         group.minv = min;
         group.maxv = max;
-
         if (size > 0) {
             group.root = buildMerge(val, 0, size - 1);
         }
-
         return group;
     }
 
     private long processQuery(int[] nums, int[] query, Map<Integer, Group> groupHashMap, int k) {
         int left = query[0];
         int right = query[1];
-
         int rem = nums[left] % k;
-
         Group group = groupHashMap.get(rem);
-
         if (group == null) {
             return -1;
         }
-
         int l = lowerBound(group.pos, left);
         int r = upperBound(group.pos, right) - 1;
-
         if (!isValidRange(left, right, l, r)) {
             return -1;
         }
-
         return calculateOperations(group, l, r);
     }
 
@@ -220,36 +196,27 @@ public class Solution {
     private long calculateOperations(Group group, int l, int r) {
         int count = r - l + 1;
         int median = findMedian(group, l, r, count);
-
         long leftCount = countLE(group.root, l, r, median);
         long leftSum = sumLE(group.root, l, r, median);
-
         long total = group.prefPos[r] - (l == 0 ? 0L : group.prefPos[l - 1]);
-
         long rightSum = total - leftSum;
         long rightCount = count - leftCount;
-
         return median * leftCount - leftSum + rightSum - median * rightCount;
     }
 
     private int findMedian(Group group, int l, int r, int count) {
         int need = (count + 1) / 2;
-
         int low = group.minv;
         int high = group.maxv;
-
         while (low < high) {
             int mid = low + ((high - low) >>> 1);
-
             int currentCount = countLE(group.root, l, r, mid);
-
             if (currentCount >= need) {
                 high = mid;
             } else {
                 low = mid + 1;
             }
         }
-
         return low;
     }
 }
