4e4f56f0fc122559636a63bc6be2556718871b00
Valentyn Kolesnikov <javadev75@gmail.com>
2026-05-08 19:57:04 +0300

    Refactor maxPoints method for efficiency

--- src/main/java/g3701_3800/s3767_maximize_points_after_choosing_k_tasks/Solution.java
+++ src/main/java/g3701_3800/s3767_maximize_points_after_choosing_k_tasks/Solution.java
@@ -1,32 +1,30 @@
 package g3701_3800.s3767_maximize_points_after_choosing_k_tasks;
 
 // #Medium #Array #Sorting #Greedy #Heap_Priority_Queue #Staff #Biweekly_Contest_171
-// #2026_05_08_Time_96_ms_(61.94%)_Space_136.84_MB_(77.61%)
+// #2026_05_08_Time_21_ms_(100.00%)_Space_130.40_MB_(85.82%)
 
-import java.util.Arrays;
+import java.util.PriorityQueue;
 
 public class Solution {
     public long maxPoints(int[] technique1, int[] technique2, int k) {
         int n = technique1.length;
-        Integer[] idx = new Integer[n];
-        for (int i = 0; i < n; i++) {
-            idx[i] = i;
-        }
-
-        // Sort by (technique1[i] - technique2[i]) descending
-        Arrays.sort(
-                idx, (a, b) -> (technique1[b] - technique2[b]) - (technique1[a] - technique2[a]));
-
+        int use2 = n - k;
+        PriorityQueue<Integer> min = new PriorityQueue<Integer>();
         long ans = 0;
-        for (int i = 0; i < k; i++) {
-            ans += technique1[idx[i]];
+        for (int i = 0; i < n; ++i) {
+            int diff = technique2[i] - technique1[i];
+            if (diff > 0) {
+                if (min.size() < use2) {
+                    min.offer(diff);
+                    ans += diff;
+                } else if (!min.isEmpty() && min.peek() < diff) {
+                    ans -= min.poll();
+                    min.offer(diff);
+                    ans += diff;
+                }
+            }
+            ans += technique1[i];
         }
-
-        // Remaining choose best from both
-        for (int i = k; i < n; i++) {
-            ans += Math.max(technique1[idx[i]], technique2[idx[i]]);
-        }
-
         return ans;
     }
 }
