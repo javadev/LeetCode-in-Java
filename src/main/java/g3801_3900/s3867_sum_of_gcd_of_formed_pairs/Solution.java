49ee5b9840d3d9c1b1c2f2820d2537b5e68ae84a
Valentyn Kolesnikov <javadev75@gmail.com>
2026-07-29 07:16:15 +0300

    Fix formatting issues in gcdSum method

--- src/main/java/g3801_3900/s3867_sum_of_gcd_of_formed_pairs/Solution.java
+++ src/main/java/g3801_3900/s3867_sum_of_gcd_of_formed_pairs/Solution.java
@@ -9,7 +9,6 @@ public class Solution {
     public long gcdSum(int[] nums) {
         int[] prefixGcd = new int[nums.length];
         int max = -1;
-
         for (int i = 0; i < nums.length; i++) {
             max = Math.max(max, nums[i]);
             prefixGcd[i] = gcd(max, nums[i]);
