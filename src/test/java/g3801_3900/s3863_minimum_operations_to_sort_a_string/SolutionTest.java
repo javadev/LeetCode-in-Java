5dacf83f222cf7e62ab2609786105b9bdd619f02
Valentyn Kolesnikov <javadev75@gmail.com>
2026-07-30 08:49:46 +0300

    Added tests

--- src/test/java/g3801_3900/s3863_minimum_operations_to_sort_a_string/SolutionTest.java
+++ src/test/java/g3801_3900/s3863_minimum_operations_to_sort_a_string/SolutionTest.java
@@ -25,4 +25,64 @@ class SolutionTest {
     void minOperations4() {
         assertThat(new Solution().minOperations("abc"), equalTo(0));
     }
+
+    @Test
+    void minOperations5() {
+        assertThat(new Solution().minOperations("a"), equalTo(0));
+    }
+
+    @Test
+    void minOperations6() {
+        assertThat(new Solution().minOperations("z"), equalTo(0));
+    }
+
+    @Test
+    void minOperations7() {
+        assertThat(new Solution().minOperations("ab"), equalTo(0));
+    }
+
+    @Test
+    void minOperations8() {
+        assertThat(new Solution().minOperations("aa"), equalTo(0));
+    }
+
+    @Test
+    void minOperations9() {
+        assertThat(new Solution().minOperations("ba"), equalTo(-1));
+    }
+
+    @Test
+    void minOperations10() {
+        assertThat(new Solution().minOperations("aaa"), equalTo(0));
+    }
+
+    @Test
+    void minOperations11() {
+        assertThat(new Solution().minOperations("abcde"), equalTo(0));
+    }
+
+    @Test
+    void minOperations12() {
+        assertThat(new Solution().minOperations("bca"), equalTo(2));
+    }
+
+    @Test
+    void minOperations13() {
+        assertThat(new Solution().minOperations("bac"), equalTo(1));
+    }
+
+    @Test
+    void minOperations14() {
+        assertThat(new Solution().minOperations("cba"), equalTo(3));
+    }
+
+    @Test
+    void minOperations15() {
+        assertThat(new Solution().minOperations("cbba"), equalTo(3));
+    }
+
+    @Test
+    void minOperations16() {
+        assertThat(new Solution().minOperations("cbca"), equalTo(2));
+    }
 }
--- src/test/java/g3801_3900/s3871_count_commas_in_range_ii/SolutionTest.java
+++ src/test/java/g3801_3900/s3871_count_commas_in_range_ii/SolutionTest.java
@@ -8,16 +8,68 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
     @Test
     void countCommas() {
-        assertThat(new Solution().countCommas(998), equalTo(0L));
+        assertThat(new Solution().countCommas(0L), equalTo(0L));
     }
 
     @Test
     void countCommas2() {
-        assertThat(new Solution().countCommas(1002), equalTo(3L));
+        assertThat(new Solution().countCommas(998L), equalTo(0L));
     }
 
     @Test
     void countCommas3() {
-        assertThat(new Solution().countCommas(1_000_000), equalTo(999_002L));
+        assertThat(new Solution().countCommas(999L), equalTo(0L));
+    }
+
+    @Test
+    void countCommas4() {
+        assertThat(new Solution().countCommas(1000L), equalTo(1L));
+    }
+
+    @Test
+    void countCommas5() {
+        assertThat(new Solution().countCommas(1002L), equalTo(3L));
+    }
+
+    @Test
+    void countCommas6() {
+        assertThat(new Solution().countCommas(999_999L), equalTo(999_000L));
+    }
+
+    @Test
+    void countCommas7() {
+        assertThat(new Solution().countCommas(1_000_000L), equalTo(999_002L));
+    }
+
+    @Test
+    void countCommas8() {
+        assertThat(new Solution().countCommas(1_000_005L), equalTo(999_012L));
+    }
+
+    @Test
+    void countCommas9() {
+        assertThat(new Solution().countCommas(1_000_000_000L), equalTo(1_998_999_003L));
+    }
+
+    @Test
+    void countCommas10() {
+        assertThat(new Solution().countCommas(1_000_000_000_000L), equalTo(2_998_998_999_004L));
+    }
+
+    @Test
+    void countCommas11() {
+        assertThat(
+                new Solution().countCommas(1_000_000_000_000_000L),
+                equalTo(3_998_998_998_999_005L));
+    }
+
+    @Test
+    void countCommas12() {
+        assertThat(new Solution().countCommas(999_999_999L), equalTo(1_998_999_000L));
+    }
+
+    @Test
+    void countCommas13() {
+        assertThat(new Solution().countCommas(999_999_999_999L), equalTo(2_998_998_999_000L));
     }
 }
